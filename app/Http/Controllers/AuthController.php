6f00db524ce391101eb81a35d2e2d1b941ee6157
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-14 15:30:25 +0530

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -28,9 +30,30 @@ class AuthController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function checkLogin(Request $request)
     {
-        //
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8'],
+        ], [
+            'email.required' => 'Please enter Email',
+            'email.email' => 'Please enter a valid Email',
+            'password.required' => 'Please enter a Password',
+            'password.min' => 'The Password must be at least 8 characters.',
+        ]);
+
+
+        $remember = $request->has('remember'); // Check if "Remember Me" checkbox is checked
+
+        if (Auth::attempt($credentials, $remember)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => __('auth.failed'),
+        ]);
     }
 
     /**
