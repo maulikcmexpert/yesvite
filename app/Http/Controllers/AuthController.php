690c0127244abf1ffe8d9cbcf966567128f7adfb
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-15 13:28:13 +0530

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
 
     public function __construct()
     {
-        dd(Auth::guard('web')->check());
+        dd(Session::has('user'));
         if (Auth::guard('web')->check()) {
             echo "hi";
             exit;
