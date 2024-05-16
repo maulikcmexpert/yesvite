e0225015fc7437b1a41929680d0597b97d96a89b
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-16 19:14:29 +0530

    Update Home.php

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -32,8 +32,8 @@ class Home extends Controller
             $file = $request->file('csv_file');
 
             // Store the file temporarily
+            $filePath =  $file->move(public_path('temp'),  $file->getClientOriginalName() . '.csv');
 
-            $filePath = $file->move(public_path('temp', $file->getClientOriginalName() . '.csv'));
             // Parse CSV and store data in the database
             $this->parseAndStoreCSV($filePath);
 
