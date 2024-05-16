90ce131baf04b31b171949060b584127dd20855c
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-16 19:43:17 +0530

    Update CSVImportService.php

--- app/Services/CSVImportService.php
+++ app/Services/CSVImportService.php
@@ -4,6 +4,7 @@
 namespace App\Services;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Session;
 
 class CSVImportService
 {
@@ -11,10 +12,17 @@ class CSVImportService
     {
         $file = fopen($filePath, 'r');
         $header = fgetcsv($file); // Assuming the first row contains column headers
-
+        $parent_userid =  decrypt(Session::get('user')['id']);
         while (($row = fgetcsv($file)) !== false) {
             $data = array_combine($header, $row);
+            $data['app_user'] =  '0';
+            $data['prefer_by'] =  'phone';
+            $data['user_parent_id'] =  $parent_userid;
+            $data['is_user_phone_contact'] =  '1';
+            $data['parent_user_phone_contact'] =  $parent_userid;
+
             dd($data);
+
             // Insert data into the database
             User::create($data);
         }
