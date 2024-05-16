e271cc3a5e392c273cd12ea5eba337376f1832c0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-16 13:43:47 +0530

    Update home.blade.php

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,5 +1,8 @@
-<form action="{{ route('import.csv') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="csv_file">
-    <button type="submit">Import CSV</button>
-</form>
+<div class="md-5">
+
+    <form action="{{ route('import.csv') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <input type="file" name="csv_file">
+        <button type="submit">Import CSV</button>
+    </form>
+</div>
