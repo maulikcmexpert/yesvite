4c155c9d64cd2e3c1a9b5d9b1b25fb7c9a036a01
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-17 17:26:42 +0530

    Update profile.js

--- public/assets/front/js/profile.js
+++ public/assets/front/js/profile.js
@@ -1,20 +1,22 @@
 $(document).ready(function () {
-    var formActionURL = $("#updateUserForm").attr("action");
+    $("#save_changes").click(function () {
+        var formActionURL = $("#updateUserForm").attr("action");
 
-    $.ajax({
-        headers: {
-            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-        },
-        method: "POST",
-        url: formActionURL,
-        dataType: "json",
-        success: function (output) {
-            if (output == true) {
-                table.ajax.reload();
-                toastr.success("Category Deleted successfully !");
-            } else {
-                toastr.error("Category don't Deleted !");
-            }
-        },
+        $.ajax({
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+            },
+            method: "POST",
+            url: formActionURL,
+            dataType: "json",
+            success: function (output) {
+                if (output == true) {
+                    table.ajax.reload();
+                    toastr.success("Category Deleted successfully !");
+                } else {
+                    toastr.error("Category don't Deleted !");
+                }
+            },
+        });
     });
 });
