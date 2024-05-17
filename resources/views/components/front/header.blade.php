dfd20387351dc53144ab1a5cde8972c1ef622ff3
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-17 17:38:37 +0530

    Update header.blade.php

--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -1,7 +1,7 @@
 <title>{{$title}}</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta name="csrf-token" content="{{ csrf_token() }}" />
 <!-- Latest compiled and minified CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
