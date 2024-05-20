5d0507f597e2fbc0c6362986a54e2eee18c72e96
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-20 12:39:10 +0530

    c

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -2,6 +2,7 @@
 
 <!-- jquery-cdn -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 <!-- custom-js -->
 <script src="{{ asset('assets/admin/js/jquery-validate.js') }}"></script>
--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -7,6 +7,6 @@
 
 <!-- custom-style -->
 <link href="{{asset('public/assets/front/css/style.css')}}" rel="stylesheet">
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 <!-- font-awesome-cdn -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
