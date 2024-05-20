8fc099408fa08ac81c26178891be6196e16b29f0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-20 12:11:21 +0530

    Update header.blade.php

--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -7,6 +7,7 @@
 
 <!-- custom-style -->
 <link href="{{asset('public/assets/front/css/style.css')}}" rel="stylesheet">
-
+<link rel="stylesheet" href="{{ asset('vendor/php-flasher/flasher/Resources/assets/flasher.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/php-flasher/flasher/Resources/assets/flasher.min.js') }}">
 <!-- font-awesome-cdn -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
