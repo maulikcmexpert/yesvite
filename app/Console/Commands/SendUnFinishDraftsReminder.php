12deeeabfe75f563d889a801d5fec7da112cf7d0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-23 13:00:23 +0530

    Update SendUnFinishDraftsReminder.php

--- app/Console/Commands/SendUnFinishDraftsReminder.php
+++ app/Console/Commands/SendUnFinishDraftsReminder.php
@@ -29,13 +29,31 @@ class SendUnFinishDraftsReminder extends Command
      */
     public function handle()
     {
-        $eventData = Event::with('user')->where('is_draft_save', '1')->get();
-
+        $eventData = Event::with(['user', 'event_schedule', 'event_image'])->where('is_draft_save', '1')->get();
+        $currentDate = Carbon::now()->toDateString();
         if (count($eventData) != 0) {
 
             foreach ($eventData as $value) {
-                $dateAfterSevenDays = Carbon::parse($value->created_at)->addDays(7);
+
+                $dateAfterSevenDays = Carbon::parse($value->created_at)->addDays(7)->toDateString();
+
+                if ($dateAfterSevenDays > $currentDate) {
+                    $event_time = "";
+                    if ($value->event_schedule->isNotEmpty()) {
+
+                        $event_time =  $value->event_schedule->first()->start_time;
+                    }
+
+                    $eventData = [
+                        'event_name' => $value->event_name,
+                        'event_image' => ($value->event_image->isNotEmpty()) ? $value->event_image[0]->image : "no_image.png",
+                        'date' =>   date('l - M jS, Y', strtotime($value->start_date)),
+                        'time' => $event_time,
+                    ];
+
+                    $invitation_email = new NotifyPendingInvitation($eventData);
+                    Mail::to($value->user->email)->send($invitation_email);
+                }
             }
-        }
     }
 }
