a87f0324f7491070b7b6b207bd882f4b0425cedb
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-23 14:31:15 +0530

    comit

---
+++ app/Mail/NewRsvpsEmailNotify.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewRsvpsEmailNotify extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public $eventData;
+    public function __construct(array $eventData)
+    {
+        $this->eventData = $eventData;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'New Rsvps Email Notify',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+
+        return new Content(
+            view: 'emails.rsvpEmail',
+            with: [
+                'eventData' => $this->eventData
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
