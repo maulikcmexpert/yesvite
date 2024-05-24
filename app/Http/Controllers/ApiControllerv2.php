4f96c99c76fd6e534fa33b1df3b3272851b80ab9
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-24 17:55:38 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7553,6 +7553,7 @@ class ApiControllerv2 extends Controller
 
                             $optionData['option'] = $optionValue->option;
                             $optionData['total_vote'] =  "0%";
+                            dd(getOptionTotalVote($polls->id));
                             if (getOptionTotalVote($polls->id) != 0) {
 
                                 $optionData['total_vote'] =  round(getOptionTotalVote($optionValue->id) / getOptionAllTotalVote($polls->id) * 100) . "%";
