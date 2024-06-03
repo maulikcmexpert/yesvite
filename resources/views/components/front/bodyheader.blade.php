5f19e77a4736a86e86612576008f002ed6aca9c1
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-03 20:36:03 +0530

    c

--- resources/views/components/front/bodyheader.blade.php
+++ resources/views/components/front/bodyheader.blade.php
@@ -33,6 +33,7 @@
 
 
              @if(Session::has('user'))
+
              <div class="header-right">
                  <div class="header-dropdown dropdown">
                      <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
@@ -75,6 +76,24 @@
                      <img src="{{Session::get('user')['profile']}}" class="" alt="user-img">
                  </a>
              </div>
+             <div class="google-add">
+                 <p>Full web functionality will be available in Q3 {{date('Y')}} - for full functionality use our apps</p>
+                 <div class="app-store ms-auto d-flex gap-2">
+                     <a href="#" class="google-app">
+                         <img src="{{asset('public/assets/front/image/google-app.png')}}" alt="google-app">
+                     </a>
+                     <a href="#" class="mobile-app">
+                         <img src="{{asset('public/assets/front/image/mobile-app.png')}}" alt="mobile-app">
+                     </a>
+                 </div>
+                 <a href="#">
+                     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                         <path d="M1.16748 1.1665L12.8334 12.8324" stroke="#E2E8F0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                         <path d="M1.16664 12.8324L12.8325 1.1665" stroke="#E2E8F0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                     </svg>
+                 </a>
+             </div>
+
              @else
              <div class="collapse navbar-collapse" id="mynavbar">
                  <ul class="navbar-nav align-items-center ms-auto">
