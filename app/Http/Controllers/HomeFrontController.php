33a185b856656e71e34b73b938930c9d1fdc9c25
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-03 15:31:21 +0530

    Update HomeFrontController.php

--- app/Http/Controllers/HomeFrontController.php
+++ app/Http/Controllers/HomeFrontController.php
@@ -2,23 +2,14 @@
 
 namespace App\Http\Controllers;
 
-
-
 use Illuminate\Http\Request;
-
-
 use App\Mail\NotifyPendingInvitation;
 use Illuminate\Support\Facades\Mail;
-
-
 
 class HomeFrontController extends Controller
 {
     public function index()
     {
-
-
-
         $title = 'Home';
         $page = 'front.homefront';
         return view('layout', compact(
