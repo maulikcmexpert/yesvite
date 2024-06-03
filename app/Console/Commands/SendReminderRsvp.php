98111b9990ed41b730ac21dfe07f745e237ec207
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-03 12:37:34 +0530

    comi

---
+++ app/Console/Commands/SendReminderRsvp.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Event;
+use Carbon\Carbon;
+
+class SendReminderRsvp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-reminder-rsvp';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reminder RSVP';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $eventData = Event::with(['user', 'event_schedule', 'event_image'])
+            ->where('start_date', '>', now())
+            ->where(['is_draft_save' => '0'])->get();
+
+        $currentDate = Carbon::now()->toDateString();
+        $eventsWithDayDifference = $eventData->map(function ($event) use ($currentDate) {
+            // Calculate the difference in days
+            $daysDifference = $currentDate->diffInDays(Carbon::parse($event->start_date), false);
+
+            // Add the days difference to the event object (optional)
+            $event->days_difference = $daysDifference;
+
+            return $event;
+        });
+
+        dd($eventsWithDayDifference);
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,13 +7,30 @@ use App\Models\User;
 use Illuminate\Support\Facades\Session;
 use App\Services\CSVImportService;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Event;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
 
+        $eventData = Event::with(['user', 'event_schedule', 'event_image'])
+            ->where('start_date', '>', now())
+            ->where(['is_draft_save' => '0'])->get();
 
+        $currentDate = Carbon::now()->toDateString();
+        $eventsWithDayDifference = $eventData->map(function ($event) use ($currentDate) {
+            // Calculate the difference in days
+            $daysDifference = $currentDate->diffInDays(Carbon::parse($event->start_date), false);
+
+            // Add the days difference to the event object (optional)
+            $event->days_difference = $daysDifference;
+
+            return $event;
+        });
+
+        dd($eventsWithDayDifference);
         $title = 'Home';
         $page = 'front.home';
         return view('layout', compact(
