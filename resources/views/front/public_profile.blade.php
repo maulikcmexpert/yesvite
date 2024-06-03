936535393841a489f1706cc1ac2e9a868d26f2b8
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-03 20:36:55 +0530

    comit

--- resources/views/front/public_profile.blade.php
+++ resources/views/front/public_profile.blade.php
@@ -1,3 +1,21 @@
+<div class="google-add">
+    <p>Full web functionality will be available in Q3 {{date('Y')}} - for full functionality use our apps</p>
+    <div class="app-store ms-auto d-flex gap-2">
+        <a href="#" class="google-app">
+            <img src="{{asset('public/assets/front/image/google-app.png')}}" alt="google-app">
+        </a>
+        <a href="#" class="mobile-app">
+            <img src="{{asset('public/assets/front/image/mobile-app.png')}}" alt="mobile-app">
+        </a>
+    </div>
+    <a href="#">
+        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.16748 1.1665L12.8334 12.8324" stroke="#E2E8F0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M1.16664 12.8324L12.8325 1.1665" stroke="#E2E8F0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </a>
+</div>
+
 <section class="contact-details profile-details public-wrp">
     <div class="container">
         <div class="row">
