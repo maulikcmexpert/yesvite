c9ca9f1f17715e593c4044a63e14cc3706d2363e
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-03 13:38:32 +0530

    comit

--- app/Console/Commands/SendReminderRsvp.php
+++ app/Console/Commands/SendReminderRsvp.php
@@ -4,8 +4,10 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Event;
-use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
 use App\Models\EventInvitedUser;
+use App\Mail\NewRsvpsReminderMail;
+use Carbon\Carbon;
 
 class SendReminderRsvp extends Command
 {
@@ -30,12 +32,19 @@ class SendReminderRsvp extends Command
     {
         $eventData = Event::with(['user', 'event_schedule', 'event_image'])
             ->where('start_date', '>', now())
-            ->where(['is_draft_save' => '0'])->get();
+            ->where(['is_draft_save' => '0'])
+            ->get();
 
+        // Get the current date
         $currentDate = Carbon::now()->toDateString();
+
+        // Iterate over the events and calculate the day difference
         $eventsWithDayDifference = $eventData->map(function ($event) use ($currentDate) {
+            // Convert event start_date to a Carbon instance
+            $eventStartDate = Carbon::parse($event->start_date);
+
             // Calculate the difference in days
-            $daysDifference = $currentDate->diffInDays(Carbon::parse($event->start_date), false);
+            $daysDifference = Carbon::parse($currentDate)->diffInDays($eventStartDate, false);
 
             // Add the days difference to the event object (optional)
             $event->days_difference = $daysDifference;
@@ -43,6 +52,23 @@ class SendReminderRsvp extends Command
             return $event;
         });
 
-        dd($eventsWithDayDifference);
+        foreach ($eventsWithDayDifference as $value) {
+            if ($value->days_difference <= 4) {
+                $invitedUser = EventInvitedUser::with('user')->where('event_id', $value->id)->where('rsvp_status', '!=', '1')->get();
+                if (count($invitedUser) != 0) {
+
+                    foreach ($invitedUser as $val) {
+                        $eventData = [
+                            'event_name' => $value->event_name,
+
+                        ];
+
+                        $invitation_email = new NewRsvpsReminderMail($eventData);
+                        $invitation_email->from('notification@yesvite.com', 'Yesvite');
+                        Mail::to($val->user->email)->send($invitation_email);
+                    }
+                }
+            }
+        }
     }
 }
--- app/Http/Controllers/HomeFrontController.php
+++ app/Http/Controllers/HomeFrontController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Event;
-use App\Models\EventInvitedUser;
+
+
 use Illuminate\Http\Request;
-use App\Mail\NewRsvpsReminderMail;
-use Carbon\Carbon;
+
+
 use App\Mail\NotifyPendingInvitation;
 use Illuminate\Support\Facades\Mail;
 
@@ -16,46 +16,7 @@ class HomeFrontController extends Controller
 {
     public function index()
     {
-        $eventData = Event::with(['user', 'event_schedule', 'event_image'])
-            ->where('start_date', '>', now())
-            ->where(['is_draft_save' => '0'])
-            ->get();
 
-        // Get the current date
-        $currentDate = Carbon::now()->toDateString();
-
-        // Iterate over the events and calculate the day difference
-        $eventsWithDayDifference = $eventData->map(function ($event) use ($currentDate) {
-            // Convert event start_date to a Carbon instance
-            $eventStartDate = Carbon::parse($event->start_date);
-
-            // Calculate the difference in days
-            $daysDifference = Carbon::parse($currentDate)->diffInDays($eventStartDate, false);
-
-            // Add the days difference to the event object (optional)
-            $event->days_difference = $daysDifference;
-
-            return $event;
-        });
-        $event = [];
-        foreach ($eventsWithDayDifference as $value) {
-            if ($value->days_difference <= 4) {
-                $invitedUser = EventInvitedUser::with('user')->where('event_id', $value->id)->where('rsvp_status', '!=', '1')->get();
-                if (count($invitedUser) != 0) {
-
-                    foreach ($invitedUser as $val) {
-                        $eventData = [
-                            'event_name' => $value->event_name,
-
-                        ];
-
-                        $invitation_email = new NewRsvpsReminderMail($eventData);
-                        $invitation_email->from('notification@yesvite.com', 'Yesvite');
-                        Mail::to($val->user->email)->send($invitation_email);
-                    }
-                }
-            }
-        }
 
 
         $title = 'Home';
