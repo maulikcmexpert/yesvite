1268f8159df6842a6719a65a8c4792099ecf766d
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-04 12:12:32 +0530

    Update edit_profile.blade.php

--- resources/views/front/edit_profile.blade.php
+++ resources/views/front/edit_profile.blade.php
@@ -8,8 +8,8 @@
                 <div class="contact-list notification-wrap edit-profile-wrp">
                     <nav class="breadcrumb-nav" aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="home.html">Home</a></li>
-                            <li class="breadcrumb-item"><a href="profile.html">Profile</a></li>
+                            <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{route('profile')}}">Profile</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Edit Profile</li>
                         </ol>
                     </nav>
@@ -26,7 +26,7 @@
                             <form>
                                 <div class="position-relative">
                                     <div id="img-preview">
-                                        <img src="./assets/image/Frame 1000005835.png" alt="" class="bg-img">
+                                        <img src="{{$user->bg_profile}}" alt="" class="bg-img">
                                     </div>
                                     <input type="file" id="choose-file" name="choose-file" accept="image/*" />
                                     <a href="#" class="Edit-img" for="choose-file" data-bs-toggle="modal" data-bs-target="#coverImg-modal">
@@ -38,7 +38,7 @@
                                 </div>
                             </form>
                             <div class="user-img">
-                                <img src="./assets/image/user-img.svg" alt="user-img">
+                                <img src="{{$user->profile}}" alt="user-img">
                                 <a href="#" data-bs-toggle="modal" data-bs-target="#Edit-modal">
                                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M11.05 4.66652L4.20829 11.9082C3.94996 12.1832 3.69996 12.7249 3.64996 13.0999L3.34162 15.7999C3.23329 16.7749 3.93329 17.4415 4.89996 17.2749L7.58329 16.8165C7.95829 16.7499 8.48329 16.4749 8.74162 16.1915L15.5833 8.94985C16.7666 7.69985 17.3 6.27485 15.4583 4.53319C13.625 2.80819 12.2333 3.41652 11.05 4.66652Z" stroke="#0F172A" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
