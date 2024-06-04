95759fd27e09d5a9422f9436571f5f6af6db898e
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-04 20:20:37 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7520,12 +7520,12 @@ class ApiControllerv2 extends Controller
                                         ->where('rsvp_status', '1')
                                         ->where('post_privacy', '2');
                                 })
-                                    ->where(function ($q) {
+                                    ->orWhere(function ($q) {
                                         $q->where('rsvp_d', '1')
                                             ->where('rsvp_status', '0')
                                             ->where('post_privacy', '3');
                                     })
-                                    ->where(function ($q) {
+                                    ->orWhere(function ($q) {
                                         $q->where('rsvp_d', '0')
                                             ->where('post_privacy', '4');
                                     });
