131dcb04991db0901681d64afd62ec2988726583
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-04 17:27:22 +0530

    Update common.js

--- public/assets/front/js/common.js
+++ public/assets/front/js/common.js
@@ -32,19 +32,19 @@ var incrementMinus = buttonMinus.click(function () {
     }
 });
 
-jQuery(($) => {
-    $('.attachment input[type="file"]').on("change", (event) => {
-        let el = $(event.target).closest(".attachment").find(".btn-file");
+// jQuery(($) => {
+//     $('.attachment input[type="file"]').on("change", (event) => {
+//         let el = $(event.target).closest(".attachment").find(".btn-file");
 
-        el.find(".btn-file__actions__item");
-        el.find(".btn-file__preview").css({
-            "background-image":
-                "url(" +
-                window.URL.createObjectURL(event.target.files[0]) +
-                ")",
-        });
-    });
-});
+//         el.find(".btn-file__actions__item");
+//         el.find(".btn-file__preview").css({
+//             "background-image":
+//                 "url(" +
+//                 window.URL.createObjectURL(event.target.files[0]) +
+//                 ")",
+//         });
+//     });
+// });
 
 const chooseFile = document.getElementById("choose-file");
 const bgChooseFile = document.getElementById("bg-choose-file");
