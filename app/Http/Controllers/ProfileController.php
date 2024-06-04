eabc7bc9441b395157e3831dd1901a34ad36bbbf
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-04 20:45:36 +0530

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -59,7 +59,7 @@ class ProfileController extends Controller
         $user['photos']   = EventPost::where(['user_id' => $user->id, 'post_type' => '1'])->count();
         $user['comments']   =  EventPostComment::where('user_id', $user->id)->count();
         $user['profile'] = ($user->profile != null) ? asset('public/storage/profile/' . $user->profile) : asset('public/storage/profile/no_profile.png');
-        $user['bg_profile'] = ($user->bg_profile != null) ? asset('public/storage/bg_profile/' . $user->bg_profileprofile) : asset('public/assets/front/image/Frame 1000005835.png');
+        $user['bg_profile'] = ($user->bg_profile != null) ? asset('public/storage/bg_profile/' . $user->bg_profile) : asset('public/assets/front/image/Frame 1000005835.png');
         return view('layout', compact(
             'title',
             'page',
