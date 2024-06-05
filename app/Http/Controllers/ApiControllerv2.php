052f7230971a7a3521e8b7afac0aa22ab22742ba
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-05 12:28:25 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7604,17 +7604,17 @@ class ApiControllerv2 extends Controller
                         })
                             ->where(function ($privacyQuery) {
                                 $privacyQuery->where(function ($q) {
-                                    $q->where('event.event_invited_user.rsvp_d', '1')
-                                        ->where('rsvp_status', '1')
+                                    $q->where('event_invited_users.rsvp_d', '1')
+                                        ->where('event_invited_users.rsvp_status', '1')
                                         ->where('post_privacy', '2');
                                 })
                                     ->orWhere(function ($q) {
-                                        $q->where('event.event_invited_user.rsvp_d', '1')
-                                            ->where('rsvp_status', '0')
+                                        $q->where('event_invited_users.rsvp_d', '1')
+                                            ->where('event_invited_users.rsvp_status', '0')
                                             ->where('post_privacy', '3');
                                     })
                                     ->orWhere(function ($q) {
-                                        $q->where('event.event_invited_user.rsvp_d', '0')
+                                        $q->where('event_invited_users.rsvp_d', '0')
                                             ->where('post_privacy', '4');
                                     });
                             });
