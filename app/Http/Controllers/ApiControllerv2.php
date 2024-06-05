1c8ef66f5447e7a30555b3fbf000c734ffd97b96
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-05 13:00:43 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7604,23 +7604,24 @@ class ApiControllerv2 extends Controller
                         })
                             ->where(function ($privacyQuery) {
                                 $privacyQuery->where(function ($q) {
-                                    // $q->where('event_invited_users.rsvp_d', '1')
-                                    //     ->where('event_invited_users.rsvp_status', '1')
-                                    $q->where('post_privacy', '2');
+                                    $q->where('event_invited_users.rsvp_d', '1')
+                                        ->where('event_invited_users.rsvp_status', '1')
+                                        ->where('post_privacy', '2');
                                 })
                                     ->orWhere(function ($q) {
-                                        // $q->where('event_invited_users.rsvp_d', '1')
-                                        // ->where('event_invited_users.rsvp_status', '0')
-                                        $q->where('post_privacy', '3');
+                                        $q->where('event_invited_users.rsvp_d', '1')
+                                            ->where('event_invited_users.rsvp_status', '0')
+                                            ->where('post_privacy', '3');
                                     })
                                     ->orWhere(function ($q) {
-                                        // $q->where('event_invited_users.rsvp_d', '0')
-                                        $q->where('post_privacy', '4');
+                                        $q->where('event_invited_users.rsvp_d', '0')
+                                            ->where('post_privacy', '4');
                                     });
                             });
                     });
             })
             ->orderBy('id', 'desc');
+
 
         // Apply filters if selected
         if (!empty($selectedFilters) && !in_array('all', $selectedFilters)) {
