a95d62c45a4dd84096506d1159acf2fe7b35e8a0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-06 11:25:13 +0530

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -100,7 +100,7 @@ class ProfileController extends Controller
             $validator = Validator::make($request->all(), [
                 'firstname' => 'required|string', // max 2MB
                 'lastname' => 'required|string', // max 2MB
-                'zip_code' => 'required|numeric|min:5|max:5', // max 2MB
+                'zip_code' => 'required|numeric|minlength:5|maxlength:5', // max 2MB
             ]);
 
             if ($validator->fails()) {
