4beed25fddf865d3b3e96f7cfad16989f83ed0ee
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-06 12:49:31 +0530

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,11 +26,11 @@
                     <!-- <input type="email" class="form-control inputText" id="email" name="email">
                     <label for="email" class="form-label input-field floating-label">Email Address <span class="required">*</span></label> -->
 
-                    <input type="email" class="form-control inputText" id="email" name="email" required>
+                    <input type="email" class="form-control inputText" id="email" name="email" value="{{ old('email') }}">
                     <label for="email" class="form-label input-field floating-label">Email Address <span class="required">*</span></label>
                 </div>
                 <div class="input-form">
-                    <input type="password" class="form-control inputText" id="password" name="password" required>
+                    <input type="password" class="form-control inputText" id="password" name="password" value="{{ old('password') }}">
                     <label for="password" class="form-label input-field floating-label">Password <span class="required">*</span></label>
                     <span toggle="#password-field" class="fa-regular fa-fw fa-eye-slash field-icon toggle-password"></span>
                 </div>
