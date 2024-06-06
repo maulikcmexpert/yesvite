9f83744d30ca1aeabd7cb5a3c9b9d762a1cd9e81
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-06 13:02:38 +0530

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,10 +65,11 @@ class AuthController extends Controller
 
                 if ($remember) {
                     Cookie::queue('email', $user->email, 120);
-                    Cookie::forget('password');
+                    Cookie::queue('password', $request->password, 120);
                 } else {
 
                     Cookie::forget('email');
+                    Cookie::forget('password');
                 }
                 toastr()->success('Logged in successfully!');
                 return redirect()->route('home');
