d969ced08275e9f2bb3ff892f17dca046cc6df57
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-10 14:33:46 +0530

    Update 2024_06_10_124138_add_step__filed_in_events.php

--- database/migrations/2024_06_10_124138_add_step__filed_in_events.php
+++ database/migrations/2024_06_10_124138_add_step__filed_in_events.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->string('step');
+            $table->integer('step')->default(0);
         });
     }
 
