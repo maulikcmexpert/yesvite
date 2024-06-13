04c114ccaf411892d1d4c92fd7a9e9ccb5078fa0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-13 16:31:25 +0530

    Update contact.blade.php

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -70,7 +70,7 @@
                                 </div>
                             </div>
                             <div id="loader" style="display: none;">
-                                <img src="{{asset('assets/front/loader.gif')}}" alt="loader" style="">
+                                <img src="{{asset('assets/front/loader.gif')}}" alt="loader" style="width:48px;height:48px;">
                             </div>
                         </div>
                         <div class="tab-pane fade" id="phone" role="tabpanel" aria-labelledby="phone-tab">
