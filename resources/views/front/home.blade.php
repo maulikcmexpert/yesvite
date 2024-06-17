a149a6316ec54a0f77d1f3e15ae48be7687a68d7
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-17 17:21:15 +0530

    Update home.blade.php

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -5,11 +5,5 @@
         <input type="file" name="csv_file">
         <button type="submit">Import CSV</button>
     </form>
-    <?php
-    $id = Auth::guard('web')->user()->id;
 
-    $userId = encrypt(327);
-    $eventId = encrypt(1505);
-    ?>
-    <a href="{{route('rsvp',['userId' => $userId,'eventId' =>$eventId])}}">RSVP</a>
 </div>
