ce081504505202bab8ba042ac0363337c9973bac
Prakashmanat01 <prakashhmanat@gmail.com>
2024-06-18 18:40:39 +0530

    commit

--- resources/views/admin/user/add.blade.php
+++ resources/views/admin/user/add.blade.php
@@ -35,30 +35,32 @@
                             <input type="text" class="form-control firstname" name="firstname" placeholder="Enter First Name" value="">
                             <span class="text-danger">{{ $errors->first('firstname') }}</span>
                         </div>
-                        <!-- <div class="form-group">
-                            <label for="middlename">Middle Name</label>
-                            <input type="text" class="form-control middlename" name="middlename" placeholder="Enter Middle Name" value="">
-                            <span class="text-danger">{{ $errors->first('middlename') }}</span>
-                        </div> -->
+                    </div>
+
+                    <div class="col-lg-4 mb-3">
                         <div class="form-group">
                             <label for="lastname">Last Name</label>
                             <input type="text" class="form-control lastname" name="lastname" placeholder="Enter Last Name" value="">
                             <span class="text-danger">{{ $errors->first('lastname') }}</span>
                         </div>
+                    </div>
+
+                    <div class="col-lg-4 mb-3">
                         <div class="form-group">
                             <label for="email">Email</label>
                             <input type="text" class="form-control email" name="email" placeholder="Enter Email Address" value="">
                             <span class="text-danger">{{ $errors->first('email') }}</span>
                         </div>
+                    </div>
 
+                    <div class="col-lg-4 mb-3">
                         <div class="form-group">
                             <label for="phone_number">Phone Number</label>
                             <input type="text" class="form-control phone_number" name="phone_number" placeholder="Enter Email Address" value="">
                             <span class="text-danger">{{ $errors->first('phone_number') }}</span>
                         </div>
-
-
                     </div>
+
 
 
 
