f22a6ae1f22511620498fa14cfe55f7371eee2df
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-19 13:21:10 +0530

    Update HomeFrontController.php

--- app/Http/Controllers/HomeFrontController.php
+++ app/Http/Controllers/HomeFrontController.php
@@ -12,8 +12,7 @@ class HomeFrontController extends Controller
     public function index()
     {
         $serverKey  = ServerKey::first();
-        $SERVER_API_KEY = $serverKey->firebase_key;
-        dd($SERVER_API_KEY);
+
         $title = 'Home';
         $page = 'front.homefront';
         return view('layout', compact(
