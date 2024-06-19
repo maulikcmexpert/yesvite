42253476452f3447d103961c2d906067208f22bb
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-19 12:09:19 +0530

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1172,9 +1172,12 @@ function send_notification_FCM($deviceToken, $notifyData)
 function send_notification_FCM_and($deviceToken, $notifyData)
 {
 
+    // Live //
 
-    // $LIVE_SERVER_API_KEY = 'key=AAAAZh4vQmc:APA91bFTE_caQlb57lsvsBgbHRwWjobttbG8TbHcSeKzRuF4OhAbscz9Trwur9ATVYIj5Wnse9GspEtSHc2WO9Czk_DKWN2IS8EK4GPWWTQRnK3G1jp5_gpuNUIaMjaa-CFwinqS--4r';
-    $DEV_SERVER_API_KEY = 'key=AAAAP6m84T0:APA91bHeuAm2ME_EmPEsOjMe2FatmHn2QU98ADg4Y5UxNMmXGg4MDD4OJQQhvsixNfhV1g2BWbgOCQGEf9_c3ngB8qH_N3MEMsgD7uuAQAq0_IO2GGPqCxjJPuwAME9MVX9ZvWgYbcPh';
+    // $SERVER_API_KEY = 'key=AAAAZh4vQmc:APA91bFTE_caQlb57lsvsBgbHRwWjobttbG8TbHcSeKzRuF4OhAbscz9Trwur9ATVYIj5Wnse9GspEtSHc2WO9Czk_DKWN2IS8EK4GPWWTQRnK3G1jp5_gpuNUIaMjaa-CFwinqS--4r';
+
+    // Dev //
+    $SERVER_API_KEY = 'key=AAAAP6m84T0:APA91bHeuAm2ME_EmPEsOjMe2FatmHn2QU98ADg4Y5UxNMmXGg4MDD4OJQQhvsixNfhV1g2BWbgOCQGEf9_c3ngB8qH_N3MEMsgD7uuAQAq0_IO2GGPqCxjJPuwAME9MVX9ZvWgYbcPh';
     $URL = 'https://fcm.googleapis.com/fcm/send';
 
 
