a423ebe37706bbf7ae0ccc64751e08d5d186be5e
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-19 13:20:28 +0530

    Update HomeFrontController.php

--- app/Http/Controllers/HomeFrontController.php
+++ app/Http/Controllers/HomeFrontController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Mail\NotifyPendingInvitation;
+use App\Models\ServerKey;
 use Illuminate\Support\Facades\Mail;
 
 class HomeFrontController extends Controller
 {
     public function index()
     {
+        $serverKey  = ServerKey::first();
+        $SERVER_API_KEY = $serverKey->firebase_key;
+        dd($SERVER_API_KEY);
         $title = 'Home';
         $page = 'front.homefront';
         return view('layout', compact(
