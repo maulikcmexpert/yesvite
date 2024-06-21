ab427484d6378ac67bcdc19ba94e94a198459b16
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-21 18:52:41 +0530

    Update report_detail.blade.php

--- resources/views/admin/post_reports/report_detail.blade.php
+++ resources/views/admin/post_reports/report_detail.blade.php
@@ -905,8 +905,9 @@
                                                                 <div class="answer">{{$optionVal->option}}</div>
                                                             </div>
                                                             <div class="progress">
-                                                                <div class="progress-bar" style="width: <?= round(getOptionTotalVote($optionVal->id) * 100 / getTotalEventInvitedUser($reportDetail->event_id)); ?>%">
-                                                                    <?= round(getOptionTotalVote($optionVal->id) * 100 / getTotalEventInvitedUser($reportDetail->event_id)) . "%"; ?>
+
+                                                                <div class="progress-bar" style="width: <?= round(getOptionTotalVote($optionValue->id) / getOptionAllTotalVote($polls->id) * 100); ?>%">
+                                                                    <?= round(getOptionTotalVote($optionValue->id) / getOptionAllTotalVote($polls->id) * 100)  . "%"; ?>
                                                                 </div>
                                                             </div>
                                                         </label>
