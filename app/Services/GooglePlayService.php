c5035b34da4834fd6f6d97dfb7bc04f72a553aa6
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-25 18:08:27 +0530

    Update GooglePlayService.php

--- app/Services/GooglePlayService.php
+++ app/Services/GooglePlayService.php
@@ -17,7 +17,7 @@ class GooglePlayService
         $this->client->addScope(AndroidPublisher::ANDROIDPUBLISHER);
 
         $this->androidPublisher = new AndroidPublisher($this->client);
-        dd($this->client);
+        dd($this->androidPublisher);
     }
 
     public function verifySubscription($packageName, $subscriptionId, $token)
