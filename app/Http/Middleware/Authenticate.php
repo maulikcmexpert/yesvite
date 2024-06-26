79634b0d0f9bc20a345c293736dc8c08c650df00
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-26 14:59:22 +0530

    comit

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,7 +14,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+
         if (!$request->expectsJson()) {
+
             return redirect('/');
         }
     }
