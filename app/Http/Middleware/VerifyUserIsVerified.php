93e6ab2f8baf3c2d34fc0dd27e56897de4e789df
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-26 14:53:50 +0530

    Update VerifyUserIsVerified.php

--- app/Http/Middleware/VerifyUserIsVerified.php
+++ app/Http/Middleware/VerifyUserIsVerified.php
@@ -38,7 +38,7 @@ class VerifyUserIsVerified
 
             $user->current_session_id = $currentSessionId;
             $user->save();
-            return $next($request);
         }
+        return $next($request);
     }
 }
