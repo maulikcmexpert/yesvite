0b2b7430f936156ab1f3aefc1d93381c3a1881b6
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-26 17:39:51 +0530

    Update 2024_06_26_155240_add_type_and_cancellation_date_in_user_subscription_table.php

--- database/migrations/2024_06_26_155240_add_type_and_cancellation_date_in_user_subscription_table.php
+++ database/migrations/2024_06_26_155240_add_type_and_cancellation_date_in_user_subscription_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('user_subscriptions', function (Blueprint $table) {
-            $table->string('type')->nullable()->after('purchaseToken');
+            $table->enum('type', ['Pro-Year', 'Pro'])->nullable()->after('purchaseToken');
             $table->string('cancellationdate')->nullable()->after('type');
         });
     }
