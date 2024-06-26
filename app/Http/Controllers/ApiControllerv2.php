135d7b59049afbd5f6e5ba111baed621184397f2
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-26 15:08:15 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -12389,7 +12389,7 @@ class ApiControllerv2 extends Controller
             $subscribed = UserSubscription::where('user_id', $user_id)
                 ->select('id', 'user_id')
                 ->first();
-            if (isset($subscribed) && !$subscribed->isEmpty()) {
+            if ($subscribed != null) {
                 $subscription = UserSubscription::where('id', $subscribed->id)
                     ->update([
                         'startDate' => now(),
