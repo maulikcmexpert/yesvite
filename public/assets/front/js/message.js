98c6c51728b35003ad6d929a7fe7cfc7828ba8c2
cmexpertise <pratik.cmexpertise@gmail.com>
2024-06-27 20:39:11 +0530

    Update message.js

--- public/assets/front/js/message.js
+++ public/assets/front/js/message.js
@@ -23,27 +23,27 @@ import {
     uploadBytes,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 // Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBwm_cUYyrAPWvp-t31PCWP_gmeHghVdTQ",
-//     authDomain: "yesvitelive.firebaseapp.com",
-//     databaseURL: "https://yesvitelive-default-rtdb.firebaseio.com",
-//     projectId: "yesvitelive",
-//     storageBucket: "yesvitelive.appspot.com",
-//     messagingSenderId: "438593077863",
-//     appId: "1:438593077863:web:51ab60b8d230a6c4f48ac2",
-//     measurementId: "G-6FRGMCQQ66",
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyAVgJQewYO8h1-_z2mrjaATCqJ4NH8eeCI",
-    authDomain: "yesvite-976cd.firebaseapp.com",
-    databaseURL: "https://yesvite-976cd-default-rtdb.firebaseio.com",
-    projectId: "yesvite-976cd",
-    storageBucket: "yesvite-976cd.appspot.com",
-    messagingSenderId: "273430667581",
-    appId: "1:273430667581:web:d5cc6f6c1cc9829de9e554",
-    measurementId: "G-99SYL4VLEF",
+    apiKey: "AIzaSyBwm_cUYyrAPWvp-t31PCWP_gmeHghVdTQ",
+    authDomain: "yesvitelive.firebaseapp.com",
+    databaseURL: "https://yesvitelive-default-rtdb.firebaseio.com",
+    projectId: "yesvitelive",
+    storageBucket: "yesvitelive.appspot.com",
+    messagingSenderId: "438593077863",
+    appId: "1:438593077863:web:51ab60b8d230a6c4f48ac2",
+    measurementId: "G-6FRGMCQQ66",
 };
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAVgJQewYO8h1-_z2mrjaATCqJ4NH8eeCI",
+//     authDomain: "yesvite-976cd.firebaseapp.com",
+//     databaseURL: "https://yesvite-976cd-default-rtdb.firebaseio.com",
+//     projectId: "yesvite-976cd",
+//     storageBucket: "yesvite-976cd.appspot.com",
+//     messagingSenderId: "273430667581",
+//     appId: "1:273430667581:web:d5cc6f6c1cc9829de9e554",
+//     measurementId: "G-99SYL4VLEF",
+// };
 
 console.log("update message . js");
 $.ajaxSetup({
