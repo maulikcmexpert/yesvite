6a08064110bffbd4ca3f1397579a5cb845a07f3a
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-28 20:41:38 +0530

    Update messages.blade.php

--- resources/views/front/chat/messages.blade.php
+++ resources/views/front/chat/messages.blade.php
@@ -105,6 +105,7 @@ use Carbon\Carbon;
                             @php
                             $i = 0;
                             @endphp
+                            @dd(messages);
                             @foreach ($messages as $k => $message)
                             @if(!isset($message['contactName']))
                             @continue
