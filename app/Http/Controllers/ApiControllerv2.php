d825c42b84d41d1ce58d3e16f7855652bf7547fc
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-28 12:50:44 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7110,7 +7110,7 @@ class ApiControllerv2 extends Controller
 
             $event_date = $eventDetail->start_date;
             if ($eventDetail->start_date != $eventDetail->end_date) {
-                $event_date = $eventDetail->start_date . ' To ' . $event_date = $eventDetail->end_date;
+                $event_date = $eventDetail->start_date . ' to ' . $event_date = $eventDetail->end_date;
             }
             $eventDetails['event_date'] = $event_date;
             $eventDetails['event_time'] =  $eventDetail->rsvp_start_time;
@@ -7118,18 +7118,18 @@ class ApiControllerv2 extends Controller
             if ($eventDetail->event_schedule->isNotEmpty()) {
                 if ($eventDetail->event_schedule->last()->end_time != NULL || $eventDetail->event_schedule->last()->end_time != "") {
 
-                    $eventDetails['event_time'] = $eventDetail->event_schedule->first()->start_time . ' To ' . $eventDetail->event_schedule->last()->end_time;
+                    $eventDetails['event_time'] = $eventDetail->event_schedule->first()->start_time . ' to ' . $eventDetail->event_schedule->last()->end_time;
                 } else {
                     $eventDetails['event_time'] = $eventDetail->event_schedule->first()->start_time;
                     if ($eventDetail->rsvp_end_time != NULL || $eventDetail->rsvp_end_time != "") {
 
-                        $eventDetails['event_time'] = $eventDetail->event_schedule->first()->start_time . ' To ' . $eventDetail->rsvp_end_time;
+                        $eventDetails['event_time'] = $eventDetail->event_schedule->first()->start_time . ' to ' . $eventDetail->rsvp_end_time;
                     }
                 }
             } else {
                 $eventDetails['event_time'] =  $eventDetail->rsvp_start_time;
                 if ($eventDetail->rsvp_end_time != NULL || $eventDetail->rsvp_end_time != "") {
-                    $eventDetails['event_time'] =  $eventDetail->rsvp_start_time . ' To ' . $eventDetail->rsvp_end_time;
+                    $eventDetails['event_time'] =  $eventDetail->rsvp_start_time . ' to ' . $eventDetail->rsvp_end_time;
                 }
             }
 
