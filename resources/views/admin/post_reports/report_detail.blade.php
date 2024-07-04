e863aed125404aa242a996b6c211e9c18e9eb1e4
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-04 16:38:23 +0530

    commit

--- resources/views/admin/post_reports/report_detail.blade.php
+++ resources/views/admin/post_reports/report_detail.blade.php
@@ -1054,7 +1054,7 @@
             </div>
 
             <div class="col-md-0 mb-4">
-                <a type="button" class="btn btn-primary" href="{{route('delete_post_report',['id' => encrypt($reportDetail->id) ])}}" data-id="{{$reportDetail->id}}" class="DeleteReport_post">Delete</a>
+                <a type="button" class="btn btn-danger" href="{{route('delete_post_report',['id' => encrypt($reportDetail->id) ])}}" data-id="{{$reportDetail->id}}" class="DeleteReport_post">Delete</a>
             </div>
         </div>
 
