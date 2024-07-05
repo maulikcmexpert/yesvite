79755c4d6cebad89aa3d2eb6e86ef99f0a46eb2b
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-05 13:33:38 +0530

    Update ApiControllerv3.php

--- app/Http/Controllers/ApiControllerv3.php
+++ app/Http/Controllers/ApiControllerv3.php
@@ -12574,7 +12574,7 @@ class ApiControllerv3 extends Controller
         $clientSecret = env('InGOOGLE_CLIENT_SECRET');
         $redirectUri = 'https://yesvite.cmexpertiseinfotech.in/google/callback';
 
-        $refreshToken = '1//0gHYN_Ai3rfAnCgYIARAAGBASNwF-L9IrdP-JOsDTkXeH-yqO_Z252HkBEfW7oqRZqcbTrsTQ_u_8eeif8HSml-a-i0Foi6iVH4Q';
+        $refreshToken = '1//0gAMN5A8aeJnnCgYIARAAGBASNwF-L9IrxUlvMKstoyZ41Nn4mH75eZQ1V3yIxcjNHj45bJwsIQfCDH04LQwYxLZ8Sgmu2rVrcyU';
 
 
         $TOKEN_URL = "https://accounts.google.com/o/oauth2/token";
