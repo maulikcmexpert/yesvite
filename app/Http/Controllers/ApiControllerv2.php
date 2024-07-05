6a3a88768d4745230a414b0db741e40dfa3ed53a
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-05 16:24:11 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7752,8 +7752,8 @@ class ApiControllerv2 extends Controller
 
                     $ischeckEventOwner = Event::where(['id' => $input['event_id'], 'user_id' => $value->user->id])->first();
 
+                    dd($value);
                     $postControl = PostControl::where(['user_id' => $user->id, 'event_id' => $input['event_id'], 'event_post_id' => $value->id])->first();
-                    dd($postControl);
                     // if ($postControl != null) {
 
                     //     if ($postControl->post_control == 'hide_post') {
