5436475a2c40d74b7d9d23ee15aaba67c2cfdbd8
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-08 19:00:13 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7487,20 +7487,20 @@ class ApiControllerv2 extends Controller
                                 $q->where('rsvp_d', '1')
                                     ->where('rsvp_status', '1');
                                 // ->where('post_privacy', '2');
-                            });
-                            // ->orWhere(function ($q) {
-                            //     $q->where('rsvp_d', '1')
-                            //         ->where('rsvp_status', '0');
-                            //     // ->where('post_privacy', '3');
-                            // })
-                            // ->orWhere(function ($q) {
-                            //     $q->where('rsvp_d', '0');
-                            //     // ->where('post_privacy', '4');
-                            // })
-                            // ->orWhere(function ($q) {
-                            //     $q->where('post_privacy', '1');
-                            // });
-                        })->where('post_privacy', '3');
+                            })
+                                ->orWhere(function ($q) {
+                                    $q->where('rsvp_d', '1')
+                                        ->where('rsvp_status', '0');
+                                    // ->where('post_privacy', '3');
+                                })
+                                ->orWhere(function ($q) {
+                                    $q->where('rsvp_d', '0');
+                                    // ->where('post_privacy', '4');
+                                })
+                                ->orWhere(function ($q) {
+                                    $q->where('post_privacy', '1');
+                                })->where('post_privacy', '3');
+                        });
                 });
             });
         }
