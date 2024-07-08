244dcf5f5437a55208bacf9a3312da10fec5378f
Prakashmanat01 <prakashhmanat@gmail.com>
2024-07-08 18:11:55 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -12621,7 +12621,7 @@ class ApiControllerv2 extends Controller
         $input = json_decode($rawData, true);
 
         Mail::send('emails.app_inivite_link',['userdata'], function ($message) use ($input) {
-            $message->to($input['email']);
+            $message->to($input->email);
             $message->subject('Email Verification Mail');
         });
     }
