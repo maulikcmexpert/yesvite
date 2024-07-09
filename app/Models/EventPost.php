0b98c0512a1e1748c7bc116ce5ace672cdeffd00
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-09 12:45:12 +0530

    Update EventPost.php

--- app/Models/EventPost.php
+++ app/Models/EventPost.php
@@ -63,9 +63,4 @@ class EventPost extends Model
 
         return $this->hasMany(UserReportToPost::class, 'event_post_id', 'id');
     }
-
-    public function event_invited_user()
-    {
-        return $this->hasMany(EventPost::class, 'event_id');
-    }
 }
