42e7f53719fccc43022384f6862f4178c3aefcac
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-11 15:33:43 +0530

    Update app_inivite_link.blade.php

--- resources/views/emails/app_inivite_link.blade.php
+++ resources/views/emails/app_inivite_link.blade.php
@@ -81,8 +81,8 @@
             <!-- -------------- -->
             <tr>
                <td>
-                  <div class="view-btn" style="display: flex;align-items: center;gap: 15px;">
-                     <button style="font-family: 'SF Pro Display', sans-serif;font-size: 14px;line-height: 20px;font-weight: 500;color: #fff;background: #F73C71;border: 1px solid #F73C71;border-radius: 10px; padding: 10px 24px 10px 24px;">View Message</button>
+                  <div class="view-btn" style="display: flex;align-items: center; gap: 15px;">
+                     <button style="font-family: 'SF Pro Display', sans-serif;font-size: 14px;line-height: 20px;font-weight: 500;color: #fff;background: #F73C71;border: 1px solid #F73C71;border-radius: 10px; padding: 10px 24px 10px 24px; margin-right: 15px;">View Message</button>
                      <button style="font-family: 'SF Pro Display', sans-serif;font-size: 14px;line-height: 20px;font-weight: 500;color: #0F172A;background: transparent;border: 1px solid #E2E8F0;border-radius: 10px; padding: 10px 24px 10px 24px;">View Maria's Profile</button>
                   </div>
                </td>
