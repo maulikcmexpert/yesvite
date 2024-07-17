be1b7de643e8d7dc902029d52cf125f009251b2a
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-17 16:33:05 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -12234,20 +12234,22 @@ class ApiControllerv2 extends Controller
                 })
                 ->paginate('10', ['*'], 'page', $page);
             $yesviteUser = [];
-            foreach ($invitedUser as $guestVal) {
-                // dd($guestVal->user);
-                $yesviteUserDetail['id'] = $guestVal->user->id;
-                $yesviteUserDetail['profile'] = empty($guestVal->user->profile) ? "" : asset('storage/profile/' . $guestVal->user->profile);
-                $yesviteUserDetail['first_name'] = (!empty($guestVal->user->firstname) || $guestVal->user->firstname != Null) ? $guestVal->user->firstname : "";;
-                $yesviteUserDetail['last_name'] = (!empty($guestVal->user->lastname) || $guestVal->user->lastname != Null) ? $guestVal->user->lastname : "";
-                $yesviteUserDetail['email'] = (!empty($guestVal->user->email) || $guestVal->user->email != Null) ? $guestVal->user->email : "";
-                $yesviteUserDetail['country_code'] = (!empty($guestVal->user->country_code) || $guestVal->user->country_code != Null) ? strval($guestVal->user->country_code) : "";
-                $yesviteUserDetail['phone_number'] = (!empty($guestVal->user->phone_number) || $guestVal->user->phone_number != Null) ? $guestVal->user->phone_number : "";
-                $yesviteUserDetail['app_user']  = $guestVal->user->app_user;
-                $yesviteUserDetail['visible'] =  $guestVal->user->visible;
-                $yesviteUserDetail['message_privacy'] =  $guestVal->user->message_privacy;
-                $yesviteUserDetail['prefer_by']  = $guestVal->user->prefer_by;
-                $yesviteUser[] = $yesviteUserDetail;
+            if ($invitedUser->isNotEmpty()) {
+                foreach ($invitedUser as $guestVal) {
+                    // dd($guestVal->user);
+                    $yesviteUserDetail['id'] = $guestVal->user->id;
+                    $yesviteUserDetail['profile'] = empty($guestVal->user->profile) ? "" : asset('storage/profile/' . $guestVal->user->profile);
+                    $yesviteUserDetail['first_name'] = (!empty($guestVal->user->firstname) || $guestVal->user->firstname != Null) ? $guestVal->user->firstname : "";;
+                    $yesviteUserDetail['last_name'] = (!empty($guestVal->user->lastname) || $guestVal->user->lastname != Null) ? $guestVal->user->lastname : "";
+                    $yesviteUserDetail['email'] = (!empty($guestVal->user->email) || $guestVal->user->email != Null) ? $guestVal->user->email : "";
+                    $yesviteUserDetail['country_code'] = (!empty($guestVal->user->country_code) || $guestVal->user->country_code != Null) ? strval($guestVal->user->country_code) : "";
+                    $yesviteUserDetail['phone_number'] = (!empty($guestVal->user->phone_number) || $guestVal->user->phone_number != Null) ? $guestVal->user->phone_number : "";
+                    $yesviteUserDetail['app_user']  = $guestVal->user->app_user;
+                    $yesviteUserDetail['visible'] =  $guestVal->user->visible;
+                    $yesviteUserDetail['message_privacy'] =  $guestVal->user->message_privacy;
+                    $yesviteUserDetail['prefer_by']  = $guestVal->user->prefer_by;
+                    $yesviteUser[] = $yesviteUserDetail;
+                }
             }
             // dd($yesviteUser);
             // $yesvitecontactList = getYesviteSelectedUserPage($user->id, "10", $page, $event_id);
