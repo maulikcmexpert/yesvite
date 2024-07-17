2b08570256156caa35032758401be3d7a750c012
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-17 16:43:09 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -12237,6 +12237,11 @@ class ApiControllerv2 extends Controller
             // dd($invitedUser);
             if ($invitedUser->isNotEmpty()) {
                 foreach ($invitedUser as $guestVal) {
+
+                    if (!isset($guestVal->user->id) ||  $guestVal->user->id == null) {
+                        continue;
+                    }
+
                     $yesviteUserDetail['id'] = (isset($guestVal->user->id) && $guestVal->user->id != null) ? $guestVal->user->id : '';
                     $yesviteUserDetail['profile'] = empty($guestVal->user->profile) ? "" : asset('storage/profile/' . $guestVal->user->profile);
                     $yesviteUserDetail['first_name'] = (isset($guestVal->user->firstname) && $guestVal->user->firstname != Null) ? $guestVal->user->firstname : "";;
