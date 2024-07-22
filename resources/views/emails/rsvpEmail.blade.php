b7360a0962b6a203b66373a01ddd8685a316e09a
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-22 18:43:48 +0530

    Update rsvpEmail.blade.php

--- resources/views/emails/rsvpEmail.blade.php
+++ resources/views/emails/rsvpEmail.blade.php
@@ -159,8 +159,8 @@
                 <tr>
                     <td class="view-more fdfd">
                         <div class="view-btn mt-0" style="display: flex;align-items: center; gap: 15px; flex-wrap:wrap;">
-                            <a href="#" style="font-family: 'SF Pro Display', sans-serif; margin-right:10px; font-size: 14px;line-height: 20px;font-weight: 500;color: #fff;background: #F73C71;border: 1px solid #F73C71;border-radius: 10px; padding: 10px 24px 10px 24px; max-width:110px; display:inline-block; width:100%; text-align:center;" class="view-btn-link">View Invitation</a>
-                            <a href="#" style="font-family: 'SF Pro Display', sans-serif;font-size: 14px;line-height: 20px;font-weight: 500;color: #0F172A;background: transparent;border: 1px solid #E2E8F0;border-radius: 10px; padding: 10px 24px 10px 24px; max-width:110px; width:100%; text-align:center; display:inline-block " class="view-btn-link">Message James</a>
+                            <a href="#" style="font-family: 'SF Pro Display', sans-serif; margin-right:10px; font-size: 14px;line-height: 20px;font-weight: 500;color: #fff;background: #F73C71;border: 1px solid #F73C71;border-radius: 10px; padding: 10px 24px 10px 24px; display:inline-block; width:100%; text-align:center;" class="view-btn-link">View Invitation</a>
+                            <a href="#" style="font-family: 'SF Pro Display', sans-serif;font-size: 14px;line-height: 20px;font-weight: 500;color: #0F172A;background: transparent;border: 1px solid #E2E8F0;border-radius: 10px; padding: 10px 24px 10px 24px; width:100%; text-align:center; display:inline-block " class="view-btn-link">Message James</a>
                         </div>
                     </td>
                 </tr>
