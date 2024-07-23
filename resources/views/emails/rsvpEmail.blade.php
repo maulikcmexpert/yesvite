c1e988614a1cbd7ff935f0e176929ff5140176c8
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-23 11:15:59 +0530

    Update rsvpEmail.blade.php

--- resources/views/emails/rsvpEmail.blade.php
+++ resources/views/emails/rsvpEmail.blade.php
@@ -128,7 +128,7 @@
                 <tr>
                     <td>
                         <div class="user-name" style="display: flex;align-items: center;gap: 15px;margin-bottom: 15px;width:100%">
-                            <span style="display: block; width:50px; height: 50px; margin-right:10px;">
+                            <span style="display: block; width:65px; height: 65px; margin-right:10px;">
                                 <img src="{{ asset('public/storage/profile/'.$eventData['profileUser'])}}" style="width: 100%;height: 100%; border-radius:50%; margin-right:10px; object-fit:cover; display:block" alt="user-img">
                             </span>
                             <div style="width:85%; word-wrap:break-word; ">
