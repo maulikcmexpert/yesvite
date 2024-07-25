5977b747498768421d7a60baac1d5859003e3096
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-25 10:52:52 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7332,9 +7332,9 @@ class ApiControllerv2 extends Controller
                     $postsNormalDetail['is_host'] =  ($ischeckEventOwner != null) ? 1 : 0;
                     $postsNormalDetail['username'] =  $value->user->firstname . ' ' . $value->user->lastname;
                     $postsNormalDetail['profile'] =  empty($value->user->profile) ? "" : asset('public/storage/profile/' . $value->user->profile);
-                    $postsNormalDetail['post_message'] = empty($value->post_message) ? "" :  $value->post_message;
-                    // $postsNormalDetail['rsvp_status'] = (isset($value->post_type) && $value->post_type == '4') ? $value->post_message : $checkUserRsvp;
-                    $postsNormalDetail['rsvp_status'] = $checkUserRsvp;
+                    $postsNormalDetail['post_message'] = (empty($value->post_message) || $value->post_type == '4') ? "" :  $value->post_message;
+                    $postsNormalDetail['rsvp_status'] = (isset($value->post_type) && $value->post_type == '4' && $value->post_message != '') ? $value->post_message : $checkUserRsvp;
+                    // $postsNormalDetail['rsvp_status'] = $checkUserRsvp;
                     $postsNormalDetail['kids'] = isset($count_kids_adult['kids']) ? $count_kids_adult['kids'] : 0;
                     $postsNormalDetail['adults'] = isset($count_kids_adult['adults']) ? $count_kids_adult['adults'] : 0;
                     $postsNormalDetail['location'] = ($value->user->city != NULL) ? $value->user->city : "";
@@ -7434,7 +7434,8 @@ class ApiControllerv2 extends Controller
                     $postsNormalDetail['profile'] =  empty($value->user->profile) ? "" : asset('public/storage/profile/' . $value->user->profile);
 
                     $postsNormalDetail['is_host'] =  ($ischeckEventOwner != null) ? 1 : 0;
-                    $postsNormalDetail['post_message'] = empty($value->post_message) ? "" :  $value->post_message;
+                    $postsNormalDetail['post_message'] = (empty($value->post_message) || $value->post_type == '4') ? "" :  $value->post_message;
+                    // $postsNormalDetail['post_message'] = empty($value->post_message) ? "" :  $value->post_message;
                     $postsNormalDetail['rsvp_status'] = (isset($value->post_type) && $value->post_type == '4' && $value->post_message != '') ? $value->post_message : $checkUserRsvp;
                     // $postsNormalDetail['rsvp_status'] = $checkUserRsvp;
                     $postsNormalDetail['kids'] = isset($count_kids_adult['kids']) ? $count_kids_adult['kids'] : 0;
