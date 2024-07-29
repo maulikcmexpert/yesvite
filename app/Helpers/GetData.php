25620950652c38a9b940408d6e79bd01e052a093
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-29 12:33:42 +0530

    Update GetData.php

--- app/Helpers/GetData.php
+++ app/Helpers/GetData.php
@@ -198,11 +198,11 @@ function getYesviteContactListPage($id, $perPage, $page, $search_name)
     $yesviteRegisteredUser = User::select('id', 'firstname', 'profile', 'lastname', 'email', 'country_code', 'phone_number', 'app_user', 'prefer_by', 'email_verified_at', 'parent_user_phone_contact', 'visible', 'message_privacy')
         ->where('id', '!=', $id)
         ->where('is_user_phone_contact', '0')
-        ->where(function ($query) {
-            $query->whereNull('email_verified_at')
-                ->where('app_user', '!=', '1')
-                ->orWhereNotNull('email_verified_at');
-        })
+        // ->where(function ($query) {
+        //     $query->whereNull('email_verified_at')
+        //         ->where('app_user', '!=', '1')
+        //         ->orWhereNotNull('email_verified_at');
+        // })
         ->where(DB::raw("CONCAT(firstname, ' ', lastname)"), 'like', "%{$search_name}%")
         ->orderBy('firstname')
         ->paginate($perPage, ['*'], 'page', $page);
