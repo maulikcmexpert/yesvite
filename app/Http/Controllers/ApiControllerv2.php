cc76dfbf590b2876b92957d7da48864799b34b65
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-30 19:00:49 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7484,10 +7484,11 @@ class ApiControllerv2 extends Controller
                     $checkUserIsReaction = EventPostReaction::where(['event_id' => $input['event_id'], 'event_post_id' => $value->id, 'user_id' => $user->id])->first();
 
                     // if ($value->post_privacy == '1') {
-                    $EventPostMessageData = [];
+                    // $EventPostMessageData = [];
                     if (isset($value->post_type) && $value->post_type == '4' && $value->post_message != '') {
+
                         $EventPostMessageData = json_decode($value->post_message, true);
-                        $rsvpstatus = (int)$EventPostMessageData["status"];
+                        $rsvpstatus = $EventPostMessageData['status'];
                         $kids = $EventPostMessageData["kids"];
                         $adults = $EventPostMessageData["adults"];
                     } else {
