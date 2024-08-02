bb25744670524e15478b11ac2ce932b865588c73
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-08-02 12:20:29 +0530

    Update ChatController.php

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -227,7 +227,7 @@ class ChatController extends Controller
         )
             ->where(DB::raw("CONCAT(firstname, ' ', lastname)"), 'LIKE', '%' . $search . '%')
             ->where('id', '!=', $currentUserId)
-            ->where('app_user', '=', '1')
+            // ->where('app_user', '=', '1')
             ->where('email', '!=', '')
             ->where('message_privacy', '=', '1')
             ->whereNotIn('id', $selectedUserIds) // Filter out selected users
