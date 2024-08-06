ba39d11a868b317a1608c0286e5743c11213ecd5
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-08-06 18:52:39 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -8760,7 +8760,7 @@ class ApiControllerv2 extends Controller
 
         DB::commit();
 
-        dd($notificationParam)
+        dd($notificationParam);
         if ($request->is_in_photo_moudle == '1') {
 
             sendNotification('photos', $notificationParam);
