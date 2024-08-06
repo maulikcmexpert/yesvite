1e2ca827d2edd981559474fed2cb554d3cc64a29
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-08-06 18:03:15 +0530

    Update message.js

--- public/assets/front/js/message.js
+++ public/assets/front/js/message.js
@@ -3250,7 +3250,7 @@ async function send_push_notification(
 $(document).on('click','.reaction ',function (){
     // var c_id=$(this).closest($(".reaction-icon").data("message-id"));
         // var c_id=$(this).closest($(".reaction-icon").data("message-id"));
-        console.log($(this).parent().find('.simple-msg-wrap').html());
+        console.log($(this).parent().html());
 
 
     // alert(c_id);
