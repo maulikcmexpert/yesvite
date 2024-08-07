a20da0b2b438df45053177f2e389c8c4fd97cb2a
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-08-07 11:41:40 +0530

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -200,7 +200,7 @@ function sendNotification($notificationType, $postData)
                                 'profileUser' => ($value->event->user->profile != NULL || $value->event->user->profile != "") ? $value->event->user->profile : "no_profile.png",
                                 'event_image' => ($value->event->event_image->isNotEmpty()) ? $value->event->event_image[0]->image : "no_image.png",
                                 'date' =>   date('l - M jS, Y', strtotime($value->event->start_date)),
-                                'time' => $event_time,
+                                'time' => $value->event->rsvp_start_time,
                                 'address' => $value->event->event_location_name . ' ' . $value->event->address_1 . ' ' . $value->event->address_2 . ' ' . $value->event->state . ' ' . $value->event->city . ' - ' . $value->event->zip_code,
                             ];
 
