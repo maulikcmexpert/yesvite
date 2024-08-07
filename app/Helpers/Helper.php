8c48151dee711e2afc6294aa32c6c4333fab4b1a
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-08-07 16:42:26 +0530

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -94,7 +94,7 @@ function getGuestPendingRsvpCount($eventId)
 
 function sendNotification($notificationType, $postData)
 {
-
+    dd($postData);
     //'invite', 'upload_post', 'like_post', 'comment', 'reply', 'poll', 'rsvp'
     $senderData = User::where('id', $postData['sender_id'])->first();
     if ($notificationType == 'owner_notify') {
