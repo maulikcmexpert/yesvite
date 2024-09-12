cd8b78c7a78baf9877ae9d72060617a36f045d42
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-12 10:19:48 +0530

    commit

--- resources/views/emails/invite_pending.blade.php
+++ resources/views/emails/invite_pending.blade.php
@@ -48,7 +48,7 @@
                     <td>
                         <h5 style="font-size: 32px;line-height: 44px;font-weight: 400;color: #0A090B;margin: 0px 0px;">Complete your invitation:</h5>
                         <h2 style="font-size: 32px;line-height: 44px;font-weight: 700;color: #0A090B;margin: 0px 0px;">{{$eventData['event_name']}}</h2>
-                        <p style="font-size: 18px;line-height: 27px;font-weight: 400;color: #0F172A;margin: 10px 0px;">{{$eventData['date'].' @ '.$eventData['time']}}</p>
+                        <p style="font-size: 18px;line-height: 27px;font-weight: 400;color: #0F172A;margin: 10px 0px;">{{$eventData['date']}} {{(isset($eventData['time']) && $eventData['time']!='')?'@ '.$eventData['time']:''}}</p>
                     </td>
                 </tr>
                 <!-- -------------- -->
