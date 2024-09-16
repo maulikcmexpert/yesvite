2ad578d576cb32f3e0cdb33be74f42ad8092fc9b
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-16 16:42:36 +0530

    Update header.blade.php

--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -31,19 +31,19 @@
 
 
 
-<!-- <link rel="stylesheet" href="{{ asset('assets/event/css/about-us.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/event/css/about-us.css') }}">
 
 <link rel="stylesheet" href="{{ asset('assets/event/css/account.css') }}">
 
 <link rel="stylesheet" href="{{ asset('assets/event/css/animate.css') }}">
 
-<link rel="stylesheet" href="{{ asset('assets/event/css/change-pwd.css') }}"> -->
+<link rel="stylesheet" href="{{ asset('assets/event/css/change-pwd.css') }}">
 
 <link rel="stylesheet" href="{{ asset('assets/event/css/common.css') }}">
 
-<!-- <link rel="stylesheet" href="{{ asset('assets/event/css/contact.css') }}"> -->
+<link rel="stylesheet" href="{{ asset('assets/event/css/contact.css') }}">
 
-<!-- <link rel="stylesheet" href="{{ asset('assets/event/css/edit-profile.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/event/css/edit-profile.css') }}">
 
 <link rel="stylesheet" href="{{ asset('assets/event/css/font.css') }}">
 
@@ -59,7 +59,7 @@
 
 <link rel="stylesheet" href="{{ asset('assets/event/css/new_event_detail.css') }}">
 
-<link rel="stylesheet" href="{{ asset('assets/event/css/notification.css') }}"> -->
+<link rel="stylesheet" href="{{ asset('assets/event/css/notification.css') }}">
 
 <link rel="stylesheet" href="{{ asset('assets/event/css/pick_card.css') }}">
 
@@ -72,10 +72,10 @@
 <!-- <link rel="stylesheet" href="{{ asset('assets/event/css/rsvp.css') }}"> -->
 
 <link rel="stylesheet" href="{{ asset('assets/event/css/setting.css') }}">
-<!-- 
+
 <link rel="stylesheet" href="{{ asset('assets/event/css/style.css') }}">
 
-<link rel="stylesheet" href="{{ asset('assets/event/css/variable.css') }}"> -->
+<link rel="stylesheet" href="{{ asset('assets/event/css/variable.css') }}">
 
 
 
