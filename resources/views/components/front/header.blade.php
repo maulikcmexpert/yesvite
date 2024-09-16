124e8ba2a1f4c2de9b8d005366b9ee0c090d9efd
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-16 17:28:53 +0530

    Update header.blade.php

--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -1,4 +1,4 @@
-<title>{{$title}}</title>
+<title>{{$page}}</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
@@ -31,14 +31,14 @@
 
 <link rel="stylesheet" href="{{ asset('assets/event/css/common.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/event/css/contact.css') }}">
-@php
+<!-- @php
 if($page=="front.profile"){
 @endphp
 <link rel="stylesheet" href="{{ asset('assets/event/css/style.css') }}">
 
 @php
 }
-@endphp
+@endphp -->
 <!-- <link rel="stylesheet" href="{{ asset('assets/event/css/style.css') }}"> -->
 
 
