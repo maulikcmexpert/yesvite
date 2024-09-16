1d2e2f2e34cbb9d12062c4978498f0b60eb20442
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-16 15:31:35 +0530

    Update profile.blade.php

--- resources/views/front/profile.blade.php
+++ resources/views/front/profile.blade.php
@@ -629,7 +629,7 @@
             <!-- ========== edit-plan-sidebar ========= -->
             <div class="edit-checkout">
                 <div class="d-flex align-items-center justify-content-between toggle-wrp">
-                    <h5>Change your plan</h5>
+                    <h5>Choose Your Experience (plan)</h5>
                     <button class="close-btn" onclick="toggleSidebar()">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M5.00098 5L19 18.9991" stroke="#64748B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -637,13 +637,13 @@
                         </svg>
                     </button>
                 </div>
-                <div class="invite-link">
+                <!-- <div class="invite-link">
                     <div class="invite-left-side recomendat-plan">
                         <h6>0</h6>
                         <p><strong>Invites | 15</strong> Left</p>
                     </div>
                     <span>15 or less <strong>$15.99</strong></span>
-                </div>
+                </div> -->
                 <div class="plans-wrap change-plan free_plan active">
                     <div class="d-flex justify-content-between">
                         <div>
