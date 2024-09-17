b334e8d3c7af773636a4015e06c617e6cec473bb
Prakashmanat01 <prakashhmanat@gmail.com>
2024-09-17 17:54:28 +0530

    commit

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
         //  alert(design);
+        $('.user_choice').prop('checked',false);
         $.ajax({
             url: base_url + "event/delete-session",
             type: "POST",
