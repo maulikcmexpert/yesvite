de7a5755bf1d4bc01e2a29806f420ac9f289ec11
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-18 12:00:35 +0530

    Update contact.blade.php

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -80,12 +80,12 @@
                         <div class="tab-pane fade" id="phone" role="tabpanel" aria-labelledby="phone-tab">
                             <div class="position-relative">
                                 <!-- <input type="search" class="search_phone" placeholder="Search Phone"> -->
-                                <span>
+                                <!-- <span>
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                         <path d="M22 22L20 20" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                     </svg>
-                                </span>
+                                </span> -->
                             </div>
                             <div class="product-scroll" id="product-scroll-groups">
                                 <div class="user-contacts" id="yesvitePhones">
