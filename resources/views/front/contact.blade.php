f9161172b8c799b1e83dbb442d9a36fc920df3b6
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-18 11:30:49 +0530

    Update contact.blade.php

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -298,7 +298,7 @@
                                 </div>
                             </div>
                             <div class="col-lg-12 col-md-12 col-sm-12 col-12">
-                                <div class="input-form">
+                                <div class="input-form add-contact-selectbox">
                                     <input type="text" class="phone_number border-0" name="countryCode" readonly>
                                     <input type="text" class="form-control edit_phone" id="phone_number" name="phone_number" value="">
                                     <label for="phone" class="floating-label">Phone
