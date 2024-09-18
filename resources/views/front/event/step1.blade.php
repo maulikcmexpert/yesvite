0e44dd50af55bc082d489fef3f2edfd8e5985525
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-18 21:01:28 +0530

    Update step1.blade.php

--- resources/views/front/event/step1.blade.php
+++ resources/views/front/event/step1.blade.php
@@ -348,7 +348,7 @@
                                 </span>
                                 <h5 class="ms-2">Edit Design</h5> --}}
                             </a>
-                            <button type="button footer-bottom-btn" class="d-flex" id="next_design">
+                            <button type="button" class="d-flex footer-bottom-btn" id="next_design">
                                 <h5 class="me-2">Next: Design</h5>
                                 <span><svg width="16" height="16" viewBox="0 0 16 16" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
