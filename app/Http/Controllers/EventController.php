9a408d7ff63eb48c3243b3d69beb933f4f56833c
Prakashmanat01 <prakashhmanat@gmail.com>
2024-09-19 10:34:03 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -874,7 +874,8 @@ class EventController extends Controller
 
         $yesvite_user = User::select('id', 'firstname', 'lastname', 'phone_number', 'email', 'profile')
             ->where('id', '!=', $id)
-            ->where(['is_user_phone_contact' => '0','email_verified_at !=' => null,'app_user' => '1'])
+            ->where(['is_user_phone_contact' => '0'])
+            ->where('email_verified_at','!=',null)
             ->orderBy('firstname')
             ->limit($request->limit)
             ->skip($request->offset)
