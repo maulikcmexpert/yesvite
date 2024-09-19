70b1666055414be23916891c90e63991fb566b6e
Prakashmanat01 <prakashhmanat@gmail.com>
2024-09-19 17:01:06 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -2820,7 +2820,7 @@ $(document).on("click", "#delete_invited_user_tel", function () {
 
     $("#event_guest_count").text(re_total_guest + " Guests");
     $(".invite-count").text(re_total_guest);
-    if(remainingCount < 0){
+    if(re_total_remaining_count < 0){
         $(".invite-left_d").text("Invites | 0 Left");
     }else{
         $(".invite-left_d").text("Invites | " + re_total_remaining_count + " Left");
