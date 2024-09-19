8106ee37789ce4201cce265cfc30fc6b38550836
Prakashmanat01 <prakashhmanat@gmail.com>
2024-09-20 00:24:45 +0530

    Update step4.blade.php

--- resources/views/front/event/step4.blade.php
+++ resources/views/front/event/step4.blade.php
@@ -535,7 +535,7 @@
             <div class="modal-body">
                 <h5 class="delete_potluck_title">Potluck data will be deleted</h5>
                 <p class="delete_potluck_text">If you turn off the potluck option.</p>
-                <p class="text-danger delete_category_text"></p>
+                <strong><p class="text-danger delete_category_text"></p></strong>
             </div>
             <div class="modal-footer">
                 <input type="hidden" id="delete_potluck_category_id" name="delete_potluck_category_id">
