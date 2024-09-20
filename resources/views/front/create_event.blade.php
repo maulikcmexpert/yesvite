a3b82e715647aa994d71731514b13b9a80a669b9
Vimal <vimal.cmexpertise@gmail.com>
2024-09-20 13:11:46 +0530

    Update create_event.blade.php

--- resources/views/front/create_event.blade.php
+++ resources/views/front/create_event.blade.php
@@ -2222,7 +2222,7 @@
                     </a>
                     <h5>Invite group member</h5>
                 </div>
-                <button class="close-btn" onclick="toggleSidebar()">
+                <button class="close-btn group_toggle_close_btn" onclick="toggleSidebar()">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <path d="M5.00098 5L19 18.9991" stroke="#64748B" stroke-width="1.5" stroke-linecap="round"
