328b0805aa4e1e5cbfb99c46a13acd5d88f778db
Vimal <vimal.cmexpertise@gmail.com>
2024-09-20 21:52:03 +0530

    commit

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -1,3 +1,6 @@
+@php
+    dd($page);
+@endphp
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 
--- resources/views/event_layout.blade.php
+++ resources/views/event_layout.blade.php
@@ -64,7 +64,7 @@
         <img src="{{asset('assets/front/loader.gif')}}" alt="loader" style="width:146px;height:146px;z-index:1000">
     </div>
     @include($page)
-    <x-front.footer :js="$js ?? []" />
+    <x-front.footer :js="$js ?? []" page="{{$page}}" />
 </body>
 
 </html>
