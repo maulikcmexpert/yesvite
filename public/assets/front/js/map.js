ae2500c2f9fc268f338ac77541c1d1c166bc727b
Vimal <vimal.cmexpertise@gmail.com>
2024-09-20 10:17:19 +0530

    Update map.js

--- public/assets/front/js/map.js
+++ public/assets/front/js/map.js
@@ -1,88 +1,87 @@
 
 function initMap() {
+    // Initialize the map
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -33.8688, lng: 151.2195},
         zoom: 13
     });
-    /*var options = {
-     types: ['(cities)']
-     }*/
+
+    // Elements for autocomplete and additional options
     var card = document.getElementById('pac-card');
     var input = document.getElementById('address1');
-    var types = document.getElementById('type-selector');
-    var strictBounds = document.getElementById('strict-bounds-selector');
-
     map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
 
+    // Initialize autocomplete and bind it to the map's bounds
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
 
+    // Info window and marker for the selected location
     var infowindow = new google.maps.InfoWindow();
     var marker = new google.maps.Marker({
         map: map,
         anchorPoint: new google.maps.Point(0, -29)
     });
 
+    // When a place is selected, update the map and form
     autocomplete.addListener('place_changed', function () {
         infowindow.close();
         marker.setVisible(false);
         var place = autocomplete.getPlace();
-        if (place) {
-            $("#latitude").val(place.geometry.location.lat);
-            $("#longitude").val(place.geometry.location.lng);
-        }
 
+        // Check if the place has geometry data
         if (!place.geometry) {
             window.alert("No details available for input: '" + place.name + "'");
             return;
         }
 
+        // Update the map view and marker position
         if (place.geometry.viewport) {
             map.fitBounds(place.geometry.viewport);
         } else {
             map.setCenter(place.geometry.location);
-            map.setZoom(17);
+            map.setZoom(17);  // Zoom in if there's no viewport
         }
+
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
 
-        var address = '';
-        if (place.address_components) {
-            address = [
-                (place.address_components[0] && place.address_components[0].short_name || ''),
-                (place.address_components[1] && place.address_components[1].short_name || ''),
-                (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-
-            var typeValShort = [];
-            var typeValLong = [];
-            var typeField = [];
-            // var addr_fields = ["premise", "route", "administrative_area_level_2", "locality", "administrative_area_level_1", "country", "postal_code"];
-            var addr_fields = ["premise", "route", "administrative_area_level_2", "locality", "administrative_area_level_1", "postal_code"];
-            var addr_inputs = ["address1", "address2", "city", "city", "state", "zipcode"];
-
-            for (var i = 0; i < place.address_components.length; i++) {
-                for (var j = 0; j < place.address_components[i].types.length; j++) {
-
-                    typeField.push(place.address_components[i].types[j]);
-                    typeValLong.push(place.address_components[i].long_name);
-                }
-            }
-            for (var k = 0; k < addr_fields.length; k++) {
-                var indexKey = $.inArray(addr_fields[k], typeField);
-                if (indexKey != -1) {
-                    $("#" + addr_inputs[k]).val(typeValLong[indexKey]);
-                    var selectVal = $("#" + addr_inputs[k] + " option").filter(function () {
-                        return $(this).html() == typeValLong[indexKey];
-                    }).val();
-                    if (selectVal) {
-                        $("#" + addr_inputs[k]).val(selectVal);
-                    }
-                } else {
-                    $("#" + addr_inputs[k]).val("");
-                }
-            }
+        // Update latitude and longitude inputs
+        if (place.geometry.location) {
+            $("#latitude").val(place.geometry.location.lat());
+            $("#longitude").val(place.geometry.location.lng());
         }
 
+        // Handle address components and update respective form fields
+        if (place.address_components) {
+            var addr_fields = ["premise", "route", "administrative_area_level_2", "locality", "administrative_area_level_1", "postal_code"];
+            var addr_inputs = ["address1", "address2", "city", "city", "state", "zipcode"];
+            var typeField = [], typeValLong = [];
+
+            // Loop through address components and match with input fields
+            place.address_components.forEach(function(component) {
+                component.types.forEach(function(type) {
+                    typeField.push(type);
+                    typeValLong.push(component.long_name);
+                });
+            });
+
+            addr_fields.forEach(function(field, index) {
+                var fieldIndex = typeField.indexOf(field);
+                if (fieldIndex !== -1) {
+                    $("#" + addr_inputs[index]).val(typeValLong[fieldIndex]);
+
+                    // If it's a select input, find matching option and set its value
+                    var selectVal = $("#" + addr_inputs[index] + " option").filter(function () {
+                        return $(this).html() == typeValLong[fieldIndex];
+                    }).val();
+
+                    if (selectVal) {
+                        $("#" + addr_inputs[index]).val(selectVal);
+                    }
+                } else {
+                    $("#" + addr_inputs[index]).val("");
+                }
+            });
+        }
     });
 }
