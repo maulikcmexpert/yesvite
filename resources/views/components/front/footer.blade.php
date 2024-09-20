076e71ab85831b0e186385028a1bfdb7af3f463e
Prakashmanat01 <prakashhmanat@gmail.com>
2024-09-20 20:13:53 +0530

    Update footer.blade.php

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -114,6 +114,26 @@ defer
 <script>
     new WOW().init()
 </script>
+
+<script>
+    toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
+</script>
 @if(isset($js))
 
 @foreach($js as $value)
