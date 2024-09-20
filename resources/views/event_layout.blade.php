1631103fbcbff8b8b0a03c403bff1dd6db152631
Prakashmanat01 <prakashhmanat@gmail.com>
2024-09-20 20:16:18 +0530

    commit

--- resources/views/event_layout.blade.php
+++ resources/views/event_layout.blade.php
@@ -3,6 +3,7 @@
 
 <head>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
     <link rel="stylesheet" href="{{ asset('assets/event/css/about-us.css') }}">
 
@@ -65,6 +66,8 @@
     </div>
     @include($page)
     <x-front.footer :js="$js ?? []" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
 </body>
 
 </html>
