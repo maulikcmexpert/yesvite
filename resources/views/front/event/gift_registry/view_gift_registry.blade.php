0c2130143981c9ea9a7babf217278514709920d6
Vimal <vimal.cmexpertise@gmail.com>
2024-09-23 21:41:56 +0530

    Update view_gift_registry.blade.php

--- resources/views/front/event/gift_registry/view_gift_registry.blade.php
+++ resources/views/front/event/gift_registry/view_gift_registry.blade.php
@@ -1,13 +1,10 @@
-@php
-    dd($registry[0]);
-@endphp
 @if(isset($registry)&&!empty($registry))
 
 @foreach ($registry as $data )
 <div class="d-flex align-items-center justify-content-center">
     <span class="me-2">
         <a href="{{ $data['registry_link'] }}">
-            <img src="{{ asset('assets/' . $data['recipient_name'] . '.jpg') }}" alt="eventpic" style="max-width: 145px;">
+            <img src="{{ asset('assets/' . $data['registry_name'] . '.jpg') }}" alt="eventpic" style="max-width: 145px;">
         </a>
     </span>
     {{-- <h6>Target</h6> --}}
