c7fe7a966c8466c4054c9cba7a7cdf32ba2d59a1
Vimal <vimal.cmexpertise@gmail.com>
2024-09-23 14:37:58 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -386,11 +386,11 @@ $(function () {
 
     $("#event-date").on('apply.daterangepicker', function(ev, picker) {
         picker.show();
-        $('#floatingfocus').addClass('floatingfocus');
+        $('#event-date').addClass('floatingfocus');
     });
     $('#event-date').on('hide.daterangepicker', function(ev, picker) {
         picker.show();
-        $('#floatingfocus').addClass('floatingfocus');
+        $('#event-date').addClass('floatingfocus');
     });
    
 });
