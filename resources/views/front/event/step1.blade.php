67217fe21e2d3fca7fea879ed3bb084e4a0918bc
Vimal <vimal.cmexpertise@gmail.com>
2024-09-23 15:43:31 +0530

    Update step1.blade.php

--- resources/views/front/event/step1.blade.php
+++ resources/views/front/event/step1.blade.php
@@ -206,11 +206,11 @@
                         <div class="input-form location-icon">
                             <input type="text" class="form-control inputText" id="address1" name="address1"
                                 oninput="clearError(this)" required="">
+                            <label for="address1" class="form-label input-field floating-label">Address 1
+                                    *</label>
                                 <input type="hidden" id="latitude" />
                                 <input type="hidden" id="longitude" />
                                 <div id="map"></div>
-                            <label for="address1" class="form-label input-field floating-label">Address 1
-                                *</label>
                             <lable for="address1" id="event-address1-error" class="error"></lable>
 
                             <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
