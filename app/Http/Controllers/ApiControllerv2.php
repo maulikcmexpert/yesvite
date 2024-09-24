823489e61b0faaa54fed5eddb21afc9e1e2fa935
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-24 13:19:44 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -871,6 +871,8 @@ class ApiControllerv2 extends Controller
                             'visible' =>  $value->user->visible,
                             'comments' => $comments,
                         ];
+                        $eventDetail['event_plan_name'] = $value->subscription_plan_name;
+
                         $eventList[] = $eventDetail;
                     }
                 }
@@ -1084,6 +1086,8 @@ class ApiControllerv2 extends Controller
                             'visible' =>  $value->event->user->visible,
                             'comments' => $comments
                         ];
+                        $eventDetail['event_plan_name'] = $value->event->subscription_plan_name;
+
                         $eventList[] = $eventDetail;
                     }
                 }
@@ -1316,6 +1320,7 @@ class ApiControllerv2 extends Controller
                             'visible' =>  $value->user->visible,
                             'comments' => $comments,
                         ];
+                        $eventDetail['event_plan_name'] = $value->subscription_plan_name;
 
                         $eventList[] = $eventDetail;
                     }
@@ -1577,6 +1582,7 @@ class ApiControllerv2 extends Controller
                             'visible' =>  $value->user->visible,
                             'comments' => $comments,
                         ];
+                        $eventDetail['event_plan_name'] = $value->subscription_plan_name;
 
                         $eventList[] = $eventDetail;
                     }
