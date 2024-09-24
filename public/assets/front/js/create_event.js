788be25240e214b458e644b72cc0fd9f6e7e32fa
Vimal <vimal.cmexpertise@gmail.com>
2024-09-24 13:11:43 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -3123,6 +3123,7 @@ $(document).on("click", ".li_setting", function () {
     var design = eventData.desgin_selected;
     var step3 = eventData.step;
     console.log(final_step);
+    console.log(eventData.step);
     if( step3 == undefined || (step3 != '3' && step3 != '4') ){
         return;
     }else{
