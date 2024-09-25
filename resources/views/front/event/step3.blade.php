d7119ad79f5c47118514452aaba4443e56a6e7d4
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-25 17:56:32 +0530

    Update step3.blade.php

--- resources/views/front/event/step3.blade.php
+++ resources/views/front/event/step3.blade.php
@@ -158,6 +158,8 @@
                                         @endforeach
 
                                     </div>
+                                    <div class="swiper-button-next"></div>
+                                    <div class="swiper-button-prev"></div>
                                     <!-- <div class="swiper-pagination"></div> -->
                                 </div>
 
