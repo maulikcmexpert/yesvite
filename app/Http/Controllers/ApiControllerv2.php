0a0621a9ea3e834a2f25a90732e5d091fdfe79c0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-26 15:53:10 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -4429,7 +4429,7 @@ class ApiControllerv2 extends Controller
                 $updateEvent->subscription_plan_name = (!empty($eventData['subscription_plan_name'])) ? $eventData['subscription_plan_name'] : "";
                 // $updateEvent->subscription_invite_count = (!empty($eventData['subscription_invite_count'])) ? $eventData['subscription_invite_count'] : 0;
                 // dd($eventData['static_information']);
-                $updateEvent->static_information = (isset($eventData['static_information']) && $eventData['static_information'] != '') ? $eventData['static_information'] : null;
+                // $updateEvent->static_information = (isset($eventData['static_information']) && $eventData['static_information'] != '') ? $eventData['static_information'] : null;
                 // $updateEvent->design_image = (!empty($eventData['design_image'])) ? $eventData['design_image'] : "";
                 // $updateEvent->design_inner_image = (!empty($eventData['design_inner_image'])) ? $eventData['design_inner_image'] : "";
 
