ab95ed8eaf8e1e665ce6105ab7f3d908f8e65ef9
cmexpertise <pratik.cmexpertise@gmail.com>
2024-09-26 12:48:33 +0530

    Update message.js

--- public/assets/front/js/message.js
+++ public/assets/front/js/message.js
@@ -1118,6 +1118,7 @@ $(".send-message").on("keypress", async function (e) {
             return;
         }
         $(this).val(""); // Clear the input field
+        $(this).style("height", "auto");
         const messageData = {
             data: message,
             url: downloadURL,
