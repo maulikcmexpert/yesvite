99bc698abc1618952267a7b1b76e2c2f6fccf805
cmexpertise <pratik.cmexpertise@gmail.com>
2024-09-26 20:32:29 +0530

    Update message.js

--- public/assets/front/js/message.js
+++ public/assets/front/js/message.js
@@ -450,7 +450,7 @@ async function handleNewConversation(snapshot) {
 function moveToTopOrBelowPinned(element) {
     const $chatList = $(".chat-list");
     const $pinnedElements = $chatList.find(".pinned");
-    console.log("dassd");
+    console.log(element);
     if ($pinnedElements.length > 0) {
         // Insert after the last pinned element
         $pinnedElements.last().after(element);
