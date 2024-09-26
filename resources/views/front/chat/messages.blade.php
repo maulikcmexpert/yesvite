be172fbfe462dc849d6c35541285fc577c86b1e5
cmexpertise <pratik.cmexpertise@gmail.com>
2024-09-26 18:04:21 +0530

    Update messages.blade.php

--- resources/views/front/chat/messages.blade.php
+++ resources/views/front/chat/messages.blade.php
@@ -212,7 +212,7 @@ use Carbon\Carbon;
                                                             <path d="M15.6797 3.66211L21.3365 9.31896" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                                                         </svg>
                                 
-                                                        <svg class="me-2 unpin-single-conversation unpin1-self-icn {{@$message['isPin']=='1'?'':'d-none'}}" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368" style="display: none;">
+                                                        <svg class="me-2 unpin-single-conversation unpin1-self-icn {{@$message['isPin']=='1'?'':'d-none'}}" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
                                                             <path d="M680-840v80h-40v327l-80-80v-247H400v87l-87-87-33-33v-47h400ZM480-40l-40-40v-240H240v-80l80-80v-46L56-792l56-56 736 736-58 56-264-264h-6v240l-40 40ZM354-400h92l-44-44-2-2-46 46Zm126-193Zm-78 149Z"></path>
                                                         </svg>
                                 
