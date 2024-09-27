dac5ee131dc5559bcef7054c86495303e658e529
Vimal <vimal.cmexpertise@gmail.com>
2024-09-27 12:56:28 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -15,6 +15,9 @@ $(document).on("click", ".design-card", function () {
     $("#modalImage").attr("src", imageUrl);
     image = imageUrl;
     $("#exampleModal").modal("show");
+    if (typeof canvas !== 'undefined' && canvas) {
+        canvas.clear();  // Clear the canvas if it exists
+    }    
     var canvas = new fabric.Canvas('imageEditor2', {
         width: 350, // Canvas width
         height: 490, // Canvas height
