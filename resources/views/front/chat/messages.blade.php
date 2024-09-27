346c3453f9cf725d8fc733e517fc915641e32211
cmexpertise <pratik.cmexpertise@gmail.com>
2024-09-27 17:24:46 +0530

    Update messages.blade.php

--- resources/views/front/chat/messages.blade.php
+++ resources/views/front/chat/messages.blade.php
@@ -653,6 +653,7 @@ use Carbon\Carbon;
                                                 <path d="M12.5 22C18.0228 22 22.5 17.5228 22.5 12C22.5 6.47715 18.0228 2 12.5 2C6.97715 2 2.5 6.47715 2.5 12C2.5 17.5228 6.97715 22 12.5 22Z" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                             </svg>
                                         </button>
+                                        <form id="upload-files">
                                         <ul class="dropdown-menu">
                                             <li>
                                                 <a href="#" class="button-wrapper">
@@ -692,6 +693,7 @@ use Carbon\Carbon;
                                                 </a>
                                             </li>
                                         </ul>
+                                        </form>
                                     </div>
                                     <div>
                                         <div id="audioControls">
