2aa1ca7c23ae9849a5bd71c1109715629940c382
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-27 13:11:56 +0530

    Update TemplateController.php

--- app/Http/Controllers/admin/TemplateController.php
+++ app/Http/Controllers/admin/TemplateController.php
@@ -110,14 +110,13 @@ class TemplateController extends Controller
                     $image->move(public_path('storage/canvas'), $imageName);
                 }
             }
-            dd($imageName);
-
             // Store the template with design ID and the uploaded image's filename
             TextData::create([
                 'event_category_id' => $request->event_design_category_id,
                 'event_sub_category_id' => $request->event_design_sub_category_id,
                 'image' => $imageName, // Save the uploaded image filename
             ]);
+            Log::info(DB::getQueryLog());
 
             DB::commit();
 
