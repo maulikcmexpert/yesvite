1421e5be41bbb5c2bd730e9556516b4665b78756
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-27 13:09:55 +0530

    Update TemplateController.php

--- app/Http/Controllers/admin/TemplateController.php
+++ app/Http/Controllers/admin/TemplateController.php
@@ -110,7 +110,7 @@ class TemplateController extends Controller
                     $image->move(public_path('storage/canvas'), $imageName);
                 }
             }
-            dd($request);
+            dd($imageName);
 
             // Store the template with design ID and the uploaded image's filename
             TextData::create([
