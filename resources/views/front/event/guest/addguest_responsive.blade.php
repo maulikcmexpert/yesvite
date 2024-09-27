efa42e0f3a5ca1930c0b1ef1cb11af9ed75d552f
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-27 12:37:49 +0530

    Update addguest_responsive.blade.php

--- resources/views/front/event/guest/addguest_responsive.blade.php
+++ resources/views/front/event/guest/addguest_responsive.blade.php
@@ -76,9 +76,12 @@ break;
 @endforeach
 
 @if($counter > 4)
+@php
+$counter = count($user_list) - 4;
+@endphp
 <div class="guest-contact all_user_list">
     <div class="guest-img">
-        <span class="update_user_count">+{{$counter-4}}</span>
+        <span class="update_user_count">+{{$counter}}</span>
     </div>
     <span class="all-contact">See all</span>
 </div>
