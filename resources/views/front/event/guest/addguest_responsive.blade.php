7ec331caeebae688161b15463fdca5ab5c34ec3f
Vimal <vimal.cmexpertise@gmail.com>
2024-09-27 11:06:08 +0530

    commit

--- resources/views/front/event/guest/addguest_responsive.blade.php
+++ resources/views/front/event/guest/addguest_responsive.blade.php
@@ -1,20 +1,20 @@
-@foreach ( $data as $guest_user)
-@if($guest_user['userdata']['prefer_by']=="email")
-<div class="guest-contact invited_user user_id-{{$guest_user['userdata']['id']}} responsive_invite_user" data-id={{$guest_user['userdata']['id']}} id="">
+@foreach ( $user_list as $guest_user)
+@if($guest_user['prefer_by']=="email")
+<div class="guest-contact invited_user user_id-{{$guest_user['id']}} responsive_invite_user" data-id={{$guest_user['id']}} id="">
     <div class="guest-img mobile-guest-icon">
         <!-- <img src="./assets/image/user-img.svg" alt="guest-img"> -->
-        @if ($guest_user['userdata']['profile'] != '')
-        <img src="{{ asset('storage/profile/' . $guest_user['userdata']['profile']) }}" alt="user-img">
+        @if ($guest_user['profile'] != '')
+        <img src="{{ asset('storage/profile/' . $guest_user['profile']) }}" alt="user-img">
         @else
         @php
-        $firstInitial = !empty($guest_user['userdata']['firstname']) ? strtoupper($guest_user['userdata']['firstname'][0]) : '';
-        $lastInitial = !empty($guest_user['userdata']['lastname']) ? strtoupper($guest_user['userdata']['lastname'][0]) : '';
+        $firstInitial = !empty($guest_user['firstname']) ? strtoupper($guest_user['firstname'][0]) : '';
+        $lastInitial = !empty($guest_user['lastname']) ? strtoupper($guest_user['lastname'][0]) : '';
         $initials = $firstInitial . $lastInitial;
         $fontColor = 'fontcolor' . $firstInitial;
         @endphp
         <h5 class="{{ $fontColor }}"> {{ $initials }}</h5>
         @endif
-        <a href="#" class="close" id="delete_invited_user" data-id="user-{{$guest_user['userdata']['id']}}" data-userid="{{$guest_user['userdata']['id']}}">
+        <a href="#" class="close" id="delete_invited_user" data-id="user-{{$guest_user['id']}}" data-userid="{{$guest_user['id']}}">
             <svg width="19" height="18" viewBox="0 0 19 18"
                 fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="1.8999" y="1" width="16" height="16"
@@ -28,24 +28,24 @@
             </svg>
         </a>
     </div>
-    <h6 class="guest-name">{{$guest_user['userdata']['firstname']}} {{$guest_user['userdata']['lastname']}}</h6>
+    <h6 class="guest-name">{{$guest_user['firstname']}} {{$guest_user['lastname']}}</h6>
 </div>
-@elseif ($guest_user['userdata']['prefer_by']=="phone")
-<div class="guest-contact invited_user user_id_tel-{{$guest_user['userdata']['id']}} responsive_invite_user" data-id={{$guest_user['userdata']['id']}} id="">
+@elseif ($guest_user['prefer_by']=="phone")
+<div class="guest-contact invited_user user_id_tel-{{$guest_user['id']}} responsive_invite_user" data-id={{$guest_user['id']}} id="">
     <div class="guest-img mobile-guest-icon">
         <!-- <img src="./assets/image/user-img.svg" alt="guest-img"> -->
-        @if ($guest_user['userdata']['profile'] != '')
-        <img src="{{ asset('storage/profile/' . $guest_user['userdata']['profile']) }}" alt="user-img">
+        @if ($guest_user['profile'] != '')
+        <img src="{{ asset('storage/profile/' . $guest_user['profile']) }}" alt="user-img">
         @else
         @php
-        $firstInitial = !empty($guest_user['userdata']['firstname']) ? strtoupper($guest_user['userdata']['firstname'][0]) : '';
-        $lastInitial = !empty($guest_user['userdata']['lastname']) ? strtoupper($guest_user['userdata']['lastname'][0]) : '';
+        $firstInitial = !empty($guest_user['firstname']) ? strtoupper($guest_user['firstname'][0]) : '';
+        $lastInitial = !empty($guest_user['lastname']) ? strtoupper($guest_user['lastname'][0]) : '';
         $initials = $firstInitial . $lastInitial;
         $fontColor = 'fontcolor' . $firstInitial;
         @endphp
         <h5 class="{{ $fontColor }}"> {{ $initials }}</h5>
         @endif
-        <a href="#" class="close" id="delete_invited_user_tel" data-id="user_tel-{{$guest_user['userdata']['id']}}" data-userid="{{$guest_user['userdata']['id']}}">
+        <a href="#" class="close" id="delete_invited_user_tel" data-id="user_tel-{{$guest_user['id']}}" data-userid="{{$guest_user['id']}}">
             <svg width="19" height="18" viewBox="0 0 19 18"
                 fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="1.8999" y="1" width="16" height="16"
@@ -59,7 +59,7 @@
             </svg>
         </a>
     </div>
-    <h6 class="guest-name">{{$guest_user['userdata']['firstname']}} {{$guest_user['userdata']['lastname']}}</h6>
+    <h6 class="guest-name">{{$guest_user['firstname']}} {{$guest_user['lastname']}}</h6>
 </div>
 @endif
 @endforeach
