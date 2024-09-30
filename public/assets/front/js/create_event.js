de378ab2503fcabf85ed3609a94cb4c1bd8ecff9
Vimal <vimal.cmexpertise@gmail.com>
2024-09-30 22:26:08 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -4283,10 +4283,10 @@ $(document).on("click", ".next_guest_step", function () {
     $(this).prop("disabled", true);
     $('.btn-close').prop("disabled", true);
     var textData = getTextDataFromCanvas();
-    console.log(textData);
+    // console.log(textData);
     eventData.textData = dbJson;
     eventData.temp_id = temp_id;
-    // save_image_design(downloadImage);
+    save_image_design(downloadImage);
     $(".main-content-wrp").addClass("blurred");
 });
 
