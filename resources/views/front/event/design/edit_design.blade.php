f2a1904e9856c137229e7e459f0750f916681d29
Vimal <vimal.cmexpertise@gmail.com>
2024-09-30 17:58:19 +0530

    Update edit_design.blade.php

--- resources/views/front/event/design/edit_design.blade.php
+++ resources/views/front/event/design/edit_design.blade.php
@@ -859,7 +859,7 @@
                                     stroke-linejoin="round" />
                             </svg>
                             
-                            <img class="photo-slider-2" src="" alt="" class="d-none">
+                            <img class="photo-slider-2" src="" alt="" style="display: none">
                             <button class="uploaded-img-card-edit">
                                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
@@ -915,7 +915,7 @@
                                     stroke="#64748B" stroke-width="1.5" stroke-linecap="round"
                                     stroke-linejoin="round" />
                             </svg>
-                            <img class="photo-slider-3" src="" alt="" class="d-none">
+                            <img class="photo-slider-3" src="" alt="" style="display: none">
                             <button class="uploaded-img-card-edit">
                                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
