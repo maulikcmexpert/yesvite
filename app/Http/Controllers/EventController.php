2dbd965f16670a5016d2b6f13f94fa986fd24bb8
Vimal <vimal.cmexpertise@gmail.com>
2024-09-30 20:57:41 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -173,7 +173,7 @@ class EventController extends Controller
 
         // $potluck = session('category');
         // dd(session()->get('gift_registry_data'));
-       
+        dd($request);
         $user_id =  Auth::guard('web')->user()->id;
         $dateString = (isset($request->event_date)) ? $request->event_date : "";
 
