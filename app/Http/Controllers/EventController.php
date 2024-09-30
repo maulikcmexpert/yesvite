9b4301f5dec1238875c8a023d0e5ce653e22ed12
Vimal <vimal.cmexpertise@gmail.com>
2024-09-30 21:17:04 +0530

    commit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -185,7 +185,7 @@ class EventController extends Controller
                 $destinationDirectory = public_path('storage/event_images/'); 
                 $destinationImagePath = $destinationDirectory . $tempData->image;
                 dd($sourceImagePath);
-                if (File::exists($sourceImagePath)) {
+                if (file_exists($sourceImagePath)) {
                     dd(1);
                     File::copy($sourceImagePath, $destinationImagePath);
                     // $event_creation->design_image = $tempData->image; 
