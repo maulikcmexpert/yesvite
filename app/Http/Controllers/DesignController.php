92153ba33181bf18a828849a45cad5112ed99993
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-09-30 23:05:42 +0530

    Update DesignController.php

--- app/Http/Controllers/DesignController.php
+++ app/Http/Controllers/DesignController.php
@@ -86,7 +86,7 @@ class DesignController extends Controller
     }
     public function user_image(Request $request)
     {
-        // dd($request);
+        dd($request);
         // Validate the image file (optional, uncomment if needed)
         // $request->validate([
         //     'image' => 'required|image|mimes:png,jpg,jpeg|max:2048',
