c4810e0f3bb5cf5385d5043bb2e46fb8e1cd1846
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-01 10:05:39 +0530

    Update editjs.blade.php

--- resources/views/admin/create_template/editjs.blade.php
+++ resources/views/admin/create_template/editjs.blade.php
@@ -40,6 +40,7 @@
                             hideStaticTextElements(); // Hide static text elements if static information is present
                             const staticInfo = JSON.parse(data.static_information);
                             console.log(staticInfo);
+                            console.log(data.id);
 
                             // Render text elements on canvas
                             staticInfo.textElements.forEach(element => {
@@ -87,7 +88,6 @@
                             showStaticTextElements();
                         }
 
-                        console.log(data.id);
                         // Set custom attribute with the fetched ID
                         var canvasElement = document.getElementById('imageEditor1');
                         canvasElement.setAttribute('data-canvas-id', data.id);
