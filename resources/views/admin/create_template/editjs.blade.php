01f8b1f351cd2c28cb7c69516a02bbabbc890ec0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-02 20:13:35 +0530

    Update editjs.blade.php

--- resources/views/admin/create_template/editjs.blade.php
+++ resources/views/admin/create_template/editjs.blade.php
@@ -1889,7 +1889,7 @@
 
         document.getElementById('saveButton').addEventListener('click', function() {
             // alert();
-            // saveTextDataToDatabase();
+            saveTextDataToDatabase();
             var id = $('#template_id').val();
             const width = userImageElement.clientWidth;
             const height = userImageElement.clientHeight;
@@ -1914,6 +1914,7 @@
                         })
                     })
                     .then(response => {
+                        alert(response);
                         if (!response.ok) {
                             throw new Error('Network response was not ok');
                         }
