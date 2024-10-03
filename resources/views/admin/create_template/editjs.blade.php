73545a72c55c0c043ff3e06eea70b4a14e66376e
cmexpertise <pratik.cmexpertise@gmail.com>
2024-10-03 12:40:17 +0530

    Update editjs.blade.php

--- resources/views/admin/create_template/editjs.blade.php
+++ resources/views/admin/create_template/editjs.blade.php
@@ -1566,7 +1566,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         canvasElement.add(imgInstance);
                         canvasElement.bringToFront(imgInstance);
                         drawCanvas();
-                        addIconsToImage(imgElement)
+                        // addIconsToImage(imgElement)
                         console.log("Image loaded and added to canvas.");
                         imageUploaded = true; // Set flag to true after image is uploaded
                     };
