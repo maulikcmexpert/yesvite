641e95a58c24ad2d50a3ef6890c18e960be7c278
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-03 09:35:24 +0530

    Update editjs.blade.php

--- resources/views/admin/create_template/editjs.blade.php
+++ resources/views/admin/create_template/editjs.blade.php
@@ -1907,45 +1907,45 @@
         document.getElementById('saveButton').addEventListener('click', function() {
             // alert();
             saveTextDataToDatabase();
-            var id = $('#template_id').val();
-            const width = userImageElement.clientWidth;
-            const height = userImageElement.clientHeight;
-            const left = imageWrapper.offsetLeft;
-            const top = imageWrapper.offsetTop;
-            const centerX = left + width / 2;
-            const centerY = top + height / 2;
-            if (imageUploaded) {
-                fetch(`/save_shape/${id}`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')
-                                .getAttribute('content')
-                        },
-                        body: JSON.stringify({
-                            shape: shape,
-                            centerX: centerX,
-                            centerY: centerY,
-                            width: width,
-                            height: height
-                        })
-                    })
-                    .then(response => {
-                        alert(response);
-                        if (!response.ok) {
-                            throw new Error('Network response was not ok');
-                        }
-                        return response.json();
-                    })
-                    .then(data => {
-                        console.log('Shape saved successfully:', data);
-                    })
-                    .catch(error => {
-                        console.error('There was a problem with saving the shape:', error);
-                    });
-            } else {
-                // alert("Please upload an image before saving the shape.");
-            }
+            // var id = $('#template_id').val();
+            // const width = userImageElement.clientWidth;
+            // const height = userImageElement.clientHeight;
+            // const left = imageWrapper.offsetLeft;
+            // const top = imageWrapper.offsetTop;
+            // const centerX = left + width / 2;
+            // const centerY = top + height / 2;
+            // if (imageUploaded) {
+            //     fetch(`/save_shape/${id}`, {
+            //             method: 'POST',
+            //             headers: {
+            //                 'Content-Type': 'application/json',
+            //                 'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')
+            //                     .getAttribute('content')
+            //             },
+            //             body: JSON.stringify({
+            //                 shape: shape,
+            //                 centerX: centerX,
+            //                 centerY: centerY,
+            //                 width: width,
+            //                 height: height
+            //             })
+            //         })
+            //         .then(response => {
+            //             alert(response);
+            //             if (!response.ok) {
+            //                 throw new Error('Network response was not ok');
+            //             }
+            //             return response.json();
+            //         })
+            //         .then(data => {
+            //             console.log('Shape saved successfully:', data);
+            //         })
+            //         .catch(error => {
+            //             console.error('There was a problem with saving the shape:', error);
+            //         });
+            // } else {
+            //     // alert("Please upload an image before saving the shape.");
+            // }
         });
 
         Object.values(resizeHandles).forEach(handle => {
