e057a746b90295190a46a509b3f6417ea125cc01
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-03 12:51:25 +0530

    commit

--- resources/views/admin/create_template/editjs.blade.php
+++ resources/views/admin/create_template/editjs.blade.php
@@ -266,7 +266,6 @@
                             // hideStaticTextElements(); // Hide static text elements if static information is present
                             const staticInfo = JSON.parse(data.static_information);
 
-
                             // Render text elements or shapes on canvas
                             staticInfo.textElements.forEach(element => {
                                 if (element.text) {
