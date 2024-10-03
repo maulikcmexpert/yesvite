e32bd68f5d88c940e89e8e4e9eff8bb87de62722
Vimal <vimal.cmexpertise@gmail.com>
2024-10-03 18:53:26 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -4295,6 +4295,10 @@ $(document).on("click", ".store_desgin_temp", function () {
     $("#loader").show();
     $(this).prop("disabled", true);
     $('.btn-close').prop("disabled", true);
+    var textData = getTextDataFromCanvas();
+    dbJson = {
+        textElements: textData
+    };
     console.log(dbJson);
     eventData.textData = dbJson;
     eventData.temp_id = temp_id;
