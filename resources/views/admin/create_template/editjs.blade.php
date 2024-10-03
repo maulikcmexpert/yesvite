bd7d24cb73c871fe961018ddec554c67e6d9b3c1
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-03 10:31:11 +0530

    Update editjs.blade.php

--- resources/views/admin/create_template/editjs.blade.php
+++ resources/views/admin/create_template/editjs.blade.php
@@ -237,7 +237,7 @@
                         // Load static information (text and shapes)
                         if (data.static_information) {
                             // hideStaticTextElements(); // Hide static text elements if static information is present
-                            const staticInfo = JSON.parse(data.static_information);
+                            const staticInfo = data.static_information;
                             // console.log(staticInfo);
 
                             // Render text elements or shapes on canvas
