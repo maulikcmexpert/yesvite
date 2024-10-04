26646017b9c498ca170d357839b214ab32c51fc1
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-04 15:38:45 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -1057,6 +1057,9 @@ function delete_invited_user(userId) {
             $(".user-list-responsive").empty();
             $(".user-list-responsive").html(response.responsive_view);
 
+            $("user-"+userId).remove();
+            $("user_tel-"+userId).remove();
+
             // if(currentInviteCount < 15){
             //     $('.user_choice').prop('disabled',false);
             // }
