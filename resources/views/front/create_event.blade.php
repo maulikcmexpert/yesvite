3e16303932a364b43b283ab1a3b78284e6406727
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-04 15:00:32 +0530

    commit

--- resources/views/front/create_event.blade.php
+++ resources/views/front/create_event.blade.php
@@ -2292,7 +2292,7 @@
                     </a>
                     <h5>All Invites</h5>
                 </div>
-                <button class="close-btn group_toggle_close_btn" onclick="toggleSidebar()">
+                <button class="close-btn" onclick="toggleSidebar()">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <path d="M5.00098 5L19 18.9991" stroke="#64748B" stroke-width="1.5" stroke-linecap="round"
