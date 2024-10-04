1a55851436355ecfe7bc6da010059e3ad3249c7e
Vimal <vimal.cmexpertise@gmail.com>
2024-10-04 15:33:02 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -278,6 +278,7 @@ $(document).on("click", ".design-card", function () {
     if (shapeImageUrl) {
         staticInfo?.shapeImageData?.forEach(element => {
             if (element.shape && element.centerX && element.centerY && element.height && element.width) {
+                $('.resize-handle').hide();
                 updateClipPath(shapeImageUrl, element);
             }
         });
@@ -627,7 +628,6 @@ function bindData() {
                 if (shapeImageUrl) {
                     staticInfo?.shapeImageData?.forEach(element => {
                         if (element.shape && element.centerX && element.centerY && element.height && element.width) {
-                            $('.resize-handle').hide();
                             updateClipPath(shapeImageUrl, element);
                         }
                     });
