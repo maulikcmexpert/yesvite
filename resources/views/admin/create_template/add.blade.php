7a56f1fcb8580b02e7fdb0cd0f40c60992556472
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-07 14:47:26 +0530

    Update add.blade.php

--- resources/views/admin/create_template/add.blade.php
+++ resources/views/admin/create_template/add.blade.php
@@ -6,7 +6,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right w-100">
                         <li class="breadcrumb-item"><a href="{{URL::to('/admin/dashboard')}}">Dashboard</a></li>
-                        <li class="breadcrumb-item"><a href="{{URL::to('/admin/template')}}">Template List</a></li>
+                        <li class="breadcrumb-item"><a href="{{URL::to('/admin/create_template')}}">Template List</a></li>
                         <li class="breadcrumb-item active">{{$title}}</li>
                     </ol>
                 </div><!-- /.col -->
