da1f253ca921b78d45e52dcf8394a665e1ad6fec
Vimal <vimal.cmexpertise@gmail.com>
2024-10-07 17:31:41 +0530

    comit

--- resources/views/front/event/design/edit_design.blade.php
+++ resources/views/front/event/design/edit_design.blade.php
@@ -30,6 +30,10 @@
             </div>
             <div id="imageWrapper" style="position:fixed; display:none;">
                 <div class="canvas-top-icon-wrp">
+                    <div class="uploadShapImage">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z"/></svg>
+                        <input type="file" name="selectShapeImage" accept="image/*">
+                    </div>
                     <div class="removeShapImage">
                         <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g filter="url(#filter0_d_5633_67674)">
@@ -41,10 +45,6 @@
                                 <path d="M13.4385 14.6055H15.9351" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
                             </g>
                         </svg>
-                    </div>
-                    <div class="uploadShapImage">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z"/></svg>
-                        <input type="file" name="selectShapeImage" accept="image/*">
                     </div>
                 </div>
                 
