45b4f5c4af8ecdf05d9cce19d378cc9d996ef63a
Vimal <vimal.cmexpertise@gmail.com>
2024-10-07 12:17:13 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -3134,7 +3134,7 @@ $(document).on("click", ".li_design", function (e) {
 
 
 function li_design_click(){
-    $('.design-span').addClass('active');
+    
     if (
         eventData.event_type != "" &&
         eventData.event_name != "" &&
@@ -3155,6 +3155,7 @@ function li_design_click(){
         eventData.state != undefined &&
         eventData.zipcode != undefined
     ) {
+        $('.design-span').addClass('active');
         $(".step_1").css("display", "none");
         $(".step_2").show();
         $(".step_3").css("display", "none");
