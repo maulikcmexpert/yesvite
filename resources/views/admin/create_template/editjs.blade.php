31501def8695272dca2db4901135991eec62fb32
Vimal <vimal.cmexpertise@gmail.com>
2024-10-08 10:05:32 +0530

    Update editjs.blade.php

--- resources/views/admin/create_template/editjs.blade.php
+++ resources/views/admin/create_template/editjs.blade.php
@@ -105,6 +105,13 @@
                                         width: textWidth
                                     });
 
+                                    textElement.on('scaling', function () {
+                                        // Update the font size based on scaling
+                                        var updatedFontSize = textElement.fontSize * (textElement.scaleX + textElement.scaleY) / 2;
+                                        textElement.set('fontSize', updatedFontSize);
+                                        canvas.renderAll();
+                                    });
+
                                  
                                     //addIconsToTextbox(textElement);
                                     canvas.add(textElement);
