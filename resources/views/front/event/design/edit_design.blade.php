a3d6f3513065ef01d9543f6e254c7565ef44ccbf
Vimal <vimal.cmexpertise@gmail.com>
2024-10-08 12:00:50 +0530

    Update edit_design.blade.php

--- resources/views/front/event/design/edit_design.blade.php
+++ resources/views/front/event/design/edit_design.blade.php
@@ -23,43 +23,44 @@
         <div class="image-edit-section-wrp">
             <div class="image-edit-inner-img">
                 <div class="canvas-container" id="border">
+                    <div id="imageWrapper" style="position:fixed; display:none;z-index:10000">
+                        <div class="canvas-top-icon-wrp">
+                            
+                            <div class="removeShapImage" style="display: none">
+                                <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g filter="url(#filter0_d_5633_67674)">
+                                        <rect x="2.70312" y="2.37207" width="23.9674" height="23.9674" rx="11.9837" fill="white" shape-rendering="crispEdges"/>
+                                        <path d="M19.1807 11.3502C17.5179 11.1855 15.8452 11.1006 14.1775 11.1006C13.1888 11.1006 12.2001 11.1505 11.2115 11.2504L10.1929 11.3502" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
+                                        <path d="M12.939 10.8463L13.0488 10.1922C13.1287 9.7178 13.1886 9.36328 14.0325 9.36328H15.3407C16.1846 9.36328 16.2495 9.73777 16.3244 10.1971L16.4342 10.8463" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
+                                        <path d="M18.1073 12.9277L17.7827 17.9559C17.7278 18.7398 17.6829 19.349 16.2898 19.349H13.0841C11.691 19.349 11.6461 18.7398 11.5912 17.9559L11.2666 12.9277" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
+                                        <path d="M13.853 16.6035H15.5158" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
+                                        <path d="M13.4385 14.6055H15.9351" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+        
+                        <div class="uploadShapImage">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z"/></svg>
+                            <input type="file" name="selectShapeImage" accept="image/*">
+                        </div>
+                        
+                        <img id="user_image" src="" style="height: 100px; width: 100px; background-color: black;" />
+                        <div class="resize-handle top-left"></div>
+                        <div class="resize-handle top-center"></div>
+                        <div class="resize-handle top-right"></div>
+                        <div class="resize-handle bottom-left"></div>
+                        <div class="resize-handle bottom-center"></div>
+                        <div class="resize-handle bottom-right"></div>
+                        <div class="resize-handle left-center"></div> <!-- Left-center handle -->
+                        <div class="resize-handle right-center"></div> <!-- Right-center handle -->
+                    </div>
                     <input type="file" id="image" accept="image/*" style="display: none" />
                     <canvas id="imageEditor1" class="canvas new"></canvas>
                 </div>
                 {{-- <img src="{{ $textData->image}}" alt=""> --}}
             </div>
-            <div id="imageWrapper" style="position:fixed; display:none;">
-                <div class="canvas-top-icon-wrp">
-                    
-                    <div class="removeShapImage" style="display: none">
-                        <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g filter="url(#filter0_d_5633_67674)">
-                                <rect x="2.70312" y="2.37207" width="23.9674" height="23.9674" rx="11.9837" fill="white" shape-rendering="crispEdges"/>
-                                <path d="M19.1807 11.3502C17.5179 11.1855 15.8452 11.1006 14.1775 11.1006C13.1888 11.1006 12.2001 11.1505 11.2115 11.2504L10.1929 11.3502" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path d="M12.939 10.8463L13.0488 10.1922C13.1287 9.7178 13.1886 9.36328 14.0325 9.36328H15.3407C16.1846 9.36328 16.2495 9.73777 16.3244 10.1971L16.4342 10.8463" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path d="M18.1073 12.9277L17.7827 17.9559C17.7278 18.7398 17.6829 19.349 16.2898 19.349H13.0841C11.691 19.349 11.6461 18.7398 11.5912 17.9559L11.2666 12.9277" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path d="M13.853 16.6035H15.5158" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path d="M13.4385 14.6055H15.9351" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
-                            </g>
-                        </svg>
-                    </div>
-                </div>
-
-                <div class="uploadShapImage">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M246.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 109.3 192 320c0 17.7 14.3 32 32 32s32-14.3 32-32l0-210.7 73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-64z"/></svg>
-                    <input type="file" name="selectShapeImage" accept="image/*">
-                </div>
-                
-                <img id="user_image" src="" style="height: 100px; width: 100px; background-color: black;" />
-                <div class="resize-handle top-left"></div>
-                <div class="resize-handle top-center"></div>
-                <div class="resize-handle top-right"></div>
-                <div class="resize-handle bottom-left"></div>
-                <div class="resize-handle bottom-center"></div>
-                <div class="resize-handle bottom-right"></div>
-                <div class="resize-handle left-center"></div> <!-- Left-center handle -->
-                <div class="resize-handle right-center"></div> <!-- Right-center handle -->
-            </div>
+            
         </div>
         <div class="edit-images-button-wrp">
             <div class="edit-images-button-inner text-box-wrp" >
