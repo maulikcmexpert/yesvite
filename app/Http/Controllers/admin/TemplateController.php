34d684582163bd0ec1457ddc928f5cd507d8f00c
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-08 16:35:15 +0530

    Update TemplateController.php

--- app/Http/Controllers/admin/TemplateController.php
+++ app/Http/Controllers/admin/TemplateController.php
@@ -276,8 +276,9 @@ class TemplateController extends Controller
 
     public function get_all_subcategory(Request $request)
     {
-        dd(1);
         $category_id = $request->input('category_id');
-        dd($category_id);
+        $sub_category = EventDesignSubCategory::where('category', $category_id)->get();
+
+        dd($sub_category);
     }
 }
