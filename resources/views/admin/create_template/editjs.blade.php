4f40d5153b0f7a89e55c2ceaa0c1d8b8936f379e
cmexpertise <pratik.cmexpertise@gmail.com>
2024-10-08 12:36:58 +0530

    Update editjs.blade.php

--- resources/views/admin/create_template/editjs.blade.php
+++ resources/views/admin/create_template/editjs.blade.php
@@ -1516,6 +1516,8 @@ $(".removeShapImage").click(function(){
                     if (tb.copyIcon) tb.copyIcon.set('visible', false);
                 });
             }
+            const pointer = canvas.getPointer(event.e);
+            const activeObject = canvas.getActiveObject();
             if (activeObject && activeObject.controls && activeObject.controls.deleteControl) {
                     const deleteControl = activeObject.controls.deleteControl;
 
