e8c3e35c34daca04c79d38cc4673dc179c3125bd
Vimal <vimal.cmexpertise@gmail.com>
2024-10-10 15:29:47 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -814,7 +814,7 @@ function bindData() {
                                             canvas.add(newImg);
                                             currentImage = newImg; 
                                             shapeImageUrl = newImg.src;
-                                            console.log(shapeImageUrl);
+                                            console.log(newImg);
                                             // Reset shape index for the new image based on the default shape
                                             currentShapeIndex = shapeIndexMap[defaultShape] || 0; // Default to rectangle if not found
                                             newImg.set({ clipPath: shapes[currentShapeIndex] });
