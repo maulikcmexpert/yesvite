b6db84a7a9e1719b9dc84e2b9d612047783f9c20
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-11 16:49:06 +0530

    Update emailVerificationEmail.blade.php

--- resources/views/emails/emailVerificationEmail.blade.php
+++ resources/views/emails/emailVerificationEmail.blade.php
@@ -60,10 +60,14 @@
                             "Please verify your email address : {{$userData['email']}} first before you can login to your
                             account".</p> -->
                     </td>
+                </tr>
+                <tr>
                     <td>
                         <p style="font-size: 16px;line-height: 25px;font-weight: 400;color: #0F172A ;margin-bottom: 10px !important;margin: 0px 0px;font-family: 'SF Pro Display'">
                             Your email address: {{$userData['email']}} was added to your Yesvite account..</p>
                     </td>
+                </tr>
+                <tr>
                     <td>
                         <p style="font-size: 16px;line-height: 25px;font-weight: 400;color: #0F172A ;margin-bottom: 10px !important;margin: 0px 0px;font-family: 'SF Pro Display'">
                             To verify your email address and secure your account, click the button below.</p>
