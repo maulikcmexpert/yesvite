27dae15295ea9f892dc27ead884e0bf8b85f8aeb
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-11 13:11:17 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -12790,7 +12790,7 @@ class ApiControllerv2 extends Controller
 
             return response()->json(['status' => 0, 'message' => "db error"]);
         } catch (\Exception $e) {
-            dd($e)
+            dd($e);
 
             return response()->json(['status' => 0, 'message' => "something went wrong"]);
         }
