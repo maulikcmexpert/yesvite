3e3314ef5b912797eff0a34d63c1f5259f6b53bb
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-14 18:32:36 +0530

    Update rsvp.js

--- public/assets/front/js/rsvp.js
+++ public/assets/front/js/rsvp.js
@@ -1,1 +1,11 @@
-alert();
+$(document).ready(function() {
+    $('#rsvpForm').on('submit', function(e) {
+        var adultsCount = parseInt($('#adultsInput').val()) || 0; 
+        var kidsCount = parseInt($('#kidsInput').val()) || 0; 
+
+        if (adultsCount === 0 && kidsCount === 0) {
+            e.preventDefault();
+            alert("Please add at least one adult or kid."); 
+        }
+    });
+});
