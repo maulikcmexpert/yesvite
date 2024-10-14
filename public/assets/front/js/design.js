97c2fb8424c7bef0f8c2aeeca96754d16af375fa
Vimal <vimal.cmexpertise@gmail.com>
2024-10-14 14:32:27 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -1960,35 +1960,7 @@ function bindData() {
             }
 
             if(obj.type ==='image'){
-                
-                function createShapes(img) {
-                    const imgWidth = img.width;
-                    const imgHeight = img.height;
-                    const starScale = Math.min(imgWidth, imgHeight) / 2; // Adjust the star size based on the image
-    
-                    // Proper 5-point star shape
-                    const starPoints = [
-                        { x: 0, y: -starScale }, // Top point
-                        { x: starScale * 0.23, y: -starScale * 0.31 }, // Top-right
-                        { x: starScale, y: -starScale * 0.31 }, // Right
-                        { x: starScale * 0.38, y: starScale * 0.12 }, // Bottom-right
-                        { x: starScale * 0.58, y: starScale }, // Bottom
-                        { x: 0, y: starScale * 0.5 }, // Center-bottom
-                        { x: -starScale * 0.58, y: starScale }, // Bottom-left
-                        { x: -starScale * 0.38, y: starScale * 0.12 }, // Top-left
-                        { x: -starScale, y: -starScale * 0.31 }, // Left
-                        { x: -starScale * 0.23, y: -starScale * 0.31 } // Top-left
-                    ];
-    
-                    return [
-                        new fabric.Rect({ width: imgWidth, height: imgHeight, originX: 'center', originY: 'center', angle: 0 }),
-                        new fabric.Circle({ radius: Math.min(imgWidth, imgHeight) / 2, originX: 'center', originY: 'center', angle: 0 }),
-                        new fabric.Triangle({ width: imgWidth, height: imgHeight, originX: 'center', originY: 'center', angle: 0 }),
-                        new fabric.Polygon(starPoints, { originX: 'center', originY: 'center', angle: 0 })
-                    ];
-                }
-
-                createShapes(obj);
+                console.log(obj);
             }
 
             obj.on('rotating', function () {
