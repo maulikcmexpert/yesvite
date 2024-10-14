6d264d40b4716aaed8f27514a648dcc27b61cceb
Vimal <vimal.cmexpertise@gmail.com>
2024-10-14 19:04:03 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -2200,8 +2200,10 @@ function bindData() {
                 $(".design-sidebar_7").removeClass("d-none");
                 $("#sidebar").addClass("design-sidebar_7");
                 $(".close-btn").attr("data-id", "design-sidebar_7");
-                getLengthofSliderImage();
             }
+            setTimeout(() => {
+                getLengthofSliderImage();
+            }, 500);
         });
 
         $(".slider_photo_2").on("change", function(event) {
@@ -2213,8 +2215,10 @@ function bindData() {
                     $(".photo-slider-2").attr("src", e.target.result).show();
                 };
                 reader.readAsDataURL(file);
-                getLengthofSliderImage();
             }
+            setTimeout(() => {
+                getLengthofSliderImage();
+            }, 500);
         });
         $(".slider_photo_3").on("change", function(event) {
             var file = event.target.files[0];
@@ -2225,8 +2229,10 @@ function bindData() {
                     $(".photo-slider-3").attr("src", e.target.result).show();
                 };
                 reader.readAsDataURL(file);
-                getLengthofSliderImage();
             }
+            setTimeout(() => {
+                getLengthofSliderImage();
+            }, 500);
         });
 
         function getLengthofSliderImage(){
