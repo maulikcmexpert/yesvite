4b6de050af162659983c0994cd72510cd09bedd0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-14 18:42:54 +0530

    Update rsvp.js

--- public/assets/front/js/rsvp.js
+++ public/assets/front/js/rsvp.js
@@ -4,17 +4,19 @@ $(document).ready(function() {
         var rsvpStatus = $('input[name="rsvp_status"]:checked').val(); 
         var adultsCount = parseInt($('#adultsInput').val()) || 0; 
         var kidsCount = parseInt($('#kidsInput').val()) || 0; 
-
-        if (adultsCount === 0 && kidsCount === 0) {
-            e.preventDefault();
-            toastr.error("Please add at least one adult or kid."); 
-            return;
-        }
-
+      
         if (!rsvpStatus) {
             e.preventDefault(); 
             toastr.error("Please select RSVP"); 
             return;
         }
+
+        if (adultsCount == 0 && kidsCount == 0) {
+            e.preventDefault();
+            toastr.error("Please add at least one adult or kid."); 
+            return;
+        }
+
+        
     });
 });
