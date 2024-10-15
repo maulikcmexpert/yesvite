dbea0b2a2a4f24f13dbac703cfbc88d160b5a91f
Vimal <vimal.cmexpertise@gmail.com>
2024-10-15 13:18:12 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -4612,6 +4612,39 @@ $(document).on("click", ".add_new_group", function () {
     }
 });
 
+$(document).ready(function () {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3.5,
+      spaceBetween: 20,
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        320: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+        },
+        576: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+          },
+        1200: {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+        },
+        1400: {
+          slidesPerView: 3.5,
+          spaceBetween: 20,
+        },
+      },
+    });
+
 $(document).on("click", ".add_new_group_member", function () {
     var group_name = $("#new_group_name").val();
     console.log(group_name);
@@ -4685,14 +4718,14 @@ $(document).on("click", ".add_new_group_member", function () {
                     </div>
                 </div>
                     `;
-                    swiper
 
-                    if (typeof mySwiper !== 'undefined') {
-                    // Proceed with Swiper operations
-                    mySwiper.appendSlide(newItem);
-                    } else {
-                    console.error('Swiper instance is undefined.');
+                    if (typeof swiper !== 'undefined') {
+                        swiper.appendSlide(newItem);  // Append the new slide
+                      } else {
+                        console.error('Swiper instance is undefined.');
+                      }
                     }
+                
                                     // $('.owl-carousel').trigger('add.owl.carousel', [$(newItem)]).trigger('refresh.owl.carousel');
 
                 // $('.swiper-wrapper').append(newItem);
@@ -4708,6 +4741,7 @@ $(document).on("click", ".add_new_group_member", function () {
     }else{
         toastr.error('Please select Member');
     }
+});
 });
 
 $(document).on("click", ".invite_group_member", function () {
@@ -5338,39 +5372,6 @@ function getStartEndTimeZone(){
 }
 
 
-$(document).ready(function () {
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3.5,
-      spaceBetween: 20,
-      loop: true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      breakpoints: {
-        320: {
-            slidesPerView: 1.5,
-            spaceBetween: 20,
-        },
-        576: {
-            slidesPerView: 2.5,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 2.5,
-            spaceBetween: 20,
-          },
-        1200: {
-          slidesPerView: 2.5,
-          spaceBetween: 20,
-        },
-        1400: {
-          slidesPerView: 3.5,
-          spaceBetween: 20,
-        },
-      },
-    });
-});
 
 $(document).on('click','.all_user_list',function(){
     $.ajax({
