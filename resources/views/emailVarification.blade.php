4ad42ea276e03bbaa988e97d20f4c4df55b7d5cc
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-16 12:38:56 +0530

    Update emailVarification.blade.php

--- resources/views/emailVarification.blade.php
+++ resources/views/emailVarification.blade.php
@@ -135,8 +135,11 @@
                 @else
                 <h3 class="text-center">Invalid Token</h3>
                 <p>{{$message}}</p>
-                <a href="{{route('ResendVerificationMail')}}" class="email-login-btn-wrp"><button type="button" class="email-login-btn">Request Another Email</button></a>
-
+                {{-- <a href="{{route('ResendVerificationMail')}}" class="email-login-btn-wrp"><button type="button" class="email-login-btn">Request Another Email</button></a> --}}
+                <form action="{{ route('ResendVerificationMail') }}" method="POST" class="email-login-btn-wrp">
+                    @csrf
+                    <button type="submit" class="email-login-btn">Request Another Email</button>
+                </form>
                  {{-- <input type="hidden" name="user_id" value="{{$user_id}}"> --}}
                 @endif
 
