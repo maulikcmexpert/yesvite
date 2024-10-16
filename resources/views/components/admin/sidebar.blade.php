df4c84bcddf0502b2f3bb77530239c2ae31020a8
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-16 11:10:22 +0530

    Update sidebar.blade.php

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -159,7 +159,7 @@
             <li class="nav-item">
               <a href="{{URL::to('/admin/user_resend_verification')}}" class="nav-link {{ (Request::segment(2) == 'user_resend_verification')? 'active':'' }}">
                 <i class="fas fa-user-tie nav-icon"></i>
-                <p>Users Resend Verification Requests</p>
+                <p>Resend Email Verification</p>
               </a>
             </li>
 
