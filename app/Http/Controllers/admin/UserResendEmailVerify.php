f600c3293676cda8061dd583a7ee24894148701c
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-16 11:27:50 +0530

    commit

--- app/Http/Controllers/admin/UserResendEmailVerify.php
+++ app/Http/Controllers/admin/UserResendEmailVerify.php
@@ -25,7 +25,7 @@ class UserResendEmailVerify extends Controller
      */
     public function index(Request $request, UserResendEmailVerifyDataTable $DataTable)
     {
-        $title = 'Post Reports';
+        $title = 'User Resend Verification Mail';
         $page = 'admin.resend_email_verification.list';
         // $js = 'admin.post_reports.post_reportsjs';
         return $DataTable->render('admin.includes.layout', compact('title', 'page'));
