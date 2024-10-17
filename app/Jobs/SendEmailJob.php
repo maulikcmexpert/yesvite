fcb94ba3af68e5902f85ade343241edb1cef1dc2
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-17 19:49:43 +0530

    Create SendEmailJob.php

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $user;
+    protected $details;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($user, $details)
+    {
+        $this->user = $user;
+        $this->details = $details;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to($this->user->email)->send(new SendMail($this->details));
+
+    }
+}
