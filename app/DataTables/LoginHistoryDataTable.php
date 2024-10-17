e1a30b66ea13a9242775262312e98ef4a416f6f0
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-17 14:42:10 +0530

    Update LoginHistoryDataTable.php

--- app/DataTables/LoginHistoryDataTable.php
+++ app/DataTables/LoginHistoryDataTable.php
@@ -27,16 +27,16 @@ class LoginHistoryDataTable extends DataTable
             ->addColumn('no', function () use (&$counter) {
                 return $counter++;
             })
-            // ->filter(function ($query) {
-            //     if ($this->request->has('search')) {
-            //         $keyword = $this->request->get('search');
-            //         $keyword = $keyword['value'];
-            //         $query->where(function ($q) use ($keyword) {
-            //             $q->where('firstname', 'LIKE', "%{$keyword}%")
-            //                 ->orWhere('lastname', 'LIKE', "%{$keyword}%");
-            //         });
-            //     }
-            // })
+            ->filter(function ($query) {
+                if ($this->request->has('search')) {
+                    $keyword = $this->request->get('search');
+                    $keyword = $keyword['value'];
+                    $query->where(function ($q) use ($keyword) {
+                        $q->where('firstname', 'LIKE', "%{$keyword}%")
+                            ->orWhere('lastname', 'LIKE', "%{$keyword}%");
+                    });
+                }
+            })
            
             ->addColumn('username', function ($row) {
                 return $row->user->firstname . ' ' . $row->user->lastname;
