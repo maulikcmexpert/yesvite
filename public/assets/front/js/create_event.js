bb46d61fb1eac29233b8c4aa3ab5e0c860bf0a08
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-17 10:53:56 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -386,7 +386,7 @@ function datepicker() {
         ignoreReadonly: true,
         stepping: 15, // Set stepping to 15 minutes
         // defaultDate: now
-        //  debug: true
+         debug: true
     });
 }
 
