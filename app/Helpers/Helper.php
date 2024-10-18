f2694bfa75958687c94be1cec94ffefdf36c6182
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-18 12:28:40 +0530

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1289,8 +1289,7 @@ function adminNotification($notificationType, $postData)
                 // SendBroadcastEmailJob::dispatch('vimal.cmexpertise@gmail.com', 'This is test mail from yesvite support team');
                 
                 foreach ($users as $user) { 
-                    dd($user['email']);
-                    // SendBroadcastEmailJob::dispatch($user, $postData['message']);
+                    SendBroadcastEmailJob::dispatch($user['email'], $postData['message']);
                 // $userData = [
                 //         'username' => $user->firstname,
                 //         'email' => $user->email,
