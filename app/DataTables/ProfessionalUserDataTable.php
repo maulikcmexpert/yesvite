cb1b23017001593d031cef304ef77f9f27db0742
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-21 19:43:32 +0530

    Update ProfessionalUserDataTable.php

--- app/DataTables/ProfessionalUserDataTable.php
+++ app/DataTables/ProfessionalUserDataTable.php
@@ -108,7 +108,7 @@ class ProfessionalUserDataTable extends DataTable
             ->columns($this->getColumns())
             ->minifiedAjax()
             //->dom('Bfrtip')
-            ->orderBy(1)
+            ->orderBy(3)
             ->setTableAttributes(['class' => 'table table-bordered data-table users-data-table dataTable no-footer'])
             ->selectStyleSingle()
             ->buttons([
