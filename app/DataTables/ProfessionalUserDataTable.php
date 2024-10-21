2975a86a16ecb6dcf476115649e733bdeab3e18e
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-21 19:34:24 +0530

    Update ProfessionalUserDataTable.php

--- app/DataTables/ProfessionalUserDataTable.php
+++ app/DataTables/ProfessionalUserDataTable.php
@@ -69,8 +69,7 @@ class ProfessionalUserDataTable extends DataTable
             ->addColumn('username', function ($row) {
                 return $row->firstname . ' ' . $row->lastname;
 
-            })->orderColumn('name', '-name $1')
-
+            })
             ->addColumn('app_user', function ($row) {
                 if ($row->app_user == '1') {
                     return '<svg width="16" height="26" viewBox="0 0 16 26" fill="none" xmlns="http://www.w3.org/2000/svg">
