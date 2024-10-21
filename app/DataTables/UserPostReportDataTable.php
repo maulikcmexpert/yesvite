d34e71807a670772158a3ec57d3834287e81ffc5
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-21 15:22:54 +0530

    Update UserPostReportDataTable.php

--- app/DataTables/UserPostReportDataTable.php
+++ app/DataTables/UserPostReportDataTable.php
@@ -143,7 +143,7 @@ class UserPostReportDataTable extends DataTable
             Column::make('no')->title('No')->render('meta.row + meta.settings._iDisplayStart + 1;'),
             Column::make('username')->title('Username(Reported By)'),
             Column::make('report_type')->title('Report Type'),
-            Column::make('report_description')->title('Report Description'),
+            Column::make('report_description')->title("Report Description")->width('250px')->className('report-description-td'),
             Column::make('event_name')->title("Event Name"),
             Column::make('post_type')->title("Post Type"),
             Column::make('action')->title("Action"),
