7d4c642a594e1557b2e26e35b853372e534aa0f2
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-22 16:18:39 +0530

    Update AccountVerificationDataTable.php

--- app/DataTables/AccountVerificationDataTable.php
+++ app/DataTables/AccountVerificationDataTable.php
@@ -129,7 +129,7 @@ class AccountVerificationDataTable extends DataTable
                     ->columns($this->getColumns())
                     ->minifiedAjax()
                     //->dom('Bfrtip')
-                    ->orderBy(1)
+                    ->orderBy(2)
                     ->selectStyleSingle()
                     ->buttons([
                         Button::make('excel'),
