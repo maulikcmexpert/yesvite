e1acc6ca6400f554bf28bb6ed01fa8cbafe23ecd
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-23 12:34:50 +0530

    Update editjs.blade.php

--- resources/views/admin/create_template/editjs.blade.php
+++ resources/views/admin/create_template/editjs.blade.php
@@ -1973,7 +1973,7 @@ $(".removeShapImage").click(function(){
             finalArray.push({
                 'textElements': textData
             })
-           console.log(shapeImageData);
+        //    console.log(shapeImageData);
             console.log('final'+finalArray);
             return finalArray;
         }
