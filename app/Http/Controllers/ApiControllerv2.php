e049f4c62b3feb1a6193ffb9bf3fa8813c45f2ce
Vimal <vimal.cmexpertise@gmail.com>
2024-10-23 16:33:09 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -5533,7 +5533,7 @@ class ApiControllerv2 extends Controller
                 ->limit(1)
                 ->first();
 
-                dd($userSubscription);
+                dd($user->id);
             if((isset($input['subscription_plan_name']) && $input['subscription_plan_name'] =='Free') || (isset($input['subscription_plan_name']) && $input['subscription_plan_name'] =='Pro-year' && $userSubscription != null)){
                 $user  = Auth::guard('api')->user();
                 $checkUserInvited = Event::withCount('event_invited_user')->where('id', $input['event_id'])->first();
