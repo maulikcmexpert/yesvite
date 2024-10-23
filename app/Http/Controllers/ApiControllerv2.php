e96d120d31eb6606d9379069b3ac93aae4b8535b
Vimal <vimal.cmexpertise@gmail.com>
2024-10-23 16:35:52 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -5527,12 +5527,12 @@ class ApiControllerv2 extends Controller
             }
 
             $userSubscription = UserSubscription::where('user_id', $user->id)
-                // ->where('endDate','>',date('Y-m-d H:i:s'))
-                // ->where('type','subscribe')
-                // ->orderBy('id', 'DESC')
+                ->where('endDate','>',date('Y-m-d H:i:s'))
+                ->where('type','subscribe')
+                ->orderBy('id', 'DESC')
+                ->limit(1)
                 ->first();
 
-                dd($userSubscription);
             if((isset($input['subscription_plan_name']) && $input['subscription_plan_name'] =='Free') || (isset($input['subscription_plan_name']) && $input['subscription_plan_name'] =='Pro-year' && $userSubscription != null)){
                 $user  = Auth::guard('api')->user();
                 $checkUserInvited = Event::withCount('event_invited_user')->where('id', $input['event_id'])->first();
