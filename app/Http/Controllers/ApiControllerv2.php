a1ef6e38abe6626922d754f962b37f68623e3ab7
Vimal <vimal.cmexpertise@gmail.com>
2024-10-23 12:44:19 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -4315,7 +4315,18 @@ class ApiControllerv2 extends Controller
                     }
                 }
 
-
+                $userSubscription = UserSubscription::where('user_id', $user->id)
+                    ->where('endDate','>',date('Y-m-d H:i:s'))
+                    ->where('type','subscribe')
+                    ->orderBy('id', 'DESC')
+                    ->limit(1)
+                    ->first();
+            if ($userSubscription != null) {
+                $purchase_status = true;
+            }else{
+                $purchase_status = false;
+            }
+            $eventDetail['purchase_status'] = $purchase_status;
 
                 return response()->json(['status' => 1, 'message' => "Event data", "data" => $eventDetail]);
             } else {
