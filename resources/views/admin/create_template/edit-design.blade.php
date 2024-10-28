3794a6d0eef23e638157a9762b24fa5bab85fada
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-10-28 17:25:01 +0530

    commit

--- resources/views/admin/create_template/edit-design.blade.php
+++ resources/views/admin/create_template/edit-design.blade.php
@@ -49,11 +49,11 @@
                     </div>
                 </div>
                 <div class="image-edit-inner-img">
-                    
+
                     <div class="canvas-container" id="border">
                         <!-- The canvas where the image will be drawn -->
                         <input type="file" id="image" accept="image/*" style="display:none;" />
-                        <img id="shape_img" src="" style="display: none;" >
+                        <img id="shape_img" src="" style="display: none;">
                         <canvas id="imageEditor1" class="canvas new"></canvas>
                     </div>
 
@@ -62,12 +62,12 @@
                         <div class="removeShapImage">
                             <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g filter="url(#filter0_d_5633_67674)">
-                                    <rect x="2.70312" y="2.37207" width="23.9674" height="23.9674" rx="11.9837" fill="white" shape-rendering="crispEdges"/>
-                                    <path d="M19.1807 11.3502C17.5179 11.1855 15.8452 11.1006 14.1775 11.1006C13.1888 11.1006 12.2001 11.1505 11.2115 11.2504L10.1929 11.3502" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M12.939 10.8463L13.0488 10.1922C13.1287 9.7178 13.1886 9.36328 14.0325 9.36328H15.3407C16.1846 9.36328 16.2495 9.73777 16.3244 10.1971L16.4342 10.8463" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M18.1073 12.9277L17.7827 17.9559C17.7278 18.7398 17.6829 19.349 16.2898 19.349H13.0841C11.691 19.349 11.6461 18.7398 11.5912 17.9559L11.2666 12.9277" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M13.853 16.6035H15.5158" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M13.4385 14.6055H15.9351" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <rect x="2.70312" y="2.37207" width="23.9674" height="23.9674" rx="11.9837" fill="white" shape-rendering="crispEdges" />
+                                    <path d="M19.1807 11.3502C17.5179 11.1855 15.8452 11.1006 14.1775 11.1006C13.1888 11.1006 12.2001 11.1505 11.2115 11.2504L10.1929 11.3502" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M12.939 10.8463L13.0488 10.1922C13.1287 9.7178 13.1886 9.36328 14.0325 9.36328H15.3407C16.1846 9.36328 16.2495 9.73777 16.3244 10.1971L16.4342 10.8463" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M18.1073 12.9277L17.7827 17.9559C17.7278 18.7398 17.6829 19.349 16.2898 19.349H13.0841C11.691 19.349 11.6461 18.7398 11.5912 17.9559L11.2666 12.9277" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M13.853 16.6035H15.5158" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M13.4385 14.6055H15.9351" stroke="#0F172A" stroke-width="0.998643" stroke-linecap="round" stroke-linejoin="round" />
                                 </g>
                             </svg>
                         </div>
@@ -94,7 +94,7 @@
                     </svg>
                 </div>
             </div>
-            
+
         </div>
 
         <div class="edit-images-button-wrp">
@@ -508,6 +508,86 @@
                     <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="ChristmasCookies" id="ChristmasCookiesButton">
                     <label class="form-check-label font-change ChristmasCookies" for="ChristmasCookiesButton">
                         ChristmasCookies
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="ArialRoundedMTBold" id="ArialRoundedMTBoldButton">
+                    <label class="form-check-label font-change ArialRoundedMTBold" for="ArialRoundedMTBoldButton">
+                        ArialRoundedMTBold
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="BebasNeue-Regular" id="BebasNeue-RegularButton">
+                    <label class="form-check-label font-change BebasNeue-Regular" for="BebasNeue-RegularButton">
+                        BebasNeue-Regular
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="Calibri" id="CalibriButton">
+                    <label class="form-check-label font-change Calibri" for="CalibriButton">
+                        Calibri
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="CCHammerHorror-BoldItalic" id="CCHammerHorror-BoldItalicButton">
+                    <label class="form-check-label font-change CCHammerHorror-BoldItalic" for="CCHammerHorror-BoldItalicButton">
+                        CCHammerHorror-BoldItalic
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="CCThatsAllFolks-Bold" id="CCThatsAllFolks-BoldButton">
+                    <label class="form-check-label font-change CCThatsAllFolks-Bold" for="CCThatsAllFolks-BoldButton">
+                        CCThatsAllFolks-Bold
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="Changa-ExtraBold" id="Changa-ExtraBoldButton">
+                    <label class="form-check-label font-change Changa-ExtraBold" for="Changa-ExtraBoldButton">
+                        Changa-ExtraBold
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="ChristmasMint-Regular" id="ChristmasMint-RegularButton">
+                    <label class="form-check-label font-change ChristmasMint-Regular" for="ChristmasMint-RegularButton">
+                        ChristmasMint-Regular
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="CinderelaPersonalUse-Regular" id="CinderelaPersonalUse-RegularButton">
+                    <label class="form-check-label font-change CinderelaPersonalUse-Regular" for="CinderelaPersonalUse-RegularButton">
+                        CinderelaPersonalUse-Regular
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="Claiborne" id="ClaiborneButton">
+                    <label class="form-check-label font-change Claiborne" for="ClaiborneButton">
+                        Claiborne
+                    </label>
+
+                </div>
+                <div class="font-selector form-check">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" data-command="fontName" data-font="Corporative-Regular" id="Corporative-RegularButton">
+                    <label class="form-check-label font-change Corporative-Regular" for="Corporative-RegularButton">
+                        Corporative-Regular
                     </label>
 
                 </div>
