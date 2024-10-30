1f8c82293cad40a132282b7f984db20bc705c8a3
Prakashmanat01 <prakashhmanat@gmail.com>
2024-10-30 17:58:44 +0530

    Update sidebar.blade.php

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -307,7 +307,7 @@
         </li> -->
 
       </ul>
-      @elseif ($check_role['role']=="designer")
+      @elseif ($check_role['role']=="designer"||$check_role['role']=="Designer")
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
           <p class="asideTitle">Design Template</p>
