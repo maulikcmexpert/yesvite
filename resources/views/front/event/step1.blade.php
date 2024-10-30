28b3cb5d1641f356bf5517f7d7a17f403d008940
Vimal <vimal.cmexpertise@gmail.com>
2024-10-30 10:24:41 +0530

    Update step1.blade.php

--- resources/views/front/event/step1.blade.php
+++ resources/views/front/event/step1.blade.php
@@ -5,10 +5,7 @@
                 <h3>Detail Pages</h3>
                 <div class="row">
                     <input type="hidden" value="{{ $user->id }}" id="user_id">
-                    @php
-                        dd($eventDetail['id']);
-                    @endphp
-                    <input type="hidden" value="" id="event_id">
+                    <input type="hidden" value="{{(isset($eventDetail['id']) && $eventDetail['id'] != NULL)?$eventDetail['id']:''}}" id="event_id">
                     <div class="col-12 mb-4">
                         <div class="input-form">
                             <select class="form-select" id="event-type" onchange="clearError(this)">
