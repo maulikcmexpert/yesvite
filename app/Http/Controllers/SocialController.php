00696be3951115d1120ca61b7fba9d49b6679e35
Vimal <vimal.cmexpertise@gmail.com>
2024-11-04 16:24:38 +0530

    Update SocialController.php

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -43,7 +43,7 @@ class SocialController extends Controller
 
         // Check if the user already exists
         $authUser = $this->findOrCreateUser($user, $provider);
-        if($authUser != 0){
+        if($authUser->account_status == 'Unblock'){
             Auth::login($authUser, true);
         }else{
             return redirect('/login')->withErrors([
@@ -65,7 +65,7 @@ class SocialController extends Controller
     {
         $user = User::where('email', $socialUser->getEmail())->first();
         if($user->account_status != 'Unblock'){
-            return 0;
+            return [];
         }
         Session::regenerate();
         if ($user) {
