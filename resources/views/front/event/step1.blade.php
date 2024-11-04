1b67e4d895ba0bda53357071f0364267d318c1a5
Vimal <vimal.cmexpertise@gmail.com>
2024-11-04 11:21:41 +0530

    Update step1.blade.php

--- resources/views/front/event/step1.blade.php
+++ resources/views/front/event/step1.blade.php
@@ -89,7 +89,7 @@
                                 @php
                                     $start_time_zone = '';
                                     if(isset($eventDetail['rsvp_start_timezone']) && $eventDetail['rsvp_start_timezone'] != ''){
-                                        $start_time_zone = eventDetail['rsvp_start_timezone'];
+                                        $start_time_zone = $eventDetail['rsvp_start_timezone'];
                                     }
                                 @endphp
                                 <option value="PST" {{($start_time_zone =='' || $start_time_zone == 'PST')?'selected':''}}>PST</option>
@@ -156,7 +156,7 @@
                                 @php
                                     $end_time_zone = '';
                                     if(isset($eventDetail['rsvp_end_time_set']) && $eventDetail['rsvp_end_time_set'] != ''){
-                                        $end_time_zone = eventDetail['rsvp_end_time_set'];
+                                        $end_time_zone = $eventDetail['rsvp_end_time_set'];
                                     }
                                 @endphp
                                 <option value="PST" {{($end_time_zone =='' || $end_time_zone == 'PST')?'selected':''}}>PST</option>
