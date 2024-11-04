55ad47f1663bbda984995f7a0fd9ee71d019fba9
Vimal <vimal.cmexpertise@gmail.com>
2024-11-04 17:06:44 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -317,7 +317,7 @@ class EventController extends Controller
         }else{
             $title = 'Create Event';
         }
-        // dd($eventDetail['events_schedule_list']->data);
+        dd($eventDetail);
         $page = 'front.create_event';
        
 
