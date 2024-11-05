f0df58e569cda3a72f7063cfe88da0f2ebd2d912
Prakashmanat01 <prakashhmanat@gmail.com>
2024-11-05 18:28:38 +0530

    Update contact_us.blade.php

--- resources/views/front/contact_us.blade.php
+++ resources/views/front/contact_us.blade.php
@@ -8,7 +8,7 @@
                     <form action="{{ route('contact.submit') }}" id="contact_us_form" method="POST" class="mt-sm-4 mt-0 mb-5 mb-md-0">
                         @csrf
                         <div class="mb-4">
-                            <div class="form-group input-form">
+                            <div class="input-form">
                                 <div class="position-relative">
                                     <input type="text" class="form-control" id="name" name="name">
                                     <label for="name" class="floating-label">Name<span>*</span></label>
@@ -18,7 +18,7 @@
                             <span class="text-danger" style="font-size: 12px">{{ $errors->first('name') }}</span>
                         </div>
                         <div class="mb-4">
-                            <div class="form-group input-form">   
+                            <div class="input-form">   
                                 <div class="position-relative">
                                     <input type="email" class="form-control" id="email" name="email">
                                     <label for="email" class="floating-label">Email<span>*</span></label>
@@ -29,13 +29,12 @@
                             <span class="text-danger" style="font-size: 12px">{{ $errors->first('email') }}</span>
                         </div>
                         <div class="mb-3">
-                            <div class="form-group input-form">
+                            <div class="input-form">
                                 <div class="position-relative">
                                     <textarea class="form-control" id="message" name="message" rows="4"></textarea>
                                     <label for="message" class="floating-label">Message <span>*</span></label>
                                 </div>
                                 <label id="message-error" class="error" for="name"></label>
-
                             </div>
                             <span class="text-danger" style="font-size: 12px">{{ $errors->first('message') }}</span>
                         </div>
