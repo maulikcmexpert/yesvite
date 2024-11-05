602d5ba1d60e86c13a5932960052c1f5f40042e1
Prakashmanat01 <prakashhmanat@gmail.com>
2024-11-05 18:35:48 +0530

    Update contact_us.js

--- public/assets/front/js/contact_us.js
+++ public/assets/front/js/contact_us.js
@@ -13,6 +13,24 @@ $("#contact_us_form").validate({
     },
 });
 
+$(".form-control").on("focusout change keyup focus", function () {
+    var text_val = $(this).val();
+    if (text_val === "") {
+        $(this).next().removeClass("floatingfocus");
+    } else {
+        $(this).next().addClass("floatingfocus");
+    }
+});
+
+$(".form-control").each(function () {
+    var text = $(this).val();
+    if (text === "") {
+        $(this).next().removeClass("floatingfocus");
+    } else {
+        $(this).next().addClass("floatingfocus");
+    }
+});
+
 $('html').mouseover(function() {
     $(".form-control").each(function () {
         var text = $(this).val();
