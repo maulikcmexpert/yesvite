bfd82cf94a879eff4e9e3807cc6f17d7bcf15c8b
Prakashmanat01 <prakashhmanat@gmail.com>
2024-11-05 15:55:44 +0530

    commit

---
+++ app/Http/Controllers/ContactUsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\Request;
+use App\Mail\ContactMail;
+
+class ContactUsController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $title = 'Contact Us';
+        $page = 'front.contact_us';
+        //   $js = ['contact'];
+        return view('layout', compact(
+            'title',
+            'page',
+        ));
+    }
+    public function submit(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ]);
+        
+        Mail::to($data['email'])->send(new ContactMail($data));
+        return back()->with('success', 'Your message has been sent successfully!');
+    }
+}
