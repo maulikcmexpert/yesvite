be306cd6e138b6facfe43bb09c0cfa71d0a09d88
Vimal <vimal.cmexpertise@gmail.com>
2024-11-05 11:40:56 +0530

    cmt

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -16,6 +16,7 @@ use App\Mail\InvitationEmail;
 use App\Models\PostControl;
 use Illuminate\Support\Facades\Mail;
 use App\Jobs\SendInvitationMailJob as sendInvitation;
+use App\Jobs\SendOwnInvitationEmail;
 use App\Mail\NewRsvpsEmailNotify;
 use App\Models\EventImage;
 use App\Models\EventPostImage;
@@ -114,7 +115,7 @@ function sendNotification($notificationType, $postData)
             'date' =>   date('l - M jS, Y', strtotime($event->start_date)),
             'time' => $event->rsvp_start_time,
         ];
-
+        dispatch(new SendOwnInvitationEmail(array($senderData->email, $eventData)));
         // $invitation_email = new OwnInvitationEmail($eventData);
         // Mail::to($senderData->email)->send($invitation_email);
     }
