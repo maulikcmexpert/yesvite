e213e2fed17258e327d996c0121f5b8a7c99592a
Prakashmanat01 <prakashhmanat@gmail.com>
2024-11-06 15:49:17 +0530

    Update contact.blade.php

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>CodePen - Email Newsletter Template</title>
-   <link href="https://fonts.cdnfonts.com/css/sf-pro-display" rel="stylesheet">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-   <style>
-      @import url('https://fonts.cdnfonts.com/css/sf-pro-display');
-      @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap');
-      .persons-right .person-span-1::after {
-         content: "";
-         position: absolute;
-         width: 2px;
-         height: 10px;
-         background: #0000001A;
-         top: 50%;
-         right: -8px;
-         transform: translate(-0%, -50%);
-      }
-      a {
-         text-decoration: none;
-      }
-      .main-center,
-      .m_-2106421672916089406main-center {
-         justify-content: center !important;
-      }
-   </style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CodePen - Email Newsletter Template</title>
+        <link href="https://fonts.cdnfonts.com/css/sf-pro-display" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <style>
+            @import url('https://fonts.cdnfonts.com/css/sf-pro-display');
+            @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap');
+            .persons-right .person-span-1::after {
+                content: "";
+                position: absolute;
+                width: 2px;
+                height: 10px;
+                background: #0000001A;
+                top: 50%;
+                right: -8px;
+                transform: translate(-0%, -50%);
+            }
+            a {
+                text-decoration: none;
+            }
+            .main-center,
+            .m_-2106421672916089406main-center {
+                justify-content: center !important;
+            }
+        </style>
 </head>
 <body style="font-family: 'SF Pro Display', sans-serif !important ;margin: 0px auto;background: #ffffff; display: flex; justify-content: center;  max-width:600px;" class="main-center">
    <div style="width: 100%;max-width: 650px;height:100%;padding: 0px 10px;" class="ui-sortable">
@@ -39,47 +38,41 @@
 
                </td>
             </tr>
-            <!-- -------------- -->
             <tr>
                <td height="50" style="font-size:0px">&nbsp;</td>
             </tr>
-            <!-- -------------- -->
             <tr>
                <td>
                   <h4 style="font-size: 20px;line-height: 28px;font-weight: 700;color: #0F172A;margin: 0px 0px;">Contact Us Submission</h4>
                </td>
             </tr>
-          
             <tr>
                <td height="30" style="font-size:0px">&nbsp;</td>
             </tr>
-            <!-- -------------- -->
             <tr>
                <td>
-                <p style="font-family: 'Manrope', sans-serif; font-size: 12px; line-height: 20px; font-weight: 500; color: #0F172A;">
-                    You have received a new message from the Contact Us form. Please find the details below:
-                </p>
-                <p style="font-family: 'Manrope', sans-serif; font-size: 12px; line-height: 20px; font-weight: 500; color: #0F172A;">
-                    <strong>Name:</strong> {{ $data['name'] }}
-                </p>
-                <p style="font-family: 'Manrope', sans-serif; font-size: 12px; line-height: 20px; font-weight: 500; color: #0F172A;">
-                    <strong>Email:</strong> {{ $data['email'] }}
-                </p>         
-                <p style="font-family: 'Manrope', sans-serif; font-size: 12px; line-height: 20px; font-weight: 500; color: #0F172A;">
-                    <strong>Message:</strong> 
-                    <br>
-                    {{ $data['message'] }}
-                </p>
-                  <p style="font-family:'Manrope';font-size: 12px;line-height: 20px;font-weight: 500;color: #0F172A;">
-                    © Yesvite 2024
-                </p>
+                    <p style="font-family: 'Manrope', sans-serif; font-size: 12px; line-height: 20px; font-weight: 500; color: #0F172A;">
+                        You have received a new message from the Contact Us form. Please find the details below:
+                    </p>
+                    <p style="font-family: 'Manrope', sans-serif; font-size: 12px; line-height: 20px; font-weight: 500; color: #0F172A;">
+                        <strong>Name:</strong> {{ $data['name'] }}
+                    </p>
+                    <p style="font-family: 'Manrope', sans-serif; font-size: 12px; line-height: 20px; font-weight: 500; color: #0F172A;">
+                        <strong>Email:</strong> {{ $data['email'] }}
+                    </p>         
+                    <p style="font-family: 'Manrope', sans-serif; font-size: 12px; line-height: 20px; font-weight: 500; color: #0F172A;">
+                        <strong>Message:</strong> 
+                        <br>
+                        {{ $data['message'] }}
+                    </p>
+                    <p style="font-family:'Manrope';font-size: 12px;line-height: 20px;font-weight: 500;color: #0F172A;">
+                        © Yesvite 2024
+                    </p>
                </td>
             </tr>
-            <!-- -------------- -->
             <tr>
                <td height="10" style="font-size:0px">&nbsp;</td>
             </tr>
-            <!-- -------------- -->
             <tr>
                <td>
                   <div class="download-form-img" style="display: flex;align-items: center;gap: 10px;">
@@ -88,40 +81,31 @@
                   </div>
                </td>
             </tr>
-            <!-- -------------- -->
             <tr>
                <td height="30" style="font-size:0px">&nbsp;</td>
             </tr>
-            <!-- -------------- -->
             <tr>
                <td>
                   <div class="social-icons" style="display: flex;align-items: center;justify-content: space-between;">
                      <div style="width: 100%;max-width: 95px;height: 24px;"><img src="{{ asset('public/storage/yesvitelogo.png')}}" alt="" style="width: 100%;height: 100%;"></div>
-                     <ul style="display: flex;align-items: center;gap: 10px;margin: 0px 0px;">
-                        <li style="list-style-type: none;"><a href="" style="color: #475569;width: 100%;max-width: 16px;height: 16px;"><i class="fa-brands fa-facebook"></i></a></li>
-                        <li style="list-style-type: none;"><a href="" style="color: #475569;width: 100%;max-width: 16px;height: 16px;"><i class="fa-brands fa-twitter"></i></a></li>
-                        <li style="list-style-type: none;"><a href="" style="color: #475569;width: 100%;max-width: 16px;height: 16px;"><i class="fa-brands fa-apple" style="font-size: 18px;"></i></a></li>
-                     </ul>
+                        <ul style="display: flex;align-items: center;gap: 10px;margin: 0px 0px;">
+                            <li style="list-style-type: none;"><a href="" style="color: #475569;width: 100%;max-width: 16px;height: 16px;"><i class="fa-brands fa-facebook"></i></a></li>
+                            <li style="list-style-type: none;"><a href="" style="color: #475569;width: 100%;max-width: 16px;height: 16px;"><i class="fa-brands fa-twitter"></i></a></li>
+                            <li style="list-style-type: none;"><a href="" style="color: #475569;width: 100%;max-width: 16px;height: 16px;"><i class="fa-brands fa-apple" style="font-size: 18px;"></i></a></li>
+                        </ul>
                   </div>
                </td>
             </tr>
-            <!-- -------------- -->
             <tr>
                <td height="20" style="font-size:0px">&nbsp;</td>
             </tr>
-            <!-- -------------- -->
             {{-- <tr>
                <td>
                   <p style="font-family:'Manrope';font-size: 12px;line-height: 20px;font-weight: 700;color: #0F172A;margin: 0px;">Invitee Email: <a href="mailto:crisilis@hotmail.com" style="font-family:'Manrope';font-size: 12px;line-height: 20px;font-weight: 500;color: #0F172A;">crisilis@hotmail.com</a></p>
                </td>
             </tr> --}}
-            <!-- -------------- -->
          </tbody>
       </table>
-
    </div>
-   <!-- partial -->
-
 </body>
-
 </html>
