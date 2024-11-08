412242f9c134b4b9543c4a08253be636e1a0466e
Prakashmanat01 <prakashhmanat@gmail.com>
2024-11-08 17:50:11 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -5664,23 +5664,23 @@ class ApiControllerv2 extends Controller
                             EventPostImage::where('event_id', $input['event_id'])->delete();
                         }
                         EventPost::where('event_id', $input['event_id'])->delete();
-                        EventInvitedUser::where('event_id', $input['event_id'])->delete();
-                        EventSchedule::where('event_id', $input['event_id'])->delete();
-                        EventGiftRegistry::where('event_id', $input['event_id'])->delete();
-                        EventGreeting::where('event_id', $input['event_id'])->delete();
-                        EventSetting::where('event_id', $input['event_id'])->delete();
-                        EventPotluckCategoryItem::where('event_id', $input['event_id'])->delete();
-                        EventPotluckCategory::where('event_id', $input['event_id'])->delete();
-                        EventPostCommentReaction::where('event_id', $input['event_id'])->delete();
-                        EventPostComment::where('event_id', $input['event_id'])->delete();
-                        EventPostPollOption::where('event_id', $input['event_id'])->delete();
-                        EventPostPoll::where('event_id', $input['event_id'])->delete();
-                        EventDesignCategory::where('event_id', $input['event_id'])->delete();
-                        EventDesign::where('event_id', $input['event_id'])->delete();
-                        EventPostPhotoComment::where('event_id', $input['event_id'])->delete();
-                        EventPostPhotoReaction::where('event_id', $input['event_id'])->delete();
-                        EventUserStory::where('event_id', $input['event_id'])->delete();
-                        EventPostReaction::where('event_id', $input['event_id'])->delete();
+                        // EventInvitedUser::where('event_id', $input['event_id'])->delete();
+                        // EventSchedule::where('event_id', $input['event_id'])->delete();
+                        // EventGiftRegistry::where('event_id', $input['event_id'])->delete();
+                        // EventGreeting::where('event_id', $input['event_id'])->delete();
+                        // EventSetting::where('event_id', $input['event_id'])->delete();
+                        // EventPotluckCategoryItem::where('event_id', $input['event_id'])->delete();
+                        // EventPotluckCategory::where('event_id', $input['event_id'])->delete();
+                        // EventPostCommentReaction::where('event_id', $input['event_id'])->delete();
+                        // EventPostComment::where('event_id', $input['event_id'])->delete();
+                        // EventPostPollOption::where('event_id', $input['event_id'])->delete();
+                        // EventPostPoll::where('event_id', $input['event_id'])->delete();
+                        // EventDesignCategory::where('event_id', $input['event_id'])->delete();
+                        // EventDesign::where('event_id', $input['event_id'])->delete();
+                        // EventPostPhotoComment::where('event_id', $input['event_id'])->delete();
+                        // EventPostPhotoReaction::where('event_id', $input['event_id'])->delete();
+                        // EventUserStory::where('event_id', $input['event_id'])->delete();
+                        // EventPostReaction::where('event_id', $input['event_id'])->delete();
 
                 }
 
