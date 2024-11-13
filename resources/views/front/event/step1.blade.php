fa7d16a2380552cba0e69e8ef66c0ea083ef6a29
Vimal <vimal.cmexpertise@gmail.com>
2024-11-13 11:57:57 +0530

    Update step1.blade.php

--- resources/views/front/event/step1.blade.php
+++ resources/views/front/event/step1.blade.php
@@ -484,6 +484,31 @@
                                                                 <div class="input-group time ">
                                                                     <input class="form-control timepicker activity_start_time" id="activity-start-time"
                                                                         name="activity-start-time[]" placeholder="HH:MM AM/PM" required="" readonly value="{{$data['start_time']}}"/><span
+                                                                        class="input-group-append input-group-addon"><span class="input-group-text">
+                                                                            <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                                <path
+                                                                                    d="M18.8334 9.99984C18.8334 14.5998 15.1 18.3332 10.5 18.3332C5.90002 18.3332 2.16669 14.5998 2.16669 9.99984C2.16669 5.39984 5.90002 1.6665 10.5 1.6665C15.1 1.6665 18.8334 5.39984 18.8334 9.99984Z"
+                                                                                    stroke="#64748B" stroke-width="1.5" stroke-linecap="round"
+                                                                                    stroke-linejoin="round" />
+                                                                                <path
+                                                                                    d="M13.5917 12.65L11.0083 11.1083C10.5583 10.8416 10.1917 10.2 10.1917 9.67497V6.2583"
+                                                                                    stroke="#64748B" stroke-width="1.5" stroke-linecap="round"
+                                                                                    stroke-linejoin="round" />
+                                                                            </svg></span></span>
+                                                                    </div>
+                                                                    <label class="error-message" id="start-error-{{Carbon::parse($currentDate)->format('Y-m-d')}}"></label>
+                                                                </div>
+                                                            {{-- <div class="input-form">
+                                                                <input type="text" class="form-control timepicker inputText" id="start-time" name="start-time" required="" value="{{$data['start_time']}}">
+                                                                <label for="start-time" class="form-label input-field floating-label select-label">Start Time</label>
+                                                            </div> --}}
+                                                        </div>
+                                                        <div class="col-6 mb-4">
+                                                            <div class="form-group">
+                                                                <label>End Time</label>
+                                                                <div class="input-group time ">
+                                                                    <input class="form-control timepicker activity_end_time" id="activity-end-time"
+                                                                        name="activity-end-time[]" placeholder="HH:MM AM/PM" required="" readonly value="{{$data['end_time']}}"/><span
                                                                         class="input-group-append input-group-addon"><span class="input-group-text"><svg width="21"
                                                                                 height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                                 <path
@@ -496,21 +521,15 @@
                                                                                     stroke-linejoin="round" />
                                                                             </svg></span></span>
                                                                           </div>
-                                                                          <label class="error-message" id="start-error-{{Carbon::parse($currentDate)->format('Y-m-d')}}"></label>
+                                                                          <label class="error-message" id="end-error-{{Carbon::parse($currentDate)->format('Y-m-d')}}"></label>
                                                             </div>
                                                             {{-- <div class="input-form">
-                                                                <input type="text" class="form-control timepicker inputText" id="start-time" name="start-time" required="" value="{{$data['start_time']}}">
-                                                                <label for="start-time" class="form-label input-field floating-label select-label">Start Time</label>
-                                                            </div> --}}
-                                                        </div>
-                                                        <div class="col-6 mb-4">
-                                                            <div class="input-form">
                                                                 <input type="text" class="form-control timepicker inputText"
                                                                     id="start-time" name="start-time" required="" value="{{$data['end_time']}}">
                                                                 <label for="start-time"
                                                                     class="form-label input-field floating-label select-label">End
                                                                     Time</label>
-                                                            </div>
+                                                            </div> --}}
                                                         </div>
                                                     </div>
                                                 </div>
