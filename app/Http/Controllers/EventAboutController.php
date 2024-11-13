b4ec49a53e5389fc35f46b0625d33bada6f8150f
Prakashmanat01 <prakashhmanat@gmail.com>
2024-11-13 18:12:04 +0530

    Update EventAboutController.php

--- app/Http/Controllers/EventAboutController.php
+++ app/Http/Controllers/EventAboutController.php
@@ -241,7 +241,7 @@ class EventAboutController extends Controller
 
             return response()->json(['status' => 0, 'message' => "db error"]);
         } catch (\Exception $e) {
-
+            dd($e);
             return response()->json(['status' => 0, 'message' => 'something went wrong']);
         }
     }
