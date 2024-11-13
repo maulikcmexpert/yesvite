282fd037eb39b748cc0bf18bb89168b67f31e586
Vimal <vimal.cmexpertise@gmail.com>
2024-11-13 19:28:11 +0530

    Update step1.blade.php

--- resources/views/front/event/step1.blade.php
+++ resources/views/front/event/step1.blade.php
@@ -479,7 +479,7 @@
                                                     </h3>
                                                     <div class="row all_activity">
                                                         <div class="col-12 mb-4">
-                                                            <div class="input-form">
+                                                            <div class="input-form position-relative">
                                                                 <input type="text" class="form-control inputText" id="description" name="description[]" required="" value="{{$data['activity_title']}}" />
                                                                 <label for="description" class="input-field floating-label select-label">Description</label>
                                                             </div>
