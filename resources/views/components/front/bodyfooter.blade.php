d42aeb9d02afd5fe1fec78dba576470f50536360
Prakashmanat01 <prakashhmanat@gmail.com>
2024-11-13 12:35:57 +0530

    Update bodyfooter.blade.php

--- resources/views/components/front/bodyfooter.blade.php
+++ resources/views/components/front/bodyfooter.blade.php
@@ -81,8 +81,8 @@
           <div class="footer-bottom d-flex justify-content-between flex-wrap">
               <ul class="footer-social-link">
                   <li>
-                      {{-- <a href="{{ route('event.event_drafts') }}"> --}}
-                        <a href="#">
+                      <a href="{{ route('event.event_drafts') }}">
+                        {{-- <a href="#"> --}}
 
                           <span>
                               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
