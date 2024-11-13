2e7f8d27a405ee4048edd2c129d046bb06d12597
Vimal <vimal.cmexpertise@gmail.com>
2024-11-13 11:45:15 +0530

    Update step1.blade.php

--- resources/views/front/event/step1.blade.php
+++ resources/views/front/event/step1.blade.php
@@ -446,7 +446,8 @@
                                                 <div class="activity-main-wrp mb-3">
                                                     <h3>Activity <span class="activity-count-{{ Carbon::parse($currentDate)->format('Y-m-d') }} activity-count">{{ $count }}</span>
                                                         <span class="ms-auto">
-                                                            <svg width="20" height="20" viewBox="0 0 20 20"
+                                                            <svg class="delete_activity" data-id="{{Carbon::parse($currentDate)->format('Y-m-d')}}" data-class="{{ Carbon::parse($currentDate)->format('Y-m-d') }}"
+                                                            data-total_activity="{{Carbon::parse($currentDate)->format('Ymd')}}" width="20" height="20" viewBox="0 0 20 20"
                                                                 fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                 <path
                                                                     d="M17.5 4.98356C14.725 4.70856 11.9333 4.56689 9.15 4.56689C7.5 4.56689 5.85 4.65023 4.2 4.81689L2.5 4.98356"
@@ -469,22 +470,38 @@
                                                             </svg>
                                                         </span>
                                                     </h3>
-                                                    <div class="row">
+                                                    <div class="row all_activity">
                                                         <div class="col-12 mb-4">
                                                             <div class="input-form">
-                                                                <input type="text" class="form-control inputText"
-                                                                    id="description" name="description" required="" value="{{$data['activity_title']}}">
-                                                                <label for="description"
-                                                                    class="form-label input-field floating-label">Description</label>
+                                                                <input type="text" class="form-control inputText" id="description" name="description[]" required="" />
+                                                                <label for="description" class="form-label input-field floating-label select-label">Description</label>
                                                             </div>
+                                                            <label class="error-message" id="desc-error-{{Carbon::parse($currentDate)->format('Y-m-d')}}"></label>
                                                         </div>
                                                         <div class="col-6 mb-4">
-                                                            <div class="input-form">
-                                                                <input type="text" class="form-control timepicker inputText" id="start-time" name="start-time" required="" value="{{$data['start_time']}}">
-                                                                <label for="start-time"
-                                                                    class="form-label input-field floating-label select-label">Start
-                                                                    Time</label>
+                                                            <div class="form-group">
+                                                                <label>Start Time</label>
+                                                                <div class="input-group time ">
+                                                                    <input class="form-control timepicker activity_start_time" id="activity-start-time"
+                                                                        name="activity-start-time[]" placeholder="HH:MM AM/PM" required="" readonly value="{{$data['start_time']}}"/><span
+                                                                        class="input-group-append input-group-addon"><span class="input-group-text"><svg width="21"
+                                                                                height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                                <path
+                                                                                    d="M18.8334 9.99984C18.8334 14.5998 15.1 18.3332 10.5 18.3332C5.90002 18.3332 2.16669 14.5998 2.16669 9.99984C2.16669 5.39984 5.90002 1.6665 10.5 1.6665C15.1 1.6665 18.8334 5.39984 18.8334 9.99984Z"
+                                                                                    stroke="#64748B" stroke-width="1.5" stroke-linecap="round"
+                                                                                    stroke-linejoin="round" />
+                                                                                <path
+                                                                                    d="M13.5917 12.65L11.0083 11.1083C10.5583 10.8416 10.1917 10.2 10.1917 9.67497V6.2583"
+                                                                                    stroke="#64748B" stroke-width="1.5" stroke-linecap="round"
+                                                                                    stroke-linejoin="round" />
+                                                                            </svg></span></span>
+                                                                          </div>
+                                                                          <label class="error-message" id="start-error-{{Carbon::parse($currentDate)->format('Y-m-d')}}"></label>
                                                             </div>
+                                                            {{-- <div class="input-form">
+                                                                <input type="text" class="form-control timepicker inputText" id="start-time" name="start-time" required="" value="{{$data['start_time']}}">
+                                                                <label for="start-time" class="form-label input-field floating-label select-label">Start Time</label>
+                                                            </div> --}}
                                                         </div>
                                                         <div class="col-6 mb-4">
                                                             <div class="input-form">
