99b8d92f9d96880f777a46eb9ed027184a3e6e1a
Prakashmanat01 <prakashhmanat@gmail.com>
2024-11-13 19:15:31 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -10827,7 +10827,7 @@ class ApiControllerv2 extends Controller
         } catch (QueryException $e) {
 
             DB::rollBack();
-            dd($e);
+            
             return response()->json(['status' => 0, 'message' => 'db error']);
         } catch (Exception $e) {
 
