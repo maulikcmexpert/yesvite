cbd6d5a2281d24a2d2bf2771a0b36ba422bc4457
Vimal <vimal.cmexpertise@gmail.com>
2024-11-14 14:40:33 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -400,8 +400,9 @@ class EventController extends Controller
         $endDateFormat = DateTime::createFromFormat('m-d-Y', $endDate)->format('Y-m-d');
 
         if (isset($request->rsvp_by_date) && $request->rsvp_by_date != '') {
-            dd($request->rsvp_by_date);
-            $rsvp_by_date = DateTime::createFromFormat('m-d-Y', $request->rsvp_by_date)->format('Y-m-d');
+            // dd($request->rsvp_by_date);
+            $rsvp_by_date = Carbon::parse($request->rsvp_by_date)->format('m-d-Y');
+            // $rsvp_by_date = DateTime::createFromFormat('m-d-Y', $request->rsvp_by_date)->format('Y-m-d');
             $rsvp_by_date_set = '1';
         } else {
             if ($startDateFormat) {
