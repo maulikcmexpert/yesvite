3032dea55b9d8b8d7ab347fdb51b369bffdc7114
Vimal <vimal.cmexpertise@gmail.com>
2024-11-19 15:10:13 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -137,6 +137,7 @@ class EventController extends Controller
                     $eventDetail['step'] = ($getEventData->step != NULL) ? $getEventData->step : 0;
                     $eventDetail['subscription_plan_name'] = ($getEventData->subscription_plan_name != NULL) ? $getEventData->subscription_plan_name : "";
                     $eventDetail['subscription_invite_count'] = ($getEventData->subscription_invite_count != NULL) ? $getEventData->subscription_invite_count : 0;
+                    $eventDetail['static_information'] = ($getEventData->static_information != NULL) ? $getEventData->static_information : '';
                     $eventDetail['event_images'] = [];
                     $getEventImages = EventImage::where('event_id', $getEventData->id)->get();
                     if (!empty($getEventImages)) {
@@ -318,7 +319,7 @@ class EventController extends Controller
         }else{
             $title = 'Create Event';
         }
-        // dd($eventDetail);
+        dd($eventDetail);
         $page = 'front.create_event';
        
 
