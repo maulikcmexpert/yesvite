eb8ff002ee3ac80624ae0841fff3d729e09940ab
Vimal <vimal.cmexpertise@gmail.com>
2024-11-20 15:57:08 +0530

    Update rsvpEmail.blade.php

--- resources/views/emails/rsvpEmail.blade.php
+++ resources/views/emails/rsvpEmail.blade.php
@@ -162,7 +162,7 @@
                                     <h5 style="font-size: 10px;line-height: normal;font-weight: 600;color: #0F172A;margin: 0px;">RSVP’d <span style="color: #23AA26;">YES</span></h5>
                                 </div>
                                 <div class="persons-right" style="display: flex;align-items: center;gap: 15px; margin-left:auto">
-                                    <span class="person-span-1" style="font-size: 10px;line-height: normal;font-weight: 600;color: #0F172A;position: relative; border-right: 2px solid gray; padding-right:10px;"><img src="{{ $base64 }}" alt="user-img"><i class="fa-solid fa-user-group" style="color: #DDE3E0;font-size: 8px;"></i> {{ $eventData['adults'] }} Adults</span>
+                                    <span class="person-span-1" style="font-size: 10px;line-height: normal;font-weight: 600;color: #0F172A;position: relative; border-right: 2px solid gray; padding-right:10px;"><img src="{{ 'data:image/svg+xml;base64,' . base64_encode(file_get_contents($path)) }}" alt="user-img"><i class="fa-solid fa-user-group" style="color: #DDE3E0;font-size: 8px;"></i> {{ $eventData['adults'] }} Adults</span>
                                     <span style="font-size: 10px;line-height: normal;font-weight: 600;color: #0F172A; padding-left: 10px">{{ $eventData['kids'] }} Kids</span>
                                 </div>
                             </div>
@@ -173,7 +173,7 @@
                                     <h5 style="font-size: 10px;line-height: normal;font-weight: 600;color: #0F172A;margin: 0px;">RSVP’d <span style="color: #e52121;">NO</span></h5>
                                 </div>
                                 <div class="persons-right" style="display: flex;align-items: center;gap: 15px;margin-left:auto">
-                                    <span class="person-span-1" style="font-size: 10px;line-height: normal;font-weight: 600;color: #0F172A;position: relative; border-right: 2px solid red; padding-right:10px;"><img src="{{ $base64 }}" alt="user-img"><i class="fa-solid fa-users-line" style="color: #DDE3E0;font-size: 8px;"></i> {{ $eventData['adults'] }} Adults</span>
+                                    <span class="person-span-1" style="font-size: 10px;line-height: normal;font-weight: 600;color: #0F172A;position: relative; border-right: 2px solid red; padding-right:10px;"><img src="{{ 'data:image/svg+xml;base64,' . base64_encode(file_get_contents($path)) }}" alt="user-img"><i class="fa-solid fa-users-line" style="color: #DDE3E0;font-size: 8px;"></i> {{ $eventData['adults'] }} Adults</span>
                                     <span style="font-size: 10px;line-height: normal;font-weight: 600;color: #0F172A; padding-left: 10px">{{ $eventData['kids'] }} Kids</span>
                                 </div>
                             </div>
