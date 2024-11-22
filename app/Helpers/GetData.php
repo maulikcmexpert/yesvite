9f96b0469be41ab0c3b9eff2c16450e0b4de7b3d
Vimal <vimal.cmexpertise@gmail.com>
2024-11-22 14:55:58 +0530

    Update GetData.php

--- app/Helpers/GetData.php
+++ app/Helpers/GetData.php
@@ -220,10 +220,7 @@ function getYesviteContactListPage($id, $perPage, $page, $search_name)
         ->where(function ($query) use($id){
             $query->whereNull('email_verified_at')
                 ->where('app_user', '!=', '1')
-                ->orWhereNotNull('email_verified_at');
-        })
-        ->where(function ($query) use($id){
-            $query->whereNull('email_verified_at')
+                ->orWhereNotNull('email_verified_at')
                 ->where('parent_user_phone_contact',$id);
         })
         
