881f440b9c4f31ebc63de79e65441c9603662ffe
Vimal <vimal.cmexpertise@gmail.com>
2024-11-22 14:38:28 +0530

    Update GetData.php

--- app/Helpers/GetData.php
+++ app/Helpers/GetData.php
@@ -217,7 +217,7 @@ function getYesviteContactListPage($id, $perPage, $page, $search_name)
     $yesviteRegisteredUser = User::select('id', 'firstname', 'profile', 'lastname', 'email', 'country_code', 'phone_number', 'app_user', 'prefer_by', 'email_verified_at', 'parent_user_phone_contact', 'visible', 'message_privacy')
         ->where('id', '!=', $id)
         ->where('is_user_phone_contact', '0')
-        ->where(function ($query) {
+        ->where(function ($query) use($id){
             // $query->whereNull('email_verified_at')
             //     ->where('app_user', '!=', '1')
             //     ->orWhereNotNull('email_verified_at');
