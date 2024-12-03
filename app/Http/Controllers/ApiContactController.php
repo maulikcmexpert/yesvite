229d2bbd44f2fac016ceff02d1884d7913feb3a4
Vimal <vimal.cmexpertise@gmail.com>
2024-12-03 16:30:05 +0530

    Update ApiContactController.php

--- app/Http/Controllers/ApiContactController.php
+++ app/Http/Controllers/ApiContactController.php
@@ -221,8 +221,13 @@ class ApiContactController extends Controller
             // Find matching contacts using array_search()
             $index = array_search(true, array_map(function ($duplicateContact) use ($userDetail) {
                 if($duplicateContact['email'] == $userDetail->email || $duplicateContact['phone'] == $userDetail->phone_number){
-                    
-                    return $duplicateContact['email'] === $userDetail->email;
+                    if($duplicateContact['email'] == $userDetail->email){
+                        return $duplicateContact['email'] === $userDetail->email;
+                    }
+
+                    if($duplicateContact['phone'] == $userDetail->phone_number){
+                        return $duplicateContact['phone'] === $userDetail->phone_number;
+                    }
                 }
                 // return $duplicateContact['email'] === $userDetail->email || $duplicateContact['phone'] === $userDetail->phone_number;
             }, $duplicateContacts));
