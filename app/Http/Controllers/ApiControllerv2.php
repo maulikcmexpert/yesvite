745fbe416a4cf626f73d386639e1d7b8ba2aab29
Vimal <vimal.cmexpertise@gmail.com>
2024-12-06 19:27:28 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -10567,7 +10567,7 @@ class ApiControllerv2 extends Controller
         }
 
         try {
-            $sendFaildInvites = EventInvitedUser::where(['event_id' => $input['event_id'], 'invitation_sent' => '9'])->get();
+            $sendFaildInvites = EventInvitedUser::where(['event_id' => $input['event_id'],'invitation_sent' => '9'])->where('user_id','!=','')->get();
 
             $faildInviteList = [];
 
