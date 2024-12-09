ba06c3a840c546fedb3cadc543399c7408fe5aea
Vimal <vimal.cmexpertise@gmail.com>
2024-12-09 17:12:10 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -10453,8 +10453,9 @@ class ApiControllerv2 extends Controller
                 foreach ($result as $value) {
                     // dd($value->user_id);
                     $rsvpUserStatus = [];
-                    $rsvpUserStatus['id'] = $value->id;
+                    
                     if(isset($value->user->id) && isset($value->user->app_user) && $value->user->app_user == '1' ){
+                        $rsvpUserStatus['id'] = $value->id;
                         $rsvpUserStatus['user_id'] = $value->user->id;
                         $rsvpUserStatus['first_name'] = $value->user->firstname;
                         $rsvpUserStatus['last_name'] = $value->user->lastname;
@@ -10500,49 +10501,53 @@ class ApiControllerv2 extends Controller
                         ];
 
                     }else if($value->sync_id != ''){
-                        $rsvpUserStatus['user_id'] = $value->sync_id;
-                        $rsvpUserStatus['first_name'] = $value->contact_user->firstName;
-                        $rsvpUserStatus['last_name'] = $value->contact_user->lastName;
-                        $rsvpUserStatus['username'] = $value->contact_user->firstName . ' ' . $value->contact_user->lastName;
-                        $rsvpUserStatus['profile'] = (!empty($value->contact_user->photo) || $value->contact_user->photo != NULL) ? $value->contact_user->photo: "";
-                        $rsvpUserStatus['email'] = ($value->contact_user->email != '') ? $value->contact_user->email : "";
-                        $rsvpUserStatus['phone_number'] = ($value->contact_user->phoneWithCode != '') ? $value->contact_user->phoneWithCode : "";
-                        $rsvpUserStatus['prefer_by'] =  $value->prefer_by;
-                        $rsvpUserStatus['kids'] = $value->kids;
-                        $rsvpUserStatus['adults'] = $value->adults;
-                        $rsvpUserStatus['rsvp_status'] =  ($value->rsvp_status != null) ? (int)$value->rsvp_status : NULL;
-    
-                        if ($value->rsvp_d == '0' && ($value->read == '1' || $value->read == '0') || $value->rsvp_status == null) {
-    
-                            $rsvpUserStatus['rsvp_status'] = 2; // no reply 
+                        $contactData = contact_sync::where('id',$value->sync_id)->first();
+                        if($contactData){
+                            $rsvpUserStatus['id'] = $value->id;
+                            $rsvpUserStatus['user_id'] = $value->sync_id;
+                            $rsvpUserStatus['first_name'] = $contactData->firstName;
+                            $rsvpUserStatus['last_name'] = $contactData->lastName;
+                            $rsvpUserStatus['username'] = $contactData->firstName . ' ' . $contactData->lastName;
+                            $rsvpUserStatus['profile'] = (!empty($contactData->photo) || $contactData->photo != NULL) ? $contactData->photo: "";
+                            $rsvpUserStatus['email'] = ($contactData->email != '') ? $contactData->email : "";
+                            $rsvpUserStatus['phone_number'] = ($contactData->phoneWithCode != '') ? $contactData->phoneWithCode : "";
+                            $rsvpUserStatus['prefer_by'] =  $value->prefer_by;
+                            $rsvpUserStatus['kids'] = $value->kids;
+                            $rsvpUserStatus['adults'] = $value->adults;
+                            $rsvpUserStatus['rsvp_status'] =  ($value->rsvp_status != null) ? (int)$value->rsvp_status : NULL;
+        
+                            if ($value->rsvp_d == '0' && ($value->read == '1' || $value->read == '0') || $value->rsvp_status == null) {
+        
+                                $rsvpUserStatus['rsvp_status'] = 2; // no reply 
+                            }
+        
+                            $rsvpUserStatus['read'] = $value->read;
+        
+                            $rsvpUserStatus['rsvp_d'] = $value->rsvp_d;
+        
+                            $rsvpUserStatus['invitation_sent'] = $value->invitation_sent;
+                            $totalEvent =  Event::where('sync_id', $value->sync_id)->count();
+                            $totalEventPhotos =  0;
+                            $comments =  0;
+                            $rsvpUserStatus['user_profile'] = [
+                                'id' => $value->sync_id,
+                                'profile' => (!empty($contactData->photo) || $contactData->photo != NULL) ? $contactData->photo: "",
+                                'bg_profile' => '',
+                                'app_user' =>  $contactData->app_user,
+                                'gender' => "",
+                                'first_name' =>$contactData->firstName,
+                                'last_name' => $contactData->lastName,
+                                'username' =>$contactData->firstName . ' ' . $contactData->lastName,
+                                'location' => "",
+                                'about_me' => "",
+                                'created_at' => empty($contactData->created_at) ? "" :   str_replace(' ', ', ', date('F Y', strtotime($contactData->created_at))),
+                                'total_events' => $totalEvent,
+                                'visible' => $contactData->visible,
+                                'total_photos' => $totalEventPhotos,
+                                'comments' => $comments,
+                                'message_privacy' =>  ''
+                            ];
                         }
-    
-                        $rsvpUserStatus['read'] = $value->read;
-    
-                        $rsvpUserStatus['rsvp_d'] = $value->rsvp_d;
-    
-                        $rsvpUserStatus['invitation_sent'] = $value->invitation_sent;
-                        $totalEvent =  Event::where('sync_id', $value->sync_id)->count();
-                        $totalEventPhotos =  0;
-                        $comments =  0;
-                        $rsvpUserStatus['user_profile'] = [
-                            'id' => $value->sync_id,
-                            'profile' => (!empty($value->contact_user->photo) || $value->contact_user->photo != NULL) ? $value->contact_user->photo: "",
-                            'bg_profile' => '',
-                            'app_user' =>  $value->contact_user->app_user,
-                            'gender' => "",
-                            'first_name' =>$value->contact_user->firstName,
-                            'last_name' => $value->contact_user->lastName,
-                            'username' =>$value->contact_user->firstName . ' ' . $value->contact_user->lastName,
-                            'location' => "",
-                            'about_me' => "",
-                            'created_at' => empty($value->contact_user->created_at) ? "" :   str_replace(' ', ', ', date('F Y', strtotime($value->contact_user->created_at))),
-                            'total_events' => $totalEvent,
-                            'visible' => $value->contact_user->visible,
-                            'total_photos' => $totalEventPhotos,
-                            'comments' => $comments,
-                            'message_privacy' =>  ''
-                        ];
                     }
 
 
