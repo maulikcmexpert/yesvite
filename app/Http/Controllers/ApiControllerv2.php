41b8c61c1b7b20daab985c0dfedfc54f8ca0b1fd
Vimal <vimal.cmexpertise@gmail.com>
2024-12-09 16:47:07 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -10450,8 +10450,8 @@ class ApiControllerv2 extends Controller
             $eventAboutHost['rsvp_status_list'] = [];
 
             if (count($result) != 0) {
-                dd($result);
                 foreach ($result as $value) {
+                    dd($value);
                     $rsvpUserStatus = [];
                     $rsvpUserStatus['id'] = $value->id;
 
