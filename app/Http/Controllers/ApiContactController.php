2ca130316da569c51c716bac00354a4239ed918b
Vimal <vimal.cmexpertise@gmail.com>
2024-12-11 12:50:38 +0530

    Update ApiContactController.php

--- app/Http/Controllers/ApiContactController.php
+++ app/Http/Controllers/ApiContactController.php
@@ -126,7 +126,7 @@ class ApiContactController extends Controller
         if (empty($contacts)) {
             return response()->json(['message' => 'No contacts provided.'], 400);
         }
-        $uniqueData = collect($contacts)->unique(function ($item) {
+        $contact = collect($contacts)->unique(function ($item) {
        
                 return $item['phoneWithCode'];
             
