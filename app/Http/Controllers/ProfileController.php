2da6b9988e1257ca90c867754d589d9ff23ee948
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-17 17:40:05 +0530

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -485,7 +485,6 @@ class ProfileController extends Controller
             if ($request->visible != null) {        
                 $user->visible = $request->visible;
                 $user->save();
-                dd($user->visible);
             }
 
             $checkProfilePrivacy = UserProfilePrivacy::where('user_id', $user->id)->count();
