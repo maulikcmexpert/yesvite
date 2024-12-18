e281df17dc9f636cec63c4f7589b8c84feb5f2e3
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-18 11:32:07 +0530

    Update header.blade.php

--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -6,7 +6,6 @@
 <link rel="icon" type="image/x-icon" href="{{ asset('storage/favicon.png')}}">
 <!-- custom-style -->
 <link href="{{asset('assets/front/css/style.css')}}" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('assets')}}/front/css/new_css/main.css" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.css" integrity="sha512-2eMmukTZtvwlfQoG8ztapwAH5fXaQBzaMqdljLopRSA0i6YKM8kBAOrSSykxu9NN9HrtD45lIqfONLII2AFL/Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
@@ -33,20 +32,21 @@
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 {{-- <link rel="stylesheet" href="http://fabricjs.com/css/googlefonts.css"> --}}
 @if(config('app.url') == 'https://yesvite.cmexpertiseinfotech.in')
-    <link rel="stylesheet" href="{{ asset('assets/event/css/stylesheet.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/event/css/stylesheet.css') }}">
 @else
-    <link rel="stylesheet" href="{{ asset('assets/event/css/stylesheet_live.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/event/css/stylesheet_live.css') }}">
 @endif
 
 @php
 if(isset($page) && $page=="front.profile"){
-@endphp
-<link rel="stylesheet" href="{{ asset('assets/event/css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/event/css/contact.css') }}">
-@php
+    @endphp
+    <link rel="stylesheet" href="{{ asset('assets/event/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/event/css/contact.css') }}">
+    @php
 }
 @endphp
 
+<link rel="stylesheet" href="{{asset('assets')}}/front/css/new_css/main.css" />
 <link href="https://cdn.jsdelivr.net/bootstrap.timepicker/0.2.6/css/bootstrap-timepicker.min.css" rel="stylesheet" />
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
