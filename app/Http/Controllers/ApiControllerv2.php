b91714a4d9779d7aee5e51c4fd61caa78a2d34b4
Vimal <vimal.cmexpertise@gmail.com>
2024-12-18 10:49:33 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -13106,7 +13106,7 @@ class ApiControllerv2 extends Controller
             }
             return response()->json(['status' => 1, 'message' => "purchase sucessfully"]);
         } catch (QueryException $e) {
-            dd($e);
+            // dd($e);
             return response()->json(['status' => 0, 'message' => "db error"]);
         } catch (Exception  $e) {
             return response()->json(['status' => 0, 'message' => 'something went wrong']);
