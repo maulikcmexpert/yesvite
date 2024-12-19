6c55a027c5967e18aaf0886937d4b61215f6a101
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-19 15:42:40 +0530

    Update account_setting.blade.php

--- resources/views/front/account_setting.blade.php
+++ resources/views/front/account_setting.blade.php
@@ -106,7 +106,8 @@
                             </div>
                         </div>
                         <a href="{{route('account_settings.notificationSetting')}}" class="d-flex align-items-center public-view border-bottom">
-                            <h6>Notifications & Reminders Settings</h6>
+                            {{-- <h6>Notifications & Reminders Settings</h6> --}}
+                            <h6>Notifications Settings</h6>
                             <svg class="ms-auto" width="7" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M0.939941 12.2802L5.28661 7.93355C5.79994 7.42021 5.79994 6.58021 5.28661 6.06688L0.939941 1.72021" stroke="#94A3B8" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
