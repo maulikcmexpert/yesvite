c177165e60365ab4e0f0b1b6beb43366e33aeff7
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-20 11:42:58 +0530

    Update bodyheader.blade.php

--- resources/views/components/front/bodyheader.blade.php
+++ resources/views/components/front/bodyheader.blade.php
@@ -462,7 +462,67 @@
                       
                                 </div>
                               </div>
-                                  
+                            @elseif($inner_data['notification_type']=="sent_rsvp" && $inner_data['rsvp_detail']['rsvpd_status']=="0" ) 
+                              <div class="notification-drodown-body-inner">
+                                <div class="notification-drodown-body-inner-img">
+                                  <img
+                                    src="./assets/img/header-profile-img.png"
+                                    alt=""
+                                  />
+                                  <span class="active-dot"></span>
+                                </div>
+                                <div
+                                  class="notification-drodown-body-inner-content"
+                                >
+                                  <div>
+                                    <h3>
+                                      James Clark
+                                      <span> RSVP’d <strong>NO</strong> for </span>
+                                    </h3>
+                                    <h6 class="notification-time-count">10min</h6>
+                                  </div>
+                                  <div>
+                                    <p>
+                                      Sarah’s Birthday
+                                      <span><strong>Wall</strong></span>
+                                    </p>
+                                    <h6 class="notification-read-dot"></h6>
+                                  </div>
+                                  <div class="notification-rsvp-wrp">
+                                    <h4>RSVP’d <span>NO</span></h4>
+                                    <a href="#" class="chat-icon">
+                                      <svg
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                      >
+                                        <path
+                                          d="M8.5 19H8C4 19 2 18 2 13V8C2 4 4 2 8 2H16C20 2 22 4 22 8V13C22 17 20 19 16 19H15.5C15.19 19 14.89 19.15 14.7 19.4L13.2 21.4C12.54 22.28 11.46 22.28 10.8 21.4L9.3 19.4C9.14 19.18 8.77 19 8.5 19Z"
+                                          stroke="#94A3B8"
+                                          stroke-width="1.5"
+                                          stroke-miterlimit="10"
+                                          stroke-linecap="round"
+                                          stroke-linejoin="round"
+                                        />
+                                        <path
+                                          d="M7 8H17"
+                                          stroke="#94A3B8"
+                                          stroke-width="1.5"
+                                          stroke-linecap="round"
+                                          stroke-linejoin="round"
+                                        />
+                                        <path
+                                          d="M7 13H13"
+                                          stroke="#94A3B8"
+                                          stroke-width="1.5"
+                                          stroke-linecap="round"
+                                          stroke-linejoin="round"
+                                        />
+                                      </svg>
+                                    </a>
+                                  </div>
+                                </div>
+                              </div>
                             @endif
                             {{-- <div class="notification-drodown-body-inner">
                               <div class="notification-drodown-body-inner-img">
