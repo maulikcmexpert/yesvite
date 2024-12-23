e11576e1fb3fec71c4dafac89937527486507f58
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-23 12:31:36 +0530

    Update TransactionDataTable.php

--- app/DataTables/TransactionDataTable.php
+++ app/DataTables/TransactionDataTable.php
@@ -60,8 +60,8 @@ class TransactionDataTable extends DataTable
     public function query(Coin_transactions $model,Request $request): QueryBuilder
 
     {
-        dd($request);
-        $userId = $this->get('userId'); // Retrieve the passed user ID
+        // dd($request);
+        $userId = $this->request()->get('userId'); // Retrieve the passed user ID
         // return $model->newQuery();
         return Coin_transactions::with([ 'users','event','user_subscriptions'])->where('user_id',$userId);
     }
