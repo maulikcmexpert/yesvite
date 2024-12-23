930d65c4f6614b8836454ad81c3b65da37f47c27
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-23 12:14:48 +0530

    Update TransactionController.php

--- app/Http/Controllers/admin/TransactionController.php
+++ app/Http/Controllers/admin/TransactionController.php
@@ -18,7 +18,7 @@ class TransactionController extends Controller
         $title = 'Coin Transaction';
         $page = 'admin.transaction.list';
         // $js = 'admin.user.userjs';
-        return $DataTable->render('admin.includes.layout', compact('title', 'page', 'js'));
+        return $DataTable->render('admin.includes.layout', compact('title', 'page'));
     }
 
     /**
