f2a14353dd180c89351e720bf0b3d0fe11580080
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-23 11:40:01 +0530

    cmt

---
+++ app/DataTables/TransactionDataTable.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Coin_transactions;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class TransactionDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        $counter = 1;
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('no', function () use (&$counter) {
+                return $counter++;
+            })
+
+            ->addColumn('user', function ($row) {
+                return $row->user_id . ' ' . $row->user_id;
+            })
+            ->addColumn('event', function ($row) {
+                return $row->event_id;
+            })
+            ->addColumn('type', function ($row) {
+                return $row->type;
+            })
+            ->addColumn('coins', function ($row) {
+                return $row->coins;
+            })
+            ->addColumn('current_balance', function ($row) {
+                return $row->current_balance;
+            })
+            ->addColumn('used_coins', function ($row) {
+                return $row->used_coins;
+            })
+            
+            
+
+            ->rawColumns(['user', 'event', 'type', 'coins','current_balance', 'used_coins']);
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(Coin_transactions $model,Request $request): QueryBuilder
+
+    {
+        // return $model->newQuery();
+        return Coin_transactions::with([ 'users','event','user_subscriptionss']);
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('transaction-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    //->dom('Bfrtip')
+                    ->orderBy(1)
+                    ->selectStyleSingle()
+                    ->buttons([
+                        Button::make('excel'),
+                        Button::make('csv'),
+                        Button::make('pdf'),
+                        Button::make('print'),
+                        Button::make('reset'),
+                        Button::make('reload')
+                    ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('no')->title('#')->render('meta.row + meta.settings._iDisplayStart + 1;')->orderable(false),
+            Column::make('user')->orderable(false),
+            Column::make('event')->orderable(true),
+            Column::make('type')->orderable(false),
+            Column::make('coins')->orderable(true),
+            Column::make('current_balance')->title('User Type')->orderable(false),
+            Column::make('used_coins')->title('Set Password')->orderable(false),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'Transaction_' . date('YmdHis');
+    }
+}
