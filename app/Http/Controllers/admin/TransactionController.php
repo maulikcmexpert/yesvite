0aa52fa380af42b48fda6ebf5de572971f64d396
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-23 17:19:44 +0530

    Update TransactionController.php

--- app/Http/Controllers/admin/TransactionController.php
+++ app/Http/Controllers/admin/TransactionController.php
@@ -68,11 +68,12 @@ class TransactionController extends Controller
         if($user){
             $total_coin = $user->coins + $coins;
             User::where('id',$user_id)->update(['coins'=>$total_coin]);
-            $subscription = UserSubscription::where('user_id', $user_id)->first();
+
+            // $subscription = UserSubscription::where('user_id', $user_id)->first();
 
             $coin_transaction = new Coin_transactions();
             $coin_transaction->user_id = $user_id;
-            $coin_transaction->user_subscription_id =($subscription!=""||!empty($subscription))?$subscription->id:null;
+            // $coin_transaction->user_subscription_id =($subscription!=""||!empty($subscription))?$subscription->id:null;
             $coin_transaction->status = '0';
             $coin_transaction->type = 'credit';
             $coin_transaction->coins = $coins;
