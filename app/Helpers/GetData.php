85f00aaad51fbf73b2121930f1a93017ecd2e6a1
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-23 10:55:08 +0530

    Update GetData.php

--- app/Helpers/GetData.php
+++ app/Helpers/GetData.php
@@ -434,9 +434,9 @@ function getNotificationList(){
                 $i=0;
                 foreach($notificationInfo as $notify_data){
                     $i++;
-                    if ($i < 5) {
-                        continue; // Skip until $i reaches 5
-                    }
+                    // if ($i < 5) {
+                    //     continue; // Skip until $i reaches 5
+                    // }
                     
                     if ($values->event_id === $notify_data['event_id']) {
                         $final_data[$values->event->event_name][] = $notify_data; 
