e7bff34cbdf50379e72ea4fa7613259b1fbd58ed
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-24 18:25:27 +0530

    Update scritp.js

--- public/assets/front/new_js/scritp.js
+++ public/assets/front/new_js/scritp.js
@@ -97,14 +97,14 @@ $("#dropdownButton").on("click", function (event) {
   }
 });
 
-document.addEventListener("click", (event) => {
-  if (
-    !dropdownMenu.contains(event.target) &&
-    !dropdownButton.contains(event.target)
-  ) {
-    dropdownMenu.classList.remove("show");
-  }
-});
+// document.addEventListener("click", (event) => {
+//   if (
+//     !dropdownMenu.contains(event.target) &&
+//     !dropdownButton.contains(event.target)
+//   ) {
+//     dropdownMenu.classList.remove("show");
+//   }
+// });
 // Close the dropdown when clicking outside
 // document.addEventListener("click", (event) => {
 //   if (
