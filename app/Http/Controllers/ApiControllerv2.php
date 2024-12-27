ea338c00278126a4ef091566604c8505f94ada7d
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-27 18:38:45 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -820,7 +820,7 @@ class ApiControllerv2 extends Controller
                         $total_invited_user = EventInvitedUser::whereHas('user', function ($query) {
 
                             $query->where('app_user', '1');
-                        })->where(['event_id' => $value->id])->count();
+                        })->where(['event_id' => $value->id,'is_co_host'=>'0'])->count();
 
 
 
