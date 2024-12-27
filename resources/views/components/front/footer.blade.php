722c55b0dd1c47c7114406987c7f2f2f1b4351b3
Vimal <vimal.cmexpertise@gmail.com>
2024-12-27 16:49:36 +0530

    Update footer.blade.php

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -1,47 +1,61 @@
 
 <script src="{{ asset('assets/front/js/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/front/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('assets/front/js/toastr.min.js') }}"></script>
-<script src="{{ asset('assets/front/js/webfont.js') }}"></script>
-<script src="{{ asset('assets/front/js/fontfaceobserver.standalone.js') }}"></script>
-<script src="{{ asset('assets/front/js/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('assets/admin/js/jquery-validate-additional.js') }}"></script>
-<script src="{{ asset('assets/admin/js/jquery-validate.js') }}"></script>
-<script src="{{ asset('assets/admin/js/croppie.min.js') }}"></script>
-<script src="{{ asset('assets/admin/js/intlTelInput-jquery.min.js') }}"></script>
-<script src="{{ asset('assets/front/js/common.js') }}"></script>
-<script src="{{ asset('assets/front/js/jquery.auto-complete.min.js') }}"></script>
-<script src="{{ asset('assets/front/js/script.js') }}"></script>
-<script src="{{ asset('assets/front/js/animation.js') }}"></script>
-<script src="{{ asset('assets/front/js/wow.min.js') }}"></script>
-<script src="{{ asset('assets/front/js/design.js') }}"></script>
-<script src="{{ asset('assets/front/js/map.js') }}"></script>
-<script src="{{ asset('assets/front/js/contact_us.js') }}"></script>
-<script src="{{ asset('assets/front/js/spectrum.min.js') }}"></script>
-<script src="{{ asset('assets/front/js/html2canvas.min.js') }}"></script>
-<script src="{{ asset('assets/front/js/html-to-image.js') }}"></script>
-<script src="{{ asset('assets/front/js/FileSaver.min.js') }}"></script>
-<script src="{{ asset('assets/front/js/dom-to-image.min.js') }}"></script>
 
-{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" stylesheet.crossOrigin = "anonymous"></script> --}}
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script> --}}
-{{-- <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script> --}}
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/fontfaceobserver/2.1.0/fontfaceobserver.standalone.js"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" stylesheet.crossOrigin = "anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fontfaceobserver/2.1.0/fontfaceobserver.standalone.js"></script>
+
 {{-- <script src="{{ asset('assets/event/js/fontLoder.js') }}"></script> --}}
 <!-- owl-carousel-js -->
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
 <!-- custom-js -->
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.js" integrity="sha512-Gs+PsXsGkmr+15rqObPJbenQ2wB3qYvTHuJO6YJzPe/dTLvhy0fmae2BcnaozxDo5iaF8emzmCZWbQ1XXiX2Ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
+<script src="{{ asset('assets/admin/js/jquery-validate.js') }}"></script>
+
+<script src="{{ asset('assets/admin/js/jquery-validate-additional.js') }}"></script>
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.js" integrity="sha512-Gs+PsXsGkmr+15rqObPJbenQ2wB3qYvTHuJO6YJzPe/dTLvhy0fmae2BcnaozxDo5iaF8emzmCZWbQ1XXiX2Ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!-- <script src="https://hammerjs.github.io/dist/hammer.min.js"></script> -->
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/js/intlTelInput-jquery.min.js"></script> --}}
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-autocomplete/1.0.7/jquery.auto-complete.min.js" integrity="sha512-TToQDr91fBeG4RE5RjMl/tqNAo35hSRR4cbIFasiV2AAMQ6yKXXYhdSdEpUcRE6bqsTiB+FPLPls4ZAFMoK5WA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/js/intlTelInput-jquery.min.js"></script>
+
+<script src="{{ asset('assets/front/js/common.js') }}"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-autocomplete/1.0.7/jquery.auto-complete.min.js" integrity="sha512-TToQDr91fBeG4RE5RjMl/tqNAo35hSRR4cbIFasiV2AAMQ6yKXXYhdSdEpUcRE6bqsTiB+FPLPls4ZAFMoK5WA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="{{ asset('assets/front/js/script.js') }}"></script>
+
 <!-- ======== wow-js ======== -->
+<script src="{{ asset('assets/front/js/animation.js') }}"></script>
+
+<script src="{{ asset('assets/front/js/wow.min.js') }}"></script>
+
+<script src="{{ asset('assets/front/js/design.js') }}"></script>
+
+<script src="{{ asset('assets/front/js/map.js') }}"></script>
 {{-- <script src="{{ asset('assets/front/js/map.js') }}"></script> --}}
-{{-- <script src="https://cdn.jsdelivr.net/npm/spectrum-colorpicker2/dist/spectrum.min.js"></script> --}}
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script> --}}
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/html-to-image/1.7.0/html-to-image.js" integrity="sha512-uIP4HNFMvnpcVpzoE17SkA3zrdBkNZy2G6t65q2m44kDBHd6emnvYOuXxtPdudQOsCMxO0HuhBnYm1ao9GvJWw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script> --}}
+<script src="{{ asset('assets/front/js/contact_us.js') }}"></script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/spectrum-colorpicker2/dist/spectrum.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html-to-image/1.7.0/html-to-image.js" integrity="sha512-uIP4HNFMvnpcVpzoE17SkA3zrdBkNZy2G6t65q2m44kDBHd6emnvYOuXxtPdudQOsCMxO0HuhBnYm1ao9GvJWw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
+
+<script src="{{ asset('assets/front/js/dom-to-image.min.js') }}"></script>
+
+
 {{-- <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script> --}}
+
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
