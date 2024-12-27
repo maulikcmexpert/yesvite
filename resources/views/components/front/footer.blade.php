751d688e0d9f84a793cf84b2ca9b9ae08dd08399
Vimal <vimal.cmexpertise@gmail.com>
2024-12-27 15:03:36 +0530

    cmt

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -1,5 +1,5 @@
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="{{ asset('assets/front/js/jquery.min.js') }}"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" stylesheet.crossOrigin = "anonymous"></script>
 
