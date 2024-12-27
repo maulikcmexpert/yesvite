41bb1a980e916f1b39777d3bec7e4bdabbe157a1
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-27 16:39:07 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -10627,7 +10627,7 @@ class ApiControllerv2 extends Controller
                 // ->whereHas('contact_sync', function ($query) {
                 // $query->where('app_user', '1');
                 // })
-            ->where('is_co_host','0')
+            ->where('is_co_host','1')
             ->where(['event_id' => $eventDetail->id, 'invitation_sent' => '1'])->get();
 
             $selectedFilters = $request->input('filters');
