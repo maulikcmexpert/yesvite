e31d27f38d89b3337ab87588ae30014ea287679c
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-27 19:50:01 +0530

    Update RsvpController.php

--- app/Http/Controllers/RsvpController.php
+++ app/Http/Controllers/RsvpController.php
@@ -367,7 +367,7 @@ class RsvpController extends Controller
     public function store(Request $request)
     {
 
-        dd($request);
+        // dd($request);
         $userId = decrypt($request->user_id);
         $eventId = decrypt($request->event_id);
 
