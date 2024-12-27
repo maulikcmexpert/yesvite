a832c4e03eabf8fe448914055ac1f83f875509b0
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-27 12:11:44 +0530

    Update RsvpController.php

--- app/Http/Controllers/RsvpController.php
+++ app/Http/Controllers/RsvpController.php
@@ -343,6 +343,7 @@ class RsvpController extends Controller
         
                     return response()->json(['status' => 0, 'message' => "db error"]);
                 } catch (\Exception $e) {
+                    dd($e);
                     return response()->json(['status' => 0, 'message' => 'something went wrong']);
                 }        
             
