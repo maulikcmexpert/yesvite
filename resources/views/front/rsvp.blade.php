1970c277a9e40cc35773e9ac94140ab8185b0516
Prakashmanat01 <prakashhmanat@gmail.com>
2024-12-30 19:43:46 +0530

    Update rsvp.blade.php

--- resources/views/front/rsvp.blade.php
+++ resources/views/front/rsvp.blade.php
@@ -584,7 +584,7 @@
                                     <div class="rsvp-msgbox">
                                         <h5>Message</h5>
                                         <div class="input-form">
-                                            <textarea name="" id="" class="form-control inputText" id="message_to_host" name="message_to_host" message="">Thanks for create this event! so exited!</textarea>
+                                            <textarea name="" id="" class="form-control inputText" id="message_to_host" name="message_to_host" message=""></textarea>
                                             <label for="Fname" class="form-label input-field floating-label">Message with your RSVP</label>
                                         </div>
                                         <div class="d-flex align-items-center">
@@ -674,7 +674,7 @@
                             <div class="rsvp-msgbox">
                                 <h5>Message</h5>
                                 <div class="input-form">
-                                    <textarea name="" id="" class="form-control inputText" id="message_to_host" name="message_to_host" required="">Thanks for create this event! so exited!</textarea>
+                                    <textarea name="" id="" class="form-control inputText" id="message_to_host" name="message_to_host" required=""></textarea>
                                     <label for="Fname" class="form-label input-field floating-label">Message with your RSVP</label>
                                 </div>
                                 <div class="d-flex align-items-center">
