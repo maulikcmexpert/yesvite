88a0e5fc2571ee3bc3a00b3d2077bbb13cc3c253
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-02 17:35:48 +0530

    Update contact.blade.php

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -73,6 +73,23 @@
                                 </div>
                                 @endif
                                 <div class="user-contacts" id="yesviteUser">
+                                    <div class="design-seting">
+                                        <a href="#" class="d-flex">
+                                            <span>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M10.0002 13.2797L5.65355 8.93306C5.14022 8.41973 5.14022 7.57973 5.65355 7.06639L10.0002 2.71973" stroke="#0F172A" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                </svg>
+                                            </span>
+                                            <h5 class="ms-2 li_design">Edit Design</h5>
+                                        </a>
+                                        <a href="#" class="d-flex" id="next_setting">
+                                            <h5 class="me-2">Next: Settings</h5>
+                                            <span><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M5.93994 13.2797L10.2866 8.93306C10.7999 8.41973 10.7999 7.57973 10.2866 7.06639L5.93994 2.71973" stroke="#0F172A" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                </svg>
+                                            </span>
+                                        </a>
+                                    </div>
                                     @include('front.ajax_contacts', ['yesvite_user' => $yesvite_user])
                                 </div>
                             </div>
