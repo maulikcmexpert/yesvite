cf0e9e196dbd64fc8ac77de5e1933fc9da2c9455
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-02 11:46:44 +0530

    Update RsvpController.php

--- app/Http/Controllers/RsvpController.php
+++ app/Http/Controllers/RsvpController.php
@@ -466,23 +466,24 @@ class RsvpController extends Controller
                 $creatEventPost->save();
                 // dd($creatEventPost);
             }
+        //     if(!empty($request->input('notifications')) &&$request->input('notifications')[0]=="1"){
+        //         if($request->rsvp_status=="1"){      
+        //             if($sync_id!=""||$sync_id!=null){
+        //                 $updateUser = EventInvitedUser::where(['user_id' => $userId,'sync_id'=>$sync_id,'is_co_host'=>'0','event_id' => $eventId])->first();
+        //                 if ($updateUser != null) {
+        //                     $updateUser->notification_on_off = "1";
+        //                     $updateUser->save();
+        //                 }
 
-            if($request->rsvp_status=="1"){      
-                if($sync_id!=""||$sync_id!=null){
-                    $updateUser = EventInvitedUser::where(['user_id' => $userId,'sync_id'=>$sync_id,'is_co_host'=>'0','event_id' => $eventId])->first();
-                    if ($updateUser != null) {
-                        $updateUser->notification_on_off = "1";
-                        $updateUser->save();
-                    }
-
-                }else{
-                    $updateUser = EventInvitedUser::where(['user_id' => $userId,'is_co_host'=>'0','event_id' => $eventId])->first();
-                    if ($updateUser != null) {
-                        $updateUser->notification_on_off = "1";
-                        $updateUser->save();
-                    }
-                }
-            }
+        //             }else{
+        //                 $updateUser = EventInvitedUser::where(['user_id' => $userId,'is_co_host'=>'0','event_id' => $eventId])->first();
+        //                 if ($updateUser != null) {
+        //                     $updateUser->notification_on_off = "1";
+        //                     $updateUser->save();
+        //                 }
+        //             }
+        //         }
+        // }
 
             // if($request->rsvp_status=="0"){
 
