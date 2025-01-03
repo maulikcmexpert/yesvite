3c78106064d45e54a4293e40371f82708807c5cd
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-03 18:22:06 +0530

    Update script.js

--- public/assets/front/js/script.js
+++ public/assets/front/js/script.js
@@ -67,19 +67,19 @@ function progressBar(elem){
 }
 
 // rsvp-slider
-$('.owl-carousel').owlCarousel({
-  loop:false,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
-  }
-})
+// $('.owl-carousel').owlCarousel({
+//   loop:false,
+//   margin:10,
+//   nav:true,
+//   responsive:{
+//       0:{
+//           items:1
+//       },
+//       600:{
+//           items:1
+//       },
+//       1000:{
+//           items:1
+//       }
+//   }
+// })
