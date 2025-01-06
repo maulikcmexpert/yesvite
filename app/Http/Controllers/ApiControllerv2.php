8d8df626f7a42ef496a2f7c7aa009198b9762259
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-06 11:31:11 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7371,6 +7371,7 @@ class ApiControllerv2 extends Controller
             $eventDetails['is_host'] = ($eventDetail->user_id == $user->id) ? 1 : 0;
 
             $isCoHost =  EventInvitedUser::where(['event_id' => $input['event_id'], 'user_id' => $user->id])->first();
+            dd($isCoHost);
             $eventDetails['is_co_host'] = (isset($isCoHost)&&$isCoHost->is_co_host!="")?$isCoHost->is_co_host:"0";
 
             $eventDetails['event_timezone'] = (isset($eventDetail->rsvp_start_timezone) && $eventDetail->rsvp_start_timezone != '')?$eventDetail->rsvp_start_timezone:'';
