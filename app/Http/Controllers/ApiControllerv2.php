f8f46835afc6ae54a781594a374f77adb998abb7
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-06 11:32:48 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7370,7 +7370,7 @@ class ApiControllerv2 extends Controller
             $eventDetails['hosted_by'] = $eventDetail->hosted_by;
             $eventDetails['is_host'] = ($eventDetail->user_id == $user->id) ? 1 : 0;
 
-            $isCoHost =  EventInvitedUser::where(['event_id' => $input['event_id'], 'user_id' => $user->id])->first();
+            $isCoHost =  EventInvitedUser::where(['event_id' => $input['event_id'], 'user_id' => $user->id,'is_co_host'=>'1'])->first();
             dd($isCoHost);
             $eventDetails['is_co_host'] = (isset($isCoHost)&&$isCoHost->is_co_host!="")?$isCoHost->is_co_host:"0";
 
