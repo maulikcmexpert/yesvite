d61e8bb0e445c40ca8093df13acd305270c6fc6d
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-07 18:33:51 +0530

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -318,6 +318,7 @@ class ContactController extends Controller
             // return view('front.ajax_phones', compact('yesvite_phone'))->render();
 
             if($searchPhone!=''){
+                dd(1);
                 return response()->json([
                     'view' => view('front.ajax_phones', compact('yesvite_phone'))->render(),
                     'search' =>'1',
