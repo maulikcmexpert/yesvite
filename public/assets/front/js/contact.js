0e13b22433561ace7b727dad95d79f4c5ef30627
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-08 16:26:39 +0530

    Update contact.js

--- public/assets/front/js/contact.js
+++ public/assets/front/js/contact.js
@@ -589,8 +589,8 @@ $(document).on("keyup", ".search_phone", function () {
                 console.log(output);
 
                 if (output.status == 1) {
-                    alert();
                     removeLoadeerHandle("#save_contact", "Save Contact");
+                    alert();
                     if(output.user!=""||output.user!=undefined){
                         $("#Fname").val(output.user.firstname);
                         $("#Lname").val(output.user.lastname);
