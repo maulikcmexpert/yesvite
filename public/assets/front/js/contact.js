6d7e38afab2b28650261702261a07320cf232cd5
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-08 15:39:54 +0530

    Update contact.js

--- public/assets/front/js/contact.js
+++ public/assets/front/js/contact.js
@@ -447,7 +447,7 @@ $(document).on("keyup", ".search_phone", function () {
                     $("#phone_number").val(output.edit.phone_number);
                     $("#edit_id").val(output.edit.id);
                     $("#is_phone_contact").val(0);
-                    // $(this).datas('')
+                    $(this).attr("data-is_phone_contact", "0");
                 }
             },
         });
