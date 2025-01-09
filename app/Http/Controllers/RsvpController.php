ebb750901b599b78f6694b6798ddef4e8802fddf
Vimal <vimal.cmexpertise@gmail.com>
2025-01-09 19:04:29 +0530

    Update RsvpController.php

--- app/Http/Controllers/RsvpController.php
+++ app/Http/Controllers/RsvpController.php
@@ -565,8 +565,8 @@ class RsvpController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request);
-        
+        // dd($request);
+
         $userId = decrypt($request->user_id);
         // dd($userId);
         $eventId = decrypt($request->event_id);
