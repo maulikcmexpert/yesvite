9518092e0dd3184182b7f4b947b3850e8a379745
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-10 13:57:26 +0530

    Update bodyfooter.blade.php

--- resources/views/components/front/bodyfooter.blade.php
+++ resources/views/components/front/bodyfooter.blade.php
@@ -151,10 +151,10 @@
               </ul>
               <div class="app-store d-flex gap-2">
 
-                  <a href="{{isset($getSocialLink->playstore_link) && $getSocialLink->playstore_link != null ? $getSocialLink->playstore_link : "#"}}" class="google-app">
+                  <a href="{{isset($getSocialLink->playstore_link) && $getSocialLink->playstore_link != null ? $getSocialLink->playstore_link : "#"}}" class="google-app" target="_blank">
                       <img src="{{asset('assets/front/image/google-app.png')}}" alt="google-app">
                   </a>
-                  <a href="{{isset($getSocialLink->appstore_link) && $getSocialLink->appstore_link !=null ? $getSocialLink->appstore_link : "#"}}" class="mobile-app">
+                  <a href="{{isset($getSocialLink->appstore_link) && $getSocialLink->appstore_link !=null ? $getSocialLink->appstore_link : "#"}}" class="mobile-app" target="_blank">
                       <img src="{{asset('assets/front/image/mobile-app.png')}}" alt="mobile-app">
                   </a>
               </div>
