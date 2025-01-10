0d1493094169fa058220d9c4424248eea9841969
Vimal <vimal.cmexpertise@gmail.com>
2025-01-10 18:04:10 +0530

    Update rsvp.blade.php

--- resources/views/front/rsvp.blade.php
+++ resources/views/front/rsvp.blade.php
@@ -1891,7 +1891,7 @@
                                 @endif
                               </div>
                             @if($guest_data['rsvp_status']=="1")
-                            <div class="sucess-yes"  data-bs-toggle="modal" data-bs-target="{{$no_model}}">
+                            <div class="sucess-yes"  data-bs-toggle="modal" data-bs-target="{{$no_modal}}">
                             <h5 class="green">RSVP'd YES</h5>
                             <div class="sucesss-cat ms-auto">
                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
