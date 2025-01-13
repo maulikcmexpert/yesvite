755c5fc944fab6a0058988268d2ddd5919dd48bb
Prakashmanat01 <prakashhmanat@gmail.com>
2025-01-13 17:22:13 +0530

    Update rsvp.js

--- public/assets/front/js/rsvp.js
+++ public/assets/front/js/rsvp.js
@@ -360,9 +360,9 @@ $('#rsvp-yes-modal').on('show.bs.modal', function () {
   }
 });
 $('#rsvp-yes-modal').on('hidden.bs.modal', function () {
-    $('#firstname').val('').rules('remove');
-    $('#lastname').val('').rules('remove');
-    
+    $('#lastnameErrorLabel').text('');
+    $('#firstnameErrorLabel').text('');
+
     $('#firstname').val(initialFormData['firstname']);
     $('#lastname').val(initialFormData['lastname']);
   });
