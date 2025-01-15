0f669a938949af68392c3d2811ef19e42b4e29d2
Vimal <vimal.cmexpertise@gmail.com>
2025-01-15 16:26:04 +0530

    update code

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -4727,7 +4727,7 @@ $(document).on("change", 'input[name="guest_list[]"]', function () {
 });
 
 $(document).on("click",".remove_co_host",function(){
-    lengtUSer=0
+    lengtUSer=0;
     var hostId = $(this).data('id');
     eventData.co_host = '';
     eventData.co_host_prefer_by = '';
@@ -4741,7 +4741,7 @@ $(document).on("click",".remove_co_host",function(){
             </span>
             <h5>Select your co-host</h5>`);
     var delete_co_host = $('#remove_co_host_id').val();
-  
+  alert(delete_co_host);
     $('.'+delete_co_host).prop("checked", false);
 })
 
