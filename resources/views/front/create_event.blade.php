a2cbfe4b810fe4ea0b7c8090de49dcaca8268c74
Vimal <vimal.cmexpertise@gmail.com>
2025-01-18 14:03:48 +0530

    Update create_event.blade.php

--- resources/views/front/create_event.blade.php
+++ resources/views/front/create_event.blade.php
@@ -2376,14 +2376,13 @@
 
 
 <input type="hidden" id="eventEditId" value="{{$eventDetail['eventeditId']}}">
-{{$thankuCardId =  (isset($eventDetail['greeting_card_list']) &&   count($eventDetail['greeting_card_list'])>0) ? $eventDetail['greeting_card_list'][0]:""}}
-@php
-    
-    $cohostId =  (isset($eventDetail['co_host_list']) &&   count($eventDetail['co_host_list'])>0) ? $eventDetail['co_host_list'][0]['id']:""
-    $app_user =  (isset($eventDetail['co_host_list']) &&   count($eventDetail['co_host_list'])>0) ? $eventDetail['co_host_list'][0]['app_user']:""
-@endphp
+{{
+$thankuCardId =  (isset($eventDetail['greeting_card_list']) &&   count($eventDetail['greeting_card_list'])>0) ? $eventDetail['greeting_card_list'][0]:"";
 
-<input type="hidden" id="app_user" value="{{$app_user}}">
+    $cohostId =  (isset($eventDetail['co_host_list']) &&   count($eventDetail['co_host_list'])>0) ? $eventDetail['co_host_list'][0]['id']:"";
+    $app_user =  (isset($eventDetail['co_host_list']) &&   count($eventDetail['co_host_list'])>0) ? $eventDetail['co_host_list'][0]['app_user']:"";
+}}
+<input type="hidden" id="app_user" value="{{$cohostId}}">
 <input type="hidden" id="cohostId" value="{{$cohostId}}">
 <input type="hidden" id="thankuCardId" value="{{$thankuCardId}}">
 <input type="hidden" id="inviteTotalCount" value="{{$eventDetail['inviteCount']}}">
