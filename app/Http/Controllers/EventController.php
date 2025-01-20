66321593f5ab5e1189fce1edb2b334a0e9b084e9
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-20 14:06:44 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -532,7 +532,7 @@ class EventController extends Controller
         $event_creation->hosted_by = (isset($request->hosted_by) && $request->hosted_by) ? $request->hosted_by : "";
         $event_creation->start_date = (isset($startDate) && $startDate != "") ? $startDateFormat : null;
         $event_creation->end_date = (isset($endDate) && $endDate != "") ? $endDateFormat : null;
-        $event_creation->rsvp_by_date_set = (isset($request->rsvp_by_date_set) && $request->rsvp_by_date_set != "") ? $request->rsvp_by_date_set : "0";
+        $event_creation->rsvp_by_date_set = (isset($rsvp_by_date_set) && $rsvp_by_date_set != "") ? $rsvp_by_date_set : "0";
         $event_creation->rsvp_by_date = (isset($rsvp_by_date) && $rsvp_by_date != "") ? $rsvp_by_date : null;
         $event_creation->rsvp_start_time = (isset($request->start_time) && $request->start_time != "") ? $request->start_time : "";
         $event_creation->rsvp_start_timezone = (isset($request->rsvp_start_timezone) && $request->rsvp_start_timezone != "") ? $request->rsvp_start_timezone : "";
