56b01087a7b18f448d3a29541064be968938440c
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-20 16:15:02 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -4909,12 +4909,12 @@ $(document).on("change", 'input[name="guest_list[]"]', function () {
                 $('.selected-co-host-image').attr('src',profilePhoto);
                 $('.selected-host-h5').css('display','none');
             }else{
-                $('.guest-img').html(profilePhoto);
+                // $('.guest-img').html(profilePhoto);
                 $('.selected-host-h5').show();
                 $('.selected-co-host-image').css('display','none');
-                // $('.selected-host-h5').text(initial);
-                // var firstinitial = initial.charAt(0);
-                // $('.selected-host-h5').addClass('fontcolor'+firstinitial);
+                $('.selected-host-h5').text(initial);
+                var firstinitial = initial.charAt(0);
+                $('.selected-host-h5').addClass('fontcolor'+firstinitial);
             }
             $('.remove_co_host').attr('data-id',selected_co_host);
             $('#remove_co_host_id').val('user-'+selected_co_host);
