3248e902a5233d825306ba027cb2f09cee7f575d
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-20 20:51:39 +0530

    Update view_gift_registry.blade.php

--- resources/views/front/event/gift_registry/view_gift_registry.blade.php
+++ resources/views/front/event/gift_registry/view_gift_registry.blade.php
@@ -3,17 +3,6 @@
 @foreach ($registry as $data )
 <div class="d-flex align-items-center justify-content-center">
     <span class="me-2">
-        <!-- @if (strpos($data['registry_link'], 'amazon.com') !== false) 
-            <a href="{{ $data['registry_link'] }}">
-                <img src="{{ asset('assets/amazon.jpg') }}" alt="eventpic" style="max-width: 145px;">
-            </a>
-        @endif
-        @if (strpos($data['registry_link'], 'target.com') !== false) 
-            <a href="{{ $data['registry_link'] }}">
-                <img src="{{ asset('assets/target.jpg') }}" alt="eventpic" style="max-width: 145px;">
-            </a>
-        @endif -->
-
          @php
                                                     $url=$data['registry_link'];
                                                     $logo="";
