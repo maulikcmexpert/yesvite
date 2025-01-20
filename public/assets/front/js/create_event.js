b83171eab6e325a31e3f60c5e85f0a6ce3ceda3d
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-20 20:36:14 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -6025,17 +6025,17 @@ var cohostoffset=0;
 var cohostNoMoreData = false; 
 
 $(document).on('click','.add_co_host',function(){
-    var cur_status=$(this).data('contact');
-       console.log(cur_status);
-   if(cur_status=="contacts"){
-        $('#phone-tab-cantact').addClass('active');
-        $('.add_co_host_tab').removeClass('active');
-        get_phone_host_list(null,cohostphoneLimit,cohostphoneOffset,false);
-        setTimeout(() => {
-            toggleSidebar('sidebar_add_co_host');
-        }, 500);
-        return;
-   }
+//     var cur_status=$(this).data('contact');
+//        console.log(cur_status);
+//    if(cur_status=="contacts"){
+//         $('#phone-tab-cantact').addClass('active');
+//         $('.add_co_host_tab').removeClass('active');
+//         get_phone_host_list(null,cohostphoneLimit,cohostphoneOffset,false);
+//         setTimeout(() => {
+//             toggleSidebar('sidebar_add_co_host');
+//         }, 500);
+//         return;
+//    }
     isPhonecontact = 0;
     if(selected_co_host!=""){
         lengtUSer= 1;
@@ -6290,6 +6290,48 @@ function get_phone_host_list(search_name=null,limit,offset,scroll){
     if (checkedCheckbox.length > 0) {
         checkedCheckbox.prop('checked', false);  // Uncheck all checked checkboxes
     }
+
+    $('input[name="guest_list[]"]:checked').each(function () {
+        var profilePhoto = $(this).data('profile');
+        var user_name = $(this).data('username');
+        var profile_or_text = $(this).data("profile_or_text");
+        var initial = $(this).data("initial");
+        var prefer_by_email = $(this).data('prefer_by');
+    
+        // Log or process the data
+        console.log("Profile Photo:", profilePhoto);
+        console.log("User Name:", user_name);
+        console.log("Profile or Text:", profile_or_text);
+        console.log("Initial:", initial);
+        console.log("Prefer By Email:", prefer_by_email);
+    
+        // Update UI based on the `profile_or_text` condition
+        if (profile_or_text == '1') {
+            $('.guest-img .selected-co-host-image').show();
+            $('.guest-img .selected-co-host-image').attr('src', profilePhoto);
+            $('.guest-img .selected-host-h5').css('display', 'none');
+        } else {
+            // $('.guest-img').html(profilePhoto    );
+            $('.selected-host-h5').show();
+            $('.selected-co-host-image').css('display', 'none');
+            $('.guest-img .selected-host-h5').text(initial);
+            var firstinitial = initial.charAt(0);
+
+            // $('.selected-host-h5').removeClass(function (index, className) {
+            //     return (className.match(/\bfontcolor\S+/g) || []).join(' ');
+            // });
+            // $('.selected-host-h5').addClass('fontcolor' + firstinitial);
+
+            $('.guest-img .selected-host-h5').removeClass(function (index, className) {
+                return (className.match(/\bfontcolor\S+/g) || []).join(' ');
+            });
+        
+            // Add the new class
+            $('.guest-img .selected-host-h5').addClass('fontcolor' + firstinitial);
+        }
+        $('.selected-host-name').text(user_name);
+    });
+
     $.ajax({
         url: base_url+'event/getPhoneContact',
         type: "GET",
