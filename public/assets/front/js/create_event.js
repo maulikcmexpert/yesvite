cb8f091e369d83627e8e00b18607faba68f42c25
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-20 17:48:23 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -6009,7 +6009,23 @@ $(document).on('click','.add_co_host',function(){
     $('#phone-tab-cantact').removeClass('active');
     get_co_host_list(null,cohostlimit,cohostoffset,false);
     $('#select_event_cohost').css('display','block');  
-
+    $('.list_all_invited_user').css('display','block');
+        $('.list_all_contact_user').css('display','none');
+        cohostNoMoreData = false;
+        cohostphoneOffset=0;
+        cohostphoneLimit=10;
+      if(co_host_is_selected_close==true){
+                $('.guest-contacts-wrp').css('display','none');
+                $('.guest-contacts-wrp').removeClass('guest-contacts-test');   
+                var checkedCheckbox = $('input[name="guest_list[]"]:checked');
+    
+                if (checkedCheckbox.length > 0) {
+                    checkedCheckbox.prop('checked', false);  // Uncheck all checked checkboxes
+                }
+            }else{
+                $('.guest-contacts-wrp').css('display','flex');
+                $('.guest-contacts-wrp').addClass('guest-contacts-test');
+            }
     setTimeout(() => {
         toggleSidebar('sidebar_add_co_host');
     }, 500);
