2933475730b6f81f0849f7c5e2961d9d3a822110
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-21 14:37:47 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -5742,18 +5742,18 @@ $(document).on('click','.free_plan',function () {
     //         gr_id: gr_id,
     //     });
     // }
-    $('input[name="gift_registry[]"]:checked').each(function() {
-        var registry_name = $(this).data('item');
-        var registry_link = $(this).data('registry');
-        var gr_id = $(this).val();
-        selected_gift.push({
-            registry_name: registry_name,
-            registry_link: registry_link,
-            gr_id: gr_id,
-        });
-    });
-    eventData.gift_registry_data=selected_gift;
-    console.log(eventData);
+    // $('input[name="gift_registry[]"]:checked').each(function() {
+    //     var registry_name = $(this).data('item');
+    //     var registry_link = $(this).data('registry');
+    //     var gr_id = $(this).val();
+    //     selected_gift.push({
+    //         registry_name: registry_name,
+    //         registry_link: registry_link,
+    //         gr_id: gr_id,
+    //     });
+    // });
+    // eventData.gift_registry_data=selected_gift;
+    // console.log(eventData);
 
     var selected = $('input[name="gift_registry[]"]:checked');
         if (selected.length > 2) {
