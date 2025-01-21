8157d181a9efc685aa16fee60c32a7b785930367
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-21 13:00:07 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -6511,7 +6511,7 @@ $("#select_event_cohost").on("scroll", function () {
         //     var scroll=true;
         //     get_co_host_list(search_name=null,cohostlimit,cohostoffset,scroll);
         //     }
-        if (scrollTop > previousScrollTop) {
+        // if (scrollTop > previousScrollTop) {
         if (scrollTop + elementHeight >= scrollHeight) { 
            
             
@@ -6523,8 +6523,8 @@ $("#select_event_cohost").on("scroll", function () {
                 get_co_host_list(search_name = null, cohostlimit, cohostoffset, scroll);
             }
         }
-    }
-    previousScrollTop = scrollTop;
+    // }
+    // previousScrollTop = scrollTop;
 });
 
 // var cohostphoneOffset=0;
