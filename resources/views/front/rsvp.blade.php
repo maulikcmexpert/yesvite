6bb83f4ea4507662a38cbd3410b4e05ee13f7875
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-21 20:42:56 +0530

    Update rsvp.blade.php

--- resources/views/front/rsvp.blade.php
+++ resources/views/front/rsvp.blade.php
@@ -818,10 +818,11 @@
                                                         </div>
                                                         <div class="shedule-box red">
                                                             <div class="shedule-box-left">
-                                                                <h6>{{$schedule['activity_title']}}</h6>
                                                                 @if($schedule['end_time'])
+                                                                <h6>{{$schedule['activity_title']}}</h6>
                                                                 <span>{{$schedule['end_time']}}</span>
-                                                                @endif                                                            </div>
+                                                                @endif                                    
+                                                                                    </div>
                                                             <span class="hrs ms-auto">{{$schedule['total_time']}}</span>
                                                         </div>
                                                         </div>
