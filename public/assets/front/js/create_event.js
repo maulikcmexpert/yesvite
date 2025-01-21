ffda545bf6f5adf1f72548670f4b77080ca5258d
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-21 20:24:31 +0530

    commit

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -3027,6 +3027,7 @@ function savePage1Data(close = null) {
     var rsvp_by_date = '';
     if(rsvp_by_date_set){
         rsvp_by_date = $('#rsvp-by-date').val();
+        rsvp_by_date_set='1';
         if (rsvp_by_date == "") {
             $("#event-rsvpby-error")
                 .css("display", "block")
