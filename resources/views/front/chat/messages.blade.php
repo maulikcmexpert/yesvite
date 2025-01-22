378cd761e656dd1e49782d240ee91cb8fb83fa8a
cmexpertise <pratik.cmexpertise@gmail.com>
2025-01-22 17:30:41 +0530

    update profile group

--- resources/views/front/chat/messages.blade.php
+++ resources/views/front/chat/messages.blade.php
@@ -826,7 +826,7 @@ use Carbon\Carbon;
                                 <input type="file" id="choose-file" name="choose-file" accept="image/*" />
                                 <div class="position-relative">
                                     <div class="cover-img user-cover-img" id="cover-img">
-                                        <img src="./assets/image/user-img.svg" alt="cover-img" id="profileIm">
+                                        <img src="./assets/image/user-img.svg" alt="cover-img" id="profileModel">
                                     </div>
                                     <label class="choosen-file  edit-btn mb-0" for="choose-file">
                                         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
