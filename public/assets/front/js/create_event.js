78a07ce66f36cf1a88a263a7ed487d886f9187bd
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-22 10:11:47 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -4176,6 +4176,16 @@ $(document).on("click", ".li_setting", function () {
     var state = $("#state").val();
     var zipcode = $("#zipcode").val();
 
+
+    var activity=$('.event_all_activity_list').length;
+    console.log(activity);
+    if($('#schedule').is(":checked")){
+        if(activity==0){
+            toastr.error('Event Schedule: Please set event schedule');
+            return;  
+        }
+    }
+    
     if(event_name==""){
         toastr.error('Please enter event name');
         return;
