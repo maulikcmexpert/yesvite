22d628915521b202e37c5e6eb585803317cfaa1f
Vimal <vimal.cmexpertise@gmail.com>
2025-01-22 19:23:23 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2658,7 +2658,7 @@ class EventController extends BaseController
     public function  editStore(Request $request)
     {
         // $potluck = session('category');
-        // dd($request);
+        dd($request);
 
         $user_id =  Auth::guard('web')->user()->id;
         $dateString = (isset($request->event_date)) ? $request->event_date : "";
