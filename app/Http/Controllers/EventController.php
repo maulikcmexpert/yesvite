26aad1a7683698970087ebe1eb08bd8b5b3ef3b7
Vimal <vimal.cmexpertise@gmail.com>
2025-01-22 18:37:02 +0530

    update code

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2733,7 +2733,7 @@ class EventController extends BaseController
         $event_creation->hosted_by = (isset($request->hosted_by) && $request->hosted_by) ? $request->hosted_by : "";
         $event_creation->start_date = (isset($startDate) && $startDate != "") ? $startDateFormat : null;
         $event_creation->end_date = (isset($endDate) && $endDate != "") ? $endDateFormat : null;
-        $event_creation->rsvp_by_date_set = (isset($request->rsvp_by_date_set) && $request->rsvp_by_date_set != "" || !$request->rsvp_by_date_set!=false) ? "1" : "0";
+        $event_creation->rsvp_by_date_set = (isset($request->rsvp_by_date_set) && $request->rsvp_by_date_set != "" || $request->rsvp_by_date_set!=false) ? "1" : "0";
         dd($event_creation->rsvp_by_date_set);
         $event_creation->rsvp_by_date = (isset($rsvp_by_date) && $rsvp_by_date != "") ? $rsvp_by_date : null;
         $event_creation->rsvp_start_time = (isset($request->start_time) && $request->start_time != "") ? $request->start_time : "";
