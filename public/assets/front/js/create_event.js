c8a32e6d8f740dd0de9b05238994e5a4d5f4cbfe
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-23 20:10:42 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -701,12 +701,15 @@ function datepicker() {
         useCurrent: false,
         ignoreReadonly: true,
         stepping: 15,
-        // defaultDate: getClosest15MinuteTime(), // Set the closest 15-minute time as the default
+        defaultDate: getClosest15MinuteTime(), // Set the closest 15-minute time as the default
 
         // Set stepping to 15 minutes
         // defaultDate: now
         //  debug: true
-    });
+    }).on('dp.show', function () {
+        // Set the initial time in the picker on show
+        $(this).data("DateTimePicker").date(getClosest15MinuteTime()); 
+    });;
 }
 $(".timepicker").on("dp.show", function () {
     $(this).val(""); // Clear the input when the picker is shown
