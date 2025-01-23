84fd901133e61cb6e5c2a581f6f49ed418860c87
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-23 14:54:17 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -474,7 +474,7 @@ $(document).on("click", ".edit_design_tem", function(e) {
     //console.log(json);
     var id = $(this).data("id");
 
-    $('.photo-slider-wrp').attr('data-id',id);
+    $('.photo-slider-wrp').attr('data-id',id);  
     if (eventData.textData != null && eventData.temp_id != null && eventData.temp_id == id) {
         dbJson = eventData.textData;
     } else {
