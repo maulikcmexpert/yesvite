11620e322f5fa2df7ba569654e86c8eafa02898c
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-23 17:52:09 +0530

    Update final_checkout.blade.php

--- resources/views/front/event/final_checkout.blade.php
+++ resources/views/front/event/final_checkout.blade.php
@@ -32,12 +32,13 @@
                             </div> --}}
                         </div>
                     </div>
-                    <div class="owl-carousel owl-theme event_images_slider">
-                        <!-- <div class="item">
+                    <!-- <div class="owl-carousel owl-theme event_images_slider"> -->
+                    <div class="event_images_slider">
+                        <div class="item">
                             <div class="setting-img">
                                 <img id="eventImage" src="" />
                             </div>
-                        </div> -->
+                        </div>
 
                         <!-- <div class="item">
                             <div class="setting-img">
