9ec0db23913a02af5301bd8a89e99a35f79fd7e9
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-24 18:06:06 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -700,7 +700,11 @@ function datepicker() {
 
         // Set the picker to the closest 15-minute time dynamically
         picker.date(closest15MinTime); 
-    });
+    }).on('dp.hide', function (e) {
+        // Automatically set the selected value in the input field when the picker closes
+        const selectedTime = e.date ? e.date.format("LT") : ""; // Format the selected time
+        $(this).val(selectedTime); // Set the formatted time value in the input field
+    });;
 
     // Ensure input field is clear when the page loads
     $(this).val("");
