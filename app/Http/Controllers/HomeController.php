bcaad8b1f509e15e7dced1b026a4ab1093005498
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-24 19:32:00 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -188,7 +188,7 @@ class HomeController extends BaseController
                     $eventDetail['event_potluck'] = isset($value->event_settings->podluck) ? $value->event_settings->podluck : "";
                     $eventDetail['guest_pending_count'] = getGuestRsvpPendingCount($value->id, 1);
                     $eventDetail['adult_only_party'] = isset($value->event_settings->adult_only_party) ? $value->event_settings->adult_only_party : "";
-                    $eventDetail['post_time'] =  $this->setpostTime($value->updated_at);
+                    $eventDetail['post_time'] =  $this->setupcomingpostTime($value->updated_at);
                     $rsvp_status = "";
                     $checkUserrsvp = EventInvitedUser::whereHas('user', function ($query) {
                         $query->where('app_user', '1');
@@ -406,6 +406,32 @@ class HomeController extends BaseController
         $timeAgo = $commentTime->diffForHumans();
         return $timeAgo;
     }
+
+    private function setupcomingpostTime($updatedAt)
+{
+    $now = Carbon::now(); // Current time
+    $updatedTime =Carbon::parse($updatedAt); // Parse the updated_at value
+
+    $diffInDays = $updatedTime->diffInDays($now);
+
+    if ($diffInDays > 0) {
+        return $diffInDays . 'd'; // Return in 'Xd' format
+    }
+
+    $diffInHours = $updatedTime->diffInHours($now);
+
+    if ($diffInHours > 0) {
+        return $diffInHours . 'h'; // Return in 'Xh' format
+    }
+
+    $diffInMinutes = $updatedTime->diffInMinutes($now);
+
+    if ($diffInMinutes > 0) {
+        return $diffInMinutes . 'm'; // Return in 'Xm' format
+    }
+
+    return 'just now'; // For moments less than a minute
+}
 
     function upcomingEventsCount($userId)
     {
