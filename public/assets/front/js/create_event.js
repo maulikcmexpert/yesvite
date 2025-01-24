80a44f82016acd714d0b589a386471f70f08b349
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-24 17:56:22 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -731,6 +731,9 @@ function start_timepicker() {
 
         // Set the picker to the closest 15-minute time dynamically
         picker.date(closest15MinTime); 
+
+        $(".start_timepicker").val("");
+
         
     }).on('dp.hide', function (e) {
         // Automatically set the selected value in the input field when the picker closes
