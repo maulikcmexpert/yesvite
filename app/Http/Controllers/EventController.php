6b73744471a7f096a2dcb046b3a7f9a1191f5a3f
Vimal <vimal.cmexpertise@gmail.com>
2025-01-24 14:31:44 +0530

    update code

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2682,24 +2682,23 @@ class EventController extends BaseController
     public function  editStore(Request $request)
     {
         // $potluck = session('category');
-        dD($request);
-
+        
         $user_id =  Auth::guard('web')->user()->id;
         $dateString = (isset($request->event_date)) ? $request->event_date : "";
-
-
-
+        
+        
+        
         // if (strpos($dateString, ' To ') !== false) {
-        //     list($startDate, $endDate) = explode(' To ', $dateString);
-        // } else {
-        //     $startDate = $dateString;
-        //     $endDate = $dateString;
-        // }
-
-        // $startDateFormat = DateTime::createFromFormat('m-d-Y', $startDate)->format('Y-m-d');
-        // $endDateFormat = DateTime::createFromFormat('m-d-Y', $endDate)->format('Y-m-d');
-        if (strpos($dateString, ' To ') !== false) {
-            list($startDate, $endDate) = explode(' To ', $dateString);
+            //     list($startDate, $endDate) = explode(' To ', $dateString);
+            // } else {
+                //     $startDate = $dateString;
+                //     $endDate = $dateString;
+                // }
+                
+                // $startDateFormat = DateTime::createFromFormat('m-d-Y', $startDate)->format('Y-m-d');
+                // $endDateFormat = DateTime::createFromFormat('m-d-Y', $endDate)->format('Y-m-d');
+                if (strpos($dateString, ' To ') !== false) {
+                    list($startDate, $endDate) = explode(' To ', $dateString);
         } else {
             $startDate = $dateString;
             $endDate = $dateString;
@@ -2707,14 +2706,14 @@ class EventController extends BaseController
 
         $startDateObj = DateTime::createFromFormat('m-d-Y', $startDate);
         $endDateObj = DateTime::createFromFormat('m-d-Y', $endDate);
-
+        
         $startDateFormat = "";
         $endDateFormat = "";
         if ($startDateObj && $endDateObj) {
             $startDateFormat = $startDateObj->format('Y-m-d');
             $endDateFormat = $endDateObj->format('Y-m-d');
         }
-
+        
         if (isset($request->rsvp_by_date) && $request->rsvp_by_date != '') {
             // dd($request->rsvp_by_date);
             $rsvp_by_date = Carbon::parse($request->rsvp_by_date)->format('Y-m-d');
@@ -2722,14 +2721,15 @@ class EventController extends BaseController
             $rsvp_by_date_set = '1';
         } else {
             if ($startDateFormat) {
-
+                
                 $start = new DateTime($startDateFormat);
                 $start->modify('-1 day');
                 $rsvp_by_date = $start->format('Y-m-d');
             }
         }
-
-
+        
+        dD($startDateFormat);
+        
         $greeting_card_id = "";
         if (isset($request->thankyou_message) && $request->thankyou_message == '1') {
             if (isset($request->thank_you_card_id) && $request->thank_you_card_id != '') {
