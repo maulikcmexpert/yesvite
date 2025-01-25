3881201c8472ad7828d13f3d48206a92553af0fa
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-25 13:24:07 +0530

    Update RsvpController.php

--- app/Http/Controllers/RsvpController.php
+++ app/Http/Controllers/RsvpController.php
@@ -617,7 +617,7 @@ class RsvpController extends BaseController
             if ($sync_id != "" || $sync_id != null) {
                 $rsvpSent = EventInvitedUser::whereHas('user', function ($query) {})->where(['user_id' => $userId, 'sync_id' => $sync_id, 'is_co_host' => '0', 'event_id' => $eventId])->first();
             } else {
-                dd(1);
+                // dd(1);
                 $rsvpSent = EventInvitedUser::whereHas('user', function ($query) {
                     $query->where('app_user', '1');
                 })->where(['user_id' => $userId, 'is_co_host' => '0', 'event_id' => $eventId])->first();
