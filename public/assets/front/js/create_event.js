d1e1fade0103d8324269732ff27b404835f0dc1f
Vimal <vimal.cmexpertise@gmail.com>
2025-01-25 15:52:09 +0530

    update code

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -4653,12 +4653,14 @@ function plusBTN(that) {
     var value = parseInt(input.val());
     input.val(value + 1);
     var quantity = parseInt(that.parent().find(".input-qty").val());
+    var innerUserQnt = parseInt(that.parent().find(".innerUserQnt").val());
     if (quantity > 0) {
         that.parent().find(".item-quantity-minus").val(1);
     }
     console.log({ categoryItemQuantity, quantity });
     if (categoryItemQuantity >= quantity) {
         update_self_bring(
+            innerUserQnt,
             categoryItemKey,
             categoryIndexKey,
             quantity,
@@ -4691,11 +4693,13 @@ function minusBTN(that) {
         input.val(value - 1);
     }
     var quantity = parseInt(that.parent().find(".input-qty").val());
+    var innerUserQnt = parseInt(that.parent().find(".innerUserQnt").val());
     console.log({ categoryItemQuantity, quantity });
 
     if (categoryItemQuantity >= quantity) {
         if (itemQuantityMinus == 1) {
             update_self_bring(
+                innerUserQnt,
                 categoryItemKey,
                 categoryIndexKey,
                 quantity,
@@ -4712,6 +4716,7 @@ function minusBTN(that) {
 }
 
 function update_self_bring(
+    innerUserQnt,
     categoryItemKey,
     categoryIndexKey,
     quantity,
@@ -4730,7 +4735,7 @@ function update_self_bring(
         success: function (response) {
             console.log(quantity + "/" + categoryItemQuantity);
             $("#h6-" + categoryItemKey + "-" + categoryIndexKey).text(
-                quantity + "/" + categoryItemQuantity
+                (quantity) + "/" + categoryItemQuantity
             );
 
             $("#missing-category-" + categoryIndexKey).text(response);
@@ -4773,8 +4778,8 @@ function update_self_bring(
                 current_item = current_item - 1;
                 $(".total-self-bring-" + categoryIndexKey).text(current_item);
             }
-
-            if (quantity == categoryItemQuantity) {
+            let updatedQty = innerUserQnt;
+            if (updatedQty <= categoryItemQuantity) {
                 $(
                     "#lumpia-collapseOne" +
                         "-" +
@@ -4853,6 +4858,9 @@ $(document).on("click", ".delete-self-bring", function () {
     var categoryIndexKey = $(this).data("categoryindex");
     var itemquantity = $(this).data("itemquantity");
     var userquantity = $(this).data("userquantity");
+    var innerUserQnt = $(this).data("innerUserQnt");
+
+
 
     $(this).parent().parent().hide();
     var self_bring_quantity = $(this)
@@ -4877,7 +4885,7 @@ $(document).on("click", ".delete-self-bring", function () {
 
     // console.log({categoryItemKey,categoryIndexKey, itemquantity,self_bring_quantity})
     // $(this).parent().closest('.qty-container').find('.input-qty').val(0);
-    update_self_bring(categoryItemKey, categoryIndexKey, userquantity, itemquantity);
+    update_self_bring(innerUserQnt,categoryItemKey, categoryIndexKey, userquantity, itemquantity);
 });
 
 $(document).on("click", ".add-user-list", function () {
