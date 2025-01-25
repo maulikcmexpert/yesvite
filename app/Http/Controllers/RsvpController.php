29b4ddb7a0b4f45b8c6f83501726bed1eff23a83
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-25 12:41:14 +0530

    Update RsvpController.php

--- app/Http/Controllers/RsvpController.php
+++ app/Http/Controllers/RsvpController.php
@@ -152,7 +152,7 @@ class RsvpController extends BaseController
         $user_id= EventInvitedUser::where(['id',$event_invited_user_id])->first()->user_id;
         $sync_id="";
         if($user_id==null||$user_id==""){
-            $user_id= EventInvitedUser::where(['id',$event_invited_user_id])->first()->sync_id;
+            $sync_id= EventInvitedUser::where(['id',$event_invited_user_id])->first()->sync_id;
         }
         try {
             $eventDetail = Event::with(['user', 'event_image', 'event_schedule', 'event_settings', 'event_invited_user' => function ($query) {
