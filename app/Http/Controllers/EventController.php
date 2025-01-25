47aaa960531c726ec4886c64f24c60c0d4c278c5
Vimal <vimal.cmexpertise@gmail.com>
2025-01-25 15:18:17 +0530

    update code

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -398,6 +398,7 @@ class EventController extends BaseController
                         $categoryQuantity = 0;
                         $remainingQnt = 0;
                         $totalItem=0;
+                        $innnerUserItem=0;
                         if (!empty($value->event_potluck_category_item) || $value->event_potluck_category_item != null) {
 
                             $itemData = [];
@@ -437,10 +438,14 @@ class EventController extends BaseController
                                     $potluckItem['item_carry_users'][] = $userPotluckItem;
                                     $itmquantity = $itmquantity +  $itemcarryUser->quantity;
                                     $categoryQuantity = $categoryQuantity + $itemcarryUser->quantity;
+                                    if($itemcarryUser->id !=$id){
+                                        $innnerUserItem =$innnerUserItem + $itemcarryUser->quantity;
+                                    }
                                 }
                                 $totalItem = $totalItem +1; 
                                 $remainingQnt = $remainingQnt + $itemValue->quantity;
                                 $potluckItem['itmquantity'] =  $itmquantity;
+                                $potluckItem['innnerUserItem'] =  $innnerUserItem;
                                 $potluckCategory['items'][] = $potluckItem;
                             }
                         }
@@ -455,6 +460,7 @@ class EventController extends BaseController
                     session()->put('category', $categories);
                     session()->put('category_item', $categories_item);
                     Session::save();
+                    dd($eventDetail['podluck_category_list']);
                 }
             }
         } else {
