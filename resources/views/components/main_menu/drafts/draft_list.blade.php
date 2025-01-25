101de0f6b66c5f0bf662f670ee16ef4e4d5c2cb7
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-25 19:46:49 +0530

    Update draft_list.blade.php

--- resources/views/components/main_menu/drafts/draft_list.blade.php
+++ resources/views/components/main_menu/drafts/draft_list.blade.php
@@ -111,8 +111,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (formattedDate) {
       // Ensure AM/PM is uppercase by replacing only the AM/PM part
-      const finalDate = formattedDate.replace(' at ', ' - ');
-      const finalDate = formattedDate.replace(/\b(am|pm)\b/i, match => match.toUpperCase());
+      // const finalDate = formattedDate.replace(' at ', ' - ');
+      // const finalDate = formattedDate.replace(/\b(am|pm)\b/i, match => match.toUpperCase());
+      const finalDate = formattedDate.replace(' at ', ' - ').replace(/\b(am|pm)\b/i, match => match.toUpperCase());
       saveDateElement.innerHTML = `Last Save: ${finalDate}`;
     } else {
       console.error('Date formatting failed:', savedDate);
