68f26e0abcbdd47dc6a27cb8d11bceabf3b843a5
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-25 10:46:59 +0530

    Update event.js

--- public/assets/front/js/event.js
+++ public/assets/front/js/event.js
@@ -505,7 +505,7 @@ $(document).on('click','#confirm_cancel_event_btn',function () {
         toastr.error("Please Enter CANCEL");
         return;
     }
-    ('#loader').css('display','block');
+    ('#home_loader').css('display','block');
     console.log(event);
     $.ajax({
         url: `${base_url}event/cancel_event`,
@@ -521,7 +521,7 @@ $(document).on('click','#confirm_cancel_event_btn',function () {
                 toastr.success("Event Cancelled successfully");
                 $('#cancelevent').modal('hide');
                 window.location.reload();
-                ('#loader').css('display','block');
+                ('#home_loader').css('display','block');
 
 
             }
@@ -532,7 +532,7 @@ $(document).on('click','#confirm_cancel_event_btn',function () {
             // $('.loader').css('display','none');    
         },
         complete: function () {
-         $('.loader').css('display','none');    
+         $('.home_loader').css('display','none');    
         }
 
     });
