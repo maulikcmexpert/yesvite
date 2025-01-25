d5f7049c9df2a4dfd6e7a98b294899a39790f60e
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-25 11:34:10 +0530

    Update scritp.js

--- public/assets/front/new_js/scritp.js
+++ public/assets/front/new_js/scritp.js
@@ -138,9 +138,8 @@ $("#dropdownButton").on("click", function (event) {
 
 $("#moblie-menu-bar").on("click", function (event) {
     event.stopPropagation(); // Prevent the click event from bubbling up
-    $(".notification-dropdown-menu").toggleClass("hide");
-    if ($(".header-profile-dropdown").hasClass("show")) {
-        $(".header-profile-dropdown").removeClass("show");
+    if ($(".notification-dropdown-menu").hasClass("show")) {
+        $(".notification-dropdown-menu").removeClass("show");
     }
 });
 
