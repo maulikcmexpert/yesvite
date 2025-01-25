9dfb99f24504506a8f4dfbc29cbfe0fffefb95b7
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-25 17:16:13 +0530

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -115,6 +115,7 @@ function getGuestPendingRsvpCount($eventId)
 
 function sendNotification($notificationType, $postData)
 {
+    dd($postData);
     //'invite', 'upload_post', 'like_post', 'comment', 'reply', 'poll', 'rsvp'
     $user  = Auth::guard('api')->user();
 
