4d552bf129fd4b3dbf61f444bd129a2135e16600
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-27 20:12:37 +0530

    Update sidebar.blade.php

--- resources/views/components/front/sidebar.blade.php
+++ resources/views/components/front/sidebar.blade.php
@@ -316,7 +316,7 @@
                         <div>
                           <h4 class="modal-title" id="notification_rsvp_eventName"></h4>
                           <span>Hosted by: <span id="notification_rsvp_host"></span></span>
-                          <input type="hidden" id="rsvp_user_id" />
+                          <input type="hidden" name="rsvp_user_id" id="rsvp_user_id" />
                         </div>
                     </div>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
