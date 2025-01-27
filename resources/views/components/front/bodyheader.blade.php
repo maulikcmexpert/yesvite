3fc96830b24fe2ed9b49de485297c29571ad10d9
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-27 17:14:58 +0530

    Update bodyheader.blade.php

--- resources/views/components/front/bodyheader.blade.php
+++ resources/views/components/front/bodyheader.blade.php
@@ -392,7 +392,56 @@
                                         </button>
                                       </div> --}}
                                     </div>
-                                  </div>          
+                                  </div> 
+                            @elseif($inner_data['notification_type']=="update_time")
+                                  <div class="notification-drodown-body-inner">
+                                    <div class="notification-drodown-body-inner-img">
+                                      @php
+                                                  $initials = strtoupper($inner_data['first_name'][0]) . strtoupper($inner_data['last_name'][0]);
+                                                  $fontColor = "fontcolor" . strtoupper($inner_data['first_name'][0]);
+                                                  $userProfile = "<h5 class='<?= $fontColor ?>' >" . $initials . "</h5>";
+                                      @endphp
+                                      @if($inner_data['profile']!="")
+                                      <img src="{{$inner_data['profile']}}" alt=""loading="lazy" />
+                                     @else
+                                       {!! $userProfile !!}
+                                    <span class="active-dot"></span>
+                                    @endif
+                                      {{-- <span class="active-dot"></span> --}}
+                                    </div>
+                                    <div class="notification-drodown-body-inner-content">
+                                      <div>
+                                        <h3>
+                                          {{-- {{$inner_data['notification_message']}} --}}
+                                          {{$inner_data['first_name']}} {{$inner_data['last_name']}}
+                                          <span> has updated the event time for <span style="font-family: var(--SFProDisplay-Bold);font-size: 14px;line-height:normal;color: #F73C71;">{{$inner_data['event_name']}} </span></span>
+                                        </h3>
+                                        <div>
+                                          <h6 class="notification-time-count">{{$inner_data['post_time']}}</h6>
+                                          @if($inner_data=="0")
+                                          <h6 class="notification-read-dot mt-1 text-right"></h6>
+                                          @endif
+                                        </div>
+                                      </div>
+                                      <div>
+                                        <!-- <p>{{$inner_data['event_name']}}</p> -->
+                                      </div>
+                                      <div class="d-block">
+                                        <h3 class="mb-1">Time : <span style="font-family: var(--SFProDisplay-Regular);">From {{$inner_data['from_time']}} To {{$inner_data['to_time']}}</span></h3>
+                                      </div>
+                                      
+                                      {{-- <div class="notification-accept-invite-btn-wrp">
+                                        <button class="accept-btn">
+                                          <i class="fa-regular fa-circle-check"></i>
+                                          Accept
+                                        </button>
+                                        <button class="decline-btn">
+                                          <i class="fa-regular fa-circle-xmark"></i>
+                                          Decline
+                                        </button>
+                                      </div> --}}
+                                    </div>
+                                  </div>                 
                             @elseif($inner_data['notification_type']=="potluck_bring")
                                   <div class="notification-drodown-body-inner">
                                     <div class="notification-drodown-body-inner-img">
