ab4a22de89b22220234dbe6520660b4c543d09d0
Vimal <vimal.cmexpertise@gmail.com>
2025-01-28 09:32:48 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -1470,6 +1470,7 @@ class EventController extends BaseController
             'category_index' => $category_index,
             'category_item' => --$category_item,
         ];
+        dd($total_item);
         // Dd($data)
         // return view('front.event.potluck.potluckCategoryItem', $data);
 
