1ac605008e28dfac81d90035592ede3fd237cc39
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-28 16:36:14 +0530

    Update search_filter_event.blade.php

--- resources/views/front/notification/search_filter_event.blade.php
+++ resources/views/front/notification/search_filter_event.blade.php
@@ -1,4 +1,5 @@
 @foreach ($eventList as $event)
+{{dd($event)}}
 <div class="form-check">
                       <input class="form-check-input" name="selectedEvents[]" data-event_name="{{$event['name']}}" data-event_id="{{$event['if']}}" type="checkbox" value="" id="flexCheckDefault2">
                       <label class="form-check-label invited_to_chk_lbl" for="flexCheckDefault2">
