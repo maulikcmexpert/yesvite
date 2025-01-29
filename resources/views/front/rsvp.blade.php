74e3906429b69f73c95e3f4222f76a71eed03f72
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-30 00:55:14 +0530

    Update rsvp.blade.php

--- resources/views/front/rsvp.blade.php
+++ resources/views/front/rsvp.blade.php
@@ -1880,15 +1880,12 @@
                                             </div>
                                             <div class="col-lg-12">
                                                 <div class="input-form">
-                                                    @if($sync_contact_user_id!="" && ($user_id=="" && $user_id==null))
-                                                                @if($email!="")
-                                                                    <input type="email"  name="email" id="email" value="{{(isset($email)&&$email!="")?$email:""}}" class="form-control inputText">
-                                                                 @else
+                                                                 @if($email!="")
                                                                     <input type="email"  name="email" id="email" value="{{(isset($email)&&$email!="")?$email:""}}" class="form-control inputText" readonly>
+                                                                 @else
+                                                                    <input type="email"  name="email" id="email" value="{{(isset($email)&&$email!="")?$email:""}}" class="form-control inputText">
                                                                 @endif
-                                                    @else   
-                                                        <input type="email"  name="email" id="email" value="{{(isset($email)&&$email!="")?$email:""}}" class="form-control inputText">
-                                                    @endif
+                                                
                                                     <label for="Fname" class="form-label input-field floating-label">Email Address</label>
                                                 </div>
                                             </div>
