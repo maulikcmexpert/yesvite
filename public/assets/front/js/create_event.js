5f35623dc74a256e0255a9dbc3a3e3343d8c354c
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-29 15:47:50 +0530

    commit

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -3141,6 +3141,7 @@ function convertTo12Hour(time) {
 
 
 $(document).on("click", "#save_activity_schedule", function () {
+    
     var start_time = $("#ac-start-time").val();
     var end_time = $("#ac-end-time").val();
     // checkEndTimes();
