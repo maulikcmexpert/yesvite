ad38b976b66aa4174c1af4909ca8be9cd1e09b37
Vimal <vimal.cmexpertise@gmail.com>
2025-01-29 10:47:58 +0530

    update code

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2811,13 +2811,13 @@ class EventController extends BaseController
         }
 
         if (isset($request->rsvp_by_date) && $request->rsvp_by_date != '') {
-            $carbonDate = Carbon::createFromFormat('Y-m-d', $request->rsvp_by_date);
-            dd($carbonDate);
-            if ($carbonDate && $carbonDate->format('Y-m-d') === $request->rsvp_by_date) {
-                $rsvp_by_date = $request->rsvp_by_date;
-            } else {
+            // $carbonDate = Carbon::createFromFormat('Y-m-d', $request->rsvp_by_date);
+            // dd($carbonDate);
+            // if ($carbonDate && $carbonDate->format('Y-m-d') === $request->rsvp_by_date) {
+            //     $rsvp_by_date = $request->rsvp_by_date;
+            // } else {
                 $rsvp_by_date = DateTime::createFromFormat('m-d-Y', $request->rsvp_by_date)->format('Y-m-d');
-            }
+            // }
             // $rsvp_by_date = Carbon::parse($request->rsvp_by_date)->format('Y-m-d');
            
             $rsvp_by_date_set = '1';
