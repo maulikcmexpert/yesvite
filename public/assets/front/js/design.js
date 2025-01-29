c7c3c4bb434e111ee6a518f101142a4a0e392ccc
cmexpertise <pratik.cmexpertise@gmail.com>
2025-01-29 18:56:27 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -2584,7 +2584,7 @@ $(".edit-design-sidebar").on("click", function () {
     }
 });
 function loadAgain() {
-    $(".side-bar-list").removeClass("active");
+    //  $(".side-bar-list").removeClass("active");
     $(".edit-design-sidebar").addClass("active");
     $("#close_createEvent").css("display", "none");
     // e.preventDefault();
