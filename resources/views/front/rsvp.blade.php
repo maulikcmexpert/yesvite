c92651192fb0accd7c9667ce0e6117143dc2b4d0
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-30 00:28:51 +0530

    Update rsvp.blade.php

--- resources/views/front/rsvp.blade.php
+++ resources/views/front/rsvp.blade.php
@@ -1881,7 +1881,11 @@
                                             </div>
                                             <div class="col-lg-12">
                                                 <div class="input-form">
-                                                    <input type="email"  name="email" id="email" value="{{(isset($email)&&$email!="")?$email:""}}" class="form-control inputText" readonly>
+                                                    @if($sync_contact_user_id!="" && ($user_id!==""|| $user_id==null))
+                                                        <input type="email"  name="email" id="email" value="{{(isset($email)&&$email!="")?$email:""}}" class="form-control inputText" readonly>
+                                                    @else   
+                                                        <input type="email"  name="email" id="email" value="" class="form-control inputText">
+                                                    @endif
                                                     <label for="Fname" class="form-label input-field floating-label">Email Address</label>
                                                 </div>
                                             </div>
