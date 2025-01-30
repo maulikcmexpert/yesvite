0cdb3954456b2821ed30d0ae5bf97e5f7def2e43
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-01-30 19:07:46 +0530

    commit vrushali

--- resources/views/front/event_wall/event_about.blade.php
+++ resources/views/front/event_wall/event_about.blade.php
@@ -3,7 +3,7 @@
 @endphp
 @isset($eventDetails)
     <main class="new-main-content">
-        {{-- {{ dd($eventDetails) }} --}}
+        {{ dd($eventDetails) }}
         <div class="container">
             <div class="row">
                 <div class="col-xl-3 col-lg-4">
