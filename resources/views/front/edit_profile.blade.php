2a1b7244efc3664cc07dc8a2953e795fa35c4078
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-30 15:05:27 +0530

    Update edit_profile.blade.php

--- resources/views/front/edit_profile.blade.php
+++ resources/views/front/edit_profile.blade.php
@@ -152,8 +152,8 @@
                                     </div>
                                     <div class="col-lg-6 mb-2">
                                         <div class="input-form">
-                                            <input type="text" class="phone_number border-0" id="country_code" name="countryCode" readonly>
-                                            <input type="text" class="form-control phone_number" id="phone_number" name="phone_number" value="{{ $user->phone_number}}"oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+                                            <input type="text" class="phone_number border-0" id="" name="countryCode" readonly>
+                                            <input type="text" class="form-control" id="phone_number" name="phone_number" value="{{ $user->phone_number}}"oninput="this.value = this.value.replace(/[^0-9]/g, '');">
                                             <label for="phone" class="floating-label">Phone Number</label>
                                             <input type="hidden" name="country_code" id="country_code" value="{{ $user->country_code}}">
                                             <div class="label-error">
