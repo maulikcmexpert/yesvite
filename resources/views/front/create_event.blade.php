7e28afd3e33aa02735dc698344e47b0da4d67e94
Vimal <vimal.cmexpertise@gmail.com>
2025-01-31 15:55:41 +0530

    update code

--- resources/views/front/create_event.blade.php
+++ resources/views/front/create_event.blade.php
@@ -1516,7 +1516,7 @@
                                                             </div>
                                                         </div>
                                                         <input type="hidden"
-                                                                            class="innerUserQnt-{{$itemkey}}"
+                                                                            class="innerUserQnt-{{ $itemkey }}-{{ $index }}"
                                                                             value="{{ $items['innerUserQnt'] }}">
                                                         <div class="ms-auto">
 
