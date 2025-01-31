ed49f776d625fc03da9348b89355c231d8a64093
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-01-31 07:38:33 +0530

    commit vrushali

--- resources/views/front/event_wall/event_about.blade.php
+++ resources/views/front/event_wall/event_about.blade.php
@@ -370,7 +370,7 @@
                                                         $isDisabled =
                                                             $eventDetails['host_id'] == $login_user_id ? 'd-none' : ''; // Using 'd-none' to hide the link
                                                     @endphp
-                                                    {{ dd($rsvpSent)}}
+                                                    {{-- {{ dd($rsvpSent)}} --}}
                                                     @if (!empty($rsvpSent['rsvp_status'] && $rsvpSent['rsvp_status'] == null))
                                                         <a href="#" class="rsvp-btn btn {{ $isDisabled }}"
                                                             data-bs-toggle="modal" data-bs-target="#aboutsuccess">
@@ -385,7 +385,7 @@
                                                             </svg>
                                                         </a>
                                                     @endif
-                                                    @if (!empty($rsvpSent['rsvp_status'] && $rsvpSent['rsvp_status'] == 0))
+                                                    @if (!empty($rsvpSent['rsvp_status'] && $rsvpSent['rsvp_status'] == "0"))
                                                         <a href="#"
                                                             class="rsvp-btn noattending-btn btn {{ $isDisabled }}"data-bs-toggle="modal"
                                                             data-bs-target="#aboutsuccess">Not Attending
@@ -399,7 +399,7 @@
                                                             </svg>
                                                         </a>
                                                     @endif
-                                                    @if (!empty($rsvpSent['rsvp_status'] && $rsvpSent['rsvp_status'] == 1))
+                                                    @if (!empty($rsvpSent['rsvp_status'] && $rsvpSent['rsvp_status'] == "1"))
                                                         <a href="#"
                                                             class="rsvp-btn attending-btn btn {{ $isDisabled }}"
                                                             data-bs-toggle="modal"
