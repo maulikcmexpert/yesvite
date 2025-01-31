a85cabe3f9f82421ddb0509b8b16af8a994d691b
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-31 11:19:55 +0530

    commit

--- app/Http/Controllers/EventListController.php
+++ app/Http/Controllers/EventListController.php
@@ -2653,7 +2653,11 @@ if ($rsvpSent != null) {
         
         foreach ($eventData as $val) {
     
-            $eventDatas =   Event::select('id', 'event_name')->where('id', $val->event_id)->where('event_name', 'like', "%$eventName%")->get();
+            if($eventName!=""){
+                $eventDatas =   Event::select('id', 'event_name')->where('id', $val->event_id)->where('event_name', 'like', "%$eventName%")->get();
+            }else{
+                $eventDatas =   Event::select('id', 'event_name')->where('id', $val->event_id)->get();
+            }
             foreach ($eventDatas as $vals) {
                 $eventDetail['id'] = $vals->id;
                 $eventDetail['event_name'] = $vals->event_name;
