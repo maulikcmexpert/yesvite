ad5f07140c63c898150b0e9141a9c49c3d46f509
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-31 14:39:57 +0530

    Update event_upcoming.blade.php

--- resources/views/components/main_menu/home/event_upcoming.blade.php
+++ resources/views/components/main_menu/home/event_upcoming.blade.php
@@ -15,9 +15,9 @@
           $colorClass = $series[$colorIndex % count($series)];
           $colorIndex++;
           @endphp
-    <div class="home-center-upcoming-events-card mb-3">
+    <div href="{{ route('event.event_wall', encrypt($events['id']))  }}" class="home-center-upcoming-events-card mb-3">
         <div class="home-upcoming-events-card-left">
-            <a href="{{ route('event.event_wall', encrypt(value: $events['id']))  }}" class="home-upcoming-events-card-left-profile">
+            <a href="{{ route('event.event_wall', encrypt($events['id']))  }}" class="home-upcoming-events-card-left-profile">
                 <div class="home-upcoming-events-card-left-profile-img">
                     {{-- <img src="{{$events['host_profile']}}" class="lazy" alt=""> --}}
                     @if($events['host_profile'] != "")
