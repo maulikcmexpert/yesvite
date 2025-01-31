1072ebedc10d1581ddf9fbac9ee84ea9331af500
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-01-31 07:36:47 +0530

    commit vrushali

--- resources/views/front/event_wall/event_about.blade.php
+++ resources/views/front/event_wall/event_about.blade.php
@@ -370,7 +370,7 @@
                                                         $isDisabled =
                                                             $eventDetails['host_id'] == $login_user_id ? 'd-none' : ''; // Using 'd-none' to hide the link
                                                     @endphp
-                                                    {{-- {{ dd($rsvpSent) --}}
+                                                    {{ dd($rsvpSent)}}
                                                     @if (!empty($rsvpSent['rsvp_status'] && $rsvpSent['rsvp_status'] == null))
                                                         <a href="#" class="rsvp-btn btn {{ $isDisabled }}"
                                                             data-bs-toggle="modal" data-bs-target="#aboutsuccess">
