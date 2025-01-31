ab51d1dcbebd0a266c7144d8806fb52a68896542
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-01-31 08:43:26 +0530

    Update event_about.blade.php

--- resources/views/front/event_wall/event_about.blade.php
+++ resources/views/front/event_wall/event_about.blade.php
@@ -370,9 +370,9 @@
                                                         $isDisabled =
                                                             $eventDetails['host_id'] == $login_user_id ? 'd-none' : ''; // Using 'd-none' to hide the link
                                                     @endphp
-                                                    {{ dd($rsvpSent)}}
+                                                    {{-- {{ dd($rsvpSent)}} --}}
 
-                                                    @if (!empty($rsvpSent) && empty($rsvpSent!= null) && $rsvpSent['rsvp_status'] == null)
+                                                    @if (!empty($rsvpSent) &&  $rsvpSent['rsvp_status'] == null)
                                                         <a href="#" class="rsvp-btn btn {{ $isDisabled }}"
                                                             data-bs-toggle="modal" data-bs-target="#aboutsuccess">
                                                             RSVP
