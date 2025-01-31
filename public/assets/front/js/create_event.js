e050763d31e0a77596c6e19ad5f350030c50ab8d
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-31 20:42:11 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -383,20 +383,33 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3.5,
     spaceBetween: 20,
     loop: true,
-    loopAdditionalSlides: 1,  // Ensures smooth loop transition
+    loopFillGroupWithBlank: false, // Prevents blank slides at the end
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
     breakpoints: {
-        320: { slidesPerView: 1.5 },
-        576: { slidesPerView: 2.5 },
-        768: { slidesPerView: 3.5 },
-        992: { slidesPerView: 2 },
-        1200: { slidesPerView: 2 },
-        1400: { slidesPerView: 3.5 },
+        320: {
+            slidesPerView: 1.5,
+        },
+        576: {
+            slidesPerView: 2.5,
+        },
+        768: {
+            slidesPerView: 3.5,
+        },
+        992: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 2,
+        },
+        1400: {
+            slidesPerView: 3.5,
+        },
     },
 });
+
 $(document).on("click", ".create-event-btn", function () {
     toggleSidebar("sidebar_create_event");
 });
