9ff30cfe9920452c6f0e453ad714ed46f2df437a
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-01-31 09:34:00 +0530

    Update search_filter_event.blade.php

--- resources/views/front/notification/search_filter_event.blade.php
+++ resources/views/front/notification/search_filter_event.blade.php
@@ -1,6 +1,8 @@
 @foreach ($eventList as $event)
   <div class="form-check">
-                        <input class="form-check-input" name="selectedEvents[]" data-event_name="{{$event['event_name']}}" data-event_id="{{$event['id']}}" type="checkbox" value="" id="flexCheckDefault2">
+                        <input class="form-check-input" name="selectedEvents[]" data-event_name="{{$event['event_name']}}" data-event_id="{{$event['id']}}" type="checkbox" value=""
+                        id="flexCheckDefault2"
+                        @if(in_array($event['id'], session()->get('notification_event_ids', []))) checked @endif>
                         <label class="form-check-label invited_to_chk_lbl" for="flexCheckDefault2">
                           {{$event['event_name']}}
                         </label>
