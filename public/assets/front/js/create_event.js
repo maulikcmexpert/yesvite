0cbd3562cd147a079bef9f6c5cdd51a43aeef93a
Vimal <vimal.cmexpertise@gmail.com>
2025-02-01 14:10:00 +0530

    update code

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -2012,7 +2012,7 @@ function guest_counter(total_guest, max_guest) {
     $("#event_guest_count").text(total_guest + Alreadyguest + " Guests");
     $(".invite-count").text(total_guest + Alreadyguest);
     
-   var remainingCount = max_guest - (total_guest);
+   var remainingCount = max_guest - (total_guest+Alreadyguest);
  
     if (remainingCount < 0) {
         $(".invite-left_d").text("Invites | 0 Left");
