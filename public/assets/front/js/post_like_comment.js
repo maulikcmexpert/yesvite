8fd21e1c11675992119967b2cb1d5b576386a055
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-01 16:49:42 +0530

    Update post_like_comment.js

--- public/assets/front/js/post_like_comment.js
+++ public/assets/front/js/post_like_comment.js
@@ -807,9 +807,9 @@ function addToGuestList(id, preferBy, appUser,first_name,last_name,email,profile
 
         console.log("Updated guest list:", guestList);
 
-}
+});
 
-    $(document).on("click", ".add_guest", function (e) {
+ $(document).on("click", ".add_guest", function (e) {
         e.preventDefault();
 
         console.log("Guest list before submit:", guestList);
@@ -842,7 +842,6 @@ function addToGuestList(id, preferBy, appUser,first_name,last_name,email,profile
             },
         });
     });
-});
 $(document).on("keyup", ".search_contact", function () {
     console.log($(this).val())
     var searchQuery = $(this).val().toLowerCase(); // Get the search input value and convert it to lowercase
