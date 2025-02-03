990a30af732791c0b6029482129a796dd6fbe014
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-03 14:01:32 +0530

    Update EventGuestController.php

--- app/Http/Controllers/EventGuestController.php
+++ app/Http/Controllers/EventGuestController.php
@@ -905,6 +905,7 @@ class EventGuestController extends Controller
     
         $yesvite_all_invite=getInvitedUsersList($request->event_id);
         $new_added_user=session()->get('add_guest_user_id');
+        dd($new_added_user);
         foreach ($new_added_user as $user_id) {
             // Try fetching the user from the User table
             $user = User::find($user_id);
