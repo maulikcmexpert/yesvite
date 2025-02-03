988b0f5bde2d687271aa35093d8fe1376a45e2e0
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-03 17:01:01 +0530

    Update EventGuestController.php

--- app/Http/Controllers/EventGuestController.php
+++ app/Http/Controllers/EventGuestController.php
@@ -949,7 +949,7 @@ class EventGuestController extends Controller
             
         }
 
-                // dd($users_data);
+                dd($yesvite_phone_data);
 
     }
         return response()->json(['view' => view( 'front.event_wall.see_invite', compact('yesvite_all_invite','yesvite_users_data','yesvite_phone_data','is_phone'))->render()]);
