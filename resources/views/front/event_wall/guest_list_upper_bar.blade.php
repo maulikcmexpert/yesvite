ccc472465891d9d77e5d64fe3ee30146d636fd60
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-03 19:43:08 +0530

    commit

--- resources/views/front/event_wall/guest_list_upper_bar.blade.php
+++ resources/views/front/event_wall/guest_list_upper_bar.blade.php
@@ -31,19 +31,21 @@ break;
                             @endif
                                 <!-- <img src="./assets/image/user-img.svg" alt="guest-img"> -->
                                                                 <!-- <img src="https://yesvite.cmexpertiseinfotech.in/storage/profile/73_profile.jpg?v=29" alt="user-img"> -->
-                                                            <!-- <a href="#" class="close">
-                                    <svg width="19" height="18" viewBox="0 0 19 18" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <rect x="1.20312" y="1" width="16" height="16" rx="8"
-                                            fill="#F73C71" />
-                                        <rect x="1.20312" y="1" width="16" height="16" rx="8" stroke="white"
-                                            stroke-width="2" />
-                                        <path d="M6.86719 6.66699L11.5335 11.3333" stroke="white" stroke-linecap="round"
-                                            stroke-linejoin="round" />
-                                        <path d="M6.8649 11.3333L11.5312 6.66699" stroke="white" stroke-linecap="round"
-                                            stroke-linejoin="round" />
-                                    </svg>
-                                </a> -->
+                                                      @if(isset($guest_user['recent'])&&$guest_user['recent']=1)           
+                                                                            <a href="#" class="close">
+                                                    <svg width="19" height="18" viewBox="0 0 19 18" fill="none"
+                                                        xmlns="http://www.w3.org/2000/svg">
+                                                        <rect x="1.20312" y="1" width="16" height="16" rx="8"
+                                                            fill="#F73C71" />
+                                                        <rect x="1.20312" y="1" width="16" height="16" rx="8" stroke="white"
+                                                            stroke-width="2" />
+                                                        <path d="M6.86719 6.66699L11.5335 11.3333" stroke="white" stroke-linecap="round"
+                                                            stroke-linejoin="round" />
+                                                        <path d="M6.8649 11.3333L11.5312 6.66699" stroke="white" stroke-linecap="round"
+                                                            stroke-linejoin="round" />
+                                                    </svg>
+                                                </a>
+                                @endphp
                             </div>
                            
                             <h6 class="guest-name">{{$guest_user['first_name']}} {{$guest_user['last_name']}}</h6>
