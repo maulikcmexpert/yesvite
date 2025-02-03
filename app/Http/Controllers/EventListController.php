d820b5a9e5228835195de280b4b52badb2a1bb8b
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-03 20:37:05 +0530

    commit

--- app/Http/Controllers/EventListController.php
+++ app/Http/Controllers/EventListController.php
@@ -2754,11 +2754,11 @@ if ($rsvpSent != null) {
             }
         }
                         // dd($yesvite_all_invite);
-        // if(!empty($yesvite_users_data)){
+        if($request->contact=="yesvite"){
             return response()->json(['view' => view( 'front.event_wall.guest_list_upper_bar', compact('yesvite_all_invite','yesvite_users_data','yesvite_phone_data'))->render(),'is_phone'=>"0"]);
-        // }else{
-        //     return response()->json(['view' => view( 'front.event_wall.guest_phone_list_upper_bar', compact('yesvite_all_invite','yesvite_users_data','yesvite_phone_data'))->render(),'is_phone'=>"1"]);
-        // }
+        }else{
+            return response()->json(['view' => view( 'front.event_wall.guest_phone_list_upper_bar', compact('yesvite_all_invite','yesvite_users_data','yesvite_phone_data'))->render(),'is_phone'=>"1"]);
+        }
         }
     
         session(['add_guest_user_id' => $userData]);
