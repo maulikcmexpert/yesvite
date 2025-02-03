5b26bd1c6fb3ca91af1c2e7d19fbbd813679df74
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-03 21:43:00 +0530

    Update post_like_comment.js

--- public/assets/front/js/post_like_comment.js
+++ public/assets/front/js/post_like_comment.js
@@ -1039,30 +1039,30 @@ $(document).on('click','.remove_new_phone_added_user',function(){
         e.preventDefault();
         console.log("Guest list before submit:", guestList);
         console.log("Sending guest list:", guestList);
-        // $.ajax({
-        //     url: base_url + "event_wall/send-invitation", // Your Laravel route
-        //     method: "POST",
-        //     headers: {
-        //         "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-        //     },
-        //     data: {
-        //         event_id: $("#event_id").val(), // Event ID from a hidden input
-        //         guest_list: guestList,
-        //     },
-        //     success: function (response) {
-        //         if (response.status === 1) {
-        //             window.location.reload();
-        //             toastr.success('Invited successfully');
-        //             // alert(response.message); // Show success message
-        //             guestList = []; // Clear guest list after successful submission
-        //         } else {
-        //             alert(response.message); // Show error message
-        //         }
-        //     },
-        //     error: function (xhr) {
-        //         alert("Something went wrong. Please try again."); // Handle AJAX errors
-        //     },
-        // });
+        $.ajax({
+            url: base_url + "event_wall/send-invitation", // Your Laravel route
+            method: "POST",
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+            },
+            data: {
+                event_id: $("#event_id").val(), // Event ID from a hidden input
+                guest_list: guestList,
+            },
+            success: function (response) {
+                if (response.status === 1) {
+                    window.location.reload();
+                    toastr.success('Invited successfully');
+                    // alert(response.message); // Show success message
+                    guestList = []; // Clear guest list after successful submission
+                } else {
+                    alert(response.message); // Show error message
+                }
+            },
+            error: function (xhr) {
+                alert("Something went wrong. Please try again."); // Handle AJAX errors
+            },
+        });
 });
 $(document).on("keyup", ".search_contact", function () {
     console.log($(this).val())
