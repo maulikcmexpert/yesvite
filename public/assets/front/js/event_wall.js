94418a63594e7fe87d8880b553127f851b78f585
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-03 14:39:17 +0530

    update code

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1225,3 +1225,7 @@ $(".modal").on("shown.bs.modal", function () {
     // Remove `d-none` class to show the div
     $(".create-post-upload-img-inner").removeClass("d-none");
 });
+
+$(".noattending-btn").on('click',function(){
+    alert();
+})
