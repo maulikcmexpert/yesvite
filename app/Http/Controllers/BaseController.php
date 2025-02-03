10b26ca5df1c44720479804ac7a966fd76b0373a
cmexpertise <pratik.cmexpertise@gmail.com>
2025-02-03 18:47:36 +0530

    Update BaseController.php

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -25,6 +25,7 @@ class BaseController extends Controller
     }
     protected function getPrices()
     {
+        dd(config('app.debug', true));
         // dd(createShortUrl("https://yesvite.com"));
 
         // $users = UserOpt::select('id', 'event_id', 'event_invited_user_id')
