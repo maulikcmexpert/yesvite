e1aa21ce2623523b74003c1042f11a40be46f2ea
cmexpertise <pratik.cmexpertise@gmail.com>
2025-02-03 13:11:17 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -669,21 +669,21 @@ function bindData(current_event_id) {
                         const textWidth = textMeasurement.width;
 
                         if (element.left === undefined) {
-                            console.log(element.centerX);
-                            console.log(scaleFactor);
-                            console.log(element.width);
-                            element.left =
-                                element.centerX + element.width * scaleFactor;
+                            console.log("centerX", element.centerX);
+
+                            console.log("width", element.width);
+                            console.log({ scaleFactor });
+                            element.left = element.centerX * scaleFactor;
                             console.log("left", element.left);
                         }
 
                         if (element.top === undefined) {
-                            console.log(element.centerY);
-                            console.log(scaleFactor);
-                            console.log(element.width);
+                            console.log("centerY", element.centerY);
 
-                            element.top =
-                                element.centerY + element.width * scaleFactor;
+                            console.log("width", element.height);
+                            console.log({ scaleFactor });
+
+                            element.top = element.centerY * scaleFactor;
                             console.log("top", element.top);
                         }
 
