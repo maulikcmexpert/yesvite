eba040995dcc5047326a1a6041c728c2dd600aca
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-03 17:35:54 +0530

    commit

--- app/Http/Controllers/EventWallController.php
+++ app/Http/Controllers/EventWallController.php
@@ -859,7 +859,7 @@ class EventWallController extends Controller
         $creatEventPost->post_privacy = $request->post_privacys;
         // $creatEventPost->post_message = $request->input('content');
         $creatEventPost->commenting_on_off = $request->commenting_on_off;
-        $creatEventPost->is_in_photo_moudle = "1";
+        $creatEventPost->is_in_photo_moudle = "0";
         $creatEventPost->save();
 
         if ($creatEventPost->id  && $request->hasFile('files')) {
