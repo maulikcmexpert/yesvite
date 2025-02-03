ae4093933a3a50a89b5e683a76a692302f6f67c4
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-03 15:14:20 +0530

    Update EventListController.php

--- app/Http/Controllers/EventListController.php
+++ app/Http/Controllers/EventListController.php
@@ -2678,7 +2678,7 @@ if ($rsvpSent != null) {
         $check_status = $request->status;
         $prefer_by=$request->prefer_by;
         // dd($request);
-        $userData = session('add_guest_user_data', []);
+        $userData = session('add_guest_user_id', []);
 
         if ($check_status == 1) {
 
@@ -2702,9 +2702,8 @@ if ($rsvpSent != null) {
             $userData = array_values(array_filter($userData, fn($user) => $user['user_id'] != $user_id));
         }
     
-        session(['add_guest_user_data' => $userData]);
+        session(['add_guest_user_id' => $userData]);
     
-        dd(session()->get('add_guest_user_id'));
         return session()->get('add_guest_user_id');
     }
 }
