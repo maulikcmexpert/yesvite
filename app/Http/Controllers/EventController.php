4a1aefa6f7f7961c12cf6d6a1f70cf652093da2b
Vimal <vimal.cmexpertise@gmail.com>
2025-02-04 19:33:18 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3139,7 +3139,7 @@ class EventController extends BaseController
     {
 
         // dd($request->slider_images);
-        dD($request);
+        // dD($request);
         Session::forget('desgin');
         Session::forget('shape_image');
         Session::forget('custom_image');
