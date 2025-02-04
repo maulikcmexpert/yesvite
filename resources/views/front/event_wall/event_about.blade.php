9bace97d8b95daa54b3c77ed1845df260647d75a
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-04 09:40:58 +0530

    Update event_about.blade.php

--- resources/views/front/event_wall/event_about.blade.php
+++ resources/views/front/event_wall/event_about.blade.php
@@ -1,6 +1,8 @@
+
+{{dd($eventDetails);}}
 @php
     use Carbon\Carbon;
-    // dd($eventDetails);
+
 @endphp
 @isset($eventDetails)
     <main class="new-main-content">
@@ -2236,7 +2238,7 @@ $longitude = !empty($eventDetails['longitude'])
 
     </main>
 @endisset
-{{-- <script>
+<script>
     // Get event date and time from PHP
     const startdate = "{{ $startdate }}"; // '2025-01-30' format
     const starttime = "{{ $starttime }}"; // '9:30 PM' format
@@ -2274,4 +2276,4 @@ $longitude = !empty($eventDetails['longitude'])
     // Update countdown every second
     setInterval(updateCountdown, 1000);
     updateCountdown(); // Initial call to set the countdown immediately
-</script> --}}
+</script>
