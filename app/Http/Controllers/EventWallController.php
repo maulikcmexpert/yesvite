921900a08e3ac884d055ae0bffc50695cdaef169
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-04 11:45:12 +0530

    commit

--- app/Http/Controllers/EventWallController.php
+++ app/Http/Controllers/EventWallController.php
@@ -2487,6 +2487,7 @@ foreach ($polls as $poll) {
         $event_id=$request->event_id;
         $all_invited_user=getInvitedUsersList($event_id);
 
+    
         return response()->json(['view' => view( 'front.event_wall.right_all_guest_list', compact('all_invited_user'))->render(),'status'=>1]);
 
         // return response()->json(['view' => 1, 'data' => $faildInviteList, 'message' => "Faild invites"]);
