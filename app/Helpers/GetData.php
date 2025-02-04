de2ac6266d03c2cb2717f9e0f5975cda92fd5ff0
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-04 19:03:49 +0530

    Update GetData.php

--- app/Helpers/GetData.php
+++ app/Helpers/GetData.php
@@ -995,7 +995,7 @@ function getInvitedUsersList($eventId)
     $eventDetail['invited_user_id'] = [];
 
     $invitedUsers = EventInvitedUser::query();
-    $invitedUsers->with(['event', 'user','contact_sync'])->where(['event_id' => $eventId,'is_co_host'=>'0'])->whereNot('invitation_sent!=','9');
+    $invitedUsers->with(['event', 'user','contact_sync'])->where(['event_id' => $eventId,'is_co_host'=>'0'])->whereNot('invitation_sent','9');
     $result = $invitedUsers->get();
 
     // dd($result);
