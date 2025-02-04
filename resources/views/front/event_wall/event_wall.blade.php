ebf6db04dd097447bed7666346e12e1819b74776
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-04 18:30:56 +0530

    comment

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -513,9 +513,9 @@
                                                                             <i class="fa-regular fa-heart" id="show_Emoji"></i>
                                                                             @endif
                                                                       </button>
-                                                                      <p id="commentTotalLike_{{ $reply['id'] }}">
+                                                                      {{-- <p id="commentTotalLike_{{ $reply['id'] }}">
                                                                         {{ isset($reply['comment_total_likes']) ? $reply['comment_total_likes'] : 0 }}
-                                                                    </p>
+                                                                    </p> --}}
                                                                         </div>
                                                                         <button
                                                                             class="commented-user-reply-btn">Reply</button>
