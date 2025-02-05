6a2ceaad61b1530555075507a7cf805e74d811e9
vivekcme <73165162+vivekcme@users.noreply.github.com>
2025-02-05 17:16:30 +0530

    update code

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2225,7 +2225,7 @@ class EventController extends BaseController
         // dd($yesvite_users);
 
         $yesvite_users = User::select('id', 'firstname', 'profile', 'lastname', 'email', 'country_code', 'phone_number', 'app_user', 'prefer_by', 'email_verified_at', 'parent_user_phone_contact', 'visible', 'message_privacy')
-        ->where('id', '!=', $id)
+        // ->where('id', '!=', $id)
         ->where(['app_user' => '1'])
         ->whereIn('email', $emails)
         ->orderBy('firstname')
