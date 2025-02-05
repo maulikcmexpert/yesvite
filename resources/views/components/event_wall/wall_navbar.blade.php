43fb0bf6af462c1419760971e048b53d34dd9a04
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-05 11:48:48 +0530

    Update wall_navbar.blade.php

--- resources/views/components/event_wall/wall_navbar.blade.php
+++ resources/views/components/event_wall/wall_navbar.blade.php
@@ -3,18 +3,27 @@
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
         <!-- Wall Tab -->
         @if (
-            ($eventDetails['is_host'] == 1 && $eventDetails['event_wall'] == 0) ||  // Host and Potluck enabled
-            ($eventDetails['is_co_host'] == 1 && $eventDetails['event_wall'] ==  0 ) // Not host but RSVP confirmed
+            ($eventDetails['is_host'] == 1 && $eventDetails['event_wall'] == 1) ||  // Host and Potluck enabled
+            ($eventDetails['is_co_host'] == 1 && $eventDetails['event_wall'] ==  1 ) // Not host but RSVP confirmed
         )
-        <a href="{{ route('event.event_wall', ['id' => encrypt($event)]) }}"
-           class="nav-link {{ $page == 'wall' ? 'active' : '' }}"
-           id="nav-wall-tab"
-           role="tab"
-           aria-controls="nav-wall"
-           aria-selected="{{ $page == 'wall' ? 'true' : 'false' }}">
-            Wall
-        </a>
-@endif
+            <a href="{{ route('event.event_wall', ['id' => encrypt($event)]) }}"
+            class="nav-link {{ $page == 'wall' ? 'active' : '' }}"
+            id="nav-wall-tab"
+            role="tab"
+            aria-controls="nav-wall"
+            aria-selected="{{ $page == 'wall' ? 'true' : 'false' }}">
+                Wall
+            </a>
+        @else
+            <a href="{{ route('event.event_wall', ['id' => encrypt($event)]) }}"
+                class="nav-link {{ $page == 'wall' ? 'active' : '' }}"
+                id="nav-wall-tab"
+                role="tab"
+                aria-controls="nav-wall"
+                aria-selected="{{ $page == 'wall' ? 'true' : 'false' }}">
+                    Wall
+                </a>
+        @endif
         <!-- About Tab -->
         <a href="{{ route('event.event_about', ['id' => encrypt($event)]) }}"
            class="nav-link {{ $page == 'about' ? 'active' : '' }}"
