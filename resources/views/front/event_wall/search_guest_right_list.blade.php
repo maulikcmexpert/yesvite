fe5bbd291361dbbe77dc22cc59f8fe11ea84f0de
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-05 22:21:41 +0530

    Update search_guest_right_list.blade.php

--- resources/views/front/event_wall/search_guest_right_list.blade.php
+++ resources/views/front/event_wall/search_guest_right_list.blade.php
@@ -1,12 +1,12 @@
 @foreach ($guestArray as $index => $guest)
 
-                        @php
+                        <!-- @php
                         if($see_all=="0"){
                             $index == 7;
                             break;
                         }
                         @endphp
-                       
+                        -->
                             @php
                                 //$user = $guest['user']; // Fetch user array
                                 $firstInitial = isset($guest['first_name'][0]) ? strtoupper($guest['first_name'][0]) : '';
