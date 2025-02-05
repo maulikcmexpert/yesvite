e3a7985f3814f387659d7cbdb0a8e5597cc47078
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-05 11:08:24 +0530

    coomit

--- app/Http/Controllers/EventWallController.php
+++ app/Http/Controllers/EventWallController.php
@@ -689,7 +689,7 @@ foreach ($polls as $poll) {
         //}
 
         $eventDetail = Event::with(['user', 'event_image', 'event_schedule', 'event_settings' => function ($query) {
-            $query->select('event_id', 'podluck', 'allow_limit', 'adult_only_party');
+            $query->select('event_id', 'podluck', 'allow_limit', 'adult_only_party','event_wall','guest_list_visible_to_guests');
         }, 'event_invited_user' => function ($query) {
             $query->where('is_co_host', '1')->with('user');
         }])->where('id', $event)->first();
