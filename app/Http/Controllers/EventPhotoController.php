0b77fa247e2081813b48b6aeba1e51d37dc9feac
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-05 18:28:38 +0530

    Update EventPhotoController.php

--- app/Http/Controllers/EventPhotoController.php
+++ app/Http/Controllers/EventPhotoController.php
@@ -34,7 +34,7 @@ class EventPhotoController extends Controller
 {
     public function index(String $id)
     {
-        DD(1);
+        dd(1);
         $title = 'event photos';
         $page = 'front.event_wall.event_photos';
         $user  = Auth::guard('web')->user();
