99151a6a1fe6be4384084e4c838bcdedcb04dd35
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-05 20:11:10 +0530

    Update post_like_comment.js

--- public/assets/front/js/post_like_comment.js
+++ public/assets/front/js/post_like_comment.js
@@ -982,7 +982,6 @@ function addToGuestList(id, preferBy, appUser,first_name,last_name,email,profile
             var upper_see=$('.selected-contacts-list .add_guest_seeall').length;
             // alert(upper_see);
             if(upper_see==0&&is_duplicate==0){
-                console.log(is_duplicate);
 
                 const totalHtml = `
                 <a class="guest-user d-block yesvite add_guest_seeall">
@@ -995,11 +994,9 @@ function addToGuestList(id, preferBy, appUser,first_name,last_name,email,profile
                   $modalBody.append(totalHtml);
             }
             if(upper_see>0){
-                console.log(upper_see,is_duplicate);
                 if(is_duplicate==0){
                     var initial= parseInt($('#total-selected-email').attr('data-count'));
                     var new_value= initial+1 ;
-                    alert(new_value);
                     $('#total-selected-email').attr('data-count',new_value);
                     $('#total-selected-email').text('+'+new_value);
                 }
