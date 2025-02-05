4e581598b8130bcad1c60956ba830fb3df630b9f
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-05 09:15:22 +0530

    Update event_wall.blade.php

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -1182,9 +1182,11 @@
                                                                         <i class="fa-regular fa-heart" id="show_Emoji"></i>
                                                                         @endif
                                                                   </button>
-                                                                  <p id="commentTotalLike_{{ $reply['id'] }}">
-                                                                    {{ isset($reply['comment_total_likes']) ? $reply['comment_total_likes'] : 0 }}
-                                                                </p>
+                                                                  @isset($reply)
+                                                                    <p id="commentTotalLike_{{ $reply['id'] }}">
+                                                                        {{ isset($reply['comment_total_likes']) ? $reply['comment_total_likes'] : 0 }}
+                                                                    </p>
+                                                                @endisset
 
                                                                     </div>
                                                                     <button
