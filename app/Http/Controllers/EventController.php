515b264770ed1af0ef86cf621829c3ad265ee67b
vivekcme <73165162+vivekcme@users.noreply.github.com>
2025-02-06 17:01:08 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -517,7 +517,7 @@ class EventController extends BaseController
                     session()->put('category_item', $categories_item);
                     Session::save();
                     $eventDetail['totalCategoryItem'] =  $totalCategoryItem;
-                    dd(session('category'));
+                    dd($eventDetail['podluck_category_list']);
                 }
             }
         } else {
