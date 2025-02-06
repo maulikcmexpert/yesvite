fb6276a40dac3a2fcb15cde08bf8a5631401834c
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-06 12:17:45 +0530

    Update event_guest.js

--- public/assets/front/js/event_guest.js
+++ public/assets/front/js/event_guest.js
@@ -326,13 +326,13 @@ $(document).on('click', '.delete_failed_contact', function () {
 
 
 $(document).on('click','.re_send_failed_invites', function() {
-    
+    var event_id = $('#event_id').val();
+
     let userIds = [];
     $('.failed_check_resend_email').each(function() {
         let userId = $(this).attr('data-id');
         let preferBy = $(this).attr('data-prefer');
         let app_user = $(this).attr('data-app_user');
-        var event_id = $('#event_id').val();
         userIds.push({ id: userId, prefer_by: preferBy,app_user:app_user});
     });
 
