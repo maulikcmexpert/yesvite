3ceb477be0ca9b39c0b47b7def4e3b7b29f841ff
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-06 11:52:26 +0530

    commit

--- app/Http/Controllers/EventWallController.php
+++ app/Http/Controllers/EventWallController.php
@@ -2618,7 +2618,7 @@ class EventWallController extends Controller
 
     public function sendInvitation(Request $request)
     {
-        dd($request['guest_list']);
+        dd($request->is_failed);
         $user  = Auth::guard('web')->user();
         // try {
         if (!empty($request['guest_list'])) {
