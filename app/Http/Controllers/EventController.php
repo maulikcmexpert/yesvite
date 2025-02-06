f7608340f871df0a956ea3d963c2bf02d7fcf89e
vivekcme <73165162+vivekcme@users.noreply.github.com>
2025-02-06 15:52:48 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -590,7 +590,7 @@ class EventController extends BaseController
     public function store(Request $request)
     {
         $potluck = session('category');
-        // dd($potluck);
+        dd($request);
         // dd($request);
 
         Session::forget('desgin');
