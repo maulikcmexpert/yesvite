83db014a1b0c7324ef10806787371deb4db24a3d
Vimal <vimal.cmexpertise@gmail.com>
2025-02-07 13:55:14 +0530

    update cide

--- app/Http/Controllers/EventPotluckControllerOld.php
+++ app/Http/Controllers/EventPotluckControllerOld.php
@@ -609,7 +609,7 @@ class EventPotluckController extends Controller
         if ($item && intval($item->quantity) > 0) {
             $isUser = true; // If quantity is greater than zero
         }
-
+dd($isUser,$item);
         // Check if there's enough quantity available
         if ($checkCarryQty < $checkQty && $isUser==false ) {
             // Check if the user has already added this item
