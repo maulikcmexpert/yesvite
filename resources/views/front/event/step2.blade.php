ed580681e4a3ccea597272760b9e0fe736ba6d89
cmexpertise <pratik.cmexpertise@gmail.com>
2025-02-08 02:28:41 +0530

    Update step2.blade.php

--- resources/views/front/event/step2.blade.php
+++ resources/views/front/event/step2.blade.php
@@ -38,8 +38,44 @@
                                         id="Allcat">
                                 </div>
                                 <div class="accordion" id="accordionExample">
-                           
-                                @foreach ($categories as $textData)
+                                    @foreach ($categories as $category)
+
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="heading{{ $category->id }}">
+                                            <button class="accordion-button" type="button"
+                                                data-bs-toggle="collapse"
+                                                data-bs-target="#collapse{{ $category->id }}" aria-expanded="true"
+                                                aria-controls="collapse{{ $category->id }}">
+                                                {{ $category->category_name }}
+                                            </button>
+                                        </h2>
+                                        <div id="collapse{{ $category->id }}"
+                                            class="accordion-collapse collapse"
+                                            aria-labelledby="heading{{ $category->id }}"
+                                            data-bs-parent="#accordionExample">
+                                            <div class="accordion-body">
+                                                <ul>
+                                                @foreach ($category->subcategory as $subcategory)
+
+                                                        <li>
+                                                            <div class="d-flex align-items-center justify-content-between">
+                                                                <label class="form-check-label" for="subcategory{{ $subcategory->id }}">
+                                                                    {{ $subcategory->subcategory_name }}
+                                                                </label>
+                                                                <input class="form-check-input" name="design_subcategory" type="checkbox"
+                                                                    id="subcategory{{ $subcategory->id }}"
+                                                                    data-category-id="{{ $category->id }}"
+                                                                    data-subcategory-id="{{ $subcategory->id }}">
+                                                            </div>
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                            @endforeach
+
+                                {{-- @foreach ($categories as $textData)
                                         <div class="accordion-item">
                                             <h2 class="accordion-header" id="heading{{ $textData->categories->id }}">
                                                 <button class="accordion-button" type="button"
@@ -70,7 +106,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                @endforeach
+                                @endforeach --}}
 
                                 </div>
                             </div>
