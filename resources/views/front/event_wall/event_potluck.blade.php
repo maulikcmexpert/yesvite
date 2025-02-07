e36e132985e64189e7b11c3b7c8dd28a7b37e8b2
Vimal <vimal.cmexpertise@gmail.com>
2025-02-07 20:25:34 +0530

    upadte code

--- resources/views/front/event_wall/event_potluck.blade.php
+++ resources/views/front/event_wall/event_potluck.blade.php
@@ -1,4 +1,4 @@
-{{dD($potluckDetail['podluck_category_list_new'])}}
+
 <main class="new-main-content">
 
     <div class="container">
@@ -897,7 +897,7 @@
                                                                                             {{ $users['last_name'] }}
                                                                                         </h5>
                                                                                         <span
-                                                                                            class="ms-auto slide-round">{{ $item['spoken_quantity'] }}</span>
+                                                                                            class="ms-auto slide-round">{{ $item['quantity'] }}</span>
                                                                                     </div>
                                                                                 </div>
                                                                             {{-- </div> --}}
