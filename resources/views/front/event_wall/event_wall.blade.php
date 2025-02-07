3159369cd0a0984726630dfc44e60375d681a45f
cmexpertise <pratik.cmexpertise@gmail.com>
2025-02-07 21:56:51 +0530

    Update event_wall.blade.php

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -450,6 +450,10 @@
                                                                         </li>
                                                                         @php $i++; if($i==3){break;} @endphp
                                                                     @endforeach
+                                                                    @if ($j==0)
+                                                                    <li id="reactionImage_{{ $post['id'] }}">
+                                                                    </li>
+                                                                    @endphp
                                                                     <p id="likeCount_{{ $post['id'] }}">
                                                                         {{ $post['total_likes'] }} Likes</p>
                                                                 </ul>
