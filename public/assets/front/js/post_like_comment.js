6a51ec35ecb300d47cda835284fe2ba37a26c10c
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-07 09:26:57 +0530

    commit

--- public/assets/front/js/post_like_comment.js
+++ public/assets/front/js/post_like_comment.js
@@ -395,7 +395,11 @@ $(document).ready(function () {
                     </div>
                     <div class="commented-user-reply-wrp">
                         <div class="position-relative d-flex align-items-center gap-2">
-                            <button class="posts-card-like-btn"  >
+                            <button class="posts-card-like-btn" id="CommentlikeButton"
+                                data-event-id="${eventId}"
+                            data-event-post-comment-id="${data.id
+                        } "
+                        data-user-id="${login_user_id}">
 <i class="fa-regular fa-heart"></i></button>
                             <p id="commentTotalLike_${data.comment}>0</p>
                         </div>
