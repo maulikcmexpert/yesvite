3322a3ea77c20fb2b9051eea5bc79cae9ca16332
cmexpertise <pratik.cmexpertise@gmail.com>
2025-02-08 02:30:59 +0530

    update

--- resources/views/front/event/step2.blade.php
+++ resources/views/front/event/step2.blade.php
@@ -119,7 +119,7 @@
         </div>
         {{-- {{ dd($categories);}} --}}
         <div class="row list_all_design_catgeory">
-            @foreach ($categories as $textData)
+            {{-- @foreach ($categories as $textData)
         
                     <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs"
                         data-wow-duration="2s" data-wow-delay="0" data-wow-offset="0"
@@ -135,8 +135,27 @@
                         </a>
                     </div>
         
+            @endforeach --}}
+            @foreach ($categories as $category)
+            @foreach ($category->subcategory as $subcategory)
+                @foreach ($subcategory->textdatas as $image)
+                    <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs"
+                        data-wow-duration="2s" data-wow-delay="0" data-wow-offset="0"
+                        data-category-id="{{ $category->id }}" data-subcategory-id="{{ $subcategory->id }}" data-image="{{ asset('storage/canvas/' . $image->image) }}"
+                        data-shape_image="{{ $image->shape_image != '' ? asset('storage/canvas/' . $image->shape_image) : '' }}"
+                        data-json="{{ json_encode($image->static_information) }}"
+                        data-id="{{ $image->id }}">
+                        <a href="javascript:;" class="collection-card card-blue">
+                            <div class="card-img edit_design_tem design-card">
+                                <img src="{{ asset('storage/canvas/' . $image->filled_image) }}"
+                                    alt="shower-card">
+                            </div>
+                            <h4>{{ $category->category_name }}</h4>
+                        </a>
+                    </div>
+                @endforeach
             @endforeach
-
+        @endforeach
           
       
         </div>
