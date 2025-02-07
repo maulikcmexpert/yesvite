eac8de4b8b710e594bcc485181bf4bc6fd26a6d0
cmexpertise <pratik.cmexpertise@gmail.com>
2025-02-07 21:40:01 +0530

    Update event_wall.blade.php

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -1,4 +1,4 @@
-{{dd($postList)}}
+{{-- {{dd($postList)}} --}}
 <main class="new-main-content">
     <div class="container">
         <div class="row">
@@ -409,33 +409,35 @@
                                                             <div class="posts-card-like-comment-left">
                                                                 <ul type="button" data-bs-toggle="modal"
                                                                     data-bs-target="#reaction-modal-{{ $post['id'] }}">
-
+                                                                    @foreach ($postList['reactionList'] as $reaction)
                                                                     <!-- Smiley Emoji -->
-                                                                    <li id="reactionImage_{{ $post['id'] }}">
-                                                                        @if ($post['self_reaction'] == '\u{1F604}')
-                                                                            <img src="{{ asset('assets/front/img/smily-emoji.png') }}"
-                                                                                alt="Smiley Emoji">
-                                                                        @elseif ($post['self_reaction'] == '\u{1F60D}')
-                                                                            <img src="{{ asset('assets/front/img/eye-heart-emoji.png') }}"
-                                                                                alt="Eye Heart Emoji">
-                                                                        @elseif ($post['self_reaction'] == '\u{2764}')
-                                                                            <img src="{{ asset('assets/front/img/heart-emoji.png') }}"
-                                                                                alt="Heart Emoji">
+                                                                        <li >
+                                                                            @if ($reaction == '\u{1F604}')
+                                                                                <img src="{{ asset('assets/front/img/smily-emoji.png') }}"
+                                                                                    alt="Smiley Emoji">
+                                                                            @elseif ($reaction == '\u{1F60D}')
+                                                                                <img src="{{ asset('assets/front/img/eye-heart-emoji.png') }}"
+                                                                                    alt="Eye Heart Emoji">
+                                                                            @elseif ($reaction == '\u{2764}')
+                                                                                <img src="{{ asset('assets/front/img/heart-emoji.png') }}"
+                                                                                    alt="Heart Emoji">
 
-                                                                        @elseif($post['self_reaction'] == '\u{1F44D}')
-                                                                        <img src="{{ asset('assets/front/img/thumb-icon.png') }}"
-                                                                                loading="lazy" alt="Thumb Emoji"
-                                                                                class="emoji" data-emoji="👍"
-                                                                                data-unicode="\\u{1F44D}">
+                                                                            @elseif($reaction == '\u{1F44D}')
+                                                                            <img src="{{ asset('assets/front/img/thumb-icon.png') }}"
+                                                                                    loading="lazy" alt="Thumb Emoji"
+                                                                                    class="emoji" data-emoji="👍"
+                                                                                    data-unicode="\\u{1F44D}">
 
-                                                                    @elseif($post['self_reaction'] == '\u{1F44F}')
-                                                                        <img
-                                                                            src="{{ asset('assets/front/img/clap-icon.png') }}"
-                                                                            loading="lazy" alt="Clap Emoji"
-                                                                            class="emoji" data-emoji="👏"
-                                                                            data-unicode="\\u{1F44F}">
-                                                                        @endif
-                                                                    </li>
+                                                                            @elseif($reaction == '\u{1F44F}')
+                                                                                <img
+                                                                                    src="{{ asset('assets/front/img/clap-icon.png') }}"
+                                                                                    loading="lazy" alt="Clap Emoji"
+                                                                                    class="emoji" data-emoji="👏"
+                                                                                    data-unicode="\\u{1F44F}">
+                                                                                @endif
+                                                                                
+                                                                        </li>
+                                                                    @endforeach
                                                                     <p id="likeCount_{{ $post['id'] }}">
                                                                         {{ $post['total_likes'] }} Likes</p>
                                                                 </ul>
