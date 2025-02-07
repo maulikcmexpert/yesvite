56351ed7d6a8393e32bd4bd0d17bbd1d9d742fc8
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-08 01:30:55 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -668,20 +668,7 @@
                         @endforeach
                     @endforeach
                 @endforeach
-            <!-- @foreach ($categories as $textData)
-            
-                        <div class="col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs"
-                            data-wow-duration="2s" data-wow-delay="0" data-wow-offset="0"
-                            data-category-id="{{ $textData->categories->id }}" data-subcategory-id="{{ $textData->subcategories->id }}">
-                            <a href="#" class="collection-card card-blue">
-                                <div class="card-img">
-                                    <img src="{{ asset('storage/canvas/' . $textData->filled_image) }}" alt="shower-card">
-                                </div>
-                                <h4>{{ $textData->categories->category_name }}</h4>
-                            </a>
-                        </div>
-            
-            @endforeach -->
+           
 
               
                 {{-- <div class="col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown"
