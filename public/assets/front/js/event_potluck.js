29fde30864f9ca4bbbcf102a0389d9a0af32772a
Vimal <vimal.cmexpertise@gmail.com>
2025-02-07 13:31:48 +0530

    update codw

--- public/assets/front/js/event_potluck.js
+++ public/assets/front/js/event_potluck.js
@@ -675,7 +675,6 @@ $(document).on('click','#selfBringItem',function(){
 })
 
 $(document).on('click', '.itemTotalQnts', function() {
-    alert()
     var main_quantity = parseInt($("#sub_quantity").val(), 10);
     var self_quantity = parseInt($("#self_bring_qty").val(), 10);
     if (!isNaN(main_quantity) && !isNaN(self_quantity)) {
