bb7e589632b920f5b7dd1acae43743f5075a68e2
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-07 11:06:36 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -314,6 +314,8 @@ async function fetchStories(eventId, userId, isNewUpload, storyType) {
 
         if (Array.isArray(data.data.other_stories)) {
             data.data.other_stories.forEach((story) => {
+                console.log(story.user_id);
+                console.log(userId);
                 if (story.user_id !== userId) {
                     story.story.forEach((storyData) => {
                         const mediaElement = document.createElement(
