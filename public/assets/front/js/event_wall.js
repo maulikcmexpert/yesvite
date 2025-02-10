ac4e69e8c406dbae4da748d7039adf0cd0d61b9d
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-10 11:54:29 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1440,7 +1440,7 @@ $(document).on('click','.get_post_emoji_list',function(){
                 };
                 $("#nav-all-reaction-tab").html(`All  ${reactionDetail.total_count}`);
 
-                $(".tab-pane ul").html("");
+                // $(".tab-pane ul").html("");
 
                 $.each(reactionList, function (reaction, users) {
                     let tabId = "";
