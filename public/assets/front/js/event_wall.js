d89f90fcf5578fe63347db2ad010b2a30776ce72
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-10 11:32:28 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1434,45 +1434,45 @@ $(document).on('click','.get_post_emoji_list',function(){
 
                 $("#nav-all-reaction-tab").html(`All ${reactionDetail.total_count}`);
 
-                $(".tab-pane ul").html("");
+                // $(".tab-pane ul").html("");
 
-                $.each(reactionList, function (reaction, users) {
-                    let tabId = "";
-                    if (reaction === "\u{2764}") tabId = "nav-heart-reaction";
-                    else if (reaction === "\u{1F44D}") tabId = "nav-thumb-reaction";
-                    else if (reaction === "\u{1F604}") tabId = "nav-smily-reaction";
-                    else if (reaction === "\u{1F60D}") tabId = "nav-eye-heart-reaction";
-                    else if (reaction === "\u{1F44F}") tabId = "nav-clap-reaction";
+                // $.each(reactionList, function (reaction, users) {
+                //     let tabId = "";
+                //     if (reaction === "\u{2764}") tabId = "nav-heart-reaction";
+                //     else if (reaction === "\u{1F44D}") tabId = "nav-thumb-reaction";
+                //     else if (reaction === "\u{1F604}") tabId = "nav-smily-reaction";
+                //     else if (reaction === "\u{1F60D}") tabId = "nav-eye-heart-reaction";
+                //     else if (reaction === "\u{1F44F}") tabId = "nav-clap-reaction";
 
-                    let reactionHtml = "";
-                    users.forEach(user => {
-                        reactionHtml += `
-                            <li class="reaction-info-wrp">
-                                <div class="commented-user-head">
-                                    <div class="commented-user-profile">
-                                        <div class="commented-user-profile-img">
-                                            <img src="${user.profile}" alt="">
-                                        </div>
-                                        <div class="commented-user-profile-content">
-                                            <h3>${user.firstname} ${user.lastname}</h3>
-                                        </div>
-                                    </div>
-                                    <div class="posts-card-like-comment-right reaction-profile-reaction-img">
-                                        <img src="./assets/img/${reaction}-emoji.png" alt="">
-                                    </div>
-                                </div>
-                            </li>
-                        `;
-                    });
+                //     let reactionHtml = "";
+                //     users.forEach(user => {
+                //         reactionHtml += `
+                //             <li class="reaction-info-wrp">
+                //                 <div class="commented-user-head">
+                //                     <div class="commented-user-profile">
+                //                         <div class="commented-user-profile-img">
+                //                             <img src="${user.profile}" alt="">
+                //                         </div>
+                //                         <div class="commented-user-profile-content">
+                //                             <h3>${user.firstname} ${user.lastname}</h3>
+                //                         </div>
+                //                     </div>
+                //                     <div class="posts-card-like-comment-right reaction-profile-reaction-img">
+                //                         <img src="./assets/img/${reaction}-emoji.png" alt="">
+                //                     </div>
+                //                 </div>
+                //             </li>
+                //         `;
+                //     });
 
-                    if (tabId) {
-                        $(`#${tabId} ul`).append(reactionHtml);
-                    }
+                //     if (tabId) {
+                //         $(`#${tabId} ul`).append(reactionHtml);
+                //     }
 
-                    // Update Reaction Counts in Tabs
-                    let reactionCount = reactionDetail.reaction_count[reaction] || 0;
-                    $(`#${tabId}-tab`).html(`<img src="./assets/img/${reaction}-emoji.png" alt=""> ${reactionCount}`);
-                });
+                //     // Update Reaction Counts in Tabs
+                //     let reactionCount = reactionDetail.reaction_count[reaction] || 0;
+                //     $(`#${tabId}-tab`).html(`<img src="./assets/img/${reaction}-emoji.png" alt=""> ${reactionCount}`);
+                // });
 
                 // Show modal
                 $("#reaction-modal").modal("show");
