9b27272388f8b13d0234a1d8b1b0542984232643
Vimal <vimal.cmexpertise@gmail.com>
2025-02-10 12:03:21 +0530

    Update step2.blade.php

--- resources/views/front/event/step2.blade.php
+++ resources/views/front/event/step2.blade.php
@@ -86,12 +86,13 @@
             </div>
             <h5 class="total-items ms-auto total_design_count">{{$imagecount}} Items</h5>
         </div>
-        <div class="row list_all_design_catgeory_image">
+      
+        <div class="row list_all_design_catgeory">
             
             @foreach ($categories as $category)
             @foreach ($category->subcategory as $subcategory)
                 @foreach ($subcategory->textdatas as $image)
-                    <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6 mt-xl-4 mt-sm-4 mt-4  image-item-new all_designs"
+                    <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-3 col-sm-4 col-6 mt-xl-4 mt-sm-4 mt-4  image-item all_designs"
                         data-category-id="{{ $category->id }}" 
                         data-subcategory-id="{{ $subcategory->id }}" 
                        >
@@ -104,15 +105,19 @@
                                 <img src="{{ asset('storage/canvas/' . $image->filled_image) }}"
                                     alt="shower-card">
                             </div>
-                            {{-- <h4>{{ $category->category_name }}</h4> --}}
+                            <h4>{{ $category->category_name }}</h4>
                         </a>
                     </div>
+                @continue
                 @endforeach
-            @endforeach
+                @continue
+                @endforeach
         @endforeach
           
       
-        </div>  
+        </div>
+
+
         <div class="row list_all_design_catgeory">
             
             @foreach ($categories as $category)
@@ -123,25 +128,23 @@
                         data-subcategory-id="{{ $subcategory->id }}" 
                        >
                         <a href="javascript:;" class="collection-card card-blue">
-                            <div class="card-img design-card" >
+                            <div class="card-img edit_design_tem design-card"  
+                            data-image="{{ asset('storage/canvas/' . $image->image) }}"
+                                data-shape_image="{{ $image->shape_image != '' ? asset('storage/canvas/' . $image->shape_image) : '' }}"
+                                data-json="{{ json_encode($image->static_information) }}"
+                                data-id="{{ $image->id }}">
                                 <img src="{{ asset('storage/canvas/' . $image->filled_image) }}"
                                     alt="shower-card">
                             </div>
                             <h4>{{ $category->category_name }}</h4>
                         </a>
                     </div>
-                @continue
                 @endforeach
-                @continue
-                @endforeach
-                @continue
+            @endforeach
         @endforeach
           
       
         </div>
-
-
-       
     </div>
 </section>
 </div>
