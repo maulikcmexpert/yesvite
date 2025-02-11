faee7128f25f7b884bec67bd67700c7f13ca47e8
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-11 12:14:39 +0530

    Update event_photo.js

--- public/assets/front/js/event_photo.js
+++ public/assets/front/js/event_photo.js
@@ -212,6 +212,7 @@ $(document).ready(function () {
                         // ✅ User has liked the post, update the reaction image
                         console.log("Like given, updating reaction image...");
                         if (reactionIcons[selectedEmoji]) {
+                            console.log (reactionIcons[selectedEmoji]);
                             reactionImageHtml = `<img src="${reactionIcons[selectedEmoji]}" alt="Reaction Emoji">`;
                         }
                         button.addClass("liked"); // Add liked class
