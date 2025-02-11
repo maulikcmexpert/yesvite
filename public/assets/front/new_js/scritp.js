395012c5661c68c1f851b8740a81d3b939557c5b
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-11 12:37:42 +0530

    commit

--- public/assets/front/new_js/scritp.js
+++ public/assets/front/new_js/scritp.js
@@ -1126,9 +1126,22 @@ if (hiddenData != undefined) {
 }
 
 $(document).on('click', '.notification-showall-btn', function() {
-    $('.notification_drp_down_div').each(function(index) {
-        let count = index + 1; 
-        $('#collapseOne'+count).addClass('show');
-        $('.main-notification-div-list').removeClass('collapsed');
-    });
+    var show=$(this).attr('data-show');
+    if(show=="0"){
+        $(this).text('Minimize All');
+        $(this).attr('data-show','1');
+        $('.notification_drp_down_div').each(function(index) {
+            let count = index + 1; 
+            $('#collapseOne'+count).addClass('show');
+            $('.main-notification-div-list').removeClass('collapsed');
+        });
+    }else{
+        $(this).text('Show All');
+        $('.notification_drp_down_div').each(function(index) {
+            let count = index + 1; 
+            $('#collapseOne'+count).removeClass('show');
+            $('.main-notification-div-list').addClass('collapsed');
+        });
+    }
+   
 });
