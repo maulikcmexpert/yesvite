aab531be5fed5a7e9ab7b8a50d6ca9420e7a9d84
Vimal <vimal.cmexpertise@gmail.com>
2025-02-11 14:33:15 +0530

    Update event.js

--- public/assets/front/js/event.js
+++ public/assets/front/js/event.js
@@ -10,13 +10,13 @@ function getActiveTabPage() {
 }
 getActiveTabPage();
 $(document).on('click','#confirm_cancel_event_btn',function (event) {
-    alert(1)
     event.stopPropagation(); // Prevents event bubbling
     event.preventDefault();
     var event=parseInt($('#cancel_event_id').val());
     var reason=$('#reason_to_cancel_event').val();
     var cancel=$('#type_cancel').val();
     var isWall=$('#isWall').val();
+    alert(isWall)
 
 
     if(reason==""){
@@ -543,6 +543,9 @@ function get_month_data(current_month){
 $(document).on('click','.cancel_event_option',function () {
     var event_id=$(this).data('event_id');
     var isWall=$(this).data('isWall') || "0";
+    alert("isWall1")
+    alert(isWall)
+    alert("isWall2")
     console.log(event_id);
     $('#reason_to_cancel_event').val('');
     $('#type_cancel').val('');
