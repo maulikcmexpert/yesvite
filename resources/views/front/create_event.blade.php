943831053a0d1e35f764e75201c83380949b6bfc
Vimal <vimal.cmexpertise@gmail.com>
2025-02-11 19:40:54 +0530

    update code

--- resources/views/front/create_event.blade.php
+++ resources/views/front/create_event.blade.php
@@ -463,11 +463,11 @@
                 <h4>$15.00</h4>
             </div>
             <div class="invite-pro">
-                <div class="invite-left">
+                <div id="buycredits" class="invite-left">
                     <h6 class="invite-count">0</h6>
                     {{-- <p id="invite-left"><strong>0</strong>Left</p> --}}
                     <input type="hidden" id="event_guest_left_count" />
-                    <p class="invite-left_d"><strong>Invites | <p id="buycredits" class="coin-invite"><img src="https://yesvite.cmexpertiseinfotech.in/assets/front/image/credit-coin-img.png" alt=""></p><span
+                    <p class="invite-left_d"><strong>Invites | <p  class="coin-invite"><img src="https://yesvite.cmexpertiseinfotech.in/assets/front/image/credit-coin-img.png" alt=""></p><span
                                 class="available-coins">{{ $coins }}</span></strong></p>
                     <input type="hidden" id="currentInviteCount" value="0">
                 </div>
