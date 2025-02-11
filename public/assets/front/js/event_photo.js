db96ff342c2bfd18ce4c2dda5fd0a154ec234032
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-11 16:28:35 +0530

    Update event_photo.js

--- public/assets/front/js/event_photo.js
+++ public/assets/front/js/event_photo.js
@@ -1768,51 +1768,79 @@ $(document).ready(function () {
 
     //     return reactionHtml + likeCountHtml;
     // }
+    // function renderReactions(post) {
+    //     let reactionList = post.reactionList || [];
+    //     let selfReaction = post.self_reaction;
+    //     let reactionHtml = "";
+    //     let j = 0;
+    //     let i = 0;
+
+    //     for (let reaction of reactionList) {
+    //         if (i >= 3) break; // Limit to 3 reactions
+
+    //         let emojiSrc = reactionIcons[reaction] || null;
+
+    //         if (emojiSrc) {
+    //             let listItemId =
+    //                 j === 0 && selfReaction === reaction
+    //                     ? `id="reactionImage_model_${post.id}"`
+    //                     : "";
+    //             reactionHtml += `<li ${listItemId} style="display:flex;"><img src="${emojiSrc}" alt="Emoji"></li>`;
+    //             if (j === 0 && selfReaction === reaction) j++;
+    //             i++;
+    //         }
+    //     }
+
+    //     if (j === 0 && i < 3) {
+    //         reactionHtml += `<li id="reactionImage_model_${post.id}" style="display:flex;"></li>`;
+    //     }
+
+    //     let likeCountHtml = `<p id="likeCount_${post.id}">${post.total_likes} Likes</p>`;
+
+    //     return reactionHtml + likeCountHtml;
+    // }
     function renderReactions(post) {
         let reactionList = post.reactionList || [];
         let selfReaction = post.self_reaction;
         let reactionHtml = "";
         let i = 0; // Count displayed reactions
-
+    
         for (let reactionData of reactionList) {
             if (i >= 3) break; // Limit to 3 reactions
-
+    
             let { reaction, firstname, lastname, profile } = reactionData;
-
+    
             let emojiSrc = reactionIcons[reaction] || null; // Get emoji image
             if (emojiSrc) {
                 let listItemId =
                     i === 0 && selfReaction === reaction
                         ? `id="reactionImage_model_${post.id}"`
                         : "";
-
-                // Generate user profile section
-                let profileImage = profile
-                    ? `<img src="${profile}" alt="${firstname}" class="reaction-user-img">`
-                    : `<div class="reaction-user-initial">${firstname[0].toUpperCase()}${lastname[0].toUpperCase()}</div>`;
-
+    
+              
+               
+    
                 // Append reaction
                 reactionHtml += `
                     <li ${listItemId} style="display:flex; align-items:center;">
-                        ${profileImage}
-                        <span class="reaction-user-name">${firstname} ${lastname}</span>
+                       
+                        
                         <img src="${emojiSrc}" alt="Emoji">
                     </li>`;
                 i++;
             }
         }
-
+    
         // If no reactions found, show an empty reaction placeholder
         if (i === 0) {
             reactionHtml += `<li id="reactionImage_model_${post.id}" style="display:flex;"></li>`;
         }
-
+    
         let likeCountHtml = `<p id="likeCount_${post.id}">${post.total_likes} Likes</p>`;
-
+    
         return reactionHtml + likeCountHtml;
     }
-
-
+    
     $(document).on("click", "#likeButtonModel", function () {
         console.log("asd");
         setTimeout(function () {
