23091f7ebb7ec2dafc3d12919df5b55921cb207c
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-12 18:12:41 +0530

    Update event_photo.js

--- public/assets/front/js/event_photo.js
+++ public/assets/front/js/event_photo.js
@@ -1108,7 +1108,7 @@ $(document).ready(function () {
 
                     if (data.user_id == login_user_id) {
 
-                        $("#report_btn").hide(); 
+                        $("#report_btn").hide();
 
                     }
 
