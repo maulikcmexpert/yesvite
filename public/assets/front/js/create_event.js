be897ed01ef9280e09d854b7c42b0dbed4b4504a
Vimal <vimal.cmexpertise@gmail.com>
2025-02-12 11:26:20 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -4675,7 +4675,7 @@ $(document).on("click", ".li_design .pick-card", function (e) {
     li_design_click();
 });
 $(document).on("click", ".li_design .edit-design-sidebar", function (e) {
-    $("#close_createEvent").css("display", "block");
+    // $("#close_createEvent").css("display", "block");
     e.preventDefault();
     $(".subcategory-section").hide();
     $(".design-span").addClass("active");
