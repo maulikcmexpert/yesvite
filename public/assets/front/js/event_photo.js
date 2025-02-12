1599fc7b98eaa4527e52c4601ad2647ee39f5bce
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-12 18:07:51 +0530

    commit

--- public/assets/front/js/event_photo.js
+++ public/assets/front/js/event_photo.js
@@ -1104,6 +1104,12 @@ $(document).ready(function () {
                         $("#host_display").text("co_host");
                         $("#host_display").addClass('host');
                     }
+                    const login_user_id = $('#login_user_id').val();
+                    if (data.user_id != login_user_id) {
+
+                        $("#report_btn").css("display:none");
+                       
+                    }
 
                     $(".likeModel")
                         .data("event-id", data.event_id)
