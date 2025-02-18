7553d48a301df4e5b8ea660471631160558e91b4
Vimal <vimal.cmexpertise@gmail.com>
2025-02-18 14:30:11 +0530

    Update create_event.blade.php

--- resources/views/front/create_event.blade.php
+++ resources/views/front/create_event.blade.php
@@ -3335,7 +3335,7 @@
 
 
 
-
+{{dd($eventDetail['isCopy'])}}
 
 {{-- <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
