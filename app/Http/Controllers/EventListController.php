0d930048a849a25cd0afd7dd0a84d72657060795
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-18 19:32:11 +0530

    Update EventListController.php

--- app/Http/Controllers/EventListController.php
+++ app/Http/Controllers/EventListController.php
@@ -2607,7 +2607,7 @@ class EventListController extends BaseController
                     $rsvp_attempt =  'yes_to_no';
                 }
 
-                // dd($adults,$kids,$rsvpSent);
+                dd($adults,$kids,$rsvpSent);
 
                 if ($rsvpSentAttempt == "1" && $rsvp_status == "1" && ($adults==$rsvpSent->adults && $kids==$rsvpSent->kids)) {
                     return response()->json(['status' => 3, 'text' => 'You have already done rsvp yes']);
