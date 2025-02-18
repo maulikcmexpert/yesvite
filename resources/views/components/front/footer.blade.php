7ad29562c4e050a3c14a0676723202dafd66dc23
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-02-18 18:50:26 +0530

    commit

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -54,7 +54,7 @@
                         <h6>Kids</h6>
                         <div class="qty-container ms-auto">
                           <button class="qty-btn-minus rsvp_minus_notify" type="button"><i class="fa fa-minus"></i></button>
-                          <input type="number" name="rsvp_notification_kids" id="rsvp_notification_kids"value="0" class="input-qty" readonly>
+                          <input type="number" name="rsvp_notification_kids" id="rsvp_notification_kids" value="0" class="input-qty" readonly>
                           <button class="qty-btn-plus rsvp_plus_notify" type="button"><i class="fa fa-plus"></i></button>
                         </div>
                       </div>
