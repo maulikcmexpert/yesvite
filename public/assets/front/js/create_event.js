56ee8b4e7dc208b614edc104128f7cc866116c5b
Vimal <vimal.cmexpertise@gmail.com>
2025-02-18 15:07:41 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -4889,6 +4889,11 @@ $(document).on("click", ".li_design .edit-design", function (e) {
 });
 
 $(document).on("click", ".li_design .pick-card",async function (e) {
+    var design = eventData.desgin_selected;
+    if (design == undefined || design == "") {
+        await saveDesignData();
+        design = eventData.desgin_selected;
+    }
     if (isCohost != "0") {
         $("#close_editEvent").css("display", "block");
     }
@@ -4897,11 +4902,7 @@ $(document).on("click", ".li_design .pick-card",async function (e) {
     e.preventDefault();
     $(".subcategory-section").show();
     li_design_click();
-    var design = eventData.desgin_selected;
-    if (design == undefined || design == "") {
-        await saveDesignData();
-        design = eventData.desgin_selected;
-    }
+   
 });
 $(document).on("click", ".li_design .edit-design-sidebar", function (e) {
     // $("#close_createEvent").css("display", "block");
@@ -9807,6 +9808,12 @@ if (final_step == "1" && isCohost == "1") {
 colorchange();
 
 $(document).on("click", ".previousImeg", async function (e) {
+    var design = eventData.desgin_selected;
+    if (design == undefined || design == "") {
+        await saveDesignData();
+        design = eventData.desgin_selected;
+    }
+
     if (isCohost != "0") {
         $("#close_editEvent").css("display", "block");
     }
@@ -9815,9 +9822,5 @@ $(document).on("click", ".previousImeg", async function (e) {
     e.preventDefault();
     $(".subcategory-section").show();
     li_design_click();
-    var design = eventData.desgin_selected;
-    if (design == undefined || design == "") {
-        await saveDesignData();
-        design = eventData.desgin_selected;
-    }
+    
 });
