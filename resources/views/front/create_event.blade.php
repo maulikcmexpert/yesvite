0e9b13ba3cad9b7b7a996665ad76c65578725c5f
Vimal <vimal.cmexpertise@gmail.com>
2025-02-18 14:28:29 +0530

    Update create_event.blade.php

--- resources/views/front/create_event.blade.php
+++ resources/views/front/create_event.blade.php
@@ -3329,7 +3329,7 @@
 <input type="hidden" id="cohostLname" value="{{ $cohostLname }}">
 <input type="hidden" id="cohostemail" value="{{ $cohostemail }}">
 <input type="hidden" id="cohostphone_number" value="{{ $cohostphone_number }}">
-<input type="hidden" id="isCopy" value="{{ $eventDetail['isCopy'] }}">
+<input type="hidden" id="isCopy" value="{{ $eventDetail['isCopy'] ?$eventDetail['isCopy']:''  }}">
 <input type="hidden" id="alreadyCount" value="{{ $eventDetail['alreadyCount'] }}">
 <input type="hidden" id="totalCategoryItem" value="{{ $totalCategoryItem }}">
 
