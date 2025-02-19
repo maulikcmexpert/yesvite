5454f59224aa59a382f58b15a947305d7c4e85cb
Vimal <vimal.cmexpertise@gmail.com>
2025-02-19 17:18:49 +0530

    Update rsvp.blade.php

--- resources/views/front/rsvp.blade.php
+++ resources/views/front/rsvp.blade.php
@@ -190,7 +190,7 @@
                                               </h2>
                                               <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                                 <div class="accordion-body">
-                                                    <div class="rsvp-calender-body-head">
+                                                    {{-- <div class="rsvp-calender-body-head">
                                                         <h3>
                                                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                 <path d="M7.9987 14.6668C11.6654 14.6668 14.6654 11.6668 14.6654 8.00016C14.6654 4.3335 11.6654 1.3335 7.9987 1.3335C4.33203 1.3335 1.33203 4.3335 1.33203 8.00016C1.33203 11.6668 4.33203 14.6668 7.9987 14.6668Z" stroke="#0F172A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -199,7 +199,7 @@
                                                             </svg>
                                                             ADD To
                                                         </h3>
-                                                    </div>
+                                                    </div> --}}
                                                  
                                                     <a href="javascript:;" id="openOutlook" class="add-calender btn">
                                                         <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
