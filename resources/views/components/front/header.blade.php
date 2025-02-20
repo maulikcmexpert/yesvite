39dccde7e4b52104f6560d464c46706890b128cf
Vimal <vimal.cmexpertise@gmail.com>
2025-02-20 15:46:27 +0530

    commit

--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -2,7 +2,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
-
 <!-- Latest compiled and minified CSS -->
 <link rel="icon" type="image/x-icon" href="{{ asset('storage/favicon.png')}}">
 <!-- custom-style -->
