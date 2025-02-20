04980f039bb3e034a82363ceb1e2f81030356446
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-20 17:12:22 +0530

    Update step2.blade.php

--- resources/views/front/event/step2.blade.php
+++ resources/views/front/event/step2.blade.php
@@ -420,6 +420,19 @@
             // $('input[name="design_subcategory"]:not(#Allcat)').prop("checked", true);
             $("#Allcat").prop('checked', true)
             $('.image-item').show();
+            $('input[name="design_subcategory"]:not(#Allcat):checked').each(
+                    function () {
+                        const categoryId = $(this).data("category-id");
+                        const subcategoryId = $(this).data("subcategory-id");
+
+                        // Show images matching the selected categories and subcategories
+                        $(
+                            `.image-item[data-category-id="${categoryId}"]`
+                        ).show();
+                        var visibleItems = $(".all_designs:visible").length;
+                        $(".total_design_count").text(visibleItems + " Items");
+                    }
+                );
             var visibleItems = $('.all_designs:visible').length;
             $('.total_design_count').text(visibleItems + ' Items');
             if ($("#search_design_category").val() == "") {
