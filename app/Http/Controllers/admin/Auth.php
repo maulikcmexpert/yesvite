00a9a3662415db68d3496f41d0e99ccec6312227
Vimal <vimal.cmexpertise@gmail.com>
2025-02-21 19:15:10 +0530

    Update Auth.php

--- app/Http/Controllers/admin/Auth.php
+++ app/Http/Controllers/admin/Auth.php
@@ -59,14 +59,14 @@ class Auth extends Controller
 
                 $saveOtp =   Admin::where("id", $adminData->id)->first();
 
-                // if (config('app.debug', true)) {
-                //         $saveOtp->otp = '111111';
-                //         $saveOtp->save();
-                //         $sendMesage = [
-                //             "status" => true,
-                //             "message" => "success"
-                //         ];
-                // }else{
+                if (config('app.debug', true)) {
+                        $saveOtp->otp = '111111';
+                        $saveOtp->save();
+                        $sendMesage = [
+                            "status" => true,
+                            "message" => "success"
+                        ];
+                }else{
                         $token = str_pad(random_int(0, 9999), 6, '0', STR_PAD_LEFT);
                         session()->forget('otp');
                         session(['otp' => $token]);
@@ -87,7 +87,7 @@ class Auth extends Controller
                             ];
                         }
                        
-            // }
+            }
 
                 if ($sendMesage['status']  == true) {
                     return Redirect::to(URL::to('admin/factor_authenticate', encrypt($adminData->id)))->with('msg', 'Verification code is sent successfully');
