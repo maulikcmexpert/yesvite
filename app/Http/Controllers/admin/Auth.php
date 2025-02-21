6c97defd5bbbdd9be859a47433ac8749ee786405
Vimal <vimal.cmexpertise@gmail.com>
2025-02-21 19:12:00 +0530

    Update Auth.php

--- app/Http/Controllers/admin/Auth.php
+++ app/Http/Controllers/admin/Auth.php
@@ -72,8 +72,7 @@ class Auth extends Controller
                         session(['otp' => $token]);
                         $saveOtp->otp = $token;
                         $saveOtp->save();
-                        dd($adminData->phone_number);
-                        if($adminData->phone_number!=""){
+                        if($adminData->phone_number!=""||$adminData->phone_number!=null){
                             $phoneNumber = '+' . $adminData->country_code . $adminData->phone_number;
                             $message = "Your verification code for Admin is: " . $token;
                             $sendMesage =  sendSMS($phoneNumber, $message);
