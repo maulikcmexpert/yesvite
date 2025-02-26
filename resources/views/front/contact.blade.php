9cd0804a8dcae88be138821eb39ffa56eaf6ab69
Vimal <vimal.cmexpertise@gmail.com>
2025-02-26 09:59:37 +0530

    Update contact.blade.php

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -729,4 +729,112 @@
     </div>
   </div>
 
+  <div id="sidebar_groups" class="sidebar setting-side-wrp new-sidebar-group-wrp">
+
+    <div class="sidebar-content guest-sidebar">
+        <div class="d-flex align-items-center justify-content-between toggle-wrp">
+            <h5>Groups</h5>
+            <button class="close-btn group_toggle_close_btn" onclick="toggleSidebar()">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.00098 5L19 18.9991" stroke="#64748B" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round"></path>
+                    <path d="M4.99996 18.9991L18.999 5" stroke="#64748B" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="position-relative">
+            <input type="search" placeholder="Search group names" class="form-control"
+                id="group_toggle_search">
+            <span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+                        stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
+                    <path d="M22 22L20 20" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round"></path>
+                </svg>
+            </span>
+        </div>
+        <div class="group_list group_search_list_toggle">
+            @foreach ($groups as $group)
+                <div class="group-card added_group{{ $group->id }} listgroups view_members"
+                    data-id="{{ $group->id }}">
+                    <div class="view_members" data-id="{{ $group->id }}">
+                        <h4>{{ $group->name }}</h4>
+                        <p>{{ $group->group_members_count }} Guests</p>
+                    </div>
+                    <span class="ms-auto me-3">
+                        <svg width="16" id="delete_group" data-id="{{ $group->id }}" height="17"
+                            viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M14 4.48665C11.78 4.26665 9.54667 4.15332 7.32 4.15332C6 4.15332 4.68 4.21999 3.36 4.35332L2 4.48665"
+                                stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round">
+                            </path>
+                            <path
+                                d="M5.66699 3.81301L5.81366 2.93967C5.92033 2.30634 6.00033 1.83301 7.12699 1.83301H8.87366C10.0003 1.83301 10.087 2.33301 10.187 2.94634L10.3337 3.81301"
+                                stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round">
+                            </path>
+                            <path
+                                d="M12.5669 6.59375L12.1336 13.3071C12.0603 14.3537 12.0003 15.1671 10.1403 15.1671H5.86026C4.00026 15.1671 3.94026 14.3537 3.86693 13.3071L3.43359 6.59375"
+                                stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round">
+                            </path>
+                            <path d="M6.88672 11.5H9.10672" stroke="#94A3B8" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"></path>
+                            <path d="M6.33301 8.83301H9.66634" stroke="#94A3B8" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg>
+                    </span>
+                    <span class="view_members" data-id="{{ $group->id }}">
+                        <svg width="16" height="17" viewBox="0 0 16 17" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M5.94043 13.7797L10.2871 9.43306C10.8004 8.91973 10.8004 8.07973 10.2871 7.56639L5.94043 3.21973"
+                                stroke="#94A3B8" stroke-width="1.5" stroke-miterlimit="10"
+                                stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg>
+                    </span>
+                </div>
+            @endforeach
+
+        </div>
+
+
+        <!-- =========================================  group-sidebar ===================================== -->
+
+        <!-- <div class="groupsidebar">
+            <div class="d-flex align-items-center justify-content-between toggle-wrp">
+                <h5 >Create New Group</h5>
+                <button class="close-btn" onclick="toggleSidebar()">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5.00098 5L19 18.9991" stroke="#64748B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M4.99996 18.9991L18.999 5" stroke="#64748B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="guest-group-name login-form-wrap">
+                <form action="">
+                    <div class="input-form">
+                        <input class="form-control" type="email" id="email" name="email">
+                        <label for="email" class="floating-label">Group Name <span>*</span></label>
+                    </div>
+                </form>
+            </div>
+        </div> -->
+    </div>
+
+    <div class="new-event-btn">
+        <a href="#" class="cmn-btn new_group">Add new
+            Group</a>
+    </div>
+</div>
+
+<div id="sidebar_groups_overlay" class="overlay" onclick="toggleSidebar()"></div>
+
  
