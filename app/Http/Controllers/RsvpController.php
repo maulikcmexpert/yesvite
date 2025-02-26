da1c9a0e0052380c261862211ed3b151d452fd73
Vimal <vimal.cmexpertise@gmail.com>
2025-02-26 16:28:31 +0530

    Update RsvpController.php

--- app/Http/Controllers/RsvpController.php
+++ app/Http/Controllers/RsvpController.php
@@ -445,7 +445,7 @@ class RsvpController extends BaseController
             }
 
             $is_host = "";
-            if ($user_id == $eventDetail->user_id) {
+            if ($user_id == $eventDetail->user_id && $isShare==null) {
                 $is_host = "1";
             }
 
