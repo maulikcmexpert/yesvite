82d50b842f8890cac789b8f40ca9318b2b3a6c96
Vimal <vimal.cmexpertise@gmail.com>
2025-02-26 19:32:45 +0530

    Update EventPost.php

--- app/Models/EventPost.php
+++ app/Models/EventPost.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\{Event, User, EventPostImage, EventPostPoll, EventPostComment, EventPostReaction, Notification, EventInvitedUser, PostControl, UserReportToPost};
+use App\Models\{Event, User, EventPostImage, EventPostPoll, EventPostComment, EventPostReaction, Notification, EventInvitedUser, PostControl, UserReportToPost,contact_sync};
 
 class EventPost extends Model
 {
