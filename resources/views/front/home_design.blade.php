6bdfda1b496f46052caa88b28a3974d67f9434d0
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-27 10:51:21 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -189,7 +189,7 @@
                         data-wow-duration="2s" data-wow-delay="0" data-wow-offset="0"
                         data-category-id="{{ $category->id }}" data-subcategory-id="{{ $subcategory->id }}"
                         data-category_name="{{ $category->category_name }}">
-                        <a href="javascript:;" class="collection-card card-blue">
+                        <a href="#" class="collection-card card-blue">
                             <div class="card-img">
                                 <img src="{{ asset('storage/canvas/' . $image->filled_image) }}"
                                     alt="shower-card">
