2c0a80ff22664e39d9ce00deb660039199409cbb
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-27 18:02:23 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -139,13 +139,12 @@
             @endphp
 
             @foreach ($allImages as $image)
-                <div id="design_category"
-                    class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs"
+                <div
+                    class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs {{in_array($image['imageId'], $randomIds) ? '' : 'd-none'}}"
                     data-wow-duration="2s" data-wow-delay="0" data-wow-offset="0"
                     data-category-id="{{ $image['category_id'] }}"
                     data-subcategory-id="{{ $image['subcategory_id'] }}"
-                    data-category_name="{{ $image['category_name'] }}" {{in_array($image['imageId'], $randomIds)}}
-                    style="{{ in_array($image['imageId'], $randomIds) ? '' : 'display: none' }}">
+                    data-category_name="{{ $image['category_name'] }}"  >
 
                     <div class="card-img collection-card card-blue">
                         <img src="{{ $image['image_path'] }}" alt="shower-card">
