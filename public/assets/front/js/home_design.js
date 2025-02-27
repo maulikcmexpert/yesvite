099e0ed695b81a2ded16b3f7cfbfc02a7421fd3e
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-27 16:08:10 +0530

    Update home_design.js

--- public/assets/front/js/home_design.js
+++ public/assets/front/js/home_design.js
@@ -57,10 +57,6 @@ $(document).on('change', 'input[name="design_subcategory"]:not(#Allcat)', functi
         anyChecked = true;
     });
 
-    // if (!anyChecked) {
-    //     // If no checkboxes are checked, show default images again
-    //     $(".image-item").show();
-    // }
 
     updateTotalCount();
 });
