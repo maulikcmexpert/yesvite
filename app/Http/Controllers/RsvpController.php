bbb875b9c7d29af0a6d9198656914eb075c74a5e
Vimal <vimal.cmexpertise@gmail.com>
2025-02-27 16:40:13 +0530

    Update RsvpController.php

--- app/Http/Controllers/RsvpController.php
+++ app/Http/Controllers/RsvpController.php
@@ -1039,7 +1039,7 @@ class RsvpController extends BaseController
                         'rsvp_attempt' => $rsvp_attempt
                     ];
                 } 
-                dd($notificationParam);
+                // dd($notificationParam);
             DB::commit();
             
             sendNotification('sent_rsvp', $notificationParam);
