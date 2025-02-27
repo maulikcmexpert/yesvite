435f80ec5b320631874ce980c3f1a2977f0b96db
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-27 10:32:39 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -183,11 +183,11 @@
                     $firstTextData = $firstSubcategory ? $firstSubcategory->textdatas->first() : null; // Get first textdata
                 @endphp
 
-                @if ($firstTextData)
+
                     <div id="design_category"
                         class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs"
                         data-wow-duration="2s" data-wow-delay="0" data-wow-offset="0"
-                        data-category-id="{{ $category->id }}" data-subcategory-id="{{ $firstSubcategory->id }}"
+                        data-category-id="{{ $category->id }}" data-subcategory-id="{{ $subcategory->id }}"
                         data-category_name="{{ $category->category_name }}">
                         <a href="javascript:;" class="collection-card card-blue">
                             <div class="card-img">
@@ -197,7 +197,7 @@
                             {{-- <h4>{{ $category->category_name }}</h4> --}}
                         </a>
                     </div>
-                @endif
+
             @endforeach
             @endforeach
             @endforeach
