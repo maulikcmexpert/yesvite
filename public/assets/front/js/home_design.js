5591389ddb4557ad9827642c8b3f838db68496e4
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-27 18:21:47 +0530

    commit

--- public/assets/front/js/home_design.js
+++ public/assets/front/js/home_design.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     $(".image-item-new").hide(); // Hide new images initially
     $('input[name="design_subcategory"]').prop('checked', false);
     $('#Allcat').prop('checked', false);
-
+    $(".image-item").removeClass('d-none');
     updateTotalCount();
 
 
@@ -21,7 +21,8 @@ $(document).ready(function () {
 
     $('#Allcat').on('change', function () {
         $(".image-item").show(); // Show all default images
-        $(".image-item-new").hide(); // Hide new images initially
+
+
 
         if ($(this).is(':checked')) {
             // Show all default images and hide new images
