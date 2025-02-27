16ac9cf29672d3e86e274f7a5809ba4353266918
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-27 16:40:22 +0530

    commit

--- public/assets/front/js/home_design.js
+++ public/assets/front/js/home_design.js
@@ -24,23 +24,12 @@ $(document).ready(function () {
         $(".image-item-new").hide(); // Hide new images initially
 
         if ($(this).is(':checked')) {
-            // Show all default images and hide new images
-            $('.image-item').show();
-            $('.image-item-new').hide();
-
-            // Hide category name and checkbox container
-            $("#category_name").hide();
-            $("#allchecked").hide();
-
-            // Check all subcategory checkboxes
             $('input[name="design_subcategory"]').prop('checked', true);
+            $("#category_name, #allchecked").hide();
         } else {
-            // Uncheck all subcategories
             $('input[name="design_subcategory"]').prop('checked', false);
-
-            // Hide all images
-            $('.image-item').hide();
-            $('.image-item-new').hide();
+            $(".image-item").show();
+            $(".image-item-new").hide();
         }
 
         updateTotalCount();
