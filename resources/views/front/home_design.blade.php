7704de22db07b07b57d165fb5820f2e432e41060
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-28 11:47:18 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -227,8 +227,8 @@
 @push('scripts')
 <script>
     var designData = [];
-// var is_random =  $randomIds;
-    @foreach ($categoroies as $category)
+var is_random =  $randomIds;
+    @foreach ($categories as $category)
         var categoryData = {
             id: {{ $category->id }},
             name: "{{ $category->category_name }}",
