0bafff74a4036855a6863b636cb5196d31533578
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-02-28 10:33:38 +0530

    comit

--- public/assets/front/js/home_design.js
+++ public/assets/front/js/home_design.js
@@ -136,7 +136,7 @@ $(document).ready(function () {
                     .subcategories.find(s => s.id == subcategoryId).images;
 
                 let imageHTML = images.map(image => `
-                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs">
                         <div class="card-img collection-card card-blue">
                             <img src="${image.image_path}" alt="design">
                         </div>
