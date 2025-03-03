de05029b210d5b199d18bd47fc4db820bab366cf
Vimal <vimal.cmexpertise@gmail.com>
2025-03-03 16:56:57 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -1260,6 +1260,12 @@ class EventController extends BaseController
             Session::forget('shape_image');
         }
         if ($event_creation && $request->isdraft == "1") {
+            // $checkUserInvited = Event::withCount('event_invited_user')->where('id', $eventId)->first();
+            $notificationParam = [
+                'sender_id' => $user_id,
+                'event_id' => $eventId,
+                'post_id' => ""
+            ];
             sendNotification('owner_notify', $notificationParam);
 
             return 1;
