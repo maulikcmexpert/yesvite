586ae30d07f7522ed060a1b5d0a927de4c5a8fd6
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-03 19:16:32 +0530

    Update wall_navbar.blade.php

--- resources/views/components/event_wall/wall_navbar.blade.php
+++ resources/views/components/event_wall/wall_navbar.blade.php
@@ -30,15 +30,16 @@
            aria-selected="{{ ($page == 'about' ? 'true' : 'false') }}">
             About
         </a>
-
-
+@php
+     $guest_count =   $eventDetails['guest_list_visible_to_guests'] ?? '' ;
+     @endphp
         <a href="{{ route('event.event_guest', ['id' => encrypt($event)]) }}"
            class="nav-link {{ $page == 'guest' ? 'active' : '' }}"
            id="nav-guests-tab"
            role="tab"
            aria-controls="nav-guests"
            aria-selected="{{ $page == 'guest' ? 'true' : 'false' }}">
-            Guests
+           Guests({{ $guest_count }})
         </a>
 
 
