e53d3261958a47e303c1f2e3fff2df2b59c45204
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-03 12:44:10 +0530

    commit

--- resources/views/front/event/step2.blade.php
+++ resources/views/front/event/step2.blade.php
@@ -338,7 +338,7 @@
                 $("#allchecked").hide();
                 $("#Allcat").prop("checked", false);
                 $('input[name="design_subcategory"]:not(#Allcat)').prop('checked', false);
-
+                $(".selected-items").find(`[data-value='${value}']`).remove();
                 var visibleItems = $('.image-item:visible').length;
                 $('.total_design_count').text($('.default_show:visible').length + ' Items');
             });
