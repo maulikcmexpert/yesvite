84bbfd007b02704d5adbcdca9fa2ebb523d49528
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-03 10:54:35 +0530

    commit

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -57,6 +57,7 @@
                         <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
                             Categories
                         </button>
+
                         <div class="dropdown-menu collection-menu">
                             <div class="filter-head">
                                 <h5>Categories</h5>
@@ -113,6 +114,8 @@
                     </div>
                 </div>
             </div>
+
+            <div class="selected-items"></div>
             <h5 class="total-items ms-auto total_design_count">{{ $count }} Items</h5>
         </div>
 
