4de242875dc30862623a9864d4321e16d254a303
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-03 18:51:35 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1240,7 +1240,7 @@ $(".modal").on("hidden.bs.modal", function () {
     $("#pollForm")[0].reset(); // Reset poll form
     $("#photoForm")[0].reset(); // Reset photo form
     $("#imagePreview").empty(); // Clear image preview
-
+    $(".char-count").text("0/140"); // Reset char count
     // Add `d-none` class back to hide the div
     $(".create-post-upload-img-inner").addClass("d-none");
 });
