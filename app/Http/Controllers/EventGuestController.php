d3e1ebabe71c9577fc8307109d0a1b8d09fc48fc
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-03 19:29:02 +0530

    Update EventGuestController.php

--- app/Http/Controllers/EventGuestController.php
+++ app/Http/Controllers/EventGuestController.php
@@ -70,6 +70,7 @@ class EventGuestController extends BaseController
             $isCoHost =  EventInvitedUser::where(['event_id' => $eventDetail->id, 'user_id' => $user->id, 'is_co_host' => '1'])->first();
             $eventDetails['is_co_host'] = (isset($isCoHost) && $isCoHost->is_co_host != "") ? $isCoHost->is_co_host : "0";
             $eventDetails['podluck'] = $eventDetail->event_settings->podluck;
+            $eventDetails['event_wall'] = $eventDetail->event_settings->event_wall ?? "";
             $totalAdults = $eventDetail->event_invited_user->sum('adults');
             $totalKids = $eventDetail->event_invited_user->sum('kids');
             $totalGuests = $totalAdults + $totalKids;
