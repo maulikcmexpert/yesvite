a353ea4fd42b81c5ec903dbe5ed4f9aaa33db377
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-03 19:21:45 +0530

    commit

--- resources/views/components/event_wall/wall_navbar.blade.php
+++ resources/views/components/event_wall/wall_navbar.blade.php
@@ -38,7 +38,7 @@
            role="tab"
            aria-controls="nav-guests"
            aria-selected="{{ $page == 'guest' ? 'true' : 'false' }}">
-           Guests({{ $eventDetails['guest_list_visible_to_guests'] ?? '' }})
+           Guests{{ isset($eventDetails['guest_list_visible_to_guests']) ? '(' . $eventDetails['guest_list_visible_to_guests'] . ')' : '' }}
         </a>
 
 
