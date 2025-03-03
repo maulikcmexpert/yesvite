431b86c174328c3b568875fac0599ed3dee1645f
cmexpertise <pratik.cmexpertise@gmail.com>
2025-03-03 11:39:49 +0530

    Update BaseController.php

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -35,18 +35,33 @@ class BaseController extends Controller
         // dd($users);
         // handleIncomingMessage("+918780258675", "yes");
         // handleSMSInvite("+91 97238 40340", "yesvite web", "srryghhhggguvj", 814, 2787);
+        // return [
+        //     '15' => ['priceId' => 'price_1QrC0dEunmtSe18EOUZgSmuy', 'coins' => 15, 'price' => 14.99],
+
+        //     '30' => ['priceId' => 'price_1QrC6BEunmtSe18EmbwOERSM', 'coins' => 30, 'price' => 24.99],
+
+        //     '50' => ['priceId' => 'price_1QrC6BEunmtSe18ErhNVPfNH', 'coins' => 50, 'price' => 34.99],
+
+        //     '100' => ['priceId' => 'price_1QrC6BEunmtSe18E07TR4iCC', 'coins' => 100, 'price' => 59.99],
+
+        //     '200' => ['priceId' => 'price_1QrC6BEunmtSe18EQU01LFNR', 'coins' => 250, 'price' => 99.99],
+
+        //     '500' => ['priceId' => 'price_1QrC6BEunmtSe18EnzZELzAp', 'coins' => 500, 'price' => 125.99],
+        // ];
+
+
         return [
-            '15' => ['priceId' => 'price_1QrC0dEunmtSe18EOUZgSmuy', 'coins' => 15, 'price' => 14.99],
+            '15' => ['priceId' => 'price_1QySASCHXENZbvVPjTMtay1C', 'coins' => 15, 'price' => 14.99],
 
-            '30' => ['priceId' => 'price_1QrC6BEunmtSe18EmbwOERSM', 'coins' => 30, 'price' => 24.99],
+            '30' => ['priceId' => 'price_1QySASCHXENZbvVPlN5mjGgn', 'coins' => 30, 'price' => 24.99],
 
-            '50' => ['priceId' => 'price_1QrC6BEunmtSe18ErhNVPfNH', 'coins' => 50, 'price' => 34.99],
+            '50' => ['priceId' => 'price_1QySASCHXENZbvVPtri9qtrY', 'coins' => 50, 'price' => 34.99],
 
-            '100' => ['priceId' => 'price_1QrC6BEunmtSe18E07TR4iCC', 'coins' => 100, 'price' => 59.99],
+            '100' => ['priceId' => 'price_1QySASCHXENZbvVPrVx7wKCh', 'coins' => 100, 'price' => 59.99],
 
-            '200' => ['priceId' => 'price_1QrC6BEunmtSe18EQU01LFNR', 'coins' => 250, 'price' => 99.99],
+            '200' => ['priceId' => 'price_1QySASCHXENZbvVPZszQjyMy', 'coins' => 250, 'price' => 99.99],
 
-            '500' => ['priceId' => 'price_1QrC6BEunmtSe18EnzZELzAp', 'coins' => 500, 'price' => 125.99],
+            '500' => ['priceId' => 'price_1QySASCHXENZbvVPPrYfdyNA', 'coins' => 500, 'price' => 125.99],
         ];
     }
 }
