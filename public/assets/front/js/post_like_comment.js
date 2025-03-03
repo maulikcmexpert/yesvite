e9e28bf9f4be44953d5f34eace39a066906a26d5
Vimal <vimal.cmexpertise@gmail.com>
2025-03-03 19:42:02 +0530

    Update post_like_comment.js

--- public/assets/front/js/post_like_comment.js
+++ public/assets/front/js/post_like_comment.js
@@ -1439,6 +1439,21 @@ $(document).ready(function () {
         );
         $(".phone-checkbox[data-id='" + user_id + "']").prop("checked", false);
         guestList = guestList.filter((guest) => guest.id !== parseInt(user_id));
+
+        let checkedCount = $(".email-checkbox:not(:disabled):checked, .phone-checkbox:not(:disabled):checked").length;
+
+    if (checkedCount == 0) {
+        $('.add_guest').attr('disabled',true)
+    } else {
+        $('.add_guest').attr('disabled',false)
+    }
+
+    let checkedCountphone = $(".phoneContact-checkbox:not(:disabled):checked").length;
+    if (checkedCountphone == 0) {
+        $('.add_guest').attr('disabled',true)
+    } else {
+        $('.add_guest').attr('disabled',false)
+    }
     });
 
     $(document).on("click", ".remove_new_phone_added_user", function () {
