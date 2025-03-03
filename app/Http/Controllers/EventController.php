587939214b8ca498944cae4f986abecc9233bac4
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-03 11:27:04 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -366,7 +366,7 @@ class EventController extends BaseController
 
                 $eventDetail['co_host_list'] = getInvitedCohostList($getEventData->id);
                 if(isset($eventDetail['co_host_list']) && $eventDetail['co_host_list']!=""){
-                    dd(1);
+                    dd($eventDetail['co_host_list'][0]['id']);
                     if($eventDetail['co_host_list'][0]['id'] !=$id){
                         redirect('front.home');
                     }
