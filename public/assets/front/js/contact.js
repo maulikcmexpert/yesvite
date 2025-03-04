75cd7545f07801330bd455833c851549c9d92b78
Vimal <vimal.cmexpertise@gmail.com>
2025-03-04 17:25:27 +0530

    Update contact.js

--- public/assets/front/js/contact.js
+++ public/assets/front/js/contact.js
@@ -1082,7 +1082,50 @@ $(document).on('click','.click-to-upload-btn', function (e) {
         $('#upload_csv_contact').submit();
     // }
 });
+let dropArea = $(".uploadcsv-wrp");
 
+// Prevent default drag behaviors
+$(document).on("dragover dragenter drop", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+});
+
+// Drag over event
+dropArea.on("dragover", function (e) {
+    e.preventDefault();
+    $(this).addClass("drag-over");
+});
+
+// Drag leave event
+dropArea.on("dragleave", function (e) {
+    e.preventDefault();
+    $(this).removeClass("drag-over");
+});
+
+// Drop event on entire div
+dropArea.on("drop", function (e) {
+    e.preventDefault();
+    $(this).removeClass("drag-over");
+
+    let files = e.originalEvent.dataTransfer.files;
+    if (files.length > 0) {
+        handleFileUpload(files[0]);
+    }
+});
+
+// File input change event
+$("#csv_file").on("change", function (e) {
+    let file = e.target.files[0];
+    handleFileUpload(file);
+});
+
+function handleFileUpload(file) {
+    if (file && file.type === "text/csv") {
+        $(".uploadcsv-wrp h3").text(file.name); // Show file name
+    } else {
+        alert("Please upload a valid CSV file.");
+    }
+}
 // $(document).on("shown.bs.modal", "#uploadcsv", function () {
 //     const dropZone1 = document.querySelector(".uploadcsv-wrp");
 //     const fileInput = document.querySelector(".csv_file");
