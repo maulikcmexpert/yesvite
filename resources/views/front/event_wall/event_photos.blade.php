7453e5d5134b0675964946eca015c5ee3b0c9b7d
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-04 11:37:16 +0530

    Update event_photos.blade.php

--- resources/views/front/event_wall/event_photos.blade.php
+++ resources/views/front/event_wall/event_photos.blade.php
@@ -920,7 +920,7 @@
                                     </li>
                                     {{-- <p id="likeCount" >5k Likes</p> --}} --}}
                                 </ul>
-                                <h6 id="comments">354 Comments</h6>
+                                <button id="comments"  class="posts-card-comm show-comments-btn">354 Comments</buttton>
                             </div>
                             <div class="posts-card-like-comment-right emoji_set">
                                 <button class="posts-card-like-btn likeModel " id="likeButtonModel"
@@ -946,7 +946,7 @@
                                 </div>
 
 
-                                <button class="posts-card-comm show-comments-btn">
+                                <a>
                                     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path
                                             d="M8.5 19H8C4 19 2 18 2 13V8C2 4 4 2 8 2H16C20 2 22 4 22 8V13C22 17 20 19 16 19H15.5C15.19 19 14.89 19.15 14.7 19.4L13.2 21.4C12.54 22.28 11.46 22.28 10.8 21.4L9.3 19.4C9.14 19.18 8.77 19 8.5 19Z"
@@ -957,7 +957,7 @@
                                         <path d="M7 13H13" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round"
                                             stroke-linejoin="round" />
                                     </svg>
-                                </button>
+                                </a>
                             </div>
                         </div>
 
