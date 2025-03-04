8d9542a818671c716163f4fe566fe2aa7ab70bb0
Vimal <vimal.cmexpertise@gmail.com>
2025-03-04 11:37:16 +0530

    Update contact.blade.php

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -647,7 +647,7 @@
             <form id="upload_csv_contact" action="{{ route('import.csv') }}" id method="POST" enctype="multipart/form-data">
             @csrf
                 <div class="modal-body guest-tab position-relative">
-                    <input type="file" name="csv_file" id="csv_file">
+                    <input class="d-none" type="file" name="csv_file" id="csv_file">
                     {{-- <div class="uploadcsv-wrp">
                         <img src="{{asset('assets/uploadcsv-drag-img.png')}}" alt="">
                         <h3>Drag CSV Here</h3>
