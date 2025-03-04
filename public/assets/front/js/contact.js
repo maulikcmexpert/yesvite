3512873f57063338a419ffdafe213e37396d03c8
Vimal <vimal.cmexpertise@gmail.com>
2025-03-04 17:30:40 +0530

    Update contact.js

--- public/assets/front/js/contact.js
+++ public/assets/front/js/contact.js
@@ -1082,50 +1082,62 @@ $(document).on('click','.click-to-upload-btn', function (e) {
         $('#upload_csv_contact').submit();
     // }
 });
-let dropArea = $(".uploadcsv-wrp");
+$(document).ready(function () {
+    let dropArea = $(".uploadcsv-wrp");
+    let fileInput = $("#csv_file");
 
-// Prevent default drag behaviors
-$(document).on("dragover dragenter drop", function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-});
+    // Prevent default drag behaviors on document
+    $(document).on("dragover dragenter drop", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
 
-// Drag over event
-dropArea.on("dragover", function (e) {
-    e.preventDefault();
-    $(this).addClass("drag-over");
-});
+    // Drag over event
+    dropArea.on("dragover", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).addClass("drag-over");
+    });
 
-// Drag leave event
-dropArea.on("dragleave", function (e) {
-    e.preventDefault();
-    $(this).removeClass("drag-over");
-});
+    // Drag leave event
+    dropArea.on("dragleave", function () {
+        $(this).removeClass("drag-over");
+    });
 
-// Drop event on entire div
-dropArea.on("drop", function (e) {
-    e.preventDefault();
-    $(this).removeClass("drag-over");
+    // Drop event on entire div
+    dropArea.on("drop", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).removeClass("drag-over");
 
-    let files = e.originalEvent.dataTransfer.files;
-    if (files.length > 0) {
-        handleFileUpload(files[0]);
+        let files = e.originalEvent.dataTransfer.files;
+        if (files.length > 0) {
+            fileInput.prop("files", files);
+            handleFileUpload(files[0]);
+        }
+    });
+
+    // Click to trigger file input
+    dropArea.on("click", function () {
+        fileInput.trigger("click");
+    });
+
+    // File input change event
+    fileInput.on("change", function (e) {
+        let file = e.target.files[0];
+        handleFileUpload(file);
+    });
+
+    function handleFileUpload(file) {
+        if (file && file.type === "text/csv") {
+            $(".uploadcsv-wrp h3").text(file.name); // Show file name
+            alert("File uploaded successfully!");
+        } else {
+            alert("Please upload a valid CSV file.");
+        }
     }
 });
 
-// File input change event
-$("#csv_file").on("change", function (e) {
-    let file = e.target.files[0];
-    handleFileUpload(file);
-});
-
-function handleFileUpload(file) {
-    if (file && file.type === "text/csv") {
-        $(".uploadcsv-wrp h3").text(file.name); // Show file name
-    } else {
-        alert("Please upload a valid CSV file.");
-    }
-}
 // $(document).on("shown.bs.modal", "#uploadcsv", function () {
 //     const dropZone1 = document.querySelector(".uploadcsv-wrp");
 //     const fileInput = document.querySelector(".csv_file");
