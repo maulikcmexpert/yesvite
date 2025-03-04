025e6c09e49e120afd64ac9f2eb90ab0526521ad
Vimal <vimal.cmexpertise@gmail.com>
2025-03-04 11:42:43 +0530

    Update contact.blade.php

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -647,20 +647,21 @@
             <form id="upload_csv_contact" action="{{ route('import.csv') }}" id method="POST" enctype="multipart/form-data">
             @csrf
                 <div class="modal-body guest-tab position-relative">
-                    <input type="file" name="csv_file" id="csv_file">
-                    {{-- <div class="uploadcsv-wrp">
-                        <img src="{{asset('assets/uploadcsv-drag-img.png')}}" alt="">
-                        <h3>Drag CSV Here</h3>
-                        <p>Drag file here or click to upload </p>
-                      
-                    </div> --}}
+                    {{-- <input type="file" name="csv_file" id="csv_file"> --}}
                     <label class="uploadcsv-wrp" for="csv_file">
                         <img src="{{asset('assets/uploadcsv-drag-img.png')}}" alt="">
                         <h3>Drag CSV Here</h3>
                         <p>Drag file here or click to upload </p>
-                        {{-- <input type="file" name="csv_file" id="csv_file"> --}}
-                      
+                        <input type="file" name="csv_file" id="csv_file">
+
                     </label>
+                    {{-- <div class="uploadcsv-wrp">
+                        <img src="{{asset('assets/uploadcsv-drag-img.png')}}" alt="">
+                        <h3>Drag CSV Here</h3>
+                        <p>Drag file here or click to upload </p>
+                        <input type="file" name="csv_file" id="csv_file">
+
+                    </div> --}}
                     <!-- <div class="uploadedcvs-file-wrp">
                         <div class="uploadedcvs-file-card home-latest-draf-card">
                             <div class="uploadedcvs-file-card-head">
