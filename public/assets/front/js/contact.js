c071117b71c46a5221daef06211a374f1d02d2b6
Vimal <vimal.cmexpertise@gmail.com>
2025-03-04 16:21:48 +0530

    Update contact.js

--- public/assets/front/js/contact.js
+++ public/assets/front/js/contact.js
@@ -1123,9 +1123,9 @@ let dropArea = $(".uploadcsv-wrp");
     $("#csv_file").on("change", function () {
         if (this.files.length > 0) {
             
+        alert("File Selected: " + this.files[0].name);
         toastr.success("File uploaded successfully!");
-        $(".uploadcsv-wrp h3").text(file.name); 
-            // alert("File Selected: " + this.files[0].name);
+        // $(".uploadcsv-wrp h3").text(file.name); 
 
         }
     });
