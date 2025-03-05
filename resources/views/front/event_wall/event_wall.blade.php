30c93d70e297cd78b68b998dab16ea0813a10f29
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-05 14:54:29 +0530

    commit

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -1660,6 +1660,11 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Detail Photo</h1>
+                <div class="wallProfile-modal-left-side">
+                    <button class="wallProfile-modal-left-side-menu-btn">
+                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                    </button>
+                </div>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -1668,12 +1673,12 @@
                         <div class="posts-card-head">
                             <div class="posts-card-head-left">
                                 <div class="main-modal-background-img">
-                                    <img src="" alt="">
+                                    <img src="https://images.unsplash.com/photo-1741070487520-907d1359cb95?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8" alt="">
                                     <div class="posts-card-head-left-img">
                                         <img id="modal-profile-img"
                                             src="{{ asset('assets/front/img/header-profile-img.png') }}"
                                             alt="Profile Image" loading="lazy">
-                                        <h5 id="modal-initials" class="fontcolor" style="display: none;"></h5>
+                                        <h5 id="modal-initials" class="fontcolorT" style="display: none;"></h5>
                                     </div>
                                 </div>
                                 <div class="posts-card-head-left-content">
