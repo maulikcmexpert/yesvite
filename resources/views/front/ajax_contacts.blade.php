12d7a2e7c56d47237320e3477dc9b48b09c66b74
Vimal <vimal.cmexpertise@gmail.com>
2025-03-05 06:19:48 -0800

    Update ajax_contacts.blade.php

--- resources/views/front/ajax_contacts.blade.php
+++ resources/views/front/ajax_contacts.blade.php
@@ -71,7 +71,8 @@
     
                         </div>
                 <div class="text-start">
-                    <h5 class="openProfileModal"  data-userid="{{ $value->id }}" style="text-decoration: underline;cursor: pointer;">{{ $value->firstname }}
+                    <h5 class="openProfileModal"  data-bs-toggle="modal"
+                    data-bs-target="#wall_profile" data-userid="{{ $value->id }}" style="text-decoration: underline;cursor: pointer;">{{ $value->firstname }}
                         {{ $value->lastname }}
                     </h5>
                     @if (isset($value->email) && $value->email != '')
