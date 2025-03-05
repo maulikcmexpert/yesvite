a5fa1b9c018244beaa9bc98e489b0a0d1e7719b4
Vimal <vimal.cmexpertise@gmail.com>
2025-03-05 04:46:11 -0800

    commit

--- resources/views/front/event_wall/event_about.blade.php
+++ resources/views/front/event_wall/event_about.blade.php
@@ -415,7 +415,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        @if($eventDetails['is_host']==1 || $eventDetails['is_co_host']=="1")
+                                        {{-- @if($eventDetails['is_host']==1 || $eventDetails['is_co_host']=="1") --}}
                                             <div class="new_copy_event_Wrp">
                                                 <h4 class="title">Share Event Link</h4>
                                                 <div class="new_copy_event_inner">
@@ -423,7 +423,7 @@
                                                     <button type="button" id="copy_link_btn">Copy Link</button>
                                                 </div>
                                             </div>
-                                          @endif
+                                          {{-- @endif --}}
                                         {{-- {{      dd($eventDetails)}} --}}
                                         <div class="host-users-detail cmn-card">
                                         @if (!empty($eventDetails['co_hosts']))
