10d09ebbfc6e06e5e27bf1e0714cdf4ed25375b3
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-05 17:41:20 +0530

    commint

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1674,10 +1674,10 @@ $(document).on("click", ".openProfileModal", function () {
                 let background_img = $("#modal-background-img");
                if ( profileData.profile !== "") {
                     background_img.attr("src", profileData.bg_profile).show();
-                  ("#show_img").hide();
+                  ("#show_img").addClass('d-none');
                 }else{
                     background_img.attr("src", profileData.bg_profile).hide();
-                    ("#show_img").show();
+                    ("#show_img").removeClass('d-none');
                 }
 
                 // Handle Host and Co-Host labels
