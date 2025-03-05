0a24dd7237a1e208f4250fddbbfe53ee59997c67
Vimal <vimal.cmexpertise@gmail.com>
2025-03-05 11:44:26 +0530

    Update contact.js

--- public/assets/front/js/contact.js
+++ public/assets/front/js/contact.js
@@ -307,7 +307,7 @@ $("#product-scroll-phone").on("scroll", function () {
         var scrollHeight = $(this)[0].scrollHeight; 
         var elementHeight = $(this).height();
      
-            if (scrollTop + elementHeight >= scrollHeight-300) {
+            if (scrollTop + elementHeight >= scrollHeight-1) {
                 busy2 = true;
                 offset1 += limit;
                 var type="phone";
