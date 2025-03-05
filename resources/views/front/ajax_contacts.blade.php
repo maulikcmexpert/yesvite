6cc681adb33653cd230d806993178ccc96c02ab8
Vimal <vimal.cmexpertise@gmail.com>
2025-03-05 06:18:15 -0800

    Update ajax_contacts.blade.php

--- resources/views/front/ajax_contacts.blade.php
+++ resources/views/front/ajax_contacts.blade.php
@@ -71,7 +71,7 @@
     
                         </div>
                 <div class="text-start">
-                    <h5 class="openProfileModal"  data-userid="{{ $value->id }}">{{ $value->firstname }}
+                    <h5 class="openProfileModal"  data-userid="{{ $value->id }}" style="text-decoration: underline;cursor: pointer;">{{ $value->firstname }}
                         {{ $value->lastname }}
                     </h5>
                     @if (isset($value->email) && $value->email != '')
@@ -143,7 +143,7 @@
                     @endif
                 </div>
                 <div class="text-start">
-                    <h5 class="openProfileModal"  data-userid="{{ $value->id }}" >{{ $value->firstname . ' ' . $value->lastname }}</h5>
+                    <h5 class="openProfileModal"  data-userid="{{ $value->id }}" style="text-decoration: underline;cursor: pointer;" >{{ $value->firstname . ' ' . $value->lastname }}</h5>
                     @if($value->email !== null && $value->email !== "")
                     <div>
                         <a href="#">
