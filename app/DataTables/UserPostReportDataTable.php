d9872834132f5718d31d950942eba546788d2958
Vimal <vimal.cmexpertise@gmail.com>
2025-03-06 19:32:44 +0530

    Update UserPostReportDataTable.php

--- app/DataTables/UserPostReportDataTable.php
+++ app/DataTables/UserPostReportDataTable.php
@@ -239,7 +239,7 @@ class UserPostReportDataTable extends DataTable
             Column::make('event_name')->title("Event Name")->orderable(true),
             Column::make('post_type')->title("Post Type")->orderable(false),
             Column::make('account_status')->title("Account Status")->orderable(false),
-            Column::make('delete')->title("Delete")->orderable(false),
+            Column::make('delete')->title("Account Delete")->orderable(false),
             Column::make('action')->title("Action")->orderable(false),
         ];
     }
