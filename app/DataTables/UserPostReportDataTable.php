4fceb5f3d60580c4f23aa711879ca51fbf8339c2
Vimal <vimal.cmexpertise@gmail.com>
2025-03-07 17:54:25 +0530

    Update UserPostReportDataTable.php

--- app/DataTables/UserPostReportDataTable.php
+++ app/DataTables/UserPostReportDataTable.php
@@ -203,7 +203,7 @@ class UserPostReportDataTable extends DataTable
                     ->whereColumn('users.id', function ($query) {
                         $query->select('event_posts.user_id')
                             ->from('event_posts')
-                            ->whereColumn('event_posts.id', 'user_report_to_posts.post_id');
+                            ->whereColumn('event_posts.id', 'user_report_to_posts.event_post_id');
                     });
                }
 
