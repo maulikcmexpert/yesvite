e85bd1383285b89e4c871bdd2925cbfb83f3bd3e
Vimal <vimal.cmexpertise@gmail.com>
2025-03-07 17:36:52 +0530

    Update post_like_comment.js

--- public/assets/front/js/post_like_comment.js
+++ public/assets/front/js/post_like_comment.js
@@ -800,7 +800,9 @@ $(document).ready(function () {
         $(".guest_yesvite").remove();
         $(".phone_yesvite").remove();
         $(".see_invite_nav_yesvite").addClass("active");
+        $('#home').addClass('show active');
         $(".see_invite_nav_phone").removeClass("active");
+        $('#home').removeClass('show active');;
         $(".phoneContact-checkbox:not(:disabled)").prop("checked", false);
         $(".contact-checkbox:not(:disabled)").prop("checked", false);
         $(".phone-checkbox:not(:disabled)").prop("checked", false);
