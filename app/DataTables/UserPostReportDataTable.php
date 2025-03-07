e072da31790be0f7dd2fa0f40e9e651d0cd3e0c7
Vimal <vimal.cmexpertise@gmail.com>
2025-03-07 14:20:03 +0530

    Update UserPostReportDataTable.php

--- app/DataTables/UserPostReportDataTable.php
+++ app/DataTables/UserPostReportDataTable.php
@@ -81,7 +81,8 @@ class UserPostReportDataTable extends DataTable
 
             ->addColumn('username', function ($row) {
 
-                return $row->users->firstname . ' ' . $row->users->lastname;
+                // return $row->users->firstname . ' ' . $row->users->lastname;
+                return $row->event_postsusers->firstname . ' ' . $row->event_posts->users->lastname;
             })
             ->addColumn('email', function ($row) {
 
