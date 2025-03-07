0281c12b200d4d4246fa123feeab324ed6f3411f
Vimal <vimal.cmexpertise@gmail.com>
2025-03-07 11:43:07 +0530

    Update contact.blade.php

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -655,6 +655,8 @@
                         <input type="file" name="csv_file" id="csv_file">
                       
                     </div>
+                    <p>Save or export the file as a csv,xlsx,or xls file before uploading it to yesvite</p>
+                    <p>Columns are Firstname,Last Name,Email,Phone Number</p>
                     <!-- <div class="uploadedcvs-file-wrp">
                         <div class="uploadedcvs-file-card home-latest-draf-card">
                             <div class="uploadedcvs-file-card-head">
@@ -725,8 +727,7 @@
 
                                     
                 {{-- <p>Support text/csv, maximum file size of 10Mb</p> --}}
-                <p>Save or export the file as a csv,xlsx,or xls file before uploading it to yesvite</p>
-                <p>Columns are Firstname,Last Name,Email,Phone Number</p>
+            
                 </div>
             </form>
         </div>
