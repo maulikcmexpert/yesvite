f247808f9eb4b1d48c8a0ffa7465a16858d58ebe
Vimal <vimal.cmexpertise@gmail.com>
2025-03-10 12:42:18 +0530

    Update contact.js

--- public/assets/front/js/contact.js
+++ public/assets/front/js/contact.js
@@ -1217,7 +1217,7 @@ $(document).ready(function() {
                     } else {
                         uploadedKB = Math.min(uploadedKB + fileSizeKB / 100, fileSizeKB);
                         progressBar.val((uploadedKB / fileSizeKB) * 100);
-                        $(".uploadedcvs-file-card-content p").text(`${uploadedKB.toFixed(2)} KB of ${fileSizeKB} KB`);
+                        // $(".uploadedcvs-file-card-content p").text(`${uploadedKB.toFixed(2)} KB of ${fileSizeKB} KB`);
                     }
                 }, 30);
             } else {
@@ -1267,7 +1267,7 @@ function resetUploadState() {
     uploadProgress.hide();
     uploadComplete.hide();
     progressBar.val(0);
-    $(".uploadedcvs-file-card-content p").text('');
+    // $(".uploadedcvs-file-card-content p").text('');
 
 }
 
