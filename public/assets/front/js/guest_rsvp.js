ee6ccd79c0b97a44163f26a06b21b84ae380c197
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-10 10:20:18 +0530

    commit

--- public/assets/front/js/guest_rsvp.js
+++ public/assets/front/js/guest_rsvp.js
@@ -290,7 +290,7 @@ $(document).on("change", ".phoneContact-checkbox", function () {
                     let showPhotos = profilePrivacy.some(item => item.profile_privacy === "photo" && item.status === "1");
                     if (showLocation) {
                         let locationText = [profileData.address,profileData.city, profileData.state, profileData.zip_code]
-                            .filter(Boolean)
+                        .filter(value => value !== null && value !== undefined && value !== "")
                             .join(", ");
 
                         $("#location").text(locationText);
