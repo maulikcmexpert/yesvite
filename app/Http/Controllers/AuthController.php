f84b05f2b55f96a427cf796f2864ff78cd0c8dba
Vimal <vimal.cmexpertise@gmail.com>
2025-03-11 04:46:49 -0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -357,10 +357,10 @@ class AuthController extends Controller
                     ];
 
 
-                    Mail::send('emails.emailVerificationEmail', ['userData' => $userData], function ($message) use ($user) {
-                        $message->to($user->email);
-                        $message->subject('Verify your Yesvite email address');
-                    });
+                    // Mail::send('emails.emailVerificationEmail', ['userData' => $userData], function ($message) use ($user) {
+                    //     $message->to($user->email);
+                    //     $message->subject('Verify your Yesvite email address');
+                    // });
                     // dd(2);
                     return redirect()->route('auth.login')->with('msg', 'Please check and verify your email address.');
 
