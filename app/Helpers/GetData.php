b90fbeeeb6d0642e389b5b2942b714b843bf8e5c
Vimal <vimal.cmexpertise@gmail.com>
2025-03-11 03:25:07 -0700

    Update GetData.php

--- app/Helpers/GetData.php
+++ app/Helpers/GetData.php
@@ -927,7 +927,7 @@ function checkUserPhotoIsLike($event_photo_comment_id, $user_id)
 function getOptionTotalVote($event_poll_option_id)
 {
     $data=UserEventPollData::where('event_poll_option_id', $event_poll_option_id)->count();
-    dd($data);
+    dd($event_poll_option_id);
     return UserEventPollData::where('event_poll_option_id', $event_poll_option_id)->count();
 }
 function getOptionAllTotalVote($id)
