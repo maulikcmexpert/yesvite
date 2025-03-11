97faa4d0143ab84ae4c98c30c624bc7adcb7f6ce
Vimal <vimal.cmexpertise@gmail.com>
2025-03-11 05:24:23 -0700

    commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -276,7 +276,7 @@ class AuthController extends Controller
                     'msg_error' => 'Ban User: Temporarily or permanently suspend user, Contact to admin.',
                 ])->withInput();
             }
-            if (Auth::attempt($credentials, $remember)) {
+            // if (Auth::attempt($credentials, $remember)) {
                 $userIpAddress = request()->ip();
 
                 $user = Auth::guard('web')->user();
@@ -368,7 +368,7 @@ class AuthController extends Controller
 
                 }
             }
-        }
+        // }
         return redirect()->back()->withErrors([
             'email' => 'Email or Password invalid!',
         ])->withInput();
--- app/Http/Middleware/IsAuthenticate.php
+++ app/Http/Middleware/IsAuthenticate.php
@@ -24,11 +24,6 @@ class IsAuthenticate
         if (!Auth::guard('web')->user()) {
             return $next($request);
         }
-        $secondUser = Auth::guard('web')->user();
-        if ($secondUser->email_verified_at == NULL) {
-            return redirect()->route('auth.login')->with('msg', 'Please check and verify your email address.');
-            // return Redirect::to(URL::to('home'));
-        
-        }
+        return Redirect::to(URL::to('home'));
     }
 }
