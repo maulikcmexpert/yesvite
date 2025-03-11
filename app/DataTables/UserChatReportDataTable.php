025457ebd75bee47fe9539e022cf8315b5b40e0a
Vimal <vimal.cmexpertise@gmail.com>
2025-03-10 23:47:42 -0700

    Update UserChatReportDataTable.php

--- app/DataTables/UserChatReportDataTable.php
+++ app/DataTables/UserChatReportDataTable.php
@@ -78,6 +78,10 @@ class UserChatReportDataTable extends DataTable
                         }
                         $q->orWhere('report_type', 'LIKE', "%{$keyword}%")
                           ->orWhere('report_description', 'LIKE', "%{$keyword}%");
+
+                          $q->orWhere('email', 'LIKE', "%{$keyword}%");
+
+                         
                     });
                 }
                 
@@ -170,11 +174,19 @@ class UserChatReportDataTable extends DataTable
                 // Sorting by the reporter user's firstname from the users table
                 $column = User::select('firstname')
                     ->whereColumn('users.id', 'user_report_chats.reporter_user_id');
-            } elseif ($request->order[0]['column'] == '2') {
+            }elseif($request->order[0]['column'] == '2'){
+                $column = User::select('email')
+                ->whereColumn('users.id', 'user_report_chats.reporter_user_id');
+            } 
+            elseif ($request->order[0]['column'] == '6') {
                 // Sorting by the 'to' reporter user's firstname (assuming another user field)
                 $column = User::select('firstname')
                     ->whereColumn('users.id', 'user_report_chats.to_be_reported_user_id');
-            }else if($request->order[0]['column'] == '3'){
+            }elseif($request->order[0]['column'] == '7'){
+                $column = User::select('email')
+                ->whereColumn('users.id', 'user_report_chats.to_be_reported_user_id');
+            } 
+            else if($request->order[0]['column'] == '3'){
                 $column="report_type";
             }else if($request->order[0]['column'] == '5'){
                 $column="created_at";
