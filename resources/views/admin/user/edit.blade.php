3799a2b08fe2940f505e82a837ba0dfab5ec6a3f
Vimal <vimal.cmexpertise@gmail.com>
2025-03-11 00:29:19 -0700

    Update edit.blade.php

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -83,6 +83,17 @@
                             <span class="text-danger">{{ $errors->first('state') }}</span>
                         </div>
                     </div>
+                  
+                    <div class="col-lg-6 col-md-6 mb-3">
+                        <div class="form-group">
+                            <label for="about_me">About Me</label>
+    
+                                <textarea class="form-control" name="about_me" placeholder="Enter About Me">{{ $getTemData->about_me }}</textarea>
+                                {{-- <input type="text" class="form-control" name="about_me" placeholder="Enter About Me"
+                                value="{{ $getTemData->state }}"> --}}
+                            <span class="text-danger">{{ $errors->first('about_me') }}</span>
+                        </div>
+                    </div>
                     <div class="col-lg-6 col-md-6 mb-3">
                         <div class="form-group">
                             <label>Gender</label>
@@ -99,16 +110,6 @@
                                 </div>
                             </div>
                             <span class="text-danger">{{ $errors->first('gender') }}</span>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 mb-3">
-                        <div class="form-group">
-                            <label for="about_me">About Me</label>
-    
-                                <textarea class="form-control" name="about_me" placeholder="Enter About Me">{{ $getTemData->about_me }}</textarea>
-                                {{-- <input type="text" class="form-control" name="about_me" placeholder="Enter About Me"
-                                value="{{ $getTemData->state }}"> --}}
-                            <span class="text-danger">{{ $errors->first('about_me') }}</span>
                         </div>
                     </div>
              
