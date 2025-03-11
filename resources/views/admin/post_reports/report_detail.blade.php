13bd3dea226f1a99875115fd525cd99213787f90
Vimal <vimal.cmexpertise@gmail.com>
2025-03-11 03:42:36 -0700

    Update report_detail.blade.php

--- resources/views/admin/post_reports/report_detail.blade.php
+++ resources/views/admin/post_reports/report_detail.blade.php
@@ -886,12 +886,14 @@
                                 <div class="col-xl-6 col-lg-12 col-md-12">
                                     <div class="event_posts_left">
                                         <div class="product-images demo-gallery">
-                                <div class="main-img-slider">
-                                    <a data-fancybox="gallery" href="{{ asset('storage/post_image/'.$reportDetail->post_image->post_image) }}">
-                                        <img src="{{asset('storage/post_image/'.$reportDetail->post_image->post_image) }}" />
-                                    </a>
+                                            <div class="main-img-slider">
+                                                <a data-fancybox="gallery" href="{{ asset('storage/post_image/'.$reportDetail->post_image->post_image) }}">
+                                                    <img src="{{asset('storage/post_image/'.$reportDetail->post_image->post_image) }}" />
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                        </div></div></div>
                                 @endif
                                 <!-- image -->
 
