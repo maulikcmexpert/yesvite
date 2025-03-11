f74c09c7a1bf2bfe1837d8f0012cc7fd050a09a3
Vimal <vimal.cmexpertise@gmail.com>
2025-03-11 05:02:45 -0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -362,7 +362,7 @@ class AuthController extends Controller
                         $message->subject('Verify your Yesvite email address');
                     });
                     // dd(2);
-                    return redirect()->route('profile.change_password')->with('msg', 'Please changer your temparory password.');
+                    return redirect()->route('auth.login')->with('msg', 'Please changer your temparory password.');
 
                     // return redirect()->back()->with('msg', 'Please check and verify your email address.');
 
