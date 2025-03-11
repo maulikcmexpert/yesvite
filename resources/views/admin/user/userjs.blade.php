04125c8fbcaa0cca8cb925d65f821d99b23436cd
Vimal <vimal.cmexpertise@gmail.com>
2025-03-11 09:51:59 +0530

    Update userjs.blade.php

--- resources/views/admin/user/userjs.blade.php
+++ resources/views/admin/user/userjs.blade.php
@@ -412,6 +412,51 @@
                 }
             });
         });
+        $(document).on('click', '.delete_user', function() {
 
+                var user_id = $(this).attr('data-id');
+
+                Swal.fire({
+                    title: "Are you sure?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, delete it!"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+
+                        $.ajax({
+                            headers: {
+                                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                                    "content"),
+                            },
+                            // dataType: 'Json',
+                            type: "GET",
+                            url: "{{ route('delete_post_report') }}",
+                            data: {
+                                event_report_id: event_report_id
+                            },
+                            success: function(output) {
+                                if (output == true) {
+                                    Swal.fire({
+                                        title: "Deleted!",
+                                        text: "Your Reported Post has been deleted",
+                                        icon: "success"
+                                    }).then((result) => {
+                                        if (result.isConfirmed) {
+                                            window.location.href = "{{URL::to('/admin/user_post_report')}}";
+                                        }
+                                    });
+                                }
+                            },
+                            error: function() {}
+                        });
+
+
+                    }
+                });
+})
     });
 </script>
