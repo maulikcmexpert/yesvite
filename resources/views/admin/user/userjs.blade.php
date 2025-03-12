9d4c96f8b399bfb0c4aaaa2c166ceed47045cfaf
Vimal <vimal.cmexpertise@gmail.com>
2025-03-12 16:02:15 +0530

    Update userjs.blade.php

--- resources/views/admin/user/userjs.blade.php
+++ resources/views/admin/user/userjs.blade.php
@@ -442,7 +442,7 @@
                                 if (output == true) {
                                     Swal.fire({
                                         title: "Deleted!",
-                                        text: "Your Reported Post has been deleted",
+                                        text: "User has been deleted",
                                         icon: "success"
                                     }).then((result) => {
                                         if (result.isConfirmed) {
