bece2497f4d5b8c6728d56abc00707bf3a0f21f3
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-12 12:12:34 +0530

    commit

--- public/assets/front/js/event_photo.js
+++ public/assets/front/js/event_photo.js
@@ -927,7 +927,7 @@ $(document).ready(function () {
         const button = $(this);
         const eventId = button.data("event-id");
         const eventPostId = button.data("event-post-id");
-        bulkSelectActive = !bulkSelectActive;
+        bulkSelectActive = true;
         console.log("Bulk Select Mode Active:", bulkSelectActive);
 
         // if (bulkSelectActive) {
