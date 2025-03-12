1aaf83355ad06cb9cd8f266a00a3a5176b0166c5
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-12 14:32:36 +0530

    commit

--- public/assets/front/js/event_photo.js
+++ public/assets/front/js/event_photo.js
@@ -1078,7 +1078,7 @@ $(document).ready(function () {
 
                     $(".selected_bulk_image:checked").each(function () {
                         $(this).closest(".photo-card-photos-wrp").remove();
-                        $(this).closest(".delete_post_container").remove();
+                        $(".bulk_select").closest(".delete_post_container").remove();
                     });
 
                     toastr.success("Selected posts deleted successfully.");
