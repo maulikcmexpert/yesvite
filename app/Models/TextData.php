5687f5570155db7f9f3c622acf76b040e8df6e4a
Vimal <vimal.cmexpertise@gmail.com>
2025-03-12 16:35:50 +0530

    Update TextData.php

--- app/Models/TextData.php
+++ app/Models/TextData.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\{EventDesignSubCategory, EventDesignCategory};
+use App\Models\{EventDesignSubCategory, EventDesignCategory,Admin};
 
 class TextData extends Model
 {
     use HasFactory;
     protected $table = 'text_data';
     protected $fillable = [
+        'creator_id',
         'event_design_category_id',
         'event_design_sub_category_id',
         'static_information',
