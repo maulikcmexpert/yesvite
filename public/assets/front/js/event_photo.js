48a0ad54f9909a3a6bdb0dda6c4da8c9644acab4
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-12 16:02:39 +0530

    Update event_photo.js

--- public/assets/front/js/event_photo.js
+++ public/assets/front/js/event_photo.js
@@ -1096,12 +1096,6 @@ $(document).ready(function () {
         console.log("Mouse up or leave detected");
         if (!bulkSelectActive) {
             $("#detail-photo-modal").modal("show");
-        } else {
-            // Reset bulk select if the user intended to open the modal
-            bulkSelectActive = false;
-            $(".selected_bulk_image").prop("checked", false);
-            $(".selected-bulk-btn").hide();
-            toggleBulkSelectWrapper();
         }
         const commentInput = $("#post_comment");
         commentInput.val("");
