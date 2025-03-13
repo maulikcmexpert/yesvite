4ed8061b3264e1a1323d2ecd5f3fc8cf47b0ed20
Vimal <vimal.cmexpertise@gmail.com>
2025-03-13 16:07:00 +0530

    Update TemplateController.php

--- app/Http/Controllers/admin/TemplateController.php
+++ app/Http/Controllers/admin/TemplateController.php
@@ -78,7 +78,7 @@ class TemplateController extends Controller
                                 <div class="button-cover">
                                     <div class="button r" id="button-1">
 
-                                        <input type="checkbox" name="photo_via_wifi" value="1" id="photo_via_wifi" class="checkbox">
+                                        <input type="checkbox" name="photo_via_wifi" value="1" id="photo_via_wifi" class="checkbox" data-id="'.$row->id.'" '.$checked.' />
                                         <div class="knobs"></div>
                                         <div class="layer"></div>
                                     </div>
