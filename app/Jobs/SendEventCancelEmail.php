8480f2d61a811adf790646fa35df5a28fba2dcba
Vimal <vimal.cmexpertise@gmail.com>
2025-03-13 14:33:25 +0530

    Update SendEventCancelEmail.php

--- app/Jobs/SendEventCancelEmail.php
+++ app/Jobs/SendEventCancelEmail.php
@@ -15,89 +15,90 @@ use Illuminate\Support\Facades\Mail;
 
 class SendEventCancelEmail implements ShouldQueue
 {
-    // use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    // protected $user;
-    // protected $details;
-
-    // /**
-    //  * Create a new job instance.
-    //  */
-    // public function __construct($user, $details)
-    // {
-    //     $this->user = $user;
-    //     $this->details = $details;
-    // }
-
-    // /**
-    //  * Execute the job.
-    //  */
-    // public function handle()
-    // {
-    //     Mail::to($this->user->email)->send(new CancelEventMail($this->details));
-    // }
-
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $user;
+    protected $details;
 
-    public $tries = 1;
-    // public $timeout = 300;
-    public $timeout = 600; // Set timeout in seconds (10 minutes)
-
-    protected $data;
-    protected $email;
-    protected $templateData;
     /**
      * Create a new job instance.
      */
-    public function __construct($data,$templateData)
+    public function __construct($user, $details)
     {
-        // dd($templateData);
-        $this->data = $data;
-        $this->email = $this->data;
-        $this->templateData = $templateData;
+        $this->user = $user;
+        $this->details = $details;
     }
 
-    // protected $batchSize = 3 0; // Adjust the batch size as needed
-
-    // public function __construct($data)
-    // {
-    //     dd($data);
-    //     // $this->email = array_filter((array) $email, function ($e) {
-    //     //     return filter_var($e, FILTER_VALIDATE_EMAIL) && !empty($e);
-    //     // });
-
-    //     // if (empty($this->email)) {
-    //     //     throw new \Exception("Invalid email: No valid email addresses provided.");
-    //     // }
-
-    //     // $this->message = $message;
-    //     // dd($message);
-    //     $this->email = $this->data[0];
-    //     $this->templateData = $this->data[1];
-
-    // }
-    public function handle(): void
+    /**
+     * Execute the job.
+     */
+    public function handle()
     {
         dd(1);
-        $cance_mail = new CancelEventMail($this->templateData);
-        foreach ($this->email as $emails) {
-                    try {
-                        // Send the email using the BulkEmail Mailable
-                        if($emails!=""){
-                            Mail::to($this->email)->send($cance_mail); 
-                        }
-        
-                        // Mail::to('prakashmanat24@gmail.com')
-                        // ->bcc($emails) // Send to each batch of 30 via BCC
-                        // ->send(new BulkEmail($this->message));
-                    } catch (\Exception $e) {
-                        // dd($e->getMessage());
-                        // Log the error for troubleshooting (don't use dd() in jobs)
-                        Log::error("Failed to send email to $emails: " . $e->getMessage());
-        
-                        // Optionally, you can store failed emails or implement a retry mechanism
-                    }
-                }
+        Mail::to($this->user->email)->send(new CancelEventMail($this->details));
     }
+
+    // use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    // public $tries = 1;
+    // // public $timeout = 300;
+    // public $timeout = 600; // Set timeout in seconds (10 minutes)
+
+    // protected $data;
+    // protected $email;
+    // protected $templateData;
+    // /**
+    //  * Create a new job instance.
+    //  */
+    // public function __construct($data,$templateData)
+    // {
+    //     // dd($templateData);
+    //     $this->data = $data;
+    //     $this->email = $this->data;
+    //     $this->templateData = $templateData;
+    // }
+
+    // // protected $batchSize = 3 0; // Adjust the batch size as needed
+
+    // // public function __construct($data)
+    // // {
+    // //     dd($data);
+    // //     // $this->email = array_filter((array) $email, function ($e) {
+    // //     //     return filter_var($e, FILTER_VALIDATE_EMAIL) && !empty($e);
+    // //     // });
+
+    // //     // if (empty($this->email)) {
+    // //     //     throw new \Exception("Invalid email: No valid email addresses provided.");
+    // //     // }
+
+    // //     // $this->message = $message;
+    // //     // dd($message);
+    // //     $this->email = $this->data[0];
+    // //     $this->templateData = $this->data[1];
+
+    // // }
+    // public function handle(): void
+    // {
+    //     // dd(1);
+    //     $cance_mail = new CancelEventMail($this->templateData);
+    //     foreach ($this->email as $emails) {
+    //                 try {
+    //                     // Send the email using the BulkEmail Mailable
+    //                     if($emails!=""){
+    //                         Mail::to($this->email)->send($cance_mail); 
+    //                     }
+        
+    //                     // Mail::to('prakashmanat24@gmail.com')
+    //                     // ->bcc($emails) // Send to each batch of 30 via BCC
+    //                     // ->send(new BulkEmail($this->message));
+    //                 } catch (\Exception $e) {
+    //                     // dd($e->getMessage());
+    //                     // Log the error for troubleshooting (don't use dd() in jobs)
+    //                     Log::error("Failed to send email to $emails: " . $e->getMessage());
+        
+    //                     // Optionally, you can store failed emails or implement a retry mechanism
+    //                 }
+    //             }
+    // }
     // public function handle(): void
     // {
 
