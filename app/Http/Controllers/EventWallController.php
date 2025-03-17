353fcb2c21d20c2082e0690a65f4bea9e9a7232a
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-17 16:16:05 +0530

    Update EventWallController.php

--- app/Http/Controllers/EventWallController.php
+++ app/Http/Controllers/EventWallController.php
@@ -3924,7 +3924,7 @@ class EventWallController extends BaseController
                 $postPhotoDetail['mediaData'] = $photoVideoData;
             }
             if ($value->post_type == '2') {
-                $polls = EventPostPoll::with('event_post_poll.eventPollOptions')
+                $polls = EventPostPoll::with('event_post_poll.event_poll_option')
                     ->where([
                         'event_id' => $eventId,
                         'event_post_id' => $value->id
