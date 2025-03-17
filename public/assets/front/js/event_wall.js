0bb4d9c5eb542b305031de8950f7290cb8b519df
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-17 15:08:41 +0530

    commit

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1396,10 +1396,8 @@ $(document).ready(function () {
                     $('input[name="commenton"]').prop("checked", postData.comment_on_off == 1);
 
                     if (postData.post_type == "1") {
-
                         const uploadImgInner = $(".create-post-upload-img-inner");
                         const uploadHeadButton = $(".create-post-head-upload-btn");
-
 
                         uploadImgInner.addClass("d-none");
                         uploadHeadButton.removeClass("d-none");
