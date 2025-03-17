9121003a71ab7780bf7721e4e66522b06652004b
Vimal <vimal.cmexpertise@gmail.com>
2025-03-17 13:06:43 +0530

    commit

--- public/assets/front/js/event.js
+++ public/assets/front/js/event.js
@@ -1438,7 +1438,6 @@ $(document).on('click','#get_pendding_rsvp_btn',function(e){
             if (response.length > 0 && rsvp_ajax_call==0) {
                 console.log(response);
                 let slider = $(".pending_rsvp_slider");
-                // slider.trigger('destroy.owl.carousel').html(''); // Remove old items
     
                 // Loop through the image array and append new items
                 response.forEach((event, index) => {
