8e5160a5a429bd0510ffa876e9fed2ffdea9d17c
alfez1010 <158248751+alfez1010@users.noreply.github.com>
2025-03-17 19:10:30 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1156,7 +1156,8 @@ $(document).ready(function () {
 //     openstoryModal(); // Open the modal after the page loads
 // };
 $("#photos_click").on("click", function () {
-    (".create-post-upload-img-inner").removeClass('d-none')
+    $(".create-post-upload-img-inner").removeClass('d-none')
+    $(".create-post-head-upload-btn").addClass('d-none')
     $("#create-photo-btn").trigger("click");
 });
 $("#poll_click").on("click", function () {
