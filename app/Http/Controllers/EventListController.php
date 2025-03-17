ff03ae91b0bf10844d431aab03d0d29f3455b059
Vimal <vimal.cmexpertise@gmail.com>
2025-03-17 15:53:05 +0530

    Update EventListController.php

--- app/Http/Controllers/EventListController.php
+++ app/Http/Controllers/EventListController.php
@@ -3076,7 +3076,7 @@ class EventListController extends BaseController
                         'event_id' => $eventdetail->event->id,
                         'firstname' => $eventdetail->user->firstname,
                         'lastname' => $eventdetail->user->lastname,
-                        'profile' => $eventdetail->user->profile,
+                        'profile' => ($eventdetail->user->profile!=null)?$eventdetail->user->profile:"",
                         'event_name' => $eventdetail->event->event_name,
                         'event_image' => $images ? asset('storage/event_images/' . $images->image) : "",
                         'user_id'=>$user_id,
