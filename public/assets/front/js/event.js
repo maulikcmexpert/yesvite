b693209d3fe9fb914c5c060fc9531872c3902a22
Vimal <vimal.cmexpertise@gmail.com>
2025-03-17 18:02:02 +0530

    commit

--- public/assets/front/js/event.js
+++ public/assets/front/js/event.js
@@ -1710,14 +1710,11 @@ $(document).on('click', '.close_rsvp_pending', function () {
     $('#pending-rsvp-modal').modal('hide');
 });
 
-$(document).on('click', '#pending-rsvp-modal', function (e) {
-    var modalDialog = $('.modal-dialog');
-
-    // Check if the click target is outside the modal dialog
-    if (!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0) {
-        $('#pending-rsvp-modal').modal('hide'); // Hide the modal
-    }
+$(document).ready(function () {
+    var modal = new bootstrap.Modal(document.getElementById('pending-rsvp-modal'));
+    modal.show(); // Ensure Bootstrap handles modal visibility properly
 });
+
 
 
 // $(document).on('click','.notification-toggle-menu',function(){
