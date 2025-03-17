1746d9abb2d92c46f58d0b5efba5619a8f24571e
Vimal <vimal.cmexpertise@gmail.com>
2025-03-17 18:20:05 +0530

    Update event.js

--- public/assets/front/js/event.js
+++ public/assets/front/js/event.js
@@ -1706,9 +1706,9 @@ $(".pending_rsvp_slider").owlCarousel({
 // });
 
 
-$(document).on('click', '.close_rsvp_pending', function () {
-    $('#pending-rsvp-modal').modal('hide');
-});
+// $(document).on('click', '.close_rsvp_pending', function () {
+//     $('#pending-rsvp-modal').modal('hide');
+// });
 
 // $(document).ready(function () {
 //     var modal = new bootstrap.Modal(document.getElementById('pending-rsvp-modal'));
@@ -1725,6 +1725,10 @@ $(document).ready(function () {
     if (!lastClosedTime || (currentTime - lastClosedTime) > oneMinute) {
         modal.show();
     }
+    $(document).on('click', '.close_rsvp_pending', function () {
+        modal.hide();
+        localStorage.setItem('modalClosedAt', new Date().getTime());
+    });
 
     $(modalElement).on('hidden.bs.modal', function () {
         localStorage.setItem('pending_modal_ClosedAt', new Date().getTime());
