ac34c9bce1c63866be713c8c7f592c8803f40475
Vimal <vimal.cmexpertise@gmail.com>
2025-03-17 12:49:46 +0530

    Update EventListController.php

--- app/Http/Controllers/EventListController.php
+++ app/Http/Controllers/EventListController.php
@@ -3068,11 +3068,14 @@ class EventListController extends BaseController
 
             foreach ($total_need_rsvp_event as $eventdetail) {
                 if ($eventdetail->event) { 
+                    $images = EventImage::where('event_id', $eventdetail->id)->orderBy('type', 'ASC')->first();
+
                     $eventData[] = [
                         'event_id' => $eventdetail->event->id,
                         'event_name' => $eventdetail->event->event_name,
-                        // 'event_image' => $eventdetail->event->design_image,
-                        'event_image' => asset('storage/event_images/' . $eventdetail->event->design_image),
+                        // 'event_image' => $eventdetail->event->design_image,home-upcoming-events-card-right zoom_img
+                        // $eventDetail['event_images'] = ($images != null) ? asset('storage/event_images/' . $images->image) : "";
+                        'event_image' => asset('storage/event_images/' . $images->image),
                     ];
                 }
             }
