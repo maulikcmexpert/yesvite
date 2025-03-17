e8a04d6b60259504ddef6507309bf105240bc923
Vimal <vimal.cmexpertise@gmail.com>
2025-03-17 17:53:36 +0530

    commit

--- resources/views/front/events.blade.php
+++ resources/views/front/events.blade.php
@@ -229,7 +229,9 @@
         </div>
       </div> -->
       @if(!empty($eventData))
-      <div class="modal fade cmn-modal show" id="pending-rsvp-modal" tabindex="-1" style="display: block">
+      {{-- <div class="modal fade cmn-modal show" id="pending-rsvp-modal" tabindex="-1" style="display: block"> --}}
+        <div class="modal fade cmn-modal show" id="pending-rsvp-modal" tabindex="-1" style="display: block;" data-bs-backdrop="true" aria-modal="true" role="dialog">
+
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
