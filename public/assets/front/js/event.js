4273ab3c178eb6f219001ec21554ae719955bd34
Vimal <vimal.cmexpertise@gmail.com>
2025-03-17 17:51:27 +0530

    Update event.js

--- public/assets/front/js/event.js
+++ public/assets/front/js/event.js
@@ -1693,17 +1693,17 @@ $(".pending_rsvp_slider").owlCarousel({
     
 });
 
-$(document).ready(function () {
-    var pendingModal = $('#pending-rsvp-modal');
+// $(document).ready(function () {
+//     var pendingModal = $('#pending-rsvp-modal');
 
-    // Check if the modal has the 'show' class and remove it
-    if (pendingModal.hasClass('show')) {
-        pendingModal.removeClass('show');
-        pendingModal.css('display', 'none'); // Hide it properly
-        $('body').removeClass('modal-open'); // Remove Bootstrap modal-open class
-        $('.modal-backdrop').remove(); // Remove any leftover backdrop
-    }
-});
+//     // Check if the modal has the 'show' class and remove it
+//     if (pendingModal.hasClass('show')) {
+//         pendingModal.removeClass('show');
+//         pendingModal.css('display', 'none'); // Hide it properly
+//         $('body').removeClass('modal-open'); // Remove Bootstrap modal-open class
+//         $('.modal-backdrop').remove(); // Remove any leftover backdrop
+//     }
+// });
 
 
 $(document).on('click', '.close_rsvp_pending', function () {
