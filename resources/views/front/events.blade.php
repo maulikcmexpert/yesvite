074deab64afef88bc9de2e16b9f01db810c01090
Vimal <vimal.cmexpertise@gmail.com>
2025-03-18 16:28:52 +0530

    Update events.blade.php

--- resources/views/front/events.blade.php
+++ resources/views/front/events.blade.php
@@ -1,4 +1,4 @@
-{{-- {{dd($eventData[0]['event_id']);}} --}}
+{{dd($filter);}}
 <x-front.advertise />
 <!-- ============= contact-details ============ -->
 <section class="contact-details profile-details supportive-event-wrp">
