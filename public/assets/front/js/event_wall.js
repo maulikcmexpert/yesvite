ac604368a3e83837d89d5b038c32326959017865
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-19 15:55:56 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1519,7 +1519,7 @@ $(document).ready(function () {
                                     console.error("Media ID not found. Check if data-id is correctly set.");
                                     return;
                                 }
-                                if ($("#imagePreview").children().length === 0) {
+                                if ($("#imagePreview").length == 0) {
                                     console.log($("#imagePreview").children().length === 0);
 
                                     uploadImgInner.removeClass("d-none"); // Hide the container
