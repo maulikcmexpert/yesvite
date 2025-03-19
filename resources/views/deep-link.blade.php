0de4292811ef54240ef24849672d0bbaa26f61f6
Vimal <vimal.cmexpertise@gmail.com>
2025-03-19 18:15:55 +0530

    Update deep-link.blade.php

--- resources/views/deep-link.blade.php
+++ resources/views/deep-link.blade.php
@@ -9,7 +9,7 @@
         function openApp() {
             const appLink = "comappyesvite://somepage";
             const appStoreLink = "https://apps.apple.com/app/6736650042";
-            const isNot;
+            
             let appOpened = false;
 
             // Use `pagehide` to detect if the app opened successfully
@@ -30,10 +30,8 @@
             // alert(appOpened);
             // Fallback: Redirect to App Store if app doesn't open
             setTimeout(() => {
-                isNot=1;
                 const elapsed = Date.now() - now;
-                alert(elapsed);
-                if (!appOpened &&  isNot==1) {
+                if (!appOpened && elapsed < 1500) {
                     window.location.href = appStoreLink;  // App Store redirect
                 }
 
