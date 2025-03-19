dc0058ae0cb525636bae131a909aece56a804615
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-19 20:10:22 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1106,16 +1106,16 @@ $(document).ready(function () {
             let photoPostType = document.getElementById("photoPostType");
 
             // Check if no photo is uploaded AND no content is entered
-            // if (
-            //     (!photoInput || photoInput.files.length === 0) &&
-            //     imagePreview.children.length === 0
+            if (
+                (!photoInput || photoInput.files.length === 0) &&
+                imagePreview.children.length === 0
 
-            // ) {
-            //     toastr.error(
-            //         "Please upload a photo or enter some content for the photo post."
-            //     );
-            //     return;
-            // }
+            ) {
+                toastr.error(
+                    "Please upload a photo or enter some content for the photo post."
+                );
+                return;
+            }
 
             // Set post type based on presence of an uploaded image or entered content
             if (photoPostType) {
