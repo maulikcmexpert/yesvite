43417de9677fac8ddef44c28481697bfa33dc239
Vimal <vimal.cmexpertise@gmail.com>
2025-03-19 16:06:53 +0530

    Update redirect.blade.php

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Redirecting...</title>
-    {{-- <script>
-        window.onload = function () {
-            // Redirect to the main page with the deep link handler
-            window.location.href = "main.html";
-        };
-    </script> --}}
-</head>
+    <script>
+        // function openApp() {
+            // Try opening the Yesvite app
+            window.location.href ="comappyesvite://open";
+
+            // If the app is not installed, redirect to the App Store after 2 seconds
+            setTimeout(function() {
+                window.location.href = "https://apps.apple.com/app/6736650042";
+            }, 2000);
+        // }
+    </script>
 <body>
     <p>Redirecting...</p>
 </body>
