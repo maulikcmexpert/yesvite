85a4543b1a93c882b71c46fda3af1ec345e1e407
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-19 16:09:00 +0530

    Update event_wall.blade.php

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -310,6 +310,7 @@
                                                                                     Report
                                                                                 </button>
                                                                             </li>
+                                                                            @if( $post['is_host'] == 1)
                                                                             <li>
                                                                                 <button
                                                                                     class="dropdown-item editPostBtn"
@@ -364,6 +365,7 @@
                                                                                     Delete
                                                                                 </button>
                                                                             </li>
+                                                                            @endif
                                                                         @endif
                                                                     </ul>
                                                                 </div>
