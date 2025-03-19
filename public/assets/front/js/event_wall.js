910f3cc40e0c022014aeb94e3b6757a77c232de2
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-19 11:55:32 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1442,7 +1442,7 @@ $(document).ready(function () {
                             postData.mediaData.forEach((media) => {
                                 let mediaElement = `
                                     <div class="${colClass}" style="position: relative;" id="media-${media.id}" >
-                                    <input type="hidden" name="media-ids" value="${media.id}" id="media-ids" />
+                                    <input type="hidden" name="media-ids[]" value="${media.id}" id="media-ids" />
                                         <span class="uploded-delete-icon delete_img_edit" data-id="${media.id}">
                                             <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M14 3.98665C11.78 3.76665 9.54667 3.65332 7.32 3.65332C6 3.65332 4.68 3.71999 3.36 3.85332L2 3.98665" stroke="#0F172A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
