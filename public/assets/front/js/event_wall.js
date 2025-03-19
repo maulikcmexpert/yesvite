8bbdc4c41c52ce030da07598061d75e100b552c9
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-19 14:58:47 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1562,18 +1562,13 @@ $(document).ready(function () {
                             $(".poll-options input[name='options[]']").each((index, element) => {
                                 if (options[index]) {
                                     $(element).val(options[index].option);
-
-                                    // Calculate the character count correctly
-                                    const charCount = $(element).val().length;
-
-                                    // Update the character count display
-                                    $(element)
+                                    const charCount = $(this).val().length;
+                                    $(this)
                                         .closest(".mb-3")
                                         .find(".char-count")
-                                        .text(`${charCount}/${maxLength}`);
+                                        .text(`${charCount}/${maxLength}`); // Update existing inputs
                                 }
                             });
-
 
                             // Append only the missing options
                             let existingInputs = $(".poll-options input[name='options[]']").length;
