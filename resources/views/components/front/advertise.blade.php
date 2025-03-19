ce821490d5f952724b8f11cebd8378c3392dcdd5
Vimal <vimal.cmexpertise@gmail.com>
2025-03-19 20:56:49 +0530

    Update advertise.blade.php

--- resources/views/components/front/advertise.blade.php
+++ resources/views/components/front/advertise.blade.php
@@ -58,7 +58,7 @@
 
         // Redirect to App Store only if the app did NOT open
         alert(elapsed);
-        if (!appOpened && elapsed < 1500) {
+        if (!appOpened && elapsed < 1202) {
             window.location.href = appStoreLink;
         }
 
