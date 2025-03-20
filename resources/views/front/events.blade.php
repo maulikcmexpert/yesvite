01fccf63abda6e9ef2c359f7165dc77a70684455
Vimal <vimal.cmexpertise@gmail.com>
2025-03-20 04:53:02 -0700

    Update events.blade.php

--- resources/views/front/events.blade.php
+++ resources/views/front/events.blade.php
@@ -244,7 +244,7 @@
                         <div class="owl-carousel owl-theme pending_rsvp_slider">
                           @foreach ($eventData as $pending )
                             <div class="item">
-                              <div class="setting-img" data-event_id="{{$pending['event_id']}}" data-user_id="{{$pending['user_id']}}" data-event_name="{{$pending['event_name']}}" data-firstname="{{$pending['firstname']}}" data-lastname="{{$pending['lastname']}}" data-profile="{{{{ !empty($pending['profile']) ? asset('storage/profile/' . $pending['profile']) : '' }}}}" data-rsvp_kids="{{$pending['kids']}}"  data-rsvp_adults="{{$pending['adults']}}" data-rsvp="{{$pending['rsvp_status']}}">
+                              <div class="setting-img" data-event_id="{{$pending['event_id']}}" data-user_id="{{$pending['user_id']}}" data-event_name="{{$pending['event_name']}}" data-firstname="{{$pending['firstname']}}" data-lastname="{{$pending['lastname']}}" data-profile="{{ !empty($pending['profile']) ? asset('storage/profile/' . $pending['profile']) : '' }}" data-rsvp_kids="{{$pending['kids']}}"  data-rsvp_adults="{{$pending['adults']}}" data-rsvp="{{$pending['rsvp_status']}}">
                                   <img src="{{$pending['event_image']}}" />
                               </div>
                           </div>
