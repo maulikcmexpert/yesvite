774f2190754942497a31e6cb35f659454d97b5eb
Vimal <vimal.cmexpertise@gmail.com>
2025-03-21 14:19:21 +0530

    Update footer.blade.php

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -918,8 +918,8 @@
 // window.onload = openApp;
 
 function openApp() {
-    const appLink = "comappyesvite://";  // Android App Scheme
-    // const appLink = "intent://somepage#Intent;scheme=https;package=" + appPackage + ";end;";
+    const appLink = "com.yesvite.test";  // Android App Scheme
+    const appLink = "intent://scheme=https;package=" + appPackage + ";end;";
     // const appLink = "intent://somepage#Intent;scheme=https;package=" + appPackage + ";end;";
 
     const playStoreLink = "https://play.google.com/store/apps/details?id=" + appPackage;
