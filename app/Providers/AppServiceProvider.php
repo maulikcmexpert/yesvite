f5cc2b640914706cf7c94b61d6a6e8581b66c2fc
Vimal <vimal.cmexpertise@gmail.com>
2025-03-21 12:07:25 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\File;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,20 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        $path = public_path('.well-known/assetlinks.json');
+
+        if (File::exists($path)) {
+            Response::macro('assetLinks', function () use ($path) {
+                return Response::make(File::get($path), 200)
+                    ->header('Content-Type', 'application/json');
+            });
+
+            // Route::get('/.well-known/assetlinks.json', function () use ($path) {
+            //     return response()->assetLinks();
+            // });
+        }
+
+
         //
     }
 }
