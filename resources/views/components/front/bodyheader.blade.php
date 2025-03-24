5fbd152d43b71314b93f4cbd00c95c7ac3bd7196
Vimal <vimal.cmexpertise@gmail.com>
2025-03-24 11:29:34 +0530

    Update bodyheader.blade.php

--- resources/views/components/front/bodyheader.blade.php
+++ resources/views/components/front/bodyheader.blade.php
@@ -1701,10 +1701,10 @@ $notification_list=getNotificationList();
               </svg>
               </span> Home</a>
             </li>
-            <li><a href="{{route('event.event_lists')}}" class="{{ request()->segment(1) === 'event_lists' ? 'active' : '' }}"><span><svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M21.25 25.625H8.75C5 25.625 2.5 23.75 2.5 19.375V10.625C2.5 6.25 5 4.375 8.75 4.375H21.25C25 4.375 27.5 6.25 27.5 10.625V19.375C27.5 23.75 25 25.625 21.25 25.625Z" stroke="black" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            <li><a href="{{route('event.event_lists')}}" class="{{ request()->segment(1) === 'event_lists' ? 'active' : '' }}"><span><svg width="20" height="20" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M21.25 25.625H8.75C5 25.625 2.5 23.75 2.5 19.375V10.625C2.5 6.25 5 4.375 8.75 4.375H21.25C25 4.375 27.5 6.25 27.5 10.625V19.375C27.5 23.75 25 25.625 21.25 25.625Z" stroke="black" stroke-width="1.8" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
               <path d="M21.25 11.25L17.3375 14.375C16.05 15.4 13.9375 15.4 12.65 14.375L8.75 11.25" stroke="black" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
+              </svg>              
               </span> Events </a>
             </li>
             <li><a href="{{route('profile.contact')}}"><span>
