f927164603000e075cf10566e1e3dd2f3eaf4de9
Vimal <vimal.cmexpertise@gmail.com>
2025-03-24 18:35:35 +0530

    Update message.js

--- public/assets/front/js/message.js
+++ public/assets/front/js/message.js
@@ -3734,6 +3734,7 @@ $(".close-audio-btn").on("click", function () {
     $(".file_info").val("");
     const newPlayer = document.querySelector("#audioContainer");
     newPlayer.classList.remove("initialized");
+    newPlayer.classList.remove("play");
     startButton.style.display = "inline-block";
     $("#musicContainer").removeClass("musicSample");
 });
