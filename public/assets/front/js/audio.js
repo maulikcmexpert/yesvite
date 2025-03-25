a0c0c9eff008eb2cfa9ffdb7fcbbb8dca00bc548
Vimal <vimal.cmexpertise@gmail.com>
2025-03-25 17:14:32 +0530

    Update audio.js

--- public/assets/front/js/audio.js
+++ public/assets/front/js/audio.js
@@ -337,6 +337,7 @@ export function initializeAudioPlayer(player) {
 
     $(".close-audio-btn").on("click", function () {
         $("#musicContainer").hide();
+        $("#musicContainer").removeClass("musicSample");   
         $("#send_audio").hide();
         $(".preview_img").attr("src", "");
         $(".recordedAudio").attr("src", "");
