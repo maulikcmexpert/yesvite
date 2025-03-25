7cf2bb03512a9a728b529f854a6a36e35b39da81
Vimal <vimal.cmexpertise@gmail.com>
2025-03-25 18:59:40 +0530

    Update audio.js

--- public/assets/front/js/audio.js
+++ public/assets/front/js/audio.js
@@ -450,7 +450,7 @@ export function initializeAudioPlayer(player) {
             // Ensure audioDuration is updated when metadata loads
             const durationValue = parseFloat(audioDurationInput.val());
 
-            audioDuration = audio.duration;
+            audioDuration = durationValue;
             audioDurationInput.val(audioDuration); // Update the hidden input as well
             displayDuration();
         });
