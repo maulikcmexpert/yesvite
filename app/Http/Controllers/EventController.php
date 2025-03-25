2a47655b08569903e0c1865ba7d6e8e13803d52b
Vimal <vimal.cmexpertise@gmail.com>
2025-03-25 12:16:37 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3266,7 +3266,7 @@ class EventController extends BaseController
 
         $imageSources = $request->imageSources;
         // dd($imageSources);
-        dd($existingImages);
+        // dd($existingImages);
 
         $i = 0;
 
