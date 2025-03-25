87171a19ea9a202a05c5749700e891487276012a
Vimal <vimal.cmexpertise@gmail.com>
2025-03-25 18:27:11 +0530

    Update audio.js

--- public/assets/front/js/audio.js
+++ public/assets/front/js/audio.js
@@ -326,7 +326,7 @@ export function initializeAudioPlayer(player) {
     const playBtn = player.querySelector(".play");
     const audioDurationInput = $('.current_duration');
 
-    let audioDuration = parseFloat(audioDurationInput.val()); // Initialize with current stored value
+    let audioDuration =audioDurationInput.val(); // Initialize with current stored value
     // console.log(audioDuration);
     // return;
     
