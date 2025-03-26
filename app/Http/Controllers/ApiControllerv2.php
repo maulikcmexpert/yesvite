bb74275ba1e8684e6890aeecfa25e5db161374de
Vimal <vimal.cmexpertise@gmail.com>
2025-03-26 15:24:47 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -5127,35 +5127,35 @@ class ApiControllerv2 extends Controller
                                                     'quantity' => $value['quantity']
                                                 ]);
 
-                                            if (isset($value['self_bring_item']) && $value['self_bring_item'] == '1') {
+                                            // if (isset($value['self_bring_item']) && $value['self_bring_item'] == '1') {
 
-                                                //     // $userQuantity = (isset($value['self_quantity'])) ? $value['self_quantity'] : 0;
-                                                //     if (isset($value['self_quantity']) && $value['self_quantity'] == '0') {
-                                                //         UserPotluckItem::where([
-                                                //             'event_id' => $eventData['event_id'],
-                                                //             'user_id' => $user->id,
-                                                //             'event_potluck_category_id' => $eventPodluckid,
-                                                //             // 'event_potluck_item_id' => $getEventPotluckItem->id,
-                                                //             'event_potluck_item_id' => $value['id'],
-                                                //         ])->delete();
-                                                //     } elseif (isset($value['self_quantity'])) {
-                                                //         UserPotluckItem::where([
-                                                //             'event_id' => $eventData['event_id'],
-                                                //             'user_id' => $user->id,
-                                                //             'event_potluck_category_id' => $eventPodluckid,
-                                                //             // 'event_potluck_item_id' => $getEventPotluckItem->id,
-                                                //             'event_potluck_item_id' => $value['id'],
-                                                //         ])->update(['quantity' => $value['self_quantity']]);
-                                                //     }
-                                            } else {
-                                                UserPotluckItem::where([
-                                                    'event_id' => $eventData['event_id'],
-                                                    'user_id' => $user->id,
-                                                    'event_potluck_category_id' => $eventPodluckid,
-                                                    // 'event_potluck_item_id' => $getEventPotluckItem->id,
-                                                    'event_potluck_item_id' => $value['id'],
-                                                ])->delete();
-                                            }
+                                                    $userQuantity = (isset($value['self_quantity'])) ? $value['self_quantity'] : 0;
+                                                    if (isset($value['self_quantity']) && $value['self_quantity'] == '0') {
+                                                        UserPotluckItem::where([
+                                                            'event_id' => $eventData['event_id'],
+                                                            'user_id' => $user->id,
+                                                            'event_potluck_category_id' => $eventPodluckid,
+                                                            // 'event_potluck_item_id' => $getEventPotluckItem->id,
+                                                            'event_potluck_item_id' => $value['id'],
+                                                        ])->delete();
+                                                    } elseif (isset($value['self_quantity'])) {
+                                                        UserPotluckItem::where([
+                                                            'event_id' => $eventData['event_id'],
+                                                            'user_id' => $user->id,
+                                                            'event_potluck_category_id' => $eventPodluckid,
+                                                            // 'event_potluck_item_id' => $getEventPotluckItem->id,
+                                                            'event_potluck_item_id' => $value['id'],
+                                                        ])->update(['quantity' => $value['self_quantity']]);
+                                                    }
+                                            // } else {
+                                            //     UserPotluckItem::where([
+                                            //         'event_id' => $eventData['event_id'],
+                                            //         'user_id' => $user->id,
+                                            //         'event_potluck_category_id' => $eventPodluckid,
+                                            //         // 'event_potluck_item_id' => $getEventPotluckItem->id,
+                                            //         'event_potluck_item_id' => $value['id'],
+                                            //     ])->delete();
+                                            // }
 
 
 
