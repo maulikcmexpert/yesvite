b68b58f7bfe5c9007db192e1aeb866161ec39f84
Vimal <vimal.cmexpertise@gmail.com>
2025-03-26 15:21:32 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -5071,6 +5071,7 @@ class ApiControllerv2 extends Controller
                     if ($eventData['event_setting']['podluck'] == '1') {
 
                         $podluckCategoryList = $eventData['podluck_category_list'];
+                        dd($podluckCategoryList);
                         if (!empty($podluckCategoryList)) {
                             // EventPotluckCategory::where('event_id', $eventData['event_id'])->delete();
                             // dd($podluckCategoryList)
