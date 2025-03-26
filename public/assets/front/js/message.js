4013fb419eae0db972f8c50b6baef154e41abf16
Vimal <vimal.cmexpertise@gmail.com>
2025-03-26 17:34:49 +0530

    Update message.js

--- public/assets/front/js/message.js
+++ public/assets/front/js/message.js
@@ -3659,7 +3659,11 @@ let startTime, endTime; //    // Timer interval
 let seconds; //
 let counter = 1;  
 let timer = null;     // To store the interval reference
-
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+}
 // Timer interval
 // Track total seconds
 async function startRecording() {
@@ -3679,11 +3683,7 @@ async function startRecording() {
         startTime = performance.now();
 
 
-        function formatTime(seconds) {
-            const minutes = Math.floor(seconds / 60);
-            const secs = seconds % 60;
-            return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
-        }
+   
 
        
 
