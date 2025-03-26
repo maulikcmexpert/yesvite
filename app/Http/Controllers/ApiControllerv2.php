ca3914b21c750998bd0005e15eb13ad15227bd6f
Vimal <vimal.cmexpertise@gmail.com>
2025-03-26 16:19:07 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -5375,7 +5375,7 @@ class ApiControllerv2 extends Controller
                             array_filter($eventData['invited_new_guest'], fn($guest) => $guest['app_user'] === 1)
                         );
 
-                        dd($eventID,$user->id);
+                        dd(1);
                         $notificationParam = [
                             'sender_id' => $user->id,
                             'event_id' => $eventData['event_id'],
