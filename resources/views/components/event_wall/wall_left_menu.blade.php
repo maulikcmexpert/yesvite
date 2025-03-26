787a453f670801d6b6513094e7de62db3cea1295
Vimal <vimal.cmexpertise@gmail.com>
2025-03-26 10:40:47 +0530

    Update wall_left_menu.blade.php

--- resources/views/components/event_wall/wall_left_menu.blade.php
+++ resources/views/components/event_wall/wall_left_menu.blade.php
@@ -105,9 +105,9 @@
             </div>
 
 
-            @if (!empty($eventDetails['event_images']) && count($eventDetails['event_images']) > 1)
+            {{-- @if (!empty($eventDetails['event_images']) && count($eventDetails['event_images']) > 1)
             <div class="custom-pagination"></div>
-        @endif
+        @endif --}}
 
         </div>
     </div>
