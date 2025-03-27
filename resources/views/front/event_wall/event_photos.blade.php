6c0ecf163cc409deafad362586b2405b78c67a19
Vimal <vimal.cmexpertise@gmail.com>
2025-03-27 10:13:40 +0530

    Update event_photos.blade.php

--- resources/views/front/event_wall/event_photos.blade.php
+++ resources/views/front/event_wall/event_photos.blade.php
@@ -361,6 +361,10 @@
                                                                 </ul>
                                                             </div>
                                                         </div>
+                                                        <button class="selected-bulk-btn " style="">
+                                                            <input class="form-check-input selected_bulk_image" type="checkbox" value="" id="flexCheckDefault" data-image-src="[&quot;https:\/\/yesvite.cmexpertiseinfotech.in\/storage\/post_image\/17430501411_17292115280.png&quot;]" data-event-post-id="993" data-user_id="1" data-event-id="537">
+                                                        </button>
+
                                                     </div>
 
 
