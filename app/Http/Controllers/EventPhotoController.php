904f8370640f0c13122836bae180fa4c3a6b1a19
Vimal <vimal.cmexpertise@gmail.com>
2025-03-27 12:55:56 +0530

    Update EventPhotoController.php

--- app/Http/Controllers/EventPhotoController.php
+++ app/Http/Controllers/EventPhotoController.php
@@ -1310,7 +1310,6 @@ class EventPhotoController extends BaseController
             'comment_id' => $event_post_comment->id
         ];
 
-        dd($notificationParam);
         sendNotification('comment_post', $notificationParam);
 
         return response()->json(['success' => true, 'total_comments' => count($postComment), 'data' => $postCommentList, 'message' => "Post commented by you"]);
