f2b65bbc1792c86d288542e5e3771911bd722742
Vimal <vimal.cmexpertise@gmail.com>
2025-03-27 12:12:15 +0530

    Update EventWallController.php

--- app/Http/Controllers/EventWallController.php
+++ app/Http/Controllers/EventWallController.php
@@ -1698,6 +1698,7 @@ class EventWallController extends BaseController
             'image' => $image
         ];
 
+        dd($notificationParam);
         sendNotification('upload_post', $notificationParam);
 
 
