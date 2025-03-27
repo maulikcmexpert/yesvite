ecd291cf67cbdb7e223d7f7f69ab29aa3dae06ec
Vimal <vimal.cmexpertise@gmail.com>
2025-03-27 19:17:13 +0530

    Update wall_left_menu.blade.php

--- resources/views/components/event_wall/wall_left_menu.blade.php
+++ resources/views/components/event_wall/wall_left_menu.blade.php
@@ -109,7 +109,7 @@
             <div class="swiper-button-prev">
             </div>
             @if (!empty($eventDetails['event_images']) && count($eventDetails['event_images']) > 1)
-            <div class="custom-pagination"></div>
+            {{-- <div class="custom-pagination"></div> --}}
             
         @endif
 
