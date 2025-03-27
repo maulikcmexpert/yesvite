7058c294969a7ea83ee3d482b107fad87fa8d63c
Vimal <vimal.cmexpertise@gmail.com>
2025-03-27 10:36:54 +0530

    Update event_photos.blade.php

--- resources/views/front/event_wall/event_photos.blade.php
+++ resources/views/front/event_wall/event_photos.blade.php
@@ -361,6 +361,12 @@
                                                                 </ul>
                                                             </div>
                                                         </div>
+                                                        @php
+                                                        $postMedia = [];
+                                                        foreach ($photo['mediaData'] as $k => $v) {
+                                                            $postMedia[] = $photo['mediaData'][$k]['post_media'];
+                                                        }
+                                                    @endphp
                                                         <button class="selected-bulk-btn" style="display:none;">
                                                             <input class="form-check-input selected_bulk_image"
                                                                 type="checkbox" value="" id="flexCheckDefault"
