e2f7b90e261f9a54ef083719e0c102c28331746a
Vimal <vimal.cmexpertise@gmail.com>
2025-03-27 19:03:42 +0530

    Update wall_left_menu.blade.php

--- resources/views/components/event_wall/wall_left_menu.blade.php
+++ resources/views/components/event_wall/wall_left_menu.blade.php
@@ -88,7 +88,7 @@
                 @if ($eventDetails['event_images'])
                     @foreach ($eventDetails['event_images'] as $image)
                         <div class="swiper-slide">
-                            <div class="hosted-by-template-slider-img {{$disable}} image-zoom-icon">
+                            <div class="hosted-by-template-slider-img {{$disable}}">
                                 <img src="{{ $image }}" alt="Event Image" />
                             </div>
                             <button class="image-zoom-icon" data-img="{{ $image }}" data-bs-toggle="modal" data-bs-target="#event-slider-rsvp-modal"><img
