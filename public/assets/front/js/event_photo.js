4646b56c18a8ddc88bdc77167b3e0a53a3394c5c
Vimal <vimal.cmexpertise@gmail.com>
2025-03-27 10:57:04 +0530

    commit

--- public/assets/front/js/event_photo.js
+++ public/assets/front/js/event_photo.js
@@ -971,8 +971,10 @@ $(document).ready(function () {
         const eventPostId = button.data("event-post-id");
 
         bulkSelectActive = true;
-        $('.selected_bulk_image').css('display','flex');
+
+        $('.selected-bulk-btn').css('display','flex');
         $('.set_emoji_like').css('display','none');
+
         console.log("Bulk Select Mode Active:", bulkSelectActive);
         toggleBulkSelectWrapper(); // Update bulk selection UI
 
