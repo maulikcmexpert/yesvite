cdb3f93869ecacffd97a2d91beeb08b912f4be05
Vimal <vimal.cmexpertise@gmail.com>
2025-03-27 18:19:34 +0530

    Update event_photo.js

--- public/assets/front/js/event_photo.js
+++ public/assets/front/js/event_photo.js
@@ -905,6 +905,7 @@ $(document).ready(function () {
 
             // $('.selected-bulk-btn').css('display','none');
             // $('.set_emoji_like').css('display','flex');
+            
             deleteBtn.addClass("d-none"); // Hide delete button
             downloadBtn.addClass("d-none"); // Hide download button
             $('.add_new_photo_btn').removeClass('d-none'); // Show 'add new photo' button
