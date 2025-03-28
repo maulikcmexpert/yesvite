27f7e721dfa0581cbd027242ffad54f2c56da71f
Vimal <vimal.cmexpertise@gmail.com>
2025-03-28 13:19:32 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -373,7 +373,10 @@
                 };
     
                 @foreach ($subcategory->textdatas as $image)
-                    let imageTags = "{{ $image->tags ?? '' }}".split(',').map(tag => tag.trim());
+                    // let imageTags = "{{ $image->tags ?? '' }}".split(',').map(tag => tag.trim());
+                    // Inside the loop
+                    const imageTags = "{{ $image->tags ?? '' }}".split(',').map(tag => tag.trim());
+
     
                     // Store image with its tags
                     subcategoryData.images.push({
