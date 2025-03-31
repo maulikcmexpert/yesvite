150121692fe03b42f49a870535c9b3ac40728858
Vimal <vimal.cmexpertise@gmail.com>
2025-03-31 18:48:31 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3422,7 +3422,7 @@ class EventController extends BaseController
 
         $imageSources = $request->imageSources;
         $imagenames = $request->imagenames;
-        // dd($imagenames);
+        dd($imageSources);
         $validNames = array_filter(array_column($imagenames, 'name'));
         $savedFileNames = array_column($savedFiles, 'fileName');
         $missingNames = array_diff($savedFileNames, $validNames);
