18809113458e15b449eedfe2985510c9ae523ea1
Vimal <vimal.cmexpertise@gmail.com>
2025-03-31 14:18:34 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -1921,14 +1921,8 @@ $("#end_time").on("change", function () {
 //         $("#rsvp_by_date").attr('disabled',false);
 //     }
 // });
-// $(document).on("click","#rsvp_by_date", function () {
-//     // alert();
-//     if ($(this).prop("disabled")) {
-//         alert("Checkbox is already disabled!");
-//         return; 
-//     }
-// });
-$("#rsvp_by_date").on("change", function () {
+$(document).on("click","#rsvp_by_date", function () {
+    // alert();
     if($("#isRsvpEvent").is(':checked')){
         alert("Checkbox is already disabled!");
         $(this).prop("checked", false);  // Uncheck it if disabled
@@ -1937,6 +1931,9 @@ $("#rsvp_by_date").on("change", function () {
     }else{
         $("#rsvp_by_date").attr('disabled',false);
     }
+});
+$("#rsvp_by_date").on("change", function () {
+
     if ($(this).is(":checked")) {
         $(".rsvp_by_date").show();
     } else {
