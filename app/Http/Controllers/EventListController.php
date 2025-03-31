4f1bb7a12693b1a24d268ed7bc123f5e51e8ea82
Vimal <vimal.cmexpertise@gmail.com>
2025-03-31 11:09:14 +0530

    commit

--- app/Http/Controllers/EventListController.php
+++ app/Http/Controllers/EventListController.php
@@ -1565,6 +1565,7 @@ class EventListController extends BaseController
                 // $formattedDate = Carbon::createFromFormat('Y-m-d H:i:s', $value->updated_at)->format('F j, Y');
                 $formattedDate = Carbon::createFromFormat('Y-m-d H:i:s', $value->updated_at)->format('F j, Y - g:i A');
                 $eventDraftDetail['saved_date'] = $formattedDate;
+                $eventDraftDetail['update_date'] = $value->updated_at;
                 $eventDraftDetail['step'] = ($value->step != NULL) ? $value->step : 0;
                 $eventDraftDetail['event_plan_name'] = $value->subscription_plan_name;
 
