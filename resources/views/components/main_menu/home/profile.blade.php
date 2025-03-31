882ff55487f58c90f4daddbd032002d3dfd770f0
Vimal <vimal.cmexpertise@gmail.com>
2025-03-31 10:04:18 +0530

    Update profile.blade.php

--- resources/views/components/main_menu/home/profile.blade.php
+++ resources/views/components/main_menu/home/profile.blade.php
@@ -96,7 +96,7 @@
 <div class="home-latest-draf-wrp mobile-latest-draf">
                       <div class="home-center-upcoming-events-title">
                         <h3>Latest Drafts</h3>
-                        <a href="{{route('event.event_drafts')}}">All Drafts</a>
+                        <a href="{{route('event.event_drafts')}}">All Draftss</a>
                       </div>
                       <div class="mobile-draf-slider">
                         <div class="swiper latest-draf-slider">
