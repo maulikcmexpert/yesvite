e1f80f538e6d0e7b909ddaeebee8d93fe9f278aa
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-03-31 12:13:41 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -621,12 +621,6 @@ $(document).on("click", ".edit_design_tem", function (e) {
     // }
     console.log(temp_id);
     console.log(eventData.temp_id);
-    let restoredTempId = localStorage.getItem("storedTempId");
-    if(temp_id == restoredTempId ){
-alert();
-        $(".slider_img, .login_img img").attr("src", "");
-    }
-
 
 
 
