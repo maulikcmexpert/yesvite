b5ba934ef8f03898d694a94b9a7df6497aafa1ac
Vimal <vimal.cmexpertise@gmail.com>
2025-03-31 16:03:26 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7886,12 +7886,14 @@ class ApiControllerv2 extends Controller
                 //     $numberOfGuest = EventInvitedUser::where('event_id', $eventDetail->id)->count();
                 //     $eventData[] = "Number of guests : " . $numberOfGuest;
                 // }
-                dd($eventDetail->isRsvpEvent);
+                // dd($eventDetail->isRsvpEvent);
 
                 if ($eventDetail->isRsvpEvent == "1") {
                     $eventData[] = "No RSVP Needed";
                 }
                 $eventDetails['event_detail'] = $eventData;
+
+                dd($eventDetails);
             }
            
             $eventDetails['total_limit'] = $eventDetail->event_settings->allow_limit;
