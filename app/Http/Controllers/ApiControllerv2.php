00e96b5ded67745389c1393182a11ca0d7a258ec
Vimal <vimal.cmexpertise@gmail.com>
2025-03-31 20:32:45 +0530

    commit

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -7888,9 +7888,12 @@ class ApiControllerv2 extends Controller
                 // }
                 // dd($eventDetail->isRsvpEvent);
 
-                if ($eventDetail->isRsvpEvent == "1") {
-                    $eventData[] = "No RSVP Needed";
-                }
+                //new change 31-3
+                        // if ($eventDetail->isRsvpEvent == "1") {
+                        //     $eventData[] = "No RSVP Needed";
+                        // }
+                //new change 31-3
+                
                 $eventDetails['event_detail'] = $eventData;
 
             }
