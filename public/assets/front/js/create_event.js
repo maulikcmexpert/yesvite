95781e7caa717876bef4a8ea6c149c10457a0ae1
Vimal <vimal.cmexpertise@gmail.com>
2025-03-31 15:36:01 +0530

    commit

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -1929,9 +1929,13 @@ $("#rsvp_by_date").on("change", function () {
 });
 $("#isRsvpEvent").on("change", function () {
     if ($(this).is(":checked")) {
+        $('.rsvp_by_date_toggle').hide();
+
         $("#rsvp_by_date").prop("checked", false); 
         // $("#rsvp_by_date").attr("disabled",true);
     } else {
+        $('.rsvp_by_date_toggle').show();
+
         $("#rsvp_by_date").attr("disabled",false);
 
     }
