715b0b5fbcd77f3eff3f792fed418d57fa588417
cmerahul <rahul.cmexpertise@gmail.com>
2025-03-31 14:57:36 +0530

    Update invite_pending.blade.php

--- resources/views/emails/invite_pending.blade.php
+++ resources/views/emails/invite_pending.blade.php
@@ -111,15 +111,14 @@
                     <td>
                         <div class="download-form-img" style="display: flex;align-items: center;gap: 10px;">
                             <a href="#"
-                                style="width: 120px;max-width: 120px;height: 40px;border-radius: 5px;display: block;"><img
+                                style="width: 100%;max-width: 50px;height: 40px;border-radius: 5px;display: block;"><img
                                     src="{{ asset('public/storage/google-play.png')}}" width="120" height="40" alt=""
-                                    style="width: 120px;max-width: 120px;height: 40px;"></a>
+                                    style="width: 100%;height: 100%;"></a>
                             <a href="#"
-                                style="width: 120px;max-width: 120px;height: 40px;border-radius: 5px;display: block;"><img
+                                style="width: 100%;max-width: 50px;height: 40px;border-radius: 5px;display: block;"><img
                                     src="{{ asset('public/storage/app-store.png')}}" width="120" height="40" alt=""
-                                    style="width: 120px;max-width: 120px;height: 40px;"></a>
+                                    style=" width: 100%;height: 100%;"></a>
                         </div>
-
                     </td>
                 </tr>
                 <!-- -------------- -->
