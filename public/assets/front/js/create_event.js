83e32c202809ebb59ce3eab1927629117fa7315a
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-01 17:52:58 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -9119,14 +9119,14 @@ $(document).on("click", ".save-slider-image", function () {
                     return;
                 }
                 var savedImages = response.images;
-                // var newImages = response.images;
+                var newImages = response.images;
                 if ($("#isUserLoggedIn").val() === "0") {
                     let savedImages = JSON.parse(localStorage.getItem('save-slider-image')) || [];
 
                     // Append new images to the existing array
-                    // newImages.forEach(image => {
-                    //     savedImages.push(image);
-                    // });
+                    newImages.forEach(image => {
+                        savedImages.push(image);
+                    });
 
                     // Update localStorage with the combined array
                     localStorage.setItem('save-slider-image', JSON.stringify(savedImages));
