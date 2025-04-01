a73ca0c5ec1a9e6ed5d98e987fdebb61f9d52a44
Vimal <vimal.cmexpertise@gmail.com>
2025-04-01 13:54:35 +0530

    Update bodyheader.blade.php

--- resources/views/components/front/bodyheader.blade.php
+++ resources/views/components/front/bodyheader.blade.php
@@ -115,61 +115,62 @@ $notification_list=getNotificationList();
                 {{-- <span type="button" data-bs-toggle="modal" data-bs-target="#"> --}}
                   <span id="notification-filter-modal" type="button" data-bs-toggle="modal" data-bs-target="#all-notification-filter-modal">
 
-                  <svg
-                    viewBox="0 0 20 21"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M18.3359 5.91669H13.3359"
-                      stroke="#F73C71"
-                      stroke-width="1.5"
-                      stroke-miterlimit="10"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M4.9974 5.91669H1.66406"
-                      stroke="#F73C71"
-                      stroke-width="1.5"
-                      stroke-miterlimit="10"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M8.33073 8.83333C9.94156 8.83333 11.2474 7.5275 11.2474 5.91667C11.2474 4.30584 9.94156 3 8.33073 3C6.7199 3 5.41406 4.30584 5.41406 5.91667C5.41406 7.5275 6.7199 8.83333 8.33073 8.83333Z"
-                      stroke="#F73C71"
-                      stroke-width="1.5"
-                      stroke-miterlimit="10"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M18.3333 15.0833H15"
-                      stroke="#F73C71"
-                      stroke-width="1.5"
-                      stroke-miterlimit="10"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M6.66406 15.0833H1.66406"
-                      stroke="#F73C71"
-                      stroke-width="1.5"
-                      stroke-miterlimit="10"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M11.6667 18C13.2775 18 14.5833 16.6942 14.5833 15.0834C14.5833 13.4725 13.2775 12.1667 11.6667 12.1667C10.0558 12.1667 8.75 13.4725 8.75 15.0834C8.75 16.6942 10.0558 18 11.6667 18Z"
-                      stroke="#F73C71"
-                      stroke-width="1.5"
-                      stroke-miterlimit="10"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
+                    <svg
+                      viewBox="0 0 20 21"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M18.3359 5.91669H13.3359"
+                        stroke="#F73C71"
+                        stroke-width="1.5"
+                        stroke-miterlimit="10"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M4.9974 5.91669H1.66406"
+                        stroke="#F73C71"
+                        stroke-width="1.5"
+                        stroke-miterlimit="10"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M8.33073 8.83333C9.94156 8.83333 11.2474 7.5275 11.2474 5.91667C11.2474 4.30584 9.94156 3 8.33073 3C6.7199 3 5.41406 4.30584 5.41406 5.91667C5.41406 7.5275 6.7199 8.83333 8.33073 8.83333Z"
+                        stroke="#F73C71"
+                        stroke-width="1.5"
+                        stroke-miterlimit="10"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M18.3333 15.0833H15"
+                        stroke="#F73C71"
+                        stroke-width="1.5"
+                        stroke-miterlimit="10"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M6.66406 15.0833H1.66406"
+                        stroke="#F73C71"
+                        stroke-width="1.5"
+                        stroke-miterlimit="10"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M11.6667 18C13.2775 18 14.5833 16.6942 14.5833 15.0834C14.5833 13.4725 13.2775 12.1667 11.6667 12.1667C10.0558 12.1667 8.75 13.4725 8.75 15.0834C8.75 16.6942 10.0558 18 11.6667 18Z"
+                        stroke="#F73C71"
+                        stroke-width="1.5"
+                        stroke-miterlimit="10"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
                 </span>
+                <i class="fa-solid fa-ellipsis-vertical"></i>
               </h5>
             </div>
             <div class="notification-dropdown-content">
