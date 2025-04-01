33fece74fe42fa6043f9d54b1cc3f8a4188dd152
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-01 20:17:08 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -6907,31 +6907,40 @@ $(document).on("click", ".final_checkout", function () {
     const photoSliders = ["sliderImages-1", "sliderImages-2", "sliderImages-3"];
     const sliderImages = eventData.slider_images;
     console.log(sliderImages);
-    if (eventData.slider_images != undefined && eventData.slider_images != "") {
-        // alert(1);
+    if (eventData.slider_images && eventData.slider_images.length > 0) {
         $(".event_images_slider").css("display", "block");
         $(".event_images_template").css("display", "none");
-        var imageHtml;
 
-        var p=0;
-        if (eventData.slider_images && eventData.slider_images.length > 0) {
+        // Destroy existing Owl Carousel instance
+        $(".event_images_slider").trigger("destroy.owl.carousel").html('').removeClass('owl-loaded');
 
+        // Append new images
+        let imageHtml = "";
         eventData.slider_images.forEach((image) => {
-            p++;
-             imageHtml = `
+            imageHtml += `
                 <div class="item">
                     <div class="setting-img">
-                        <img id="sliderImages" src="${base_url+'public/storage/event_images/'+image.fileName}"  />
+                        <img src="${base_url}public/storage/event_images/${image.fileName}" />
                     </div>
                 </div>
             `;
-            // $('.event_images_slider').append(imageHtml);
-
-            $(".event_images_slider")
-            .trigger("add.owl.carousel", [$(imageHtml)])
-            .trigger("refresh.owl.carousel");
         });
-    }
+
+        $(".event_images_slider").html(imageHtml);
+
+        // Reinitialize Owl Carousel
+        $(".event_images_slider").owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots: true,
+            items: 1,
+            responsive: {
+                0: { items: 1 },
+                600: { items: 3 },
+                1000: { items: 5 }
+            }
+        });
 
         $('.slider_image_count').text(p+'/3 Photos');
 
