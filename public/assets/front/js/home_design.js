8d57ed168787cf3778185e682c4cbbccd9e2b82f
Vimal <vimal.cmexpertise@gmail.com>
2025-04-02 10:36:13 +0530

    Update home_design.js

--- public/assets/front/js/home_design.js
+++ public/assets/front/js/home_design.js
@@ -194,8 +194,8 @@ $(document).ready(function () {
                         var lengts= $('.image-item:visible').length;
                         console.log('sasasa'+ lengts);
                         
-                        // $('.total_design_count').text($('.image-item:visible').length + ' Items');
-                        $('.total_design_count').text($('.image-item:visible').not('.d-none').length + ' Items');
+                        $('.total_design_count').text($('.image-item:visible').length + ' Items');
+                        // $('.total_design_count').text($('.image-item:visible').not('.d-none').length + ' Items');
 
                         $('#filtered_results').hide();
     
