265a4a12ad0b00ab16e7edfe067ce8a7e2d9aa46
Vimal <vimal.cmexpertise@gmail.com>
2025-04-03 19:58:05 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -14401,9 +14401,10 @@ class ApiControllerv2 extends Controller
                 $udpated[$k]['fontStyle'] = (isset($value['fontStyle']) && $value['fontStyle'] != '') ? $value['fontStyle'] : 'normal';
                 $udpated[$k]['letterSpacing'] = (isset($value['letterSpacing']) && $value['letterSpacing'] != '') ? (int)$value['letterSpacing'] : 0;
                 $udpated[$k]['underline'] = (isset($value['underline']) && ($value['underline'] == null ||$value['underline'] == "null") ) ? false : $value['underline'];
-                echo (isset($value['underline']) && ($value['underline'] == null ||$value['underline'] == "null") );
-                print_r($value['underline']);
-                print_r($value['underline'] == null);
+              
+                print_r("val".$value['underline']);
+                print_r(stringValue($value['underline']));
+                print_r($value['underline'] == "null");
                 dd($udpated[$k]['underline']);
                 if ($udpated[$k]['text'] == null || $udpated[$k]['text'] == "") {
                     $udpated[$k]['text'] = "";
