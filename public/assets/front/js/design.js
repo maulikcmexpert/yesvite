353cd3d891ab986cbd78b6c7bd9accb0f44085a5
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-03 14:38:39 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -863,7 +863,17 @@ async function bindData(current_event_id) {
                                     // Selected: Solid blue border
                                     ctx.strokeStyle = "#2DA9FC";
                                     ctx.lineWidth = 2;
+                                    // ctx.cornerSize = 10,
+                                    // ctx.cornerColor ="#fff",
+                                    // ctx.cornerStyle ="circle",
                                     ctx.setLineDash([]);
+
+                                    obj.set({
+                                        borderColor: "#2DA9FC",
+                                        cornerSize: 10,
+                                        cornerColor: "#fff",
+                                        cornerStyle: "circle",
+                                    });
                                     setControlVisibilityForObject(obj);
                                 } else {
                                     // Unselected: Dotted blue border
