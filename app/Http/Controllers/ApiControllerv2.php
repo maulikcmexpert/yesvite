02529d53fcd9a01ee9124829ab91f1d191139cde
Vimal <vimal.cmexpertise@gmail.com>
2025-04-03 20:00:04 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -14400,12 +14400,9 @@ class ApiControllerv2 extends Controller
                 $udpated[$k]['textAlign'] = (isset($value['textAlign']) && $value['textAlign'] != '') ? $value['textAlign'] : 'center';
                 $udpated[$k]['fontStyle'] = (isset($value['fontStyle']) && $value['fontStyle'] != '') ? $value['fontStyle'] : 'normal';
                 $udpated[$k]['letterSpacing'] = (isset($value['letterSpacing']) && $value['letterSpacing'] != '') ? (int)$value['letterSpacing'] : 0;
-                $udpated[$k]['underline'] = (isset($value['underline']) && ($value['underline'] == null ||$value['underline'] == "null") ) ? false : $value['underline'];
+                $udpated[$k]['underline'] = (isset($value['underline']) && is_null($value['underline']) ) ? false : $value['underline'];
               
-                print_r(is_null($value['underline']));
-                // print_r(stringValue($value['underline']));
-                print_r($value['underline'] == "null");
-                dd($udpated[$k]['underline']);
+              
                 if ($udpated[$k]['text'] == null || $udpated[$k]['text'] == "") {
                     $udpated[$k]['text'] = "";
                     unset($udpated[$k]);
