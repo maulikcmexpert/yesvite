1d13afede839a748a9ec3b8a5646856beaa9bae2
Vimal <vimal.cmexpertise@gmail.com>
2025-04-03 19:49:43 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -14399,7 +14399,7 @@ class ApiControllerv2 extends Controller
                 $udpated[$k]['textAlign'] = (isset($value['textAlign']) && $value['textAlign'] != '') ? $value['textAlign'] : 'center';
                 $udpated[$k]['fontStyle'] = (isset($value['fontStyle']) && $value['fontStyle'] != '') ? $value['fontStyle'] : 'normal';
                 $udpated[$k]['letterSpacing'] = (isset($value['letterSpacing']) && $value['letterSpacing'] != '') ? (int)$value['letterSpacing'] : 0;
-                $udpated[$k]['underline'] = false;
+                $udpated[$k]['underline'] = (isset($value['underline']) && ($value['underline'] == null||$value['underline'] == "null") ) ? false : $value['underline'];
 
                 if ($udpated[$k]['text'] == null || $udpated[$k]['text'] == "") {
                     $udpated[$k]['text'] = "";
