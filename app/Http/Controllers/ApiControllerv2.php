aa6eb8c355f13a7ffb69bb8efbe3eb21f51430e3
Vimal <vimal.cmexpertise@gmail.com>
2025-04-03 19:58:50 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -14402,8 +14402,8 @@ class ApiControllerv2 extends Controller
                 $udpated[$k]['letterSpacing'] = (isset($value['letterSpacing']) && $value['letterSpacing'] != '') ? (int)$value['letterSpacing'] : 0;
                 $udpated[$k]['underline'] = (isset($value['underline']) && ($value['underline'] == null ||$value['underline'] == "null") ) ? false : $value['underline'];
               
-                // print_r("val".$value['underline']);
-                print_r(stringValue($value['underline']));
+                print_r("val".$value['underline']);
+                // print_r(stringValue($value['underline']));
                 print_r($value['underline'] == "null");
                 dd($udpated[$k]['underline']);
                 if ($udpated[$k]['text'] == null || $udpated[$k]['text'] == "") {
