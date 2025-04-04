41090a5133e980925c6eb1deba2a7aa9c2fc368a
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 18:15:14 +0530

    Update TemplateController.php

--- app/Http/Controllers/admin/TemplateController.php
+++ app/Http/Controllers/admin/TemplateController.php
@@ -301,6 +301,7 @@ class TemplateController extends Controller
      */
     public function update(Request $request, $id)
     {
+        dd($request);
         try {
             // Begin the transaction
             DB::beginTransaction();
