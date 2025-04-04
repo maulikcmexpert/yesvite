97254bd6c31f59eee0c9c2c30af78bf0f8a8626a
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 17:47:08 +0530

    Update TemplateController.php

--- app/Http/Controllers/admin/TemplateController.php
+++ app/Http/Controllers/admin/TemplateController.php
@@ -271,7 +271,7 @@ class TemplateController extends Controller
         // $category_id= decrypt($category);
 
         // Get the template data by ID
-        $getTemData = TextData::with('categories','subcategories')->where('id',$template_id);
+        $getTemData = TextData::with('categories','subcategories')->where('id',$template_id)->first();
 
         $title = 'Edit template';
         $page = 'admin.create_template.edit';
