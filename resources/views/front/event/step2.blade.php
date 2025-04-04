3dc298e2b74811696bff234d5ab31c5a61315cd3
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-04 14:12:19 +0530

    Update step2.blade.php

--- resources/views/front/event/step2.blade.php
+++ resources/views/front/event/step2.blade.php
@@ -288,7 +288,7 @@
             });
             $(document).on("click", ".previousImeg", async function (e) {
 
-                let count = $(".list_all_design_catgeory >  div:visible").length;
+                let count = $(".list_all_design_catgeory > div:visible").length;
 
 console.log(count);
 $('.total_design_count').text(count + ' Items');
