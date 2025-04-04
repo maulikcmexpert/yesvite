8160a3f6ff9d25d247ed564352449f140753c630
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 19:14:06 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -234,7 +234,7 @@
 @endphp
 @foreach ($allImages as $image)
 @php
-      $imagesnew[] = $image;
+      $imagesnew[] = $image['subcategory_name'];
 @endphp
     @if($image['is_visible'] == '1')
         <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs
