ca5ed7ddb662bb0b2951bcbc65e50d045d482dd6
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 19:15:40 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -242,7 +242,7 @@
             data-wow-duration="2s" data-wow-delay="0" data-wow-offset="0"
             data-category-id="{{ $image['category_id'] }}"
             data-subcategory-id="{{ $image['subcategory_id'] }}"
-            data-subcategory_name="{{ $image['subcategory_name'] }}"
+            data-subcategory_name="{{ $image['subcategory_names'] }}"
             data-category_name="{{ $image['category_name'] }}" data-tags="{{ $image['tags'] }}">
 
             <div class="card-img collection-card card-blue edit_design_tem design-card"
@@ -250,13 +250,13 @@
                 data-shape_image="{{ $image['shape_image'] }}"
                 data-json="{{ $image['static_information'] }}"
                 data-id="{{ $image['imageId'] }}"
-                data-subcategory_name="{{ $image['subcategory_name'] }}">
+                data-subcategory_name="{{ $image['subcategory_names'] }}">
                 
                 <img src="{{ $image['image_path'] }}" alt="shower-card">
                 
                 <!-- Optional: Display Subcategories -->
                 <div class="mt-2">
-                    @foreach(explode(',', $image['subcategory_name']) as $subcat)
+                    @foreach(explode(',', $image['subcategory_names']) as $subcat)
                         <span class="badge badge-secondary">{{ trim($subcat) }}</span>
                     @endforeach
                 </div>
