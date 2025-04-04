0b5c489af519a6ccc96cff8d0bdad96747f3127a
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 18:53:38 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -209,7 +209,7 @@
 @endphp
 
 
-            @foreach ($allImages as $image)
+            {{-- @foreach ($allImages as $image)
             @if($image['is_visible']=='1')
                 <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs
                  {{ in_array($image['imageId'], $randomIds) ? 'default_show' : 'd-none' }}"
@@ -228,7 +228,38 @@
 
                 </div>
             @endif
-            @endforeach
+            @endforeach --}}
+
+            @foreach ($allImages as $image)
+    @if($image['is_visible'] == '1')
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs
+            {{ in_array($image['imageId'], $randomIds) ? 'default_show' : 'd-none' }}"
+            data-wow-duration="2s" data-wow-delay="0" data-wow-offset="0"
+            data-category-id="{{ $image['category_id'] }}"
+            data-subcategory-id="{{ $image['subcategory_id'] }}"
+            data-subcategory_name="{{ $image['subcategory_name'] }}"
+            data-category_name="{{ $image['category_name'] }}" data-tags="{{ $image['tags'] }}">
+
+            <div class="card-img collection-card card-blue edit_design_tem design-card"
+                data-image="{{ $image['image'] }}"
+                data-shape_image="{{ $image['shape_image'] }}"
+                data-json="{{ $image['static_information'] }}"
+                data-id="{{ $image['imageId'] }}"
+                data-subcategory_name="{{ $image['subcategory_name'] }}">
+                
+                <img src="{{ $image['image_path'] }}" alt="shower-card">
+                
+                <!-- Optional: Display Subcategories -->
+                <div class="mt-2">
+                    @foreach(explode(',', $image['subcategory_name']) as $subcat)
+                        <span class="badge badge-secondary">{{ trim($subcat) }}</span>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    @endif
+@endforeach
+
         </div>
 
 
