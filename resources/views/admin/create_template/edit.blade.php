9a902f5443e289e2c68787d83dc985248bd35a5a
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 18:01:37 +0530

    Update edit.blade.php

--- resources/views/admin/create_template/edit.blade.php
+++ resources/views/admin/create_template/edit.blade.php
@@ -1,7 +1,7 @@
 {{-- {{dd($getTemData->subcategories);}} --}}
 @php
     $pivotSubcategoryIds = $getTemData->subcategories->pluck('id')->toArray();
-    dd($pivotSubcategoryIds);
+    // dd($pivotSubcategoryIds);
 @endphp
 
 <div class="container-fluid">
@@ -61,7 +61,7 @@
                                     
                                     <div class="inner-wrap" id="event_design_subcategory_id">
                                         @foreach($getSubCatDetail as $subcategory)
-                                        <label><input type="checkbox" value="{{ $subcategory->id }}" class="ckkBox val" name="subcategory[]" {{ $subcategory->id == $getTemData->subcategories->pivot_subcategory_id ? 'checked' : '' }} /><span> {{ $subcategory->subcategory_name }}</span> </label><br>
+                                        <label><input type="checkbox" value="{{ $subcategory->id }}" class="ckkBox val" name="subcategory[]" {{ $subcategory->id == in_array($pivotSubcategoryIds) ? 'checked' : '' }} /><span> {{ $subcategory->subcategory_name }}</span> </label><br>
                                         @endforeach
                                     </div>
                                   </div>
