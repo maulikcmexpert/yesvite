457aff13a52671c71dab25e3437559a9f092f117
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-04 14:52:47 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -457,9 +457,9 @@ $(document).on("click", ".create-event-btn", function () {
 });
 $(document).on("click", ".eventDetails_menu", function () {
 if ($("#isUserLoggedIn").val() === "0") {
-
-    const frontHomeUrl = base_url + "/";
     $("#loader").css("display", "flex");
+    const frontHomeUrl = base_url + "/";
+
     setTimeout(function () {
         window.location.href = frontHomeUrl;
     }, 500); // 500ms delay
