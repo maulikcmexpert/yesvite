5cdfe8a47974a85d0b70a350cfbaaa8992053a4b
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 17:58:41 +0530

    commit

--- app/Http/Controllers/admin/TemplateController.php
+++ app/Http/Controllers/admin/TemplateController.php
@@ -271,7 +271,7 @@ class TemplateController extends Controller
         // $category_id= decrypt($category);
 
         // Get the template data by ID
-        $getTemData = TextData::with('categories','subcategories')->where('id',$template_id)->first();
+        $getTemData = TextData::with('categories','subcategories')->where('id',$template_id)->get();
 
         $title = 'Edit template';
         $page = 'admin.create_template.edit';
--- resources/views/admin/create_template/edit.blade.php
+++ resources/views/admin/create_template/edit.blade.php
@@ -1,4 +1,4 @@
-{{-- {{dd($getTemData->subcategories);}} --}}
+{{dd($getTemData->subcategories);}}
 @php
     $pivotSubcategoryIds = $getTemData->subcategories->pluck('pivot_subcategory_id')->toArray();
     dd($pivotSubcategoryIds);
