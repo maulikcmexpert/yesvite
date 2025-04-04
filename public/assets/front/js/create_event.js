f77082c06e6efad89f348d3d2f08cd083b258821
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-04 15:29:27 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -459,7 +459,7 @@ $(document).on("click", ".eventDetails_menu", function () {
 if ($("#isUserLoggedIn").val() === "0") {
 
 
-    $(".step_1").hide();
+    $(".step_1").css("display","none");
     $(".li_event_details").trigger('click');
 
 
