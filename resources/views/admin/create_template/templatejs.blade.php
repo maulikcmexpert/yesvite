34bf0bd606fa0809998fe5351b979886de390f7f
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 15:14:22 +0530

    Update templatejs.blade.php

--- resources/views/admin/create_template/templatejs.blade.php
+++ resources/views/admin/create_template/templatejs.blade.php
@@ -230,12 +230,20 @@ function toggleCheckedAll(checkbox) {
                     success: function(output) {
                         if (Array.isArray(output) && output.length === 0) {
                             $('#event_design_sub_category_id').empty();
-                            $('#event_design_sub_category_id').append('<option value="">No SubCategory Found</option>');
+                            // $('#event_design_sub_category_id').append('<option value="">No SubCategory Found</option>');
+                            $('#event_design_sub_category_id').append(
+                                '<label><input type="checkbox" value="" class="ckkBox val" /><span>No SubCategory Found</span> </label><br>'
+
+                            );
                             return;
                         }
                         console.log(output);
                         $('#event_design_sub_category_id').empty();
-                        $('#event_design_sub_category_id').append('<option value="">Select subcategory</option>');
+                        // $('#event_design_sub_category_id').append('<option value="">Select subcategory</option>');
+                        $('#event_design_sub_category_id').append(
+                                '<label><input type="checkbox" value="" class="ckkBox val" /><span>No SubCategory Found</span> </label><br>'
+
+                            );
                         output.forEach(function(subcategory) {
                             // $('#event_design_sub_category_id').append(
                             //     '<option value="' + subcategory.sub_category_id + '">' + subcategory.sub_category_name + '</option>'
