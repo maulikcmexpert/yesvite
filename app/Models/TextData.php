0230115afdb8187e391916ea4e4a64fedeaa43ef
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 20:13:16 +0530

    Update TextData.php

--- app/Models/TextData.php
+++ app/Models/TextData.php
@@ -21,28 +21,12 @@ class TextData extends Model
     protected $casts = [
         'static_information' => 'array',
     ];
-    // public function subcategories()
-    // {
-    //     return $this->belongsToMany(EventDesignSubCategory::class, 'textdata_subcategories', 'subcategory_id', 'id');
-    // }
+
     public function subcategories()
-{
-    return $this->belongsToMany(
-        EventDesignSubCategory::class,
-        'textdata_subcategories',
-        'textdata_id',       // Foreign key on pivot table for this (TextData) model
-        'subcategory_id'     // Foreign key on pivot table for the related model
-    );
-}
+    {
+        return $this->belongsTo(EventDesignSubCategory::class, 'event_design_sub_category_id', 'id');
+    }
 
-    // public function subcategories()
-    // {
-    //     return $this->belongsTo(EventDesignSubCategory::class, 'event_design_sub_category_id', 'id');
-    //     // return $this->belongsToMany(EventDesignSubCategory::class, 'textdata_subcategories', 'subcategory_id');
-
-       
-    
-    
     public function categories()
     {
         return $this->belongsTo(EventDesignCategory::class, 'event_design_category_id', 'id');
