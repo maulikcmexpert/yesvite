7f5556476327ab9ce4432f14c02d266de6c43395
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 17:38:49 +0530

    commit

--- app/Http/Controllers/admin/TemplateController.php
+++ app/Http/Controllers/admin/TemplateController.php
@@ -271,7 +271,7 @@ class TemplateController extends Controller
         // $category_id= decrypt($category);
 
         // Get the template data by ID
-        $getTemData = TextData::findOrFail($template_id);
+        $getTemData = TextData::with('categories','subcategories')->findOrFail($template_id);
 
         $title = 'Edit template';
         $page = 'admin.create_template.edit';
