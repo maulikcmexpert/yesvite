7de134876aae69c36e01404a4e62dec6900d296f
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 16:18:29 +0530

    commit

--- app/Models/TextData.php
+++ app/Models/TextData.php
@@ -24,26 +24,26 @@ class TextData extends Model
 
     public function subcategories()
     {
-        // return $this->belongsTo(EventDesignSubCategory::class, 'event_design_sub_category_id', 'id');
-        return $this->belongsToMany(EventDesignSubCategory::class, 'textdata_subcategories', 'subcategory_id');
+        return $this->belongsTo(EventDesignSubCategory::class, 'event_design_sub_category_id', 'id');
+        // return $this->belongsToMany(EventDesignSubCategory::class, 'textdata_subcategories', 'subcategory_id');
 
        
     }
-    public function singleSubcategory()
-    {
-        return $this->belongsTo(EventDesignSubCategory::class, 'event_design_sub_category_id');
-    }
+    // public function singleSubcategory()
+    // {
+    //     return $this->belongsTo(EventDesignSubCategory::class, 'event_design_sub_category_id');
+    // }
     
-    public function getAllSubcategoriesAttribute()
-    {
-        // If many-to-many subcategories exist, use them
-        if ($this->subcategories && $this->subcategories->count() > 0) {
-            return $this->subcategories;
-        }
+    // public function getAllSubcategoriesAttribute()
+    // {
+    //     // If many-to-many subcategories exist, use them
+    //     if ($this->subcategories && $this->subcategories->count() > 0) {
+    //         return $this->subcategories;
+    //     }
     
-        // Else fallback to single subcategory (old system)
-        return collect([$this->singleSubcategory])->filter(); // filter removes nulls
-    }
+    //     // Else fallback to single subcategory (old system)
+    //     return collect([$this->singleSubcategory])->filter(); // filter removes nulls
+    // }
     
     public function categories()
     {
