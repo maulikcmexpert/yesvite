ae0bd22fc92c91362ba204b92b1f485a7580afd9
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-04 13:08:12 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -9509,7 +9509,7 @@ async function savePhotoSlider() {
                     var newImages = response.images;
                     if ($("#isUserLoggedIn").val() === "0") {
                         console.log(savedImages);
-                        
+
                         // let savedImages = JSON.parse(localStorage.getItem('save-slider-image')) || [];
                         // localStorage.removeItem("save-slider-image");
                         // Append new images to the existing array
@@ -10823,6 +10823,8 @@ if (final_step == "1" && isCohost == "1") {
 colorchange();
 
 $(document).on("click", ".previousImeg", async function (e) {
+    updateTotalCount();
+
     var design = eventData.desgin_selected;
     if (design == undefined || design == "") {
         await saveDesignData(true);
@@ -10838,6 +10840,13 @@ $(document).on("click", ".previousImeg", async function (e) {
     $(".subcategory-section").show();
     li_design_click();
 });
+
+function updateTotalCount() {
+    var visibleItems = $(
+        ".image-item:visible, .image-item-new:visible"
+    ).length;
+    $(".total_design_count").text(visibleItems + " Items");
+}
 $(document).on("click", "#event_copy_link_btn", function (e) {
     e.preventDefault(); // Prevents any default button action
     var copyText = $("#event_copy_link").val();
