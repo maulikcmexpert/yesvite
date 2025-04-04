22c6ae8ba62ea14b17ef5dc980829c797fe104d4
Vimal <vimal.cmexpertise@gmail.com>
2025-04-04 19:13:32 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -229,8 +229,13 @@
     $randomIds = array_slice($randomIds, 0, 30);
 @endphp
 
-
+@php
+    $imagesnew=[]
+@endphp
 @foreach ($allImages as $image)
+@php
+      $imagesnew[] = $image;
+@endphp
     @if($image['is_visible'] == '1')
         <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 mt-xl-4 mt-sm-4 mt-4 wow fadeInDown image-item all_designs
             {{ in_array($image['imageId'], $randomIds) ? 'default_show' : 'd-none' }}"
@@ -259,7 +264,9 @@
         </div>
     @endif
 @endforeach
-
+@php
+    dd($imagesnew);
+@endphp
         </div>
 
 
