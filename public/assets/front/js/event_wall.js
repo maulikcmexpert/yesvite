c2ef2b59f82ec98e84e5148838a7c826429b01a2
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-07 13:38:23 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1799,6 +1799,9 @@ $(".modal").on("hidden.bs.modal", function () {
 $(".btn-close").on("click", function () {
     $(".char-count").text("0/140"); // Reset char count
     $(".option-poll").empty();
+    $("#question_error").text('');
+    $("#duration_error").text('');
+    $(".option-error").remove();
     // Add `d-none` class back to hide the div
 });
 
