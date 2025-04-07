24ac4c01b2ffb4e6baddaddec0f26cc0791d95c4
Vimal <vimal.cmexpertise@gmail.com>
2025-04-07 10:50:13 +0530

    Update newLogin.blade.php

--- resources/views/front/event/newLogin.blade.php
+++ resources/views/front/event/newLogin.blade.php
@@ -975,11 +975,11 @@
                                         <div class="text-start mt-1" id="passValidation">
                                         </div>
                                     </div>
-                                    {{-- <div class="col-lg-12 col-md-12 col-sm-12 col-12 mt-4 text-center">
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-12 mt-4 text-center">
                                         <div class="g-recaptcha" style="display: inline-block" data-sitekey="{{ env('RECAPTCHA_SITE_KEY') }}"></div>
                                         <script src="https://www.google.com/recaptcha/api.js"></script>
 
-                                    </div> --}}
+                                    </div>
                                     <div class="col-lg-12 mt-2">
                                         <button type="button" class="btn btn-primary createEventUser"
                                             id="createEventUser">Create account</button>
