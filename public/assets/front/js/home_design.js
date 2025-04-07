38a39335500b46ee93141a935c792c39049a6cf0
Vimal <vimal.cmexpertise@gmail.com>
2025-04-07 15:29:07 +0530

    Update home_design.js

--- public/assets/front/js/home_design.js
+++ public/assets/front/js/home_design.js
@@ -177,79 +177,141 @@ $(document).ready(function () {
 
     // Click event for search results
 
+    // $("#search_design_category").on("keyup", function () {
+    //     let query = $(this).val().toLowerCase().trim();
+    //     let results = "";
+    //     if (query.length > 0) {
+    //         let visibleCount = 0;
+
+    //         $(".image-item").each(function () {
+    //             // let tags = $(this).data("tags")
+    //             //     ? $(this).data("tags").toLowerCase().split(",")
+    //             //     : [];
+
+    //             let tags = $(this).data("tags")
+    //             ? $(this).data("tags").toLowerCase().split(",")
+    //             : [];
+    //             let subcategory = $(this).data("subcategory_name")
+    //                 ? $(this).data("subcategory_name").toLowerCase()
+    //                 : "";
+    //             let category = $(this).data("category_name")
+    //                 ? $(this).data("category_name").toLowerCase()
+    //                 : "";
+
+
+    //                 let matches = tags.some((tag) => tag.includes(query)) ||
+    //                 subcategory.includes(query) ||
+    //                 category.includes(query);
+
+    //                 if (matches) {
+    //                     $(this).show();
+    //                         $(this).removeClass("d-none");
+    //                         $(this).removeClass("fadeInDown");
+    //                         $(this).css("visibility", "visible");
+    //                         $(this).removeClass("wow");
+    //                         $(this).removeClass("d-none").fadeIn();
+    //                         visibleCount++;
+    //                 } else {
+    //                     $(this).hide();
+    //                     $(this).fadeOut().addClass("d-none");
+    //                 }
+    //             // if (tags.some((tag) => tag.includes(query))) {
+    //             //     $(this).show();
+    //             //     $(this).removeClass("d-none");
+    //             //     $(this).removeClass("fadeInDown");
+    //             //     $(this).css("visibility", "visible");
+    //             //     $(this).removeClass("wow");
+    //             //     $(this).removeClass("d-none").fadeIn();
+    //             //     visibleCount++;
+    //             // } else {
+    //             //     $(this).hide();
+    //             //     $(this).fadeOut().addClass("d-none");
+    //             // }
+    //         });
+
+    //         console.log("Total Visible Items:", visibleCount);
+    //         $(".total_design_count").text(visibleCount + " Items");
+
+    //         if (visibleCount > 0) {
+    //             $("#filtered_results").hide();
+    //         } else {
+    //             $("#filtered_results").show();
+    //         }
+
+    //         if ($(".image-item:visible").length === 0) {
+    //             $(".total_design_count").text(
+    //                 $(".image-item:visible").length + " Items"
+    //             );
+
+    //             results += `<div class="search-item no-data">No Data Found</div>`;
+    //             $("#filtered_results").show();
+    //             $("#filtered_results").html(results);
+    //         }
+    //     } else {
+    //         $(".image-item").removeClass("d-none fadeInDown wow").show();
+    //         let allItems = $(".image-item");
+    //         if (allItems.length > 30) {
+    //             allItems.slice(30).addClass("d-none").hide();
+    //         }
+    //         $(".total_design_count").text(
+    //             $(".image-item:visible").length + " Items"
+    //         );
+    //         $("#filtered_results").hide();
+    //     }
+
+    //     // $('#filtered_results').html(results);
+    // });
+
     $("#search_design_category").on("keyup", function () {
         let query = $(this).val().toLowerCase().trim();
         let results = "";
         if (query.length > 0) {
             let visibleCount = 0;
-
+    
             $(".image-item").each(function () {
-                // let tags = $(this).data("tags")
-                //     ? $(this).data("tags").toLowerCase().split(",")
-                //     : [];
-
                 let tags = $(this).data("tags")
-                ? $(this).data("tags").toLowerCase().split(",")
-                : [];
-                // let subcategory = $(this).data("subcategory_name")
-                //     ? $(this).data("subcategory_name").toLowerCase()
-                //     : "";
-                let subcategorys = $(this).data("subcategory_name")
-                ? $(this).data("subcategory_name").toLowerCase().split(",")
-                : [];
-
+                    ? $(this).data("tags").toLowerCase().split(",")
+                    : [];
+                let subcategories = $(this).data("subcategory_name")
+                    ? $(this).data("subcategory_name").toLowerCase().split(",") // Split subcategories by comma
+                    : [];
                 let category = $(this).data("category_name")
                     ? $(this).data("category_name").toLowerCase()
                     : "";
-
-
-                    // let matches = tags.some((tag) => tag.includes(query)) ||
-                    // subcategory.includes(query) ||
-                    // category.includes(query);
-                    let matches = tags.some((tag) => tag.includes(query)) ||
-                    subcategorys.some((subcategory) => subcategory.includes(query)) ||
+    
+                // Check if any of the search criteria match
+                let matches = tags.some((tag) => tag.includes(query)) ||
+                    subcategories.some((subcategory) => subcategory.includes(query)) || // Check each subcategory
                     category.includes(query);
-
-                    if (matches) {
-                        $(this).show();
-                            $(this).removeClass("d-none");
-                            $(this).removeClass("fadeInDown");
-                            $(this).css("visibility", "visible");
-                            $(this).removeClass("wow");
-                            $(this).removeClass("d-none").fadeIn();
-                            visibleCount++;
-                    } else {
-                        $(this).hide();
-                        $(this).fadeOut().addClass("d-none");
-                    }
-                // if (tags.some((tag) => tag.includes(query))) {
-                //     $(this).show();
-                //     $(this).removeClass("d-none");
-                //     $(this).removeClass("fadeInDown");
-                //     $(this).css("visibility", "visible");
-                //     $(this).removeClass("wow");
-                //     $(this).removeClass("d-none").fadeIn();
-                //     visibleCount++;
-                // } else {
-                //     $(this).hide();
-                //     $(this).fadeOut().addClass("d-none");
-                // }
+    
+                if (matches) {
+                    $(this).show();
+                    $(this).removeClass("d-none");
+                    $(this).removeClass("fadeInDown");
+                    $(this).css("visibility", "visible");
+                    $(this).removeClass("wow");
+                    $(this).removeClass("d-none").fadeIn();
+                    visibleCount++;
+                } else {
+                    $(this).hide();
+                    $(this).fadeOut().addClass("d-none");
+                }
             });
-
+    
             console.log("Total Visible Items:", visibleCount);
             $(".total_design_count").text(visibleCount + " Items");
-
+    
             if (visibleCount > 0) {
                 $("#filtered_results").hide();
             } else {
                 $("#filtered_results").show();
             }
-
+    
             if ($(".image-item:visible").length === 0) {
                 $(".total_design_count").text(
                     $(".image-item:visible").length + " Items"
                 );
-
+    
                 results += `<div class="search-item no-data">No Data Found</div>`;
                 $("#filtered_results").show();
                 $("#filtered_results").html(results);
@@ -265,8 +327,6 @@ $(document).ready(function () {
             );
             $("#filtered_results").hide();
         }
-
-        // $('#filtered_results').html(results);
     });
     $(document).on("click", ".search-item", function () {
         let selectedText = $(this).data("name");
