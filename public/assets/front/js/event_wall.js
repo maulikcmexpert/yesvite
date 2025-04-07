f2329a70baff9da4809aee589482b5f32cf3cfa4
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-07 14:31:11 +0530

    Update event_wall.js

--- public/assets/front/js/event_wall.js
+++ public/assets/front/js/event_wall.js
@@ -1761,7 +1761,7 @@ $(document).ready(function () {
         $(".poll-options .option-poll").each(function (index) {
             $(this)
                 .find(".option-number")
-                .text(index + 3);
+                .text(index + 1);
             $(this).find(".char-count").text("0/140"); // Reset char count
         });
     }
@@ -1783,19 +1783,12 @@ $(document).ready(function () {
 
     }
 });
-function renumberOptions() {
-    $(".poll-options .option-poll").each(function (index) {
-        $(this)
-            .find(".option-number")
-            .text(index + 1);
-        $(this).find(".char-count").text("0/140"); // Reset char count
-    });
-}
+
 $(".modal").on("hidden.bs.modal", function () {
     $("#postContent").val("");
     $("#pollForm")[0].reset(); // Reset poll form
     $(this).find("#pollForm")[0].reset();
-    renumberOptions() ;
+
     $("#photoForm")[0].reset(); // Reset photo form
     $("#imagePreview").empty(); // Clear image preview
     $(".char-count").text("0/140"); // Reset char count
