a285ca54847f3d25c701c4351d303ac0187a7f3d
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-07 15:28:04 +0530

    commit

--- resources/views/front/event/newLogin.blade.php
+++ resources/views/front/event/newLogin.blade.php
@@ -1738,7 +1738,36 @@
     <input type="hidden" id="cohostFname" value="" />
     <input type="hidden" id="cohostLname" value="" />
 </section>
-
+<div class="modal fade designModel" id="loginModel" tabindex="-1" aria-labelledby="loginModel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header justify-content-center">
+                <div class="delete-img">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                            stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M12 8V13" stroke="white" stroke-width="1.5" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                        <path d="M11.9946 16H12.0036" stroke="white" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </div>
+                <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+            </div>
+            <div class="modal-body">
+                <h5>Are you sure you want to leave?</h5>
+                <p>Your event is auto saved</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn cancel-login" data-bs-dismiss="modal"
+                    >Exit</button>
+                <button type="button" class="btn continue-btn" data-bs-dismiss="modal">Continue</button>
+            </div>
+        </div>
+    </div>
+</div>
 @push('scripts')
     <script>
         $(document).ready(function() {
