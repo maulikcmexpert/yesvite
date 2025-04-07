c4920951eca06e3a966330d8e3a2c5f70085b329
Vimal <vimal.cmexpertise@gmail.com>
2025-04-07 18:02:23 +0530

    Update HomeFrontController.php

--- app/Http/Controllers/HomeFrontController.php
+++ app/Http/Controllers/HomeFrontController.php
@@ -195,6 +195,7 @@ class HomeFrontController extends BaseController
                                     ->pluck('subcategory_name')
                                     ->unique()
                                     ->implode(', '),
+                                    
                                 'subcategory_id' => $textdata->subcategories
                                     ->pluck('id')
                                     ->unique()
