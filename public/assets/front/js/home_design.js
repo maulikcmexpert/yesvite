d578fead7f62f903f83476f0e3d7545eae87ee55
Vimal <vimal.cmexpertise@gmail.com>
2025-04-07 18:39:57 +0530

    Update home_design.js

--- public/assets/front/js/home_design.js
+++ public/assets/front/js/home_design.js
@@ -106,7 +106,7 @@ $(document).ready(function () {
                         imgCategoryId == categoryId &&
                         imgSubcategoryIds.includes(subcategoryId.toString())
                     ) {
-                        alert();
+                        // alert();
                         // $(this).removeClass("d-none");      
                         // $(this).show();
                         $(this).show();
