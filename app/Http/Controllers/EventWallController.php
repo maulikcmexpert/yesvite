817a1aafa333ebd1ba2de85b9f014df82eb91adb
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-07 18:07:47 +0530

    Update EventWallController.php

--- app/Http/Controllers/EventWallController.php
+++ app/Http/Controllers/EventWallController.php
@@ -702,7 +702,7 @@ class EventWallController extends BaseController
         ///postlist
         $postList = [];
         $eventCreator = Event::where('id', $event)->first();
-        $title = $eventCreator->event_name . 'wall';
+        $title =  'wall';
         // DB::enableQueryLog();
         $eventPostList = EventPost::query();
         $eventPostList->with(['user', 'post_image'])
