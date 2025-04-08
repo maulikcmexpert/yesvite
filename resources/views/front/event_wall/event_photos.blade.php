465dd215feb31e1fbb472d764782ff35e10c7b74
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-08 15:29:31 +0530

    commit

--- resources/views/front/event_wall/event_photos.blade.php
+++ resources/views/front/event_wall/event_photos.blade.php
@@ -629,7 +629,7 @@
                         </div>
                     </div>
                     <form action="{{ route('event_photo.eventPost') }}" id="photoForm" method="POST"
-                        enctype="multipart/form-data">
+                    enctype="multipart/form-data">
                         @csrf
                         <div class="create-post-upload-img-wrp ">
                             <div class="create-post-upload-img-head">
