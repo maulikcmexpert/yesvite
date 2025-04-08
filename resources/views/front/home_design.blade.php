5426c629a08253f5b3a04478e6d62ad7663f4dfe
Vimal <vimal.cmexpertise@gmail.com>
2025-04-08 12:57:35 +0530

    Update home_design.blade.php

--- resources/views/front/home_design.blade.php
+++ resources/views/front/home_design.blade.php
@@ -343,7 +343,8 @@
                 @foreach ($subcategory->textdatas as $image)
                     subcategoryData.images.push({
                         id: {{ $image->id }},
-                        image_path: "{{ asset('storage/canvas/' . $image->filled_image) }}"
+                        image_path: "{{ asset('storage/canvas/' . $image->filled_image) }}",
+                        tags:{{$image->tags}}
 
                     });
                 @endforeach
