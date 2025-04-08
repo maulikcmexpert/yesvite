c8f507f53de75d3b8c96efa8a094d011ad23e1cd
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-08 14:11:44 +0530

    COMMIT

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -11195,7 +11195,7 @@ $(".new-create-account-close-btn").on("click", function (e) {
     $("#loginModel").modal("show");
 
     // // Clear the image src
-    // $(".login_img img").attr("src", "");
+    $(".login_img img").src("");
 
 
 
