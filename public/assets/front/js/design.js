aacfbd3603afd2509bcdfd2f306cb4c47ef925f9
Vimal <vimal.cmexpertise@gmail.com>
2025-04-08 12:24:00 +0530

    Update design.js

--- public/assets/front/js/design.js
+++ public/assets/front/js/design.js
@@ -588,7 +588,6 @@ $(document).on("click", ".modal-design-card", function (e) {
 });
 
 $(document).on("click", ".close-btn", function () {
-    alert(1);
     toggleSidebar();
     var id = $(this).data("id");
     $("#sidebar").removeClass(id);
