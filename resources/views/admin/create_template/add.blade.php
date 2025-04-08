8e554fa82a4aad8714138cc120f86690fdd7da15
Vimal <vimal.cmexpertise@gmail.com>
2025-04-08 10:11:39 +0530

    Update add.blade.php

--- resources/views/admin/create_template/add.blade.php
+++ resources/views/admin/create_template/add.blade.php
@@ -106,8 +106,8 @@
                                       </label> --}}
                                     </div>
                                 </div>
+                                <span class="text-danger subcategory_error_bx">{{ $errors->first('event_design_sub_category_id.*') }}</span>
                             </div>
-                            <span class="text-danger subcategory_error_bx">{{ $errors->first('event_design_sub_category_id.*') }}</span>
                         </div>
                     </div>
                     <div class="col-lg-3 mb-3">
