e40559cd937905b0bc8004dc562c8860e96c5e13
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-08 11:36:24 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -5543,9 +5543,6 @@ $(document).on("click", ".li_event_details", async function () {
                     let slider = $(
                         ".create-account-slider.slider_login"
                     );
-
-        // 🧹 Clear existing carousel items
-        slider.trigger("replace.owl.carousel", [""]).trigger("refresh.owl.carousel");
                     $(".slider_img").each(function () {
                         var slide_image = $(this).attr("src");
                         console.log(slide_image);
@@ -5570,13 +5567,22 @@ $(document).on("click", ".li_event_details", async function () {
                             // Append the new item inside .create-account-slider.slider_login
                             // $(".create-account-slider.slider_login").append(newItem);
 
-                            slider.trigger("add.owl.carousel", [$(newItem)]).trigger("refresh.owl.carousel");
+                            slider
+                                .trigger("add.owl.carousel", [$(newItem)])
+                                .trigger("refresh.owl.carousel");
                         }
-                        if ($(".create-account-slider.slider_login .owl-item").length <= 1) {
+                        if (
+                            $(".create-account-slider.slider_login .owl-item")
+                                .length <= 1
+                        ) {
+                            let slider = $(
+                                ".create-account-slider.slider_login"
+                            );
                             slider.trigger("refresh.owl.carousel");
-                            $(".create-account-slider.slider_login .owl-nav").hide();
+                            $(
+                                ".create-account-slider.slider_login .owl-nav"
+                            ).hide();
                         }
-
                     });
                     $("#loader").css("display", "none");
                     console.log("Captured & Stored Image:", base64Image);
