2869c042d368ae9370c559b2e2561c8fdee73494
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-09 11:38:45 +0530

    Update scritp.js

--- public/assets/front/new_js/scritp.js
+++ public/assets/front/new_js/scritp.js
@@ -597,7 +597,7 @@ function updateColumnClasses() {
 }
 
 // Drag & Drop Support
-const dropZone = document.querySelector(".create-post-uploaded-images");
+const dropZone = document.querySelector(".create-post-upload-img-main");
 if (dropZone) {
     dropZone.addEventListener("dragover", (event) => {
         event.preventDefault();
