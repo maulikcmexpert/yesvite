c794e258064ef95e48c216f2da6a051a0f6b9b6b
Vimal <vimal.cmexpertise@gmail.com>
2025-04-10 12:43:25 +0530

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,45 +4,11 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <meta name="apple-itunes-app" content="app-id=6736650042, app-argument=comappyesvite://">
-        {{-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <meta name="apple-itunes-app" content="app-id=6736650042"> --}}
+        <meta name="apple-itunes-app" content="app-id=6736650042">
 
-    @isset($css)
-    <link rel="stylesheet" href="{{asset('assets')}}/front/css/{{$css}}" />
-    @endisset
-    @isset($css1)
-    <link rel="stylesheet" href="{{asset('assets')}}/front/css/{{$css1}}" />
-    @endisset
-    {{-- <x-front.header title={{$title}} page={{$page}} /> --}}
 </head>
 
 <body>
-    <main>
-    <!-- <div id="home_loader" style="display: none;">
-        <img src="{{asset('assets/front/loader.gif')}}" alt="loader" style="width:146px;height:146px;z-index:1000">
-    </div> -->
-    <div class="loader" id="home_loader" style="display: none;">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-
-    <x-front.bodyheader title={{$title}} />
-
-    @include($page)
-{{--
-    <x-front.bodyfooter page={{$page}} />
-
-    <x-front.footer :js="$js ?? []" /> --}}
-</main>
-@if($page != 'front.event_wall.event_wall' &&  $page != 'front.event_wall.event_potluck' && $page != 'front.event_wall.event_about' &&
-$page != 'front.event_wall.event_guest' && $page != 'front.event_wall.event_photos')
-<x-front.bodyfooter page="{{ $page }}" />
-@endif
-    {{-- <x-front.footer :js="$js ?? []" page="{{ $page }}" /> --}}
-
 
 </body>
 
