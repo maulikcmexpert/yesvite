a3477afd537e02354beda88348271289e2aaa69b
Vimal <vimal.cmexpertise@gmail.com>
2025-04-10 18:25:32 +0530

    Update message.js

--- public/assets/front/js/message.js
+++ public/assets/front/js/message.js
@@ -733,7 +733,8 @@ async function updateChat(user_id) {
             $(".msg-footer").show();
         }
 
-        console.log('conversationId '+conversationId);
+        console.log('blockByMeList '+blockByMeList);
+        console.log('blockByUserList '+blockByUserList);
         
         if (isBlockedByUser) {
             $(".block-conversation").find("span").text("Unblock");
