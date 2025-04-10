d094b5c62ddb6e62e1d5c60ff1c6dd2bad29ec2a
Vimal <vimal.cmexpertise@gmail.com>
2025-04-10 12:21:38 +0530

    Update footer.blade.php

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -517,19 +517,19 @@
 <!-- {{-- <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script> --}} -->
 {{-- <script src="{{ asset('assets/front/new_js/scritp.js') }}"></script>
 <script src="{{ asset('assets/front/walljs/scritp.js') }}"></script> --}}
-<script src="{{ asset('assets/front/new_js/slider.js') }}"></script>
+{{-- <script src="{{ asset('assets/front/new_js/slider.js') }}"></script> --}}
 
-<script src="{{ asset('assets/front/new_js/calender.js') }}"></script>
+{{-- <script src="{{ asset('assets/front/new_js/calender.js') }}"></script> --}}
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
 
 {{-- <script src="{{ asset('assets/front/js/event_wall.js')}}"></script > --}}
-<script src="{{ asset('assets/front/walljs/slider.js') }}"></script>
+{{-- <script src="{{ asset('assets/front/walljs/slider.js') }}"></script> --}}
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/4.0.0/apexcharts.min.js" crossorigin="anonymous">
 </script>
-<script src="{{ asset('assets/front/walljs/guest.js') }}"></script>
-<script src="{{ asset('assets/front/walljs/potluck.js') }}"></script>
+{{-- <script src="{{ asset('assets/front/walljs/guest.js') }}"></script> --}}
+{{-- <script src="{{ asset('assets/front/walljs/potluck.js') }}"></script> --}}
 
 {{-- <script src="{{ asset('assets/front/js/event.js') }}"></script> --}}
 
@@ -540,7 +540,7 @@
 @if (isset($js))
 
     @foreach ($js as $value)
-        <script src="{{ asset('assets/front') }}/js/{{ $value }}.js"></script>
+        {{-- <script src="{{ asset('assets/front') }}/js/{{ $value }}.js"></script> --}}
     @endforeach
 
 @endif
