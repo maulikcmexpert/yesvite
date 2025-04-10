268265641d7e3ce9205573ec50a57a6af3bc90cb
Vimal <vimal.cmexpertise@gmail.com>
2025-04-10 15:33:34 +0530

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,8 @@
 
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <meta name="apple-itunes-app" content="app-id=6736650042">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         {{-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <meta name="apple-itunes-app" content="app-id=6736650042"> --}}
 
@@ -42,9 +42,9 @@ $page != 'front.event_wall.event_guest' && $page != 'front.event_wall.event_phot
 <x-front.bodyfooter page="{{ $page }}" />
 @endif
     <x-front.footer :js="$js ?? []" page="{{ $page }}" />
-    <script type="module" src="https://cdn.jsdelivr.net/npm/@mobiloud/ml-smart-banner@latest/dist/ml-smart-banner.min.js"></script>
+    {{-- <script type="module" src="https://cdn.jsdelivr.net/npm/@mobiloud/ml-smart-banner@latest/dist/ml-smart-banner.min.js"></script> --}}
 
-    <script>
+    {{-- <script>
         const SBoptions = {
               fontFamily: `"Source Sans Pro", "Arial", sans-serif`, // (string) Font family for banner texts, defaults to system safe fonts
               fallbackFontFamily: 'sans-serif', // (string) Font family for fallback icon, safe options are serif and sans-serif
@@ -78,7 +78,7 @@ $page != 'front.event_wall.event_guest' && $page != 'front.event_wall.event_phot
             new SmartBanner(SBoptions).init();
         }
           window.addEventListener('load', addSmartBanner);
-    </script>
+    </script> --}}
 
 </body>
 
