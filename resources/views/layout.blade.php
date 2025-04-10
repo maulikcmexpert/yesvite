041d11101a5937cc85b084dce46ea4d774fc710d
Vimal <vimal.cmexpertise@gmail.com>
2025-04-10 15:57:46 +0530

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,9 @@
 
     <head>
         <meta charset="UTF-8">
-        <meta name="apple-itunes-app" content="app-id=6736650042">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        {{-- <meta name="apple-itunes-app" content="app-id=6736650042"> --}}
+        <meta name="apple-itunes-app" content="app-id=6736650042, app-argument=comappyesvite://home">
         {{-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <meta name="apple-itunes-app" content="app-id=6736650042"> --}}
 
