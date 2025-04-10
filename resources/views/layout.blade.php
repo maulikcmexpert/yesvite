6c081e74900b8e5fe53d158c05e74045aba24b78
Vimal <vimal.cmexpertise@gmail.com>
2025-04-10 15:10:49 +0530

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -51,7 +51,7 @@ $page != 'front.event_wall.event_guest' && $page != 'front.event_wall.event_phot
               appName: 'ML', // (string) Initials for fallback icon.  Recommended 2 characters. Fallback Image uses button text and bg color
               textColor: '#222', // (string) Banner texts color (any color property value)
               buttonColor: '#000000', // (string) Button color (any background property value)
-              buttonText: 'Download', // (string) Button text
+              buttonText: 'Get', // (string) Button text
               buttonTextColor: '#fff', // (string) Button Text Color (any color property value)
               iconUrl: 'https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/c4/a1/70/c4a1704e-ed21-abde-cc5b-20c33be2c6a7/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp', // (string) Icon url, defaults to avatar with appName. You can use the app logo in the appstore/playstore
               textHeading: 'Download our App!', // (string) Heading Text
@@ -67,17 +67,45 @@ $page != 'front.event_wall.event_guest' && $page != 'front.event_wall.event_phot
               shadow: true, // (boolean) If true applies soft shadow, true | false
               useSession: true, // (boolean) If true, after closed, Banner is not shown upon page reload. Default: true
               zindex: 999999 // (number) Sets the z-index of the element
+              deepLink: {
+                        ios: 'comappyesvite://', // Replace with your iOS deep link URL scheme
+                        android: 'intent://yesvite.com/somepage#Intent;scheme=https;package=com.yesvite.android;end;', // Replace with your Android intent URL
+                        }
         }
     
     
-          function addSmartBanner() {
-          // only shows the banner in mobile devices & if not the app
-            if(!deviceData.isMobile || deviceData.isCanvas ){
-                return
-            }
-            new SmartBanner(SBoptions).init();
-        }
-          window.addEventListener('load', addSmartBanner);
+        //   function addSmartBanner() {
+        //   // only shows the banner in mobile devices & if not the app
+        //     if(!deviceData.isMobile || deviceData.isCanvas ){
+        //         return
+        //     }
+        //     new SmartBanner(SBoptions).init();
+        // }
+        function openAppOrStore() {
+                const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+                const isAndroid = /Android/i.test(navigator.userAgent);
+                const now = new Date().getTime();
+                let timeout;
+
+                if (isIOS) {
+                    // Try deep linking
+                    window.location = SBoptions.deepLink.ios;
+
+                    // If app is not opened in 2s, redirect to store
+                    timeout = setTimeout(() => {
+                    window.location = SBoptions.linkIos;
+                    }, 2000);
+                } else if (isAndroid) {
+                    // Use intent for Android
+                    window.location = SBoptions.deepLink.android;
+
+                    timeout = setTimeout(() => {
+                    window.location = SBoptions.linkAndroid;
+                    }, 2000);
+                }
+                }
+
+          window.addEventListener('load', openAppOrStore);
     </script>
 
 </body>
