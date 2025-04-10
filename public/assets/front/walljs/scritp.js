96a46a380dbbd5f822adec68e7313aa7e2c8d807
Vimal <vimal.cmexpertise@gmail.com>
2025-04-10 16:46:08 +0530

    Update scritp.js

--- public/assets/front/walljs/scritp.js
+++ public/assets/front/walljs/scritp.js
@@ -23,24 +23,24 @@ $(document).on("click",'.notification-btn', function () {
 });
 
 // ===header-drodpdown===
-const dropdownButton = document.getElementById("dropdownButton");
-const dropdownMenu = document.querySelector(".notification-dropdown-menu");
+// const dropdownButton = document.getElementById("dropdownButton");
+// const dropdownMenu = document.querySelector(".notification-dropdown-menu");
 
-// Toggle dropdown when clicking the button
-$("#dropdownButton").on("click", function (event) {
-    event.stopPropagation(); // Prevent the click event from bubbling up
-    $(".notification-dropdown-menu").toggleClass("show");
-});
+// // Toggle dropdown when clicking the button
+// $("#dropdownButton").on("click", function (event) {
+//     event.stopPropagation(); // Prevent the click event from bubbling up
+//     $(".notification-dropdown-menu").toggleClass("show");
+// });
 
-// Close the dropdown when clicking outside
-document.addEventListener("click", (event) => {
-    if (
-        !dropdownMenu.contains(event.target) &&
-        !dropdownButton.contains(event.target)
-    ) {
-        dropdownMenu.classList.remove("show");
-    }
-});
+// // Close the dropdown when clicking outside
+// document.addEventListener("click", (event) => {
+//     if (
+//         !dropdownMenu.contains(event.target) &&
+//         !dropdownButton.contains(event.target)
+//     ) {
+//         dropdownMenu.classList.remove("show");
+//     }
+// });
 
 // ----header-dropdown-menu---
 $(document).ready(function () {
