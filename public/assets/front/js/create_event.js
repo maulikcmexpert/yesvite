0bd4a834bf0de01d316f88851609b64e8be6cc4d
Vimal <vimal.cmexpertise@gmail.com>
2025-04-11 15:14:08 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -6030,8 +6030,8 @@ $(document).on("click", ".self_bring_quantity", function () {
             $("#self_bring_qty").val(self_quantity);
             // var item_qty=$('#item_quantity').val();
             if (main_quantity > self_quantity) {
-                // var item_qty=$('#item_quantity').val();
-                var final_qty=main_quantity+self_quantity;
+                var item_qty= parseInt($('#item_quantity').val());
+                var final_qty=main_quantity+item_qty;
                $('#item_quantity').val(final_qty);
                 return;
            }
