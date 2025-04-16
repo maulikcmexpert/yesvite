89f93cca4863b5439cdc0d00f330804b86ea424e
Vimal <vimal.cmexpertise@gmail.com>
2025-04-16 11:54:37 +0530

    Update event.js

--- public/assets/front/js/event.js
+++ public/assets/front/js/event.js
@@ -1892,14 +1892,15 @@ $(document).ready(function () {
 
         $(document).on('click', '.close_rsvp_pending', function () {
             modal.hide();
+            $('.modal-backdrop').removeClass('show');
             localStorage.setItem(`pending_modal_ClosedAt_${userId}`, new Date().getTime());  // Store for specific user
             document.body.classList.remove("no-scroll"); // Re-enable background scrolling
-            if($('.modal-backdrop').hasClass('.show')){
-                $(this).removeClass('show');
-            }
+            // if($('.modal-backdrop').hasClass('.show')){
+            // }
         });
 
         $(modalElement).on('hidden.bs.modal', function () {
+            $('.modal-backdrop').removeClass('show');
             localStorage.setItem(`pending_modal_ClosedAt_${userId}`, new Date().getTime());  // Store for specific user
             document.body.classList.remove("no-scroll"); // Re-enable background scrolling
         });
