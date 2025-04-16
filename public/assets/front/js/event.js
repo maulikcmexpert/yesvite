a4751efaab4145713267ad0d87e24fc163eefc39
Vimal <vimal.cmexpertise@gmail.com>
2025-04-16 11:47:35 +0530

    Update event.js

--- public/assets/front/js/event.js
+++ public/assets/front/js/event.js
@@ -1906,7 +1906,7 @@ $(document).ready(function () {
     }
 
     $(document).on('click', '.close_notification_rsvp', function () {
-        $('#rsvp_by_notification').hide();
+        $('#rsvp_by_notification').modal('hide');
         document.body.classList.remove("no-scroll"); // Re-enable background scrolling
     });
 
