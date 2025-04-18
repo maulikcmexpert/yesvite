28ab8021e6f7e7bf880ebf3d751ac3cab21275d6
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-18 14:36:11 +0530

    Update AppleTokenService.php

--- app/Services/AppleTokenService.php
+++ app/Services/AppleTokenService.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace App\Services;
+
+use Carbon\CarbonImmutable;
 use Lcobucci\JWT\Configuration;
 use Lcobucci\JWT\Signer\Ecdsa\Sha256;
 use Lcobucci\JWT\Signer\Ecdsa\MultibyteStringConverter;
@@ -13,27 +16,27 @@ class AppleTokenService
     {
         $privateKey = config('services.apple.private_key');
 
-        // if (empty($privateKey)) {
-        //     throw new \RuntimeException('Apple private key is not set.');
-        // }
+        if (empty($privateKey)) {
+            throw new \RuntimeException('Apple private key is not set.');
+        }
 
         $signer = new Sha256(new MultibyteStringConverter());
 
-        // $this->config = Configuration::forAsymmetricSigner(
-        //     $signer,
-        //     InMemory::plainText($privateKey),
-        //     InMemory::empty() // No public key required for signing
-        // );
+        $this->config = Configuration::forAsymmetricSigner(
+            $signer,
+            InMemory::plainText($privateKey),
+            InMemory::empty() // No public key required for signing
+        );
     }
 
     public function generate(): string
     {
-        $now = new \DateTimeImmutable();
+        $now = CarbonImmutable::now();
 
         $token = $this->config->builder()
             ->issuedBy(env('APPLE_TEAM_ID')) // Team ID
             ->issuedAt($now)
-            ->expiresAt($now->modify('+6 months'))
+            ->expiresAt($now->addMonths(6))
             ->withHeader('kid', env('APPLE_KEY_ID')) // Key ID
             ->withClaim('aud', 'https://appleid.apple.com')
             ->withClaim('sub', env('APPLE_CLIENT_ID')) // Service ID
