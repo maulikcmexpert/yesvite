358dd664998b2b8297cddaed53a27f8838cbec69
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-18 11:43:38 +0530

    commit

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+use SocialiteProviders\Apple\AppleExtendSocialite;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -20,7 +22,10 @@ class EventServiceProvider extends ServiceProvider
         ],
         'App\Events\UserRegistered' => [
             'App\Listeners\UserProfilePoliciesListener',
-        ]
+        ],
+        SocialiteWasCalled::class => [
+            AppleExtendSocialite::class, // 👈 Add this line
+        ],
     ];
 
     /**
