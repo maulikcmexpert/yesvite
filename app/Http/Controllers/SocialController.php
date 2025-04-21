aaec11c33a5cba841eee2681468b18bd0d6988c8
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-21 12:34:02 +0530

    Update SocialController.php

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -86,6 +86,7 @@ class SocialController extends Controller
 
          } catch (Exception $e) {
             Log::error('Authentication error: ' . $e->getMessage());
+            dd($e);
              return redirect('/login')->with('error', 'Authentication failed.');
          }
 
