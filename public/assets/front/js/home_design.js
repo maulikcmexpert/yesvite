2f182eb874647413cf3d23ce02ac8f9086fa8a92
Vimal <vimal.cmexpertise@gmail.com>
2025-04-22 15:01:38 +0530

    Update home_design.js

--- public/assets/front/js/home_design.js
+++ public/assets/front/js/home_design.js
@@ -206,7 +206,40 @@ $(document).ready(function () {
             $('.total_design_count').text($('.default_show:visible').length + ' Items');
         }
     });
+    $(document).on('click', '.search-item', function () {
+        let selectedText = $(this).data('name');
+        let categoryId = $(this).data('category-id');
+        let subcategoryId = $(this).data('id');
 
+        $('#search_design_category').val(selectedText);
+        $('#filtered_results').html(''); // Clear search results
+        $('#filtered_results').hide();
+        $('.image-item').hide();
+
+        if (categoryId && subcategoryId) {
+            // Show only images that match category and subcategory
+            $(`.image-item[data-category-id="${categoryId}"][data-subcategory-id="${subcategoryId}"]`)
+                .show();
+        } else if (categoryId) {
+
+
+            $(`.image-item[data-category-id="${categoryId}"]`).show();
+        }
+
+        $(`input[name="design_subcategory"][data-category-id="${categoryId}"][data-subcategory-id="${subcategoryId}"]`)
+            .prop('checked', true);
+        // if ($(this).hasClass('subcategory')) {
+
+        //     let images = designData.find(c => c.id == categoryId)
+        //         .subcategories.find(s => s.id == subcategoryId).images;
+
+
+        //     // Auto-check the corresponding subcategory checkbox
+        //
+        // }
+
+        $('.total_design_count').text($('.image-item:visible').length + ' Items');
+    });
     // Click event for search results
 
     // $("#search_design_category").on("keyup", function () {
