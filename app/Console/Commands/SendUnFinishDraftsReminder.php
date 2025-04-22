006c2313af8f329bc29ebaeacb6eeb31d914d162
Vimal <vimal.cmexpertise@gmail.com>
2025-04-22 11:55:32 +0530

    Update SendUnFinishDraftsReminder.php

--- app/Console/Commands/SendUnFinishDraftsReminder.php
+++ app/Console/Commands/SendUnFinishDraftsReminder.php
@@ -36,16 +36,14 @@ class SendUnFinishDraftsReminder extends Command
             foreach ($eventData as $value) {
 
                 // $dateAfterSevenDays = Carbon::parse($value->created_at)->addDays(7)->toDateString();
+                $dateAfterSevenDays = Carbon::parse($value->created_at)->toDateString();
 
-                // if ($dateAfterSevenDays > $currentDate) {
-                    $sendAfter = Carbon::parse($value->created_at)->addMinutes(5);
-                    $now = Carbon::now();
-            
-                    // if ($now->greaterThanOrEqualTo($sendAfter)) {
-                    // $event_time = "";
-                    // if ($value->event_schedule->isNotEmpty()) {
+                if ($dateAfterSevenDays > $currentDate) {
+                    $event_time = "";
+                    if ($value->event_schedule->isNotEmpty()) {
+
                         $event_time =  $value->event_schedule->first()->start_time;
-                    // }
+                    }
 
                     $eventData = [
                         'id'=>$value->id,
@@ -58,7 +56,7 @@ class SendUnFinishDraftsReminder extends Command
 
                     $invitation_email = new NotifyPendingInvitation($eventData);
                     Mail::to($value->user->email)->send($invitation_email);
-                // }
+                }
             }
         }
     }
