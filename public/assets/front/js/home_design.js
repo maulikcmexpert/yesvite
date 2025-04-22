781dbd8229dc8125c664b6f9989cd077e2f0c955
Vimal <vimal.cmexpertise@gmail.com>
2025-04-22 18:01:45 +0530

    Update home_design.js

--- public/assets/front/js/home_design.js
+++ public/assets/front/js/home_design.js
@@ -63,7 +63,7 @@ $(document).ready(function () {
         "change",
         'input[name="design_subcategory"]:not(#Allcat)',
         function () {
-            $(".image-item").hide(); // Hide all default images
+            // $(".image-item").hide(); // Hide all default images
             $(".image-item-new").hide(); // Hide all new images
 
             let default_s = 0;
