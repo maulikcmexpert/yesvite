6cab671554e231c1e7e5c41ec2bb5c7535b51db1
Vimal <vimal.cmexpertise@gmail.com>
2025-04-22 11:22:53 +0530

    Update homefront.blade.php

--- resources/views/front/homefront.blade.php
+++ resources/views/front/homefront.blade.php
@@ -1,6 +1,9 @@
-    <!-- ============= banner-wrapper ======== -->
+
+<!-- ============= banner-wrapper ======== -->
     @php
         $getSocialLink = getSocialLink();
+        $now = Carbon::now();
+        dd($now);
     @endphp
     <section class="banner-wrapper">
         <img src="{{ asset('assets/front/image/left-banner.png') }}" alt="left-banner" class="left-img wow fadeInLeft"
