3a625c02b48915850c2b854bf49c8ce2dd77f882
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-24 11:09:57 +0530

    Update event_potluck.js

--- public/assets/front/js/event_potluck.js
+++ public/assets/front/js/event_potluck.js
@@ -130,7 +130,7 @@ $(document).ready(function () {
             let isValid = true;
 
             const categoryName = $("#category").val().trim();
-            const categoryQuantity = $("#quantity").val().trim();
+            const categoryQuantity = $("#quantity").val();
 
             // Clear previous errors
             $(".error_message_category").text('');
