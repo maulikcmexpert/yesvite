02754411cf15d3fc1e9a7f05af55b1ed06eeeb21
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-24 12:17:21 +0530

    commit

--- public/assets/front/js/event_potluck.js
+++ public/assets/front/js/event_potluck.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
     });
     $("#addCategoryModal").on("click", function () {
         // $("#categoryForm")[0].reset(); // Reset the form
-        $("#charCount").text("0");
+        $(".cat_count").val("0");
         $(".error_message_category").text('');
         $(".error_message_quantity").text('');
            // Set the values in the modal form
--- resources/views/front/event_wall/event_potluck.blade.php
+++ resources/views/front/event_wall/event_potluck.blade.php
@@ -986,7 +986,7 @@
                         <input class="form-control close_cat" oninput="clearError(this)" maxlength="30"
                             type="text"id="category" name="category"
                             placeholder="ie, Appetizers, Salads, Main Dishes">
-                        <span id="category-sub-con" class="sub-con"><span id="charCount">0</span>/30</span>
+                        <span id="category-sub-con" class="sub-con"><span id="charCount" class="cat_count">0</span>/30</span>
                         <span class="error_message_category" style="color: red; font-size: 12px;"></span>
                     </div>
                     <input type="hidden" id="event_id" name="event_id" value="{{ $event }}">
