35cb54f970a993e3061abc17ae5af01d52e8ae8a
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-24 12:04:24 +0530

    Update event_potluck.js

--- public/assets/front/js/event_potluck.js
+++ public/assets/front/js/event_potluck.js
@@ -50,8 +50,8 @@ $(document).ready(function () {
         $(".error_message_category").text('');
         $(".error_message_quantity").text('');
            // Set the values in the modal form
-           $("#category").val("");
-           $("#quantity").val(0);
+           $("#category").text("");
+           $("#quantity").text(0);
 
     });
     $("#deletemodal").on("show.bs.modal", function (event) {
