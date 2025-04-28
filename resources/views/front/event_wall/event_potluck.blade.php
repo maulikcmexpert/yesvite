3015218d95b4a74035e8e54b4bef7af2d3d17d39
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-28 12:27:44 +0530

    Update event_potluck.blade.php

--- resources/views/front/event_wall/event_potluck.blade.php
+++ resources/views/front/event_wall/event_potluck.blade.php
@@ -680,7 +680,7 @@
 
                                                                     @foreach ($item['item_carry_users'] as $users)
                                                                         <div id="sprite-collapseOne-{{ $item['id'] }}"
-                                                                            class="accordion-collapse collapse {{ $item['self_bring_item'] == '1' ? 'show' : '' }}"
+                                                                            class="accordion-collapse collapse {{ $item['self_bring_item'] == '1' ? 'show' : 'show' }}"
                                                                             aria-labelledby="sprite-{{ $item['id'] }}"
                                                                             data-bs-parent="#accordionFlushExample">
                                                                             @if ($login_user_id === $users['user_id'])
