9178ec5d08748297ae328da2b09b9bbe301acfc3
vrushalicme <vrushali.a.cmexpertise@gmail.com>
2025-04-28 14:19:00 +0530

    Update potluck_item.blade.php

--- resources/views/front/event_wall/potluck_item.blade.php
+++ resources/views/front/event_wall/potluck_item.blade.php
@@ -27,7 +27,7 @@
                 </div>
             </div>
             <div class="ms-auto d-flex  toggle-collapse">
-                <h6 class="devide-count itemQty" id="quantity-display">0/{{ $quantity }}</h6>
+                <h6 class="devide-count itemQty" id="quantity-display">1/{{ $quantity }}</h6>
                 <span class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                     data-bs-target="#root-collapseTwo" aria-expanded="false" aria-controls="root-collapseTwo"><i
                         class="fa-solid fa-plus"></i></span>
