229ff1365324f489b3a4ef62842760defe071567
Vimal <vimal.cmexpertise@gmail.com>
2025-07-16 16:06:04 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -4840,7 +4840,8 @@ class ApiControllerv2 extends Controller
                     }
                 }
                 if($eventData['rsvp_by_date_set']=="0" &&(!empty($eventData['start_date']))){
-                    dd(1);
+                    
+                    dd($eventData['start_date']);
                 }
                 $updateEvent->event_type_id = (!empty($eventData['event_type_id'])) ? (int)$eventData['event_type_id'] : NULL;
                 $updateEvent->template_id = (!empty($eventData['template_id'])||$eventData['template_id']!=null) ? (int)$eventData['template_id'] : 0;
