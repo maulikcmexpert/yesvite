680d6fccc5a69375e566c5ae198643ceb91b1965
Vimal <vimal.cmexpertise@gmail.com>
2025-07-16 16:26:10 +0530

    Update ApiControllerv2.php

--- app/Http/Controllers/ApiControllerv2.php
+++ app/Http/Controllers/ApiControllerv2.php
@@ -4729,7 +4729,7 @@ class ApiControllerv2 extends Controller
 
     public function editEvent(Request $request)
     {
-        // dd($request);
+        dd($request);
         $user  = Auth::guard('api')->user();
 
         $rawData = $request->getContent();
