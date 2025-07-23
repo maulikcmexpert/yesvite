a60693d97dfcff4c0fcb9df358d28044b309ba90
Vimal <vimal.cmexpertise@gmail.com>
2025-07-23 11:47:20 +0530

    commit

--- resources/views/front/events.blade.php
+++ resources/views/front/events.blade.php
@@ -73,11 +73,7 @@
                                     </div>
                                 </nav>
 
-                                <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7818976609984635"
-                                data-ad-slot="7204833835" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                                <script>
-                                (adsbygoogle = window.adsbygoogle || []).push({});
-                                </script>
+                             
                                 <!-- ===tab-content-start=== -->
                                 <div class="tab-content" id="nav-tabContent">
                                             @php
