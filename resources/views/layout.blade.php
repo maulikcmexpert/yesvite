6ca50e78a2657e4d80841fbefb1109893a65b048
Vimal <vimal.cmexpertise@gmail.com>
2025-07-23 15:53:34 +0530

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -44,7 +44,7 @@ $page != 'front.event_wall.event_guest' && $page != 'front.event_wall.event_phot
 @endif
     <x-front.footer :js="$js ?? []" page="{{ $page }}" />
   
-{{-- <ins class="adsbygoogle"
+<ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-7818976609984635"
      data-ad-slot="7204833835"
@@ -52,7 +52,7 @@ $page != 'front.event_wall.event_guest' && $page != 'front.event_wall.event_phot
      data-full-width-responsive="true"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
-</script> --}}
+</script>
 
 {{-- @if((isset($display_ad)&& $display_ad==true) || in_array(request()->segment(1), ['home', 'event_lists', 'messages']))
     <img src="{{asset('assets/Your first design - Larg-970x90-px.jpg')}}" />
