70ba2852d906e6660e4c933c4cfc53d4b293e172
Vimal <vimal.cmexpertise@gmail.com>
2025-07-23 12:26:18 +0530

    Update final_checkout.blade.php

--- resources/views/front/event/final_checkout.blade.php
+++ resources/views/front/event/final_checkout.blade.php
@@ -1116,6 +1116,11 @@
                 {{-- <button type="button" class="btn continue-btn">See Invite</button> --}}
                 <a href="#" id="final_see_invite_btn" class="btn continue-btn">Go to event</a>
             </div>
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7818976609984635"
+            data-ad-slot="7204833835" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
         </div>
     </div>
 </div>
