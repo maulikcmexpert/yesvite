17cdb27270d1cb6fba3b2fc188a8df39f4f46212
Vimal <vimal.cmexpertise@gmail.com>
2025-07-23 18:44:01 +0530

    Update rsvp.blade.php

--- resources/views/front/rsvp.blade.php
+++ resources/views/front/rsvp.blade.php
@@ -35,13 +35,7 @@
 <section class="rsvp-wrp new-main-content">
    <!-- ===main-section-start=== -->
    <div class="rsvp-tab-wrp event-center-tabs-main">
-    @if((isset($display_ad)&& $display_ad==true))
-        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7818976609984635"
-        data-ad-slot="7204833835" data-ad-format="auto" data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    @endif
+
     <nav>
         <div class="nav nav-tabs" id="nav-tab" role="tablist">
             <button class="nav-link active" data-tab="invite_tab" id="nav-invite-tab" data-bs-toggle="tab" data-bs-target="#nav-invite" type="button" role="tab" aria-controls="nav-invite" aria-selected="true">
@@ -65,11 +59,13 @@
             {{-- <input type="text" value="{{$rsvp_status}}" /> --}}
             <div class="tab-pane fade show active" id="nav-invite" role="tabpanel" aria-labelledby="nav-invite-tab">
                 <section class="rsvp-wrp">
-                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7818976609984635"
-                    data-ad-slot="7204833835" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                    @if((isset($display_ad)&& $display_ad==true))
+                        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7818976609984635"
+                        data-ad-slot="7204833835" data-ad-format="auto" data-full-width-responsive="true"></ins>
                     <script>
                         (adsbygoogle = window.adsbygoogle || []).push({});
                     </script>
+                    @endif
                     <div class="container">
                         <div class="row">
                             <div class="col-lg-5 mb-lg-0 mb-sm-4 mb-md-4 mb-0">
