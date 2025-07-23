23e748108cffd9348910655ff3575df4a7212854
Vimal <vimal.cmexpertise@gmail.com>
2025-07-23 12:01:41 +0530

    Update event_wall.blade.php

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -57,6 +57,11 @@
                                         <!-- ================story================= -->
                                         <x-event_wall.wall_story :users="$users" :event="$event" :storiesList="$storiesList"
                                             :wallData="$wallData" />
+                                            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7818976609984635"
+                                            data-ad-slot="7204833835" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                                        <script>
+                                            (adsbygoogle = window.adsbygoogle || []).push({});
+                                        </script>
                                         <x-event_wall.wall_crate_poll_photo :users="$users" :event="$event" />
                                         {{-- {{                                    dd($postList)}} --}}
                                         <div class="wall-post-content">
