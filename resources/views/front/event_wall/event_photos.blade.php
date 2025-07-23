fa7e91b555393699aeda5b77ef2ec69b2f3e05fe
Vimal <vimal.cmexpertise@gmail.com>
2025-07-23 12:12:33 +0530

    Update event_photos.blade.php

--- resources/views/front/event_wall/event_photos.blade.php
+++ resources/views/front/event_wall/event_photos.blade.php
@@ -56,6 +56,11 @@
                             <div class="tab-pane fade {{ $photos_show }} {{ $photos_active }}" id="nav-photos"
                                 role="tabpanel" aria-labelledby="nav-photos-tab">
                                 <div class="photos-main-wrp">
+                                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7818976609984635"
+                                    data-ad-slot="7204833835" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                                    <script>
+                                        (adsbygoogle = window.adsbygoogle || []).push({});
+                                    </script>
                                     @php
                                         $hide_image = '';
                                         // $photo = $postPhotoList['mediaData'][0]['post_media'] ?? null;
