963cfa5cc8a2c7463badf52164ac6c1bfc9420fd
Vimal <vimal.cmexpertise@gmail.com>
2025-07-23 11:25:31 +0530

    Update home.blade.php

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,5 +1,5 @@
 @php
-$getSocialLink = getSocialLink();
+    $getSocialLink = getSocialLink();
 @endphp
 {{-- {{dd($imagecount)}} --}}
 <x-front.advertise />
@@ -7,17 +7,22 @@ $getSocialLink = getSocialLink();
     <div class="container">
         <nav>
             <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                <button class="nav-link active" data-tab="explore-designs-tab" id="nav-explore-designs-tab" data-bs-toggle="tab" data-bs-target="#nav-explore-designs" type="button" role="tab" aria-controls="nav-explore-designs" aria-selected="true">
+                <button class="nav-link active" data-tab="explore-designs-tab" id="nav-explore-designs-tab"
+                    data-bs-toggle="tab" data-bs-target="#nav-explore-designs" type="button" role="tab"
+                    aria-controls="nav-explore-designs" aria-selected="true">
                     Profile Home
                 </button>
 
-                <button class="nav-link" id="nav-profile-home-tab" data-bs-toggle="tab" data-bs-target="#nav-profile-home" type="button" role="tab" aria-controls="nav-profile-home" aria-selected="false" tabindex="-1">
+                <button class="nav-link" id="nav-profile-home-tab" data-bs-toggle="tab"
+                    data-bs-target="#nav-profile-home" type="button" role="tab" aria-controls="nav-profile-home"
+                    aria-selected="false" tabindex="-1">
                     Explore Designs
                 </button>
             </div>
         </nav>
         <div class="tab-content">
-            <div class="tab-pane fade show active" id="nav-explore-designs" role="tabpanel" aria-labelledby="explore-designs-tab">
+            <div class="tab-pane fade show active" id="nav-explore-designs" role="tabpanel"
+                aria-labelledby="explore-designs-tab">
 
                 <!-- ============= contact-details ============ -->
                 <section class="contact-details profile-details supportive-home-wrp">
@@ -31,47 +36,79 @@ $getSocialLink = getSocialLink();
                                         <div class="home-center-main">
                                             <div class="home-center-content">
                                                 <x-main_menu.home.profile :profileData="$profileData" :draftEventArray="$draftEventArray" />
-
+                                                <ins class="adsbygoogle" style="display:block"
+                                                    data-ad-client="ca-pub-7818976609984635" data-ad-slot="7204833835"
+                                                    data-ad-format="auto" data-full-width-responsive="true"></ins>
+                                                <script>
+                                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                                </script>
+                                                ddd
                                                 <x-main_menu.home.event_upcoming :eventList="$eventList" />
                                             </div>
 
                                             <div class="newEvent-btns-section">
 
-                                                <button type="button" class="mobile-calender-btn create_event_with_plan">
-                                                        <span class="responsive-text">Calendar</span>
-                                                        <span class="responsive-icon">
-                                                            <svg viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M7.16406 1.66602V4.16602" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            <path d="M13.8359 1.66602V4.16602" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            <path d="M3.41406 7.57422H17.5807" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            <path d="M18 7.08268V14.166C18 16.666 16.75 18.3327 13.8333 18.3327H7.16667C4.25 18.3327 3 16.666 3 14.166V7.08268C3 4.58268 4.25 2.91602 7.16667 2.91602H13.8333C16.75 2.91602 18 4.58268 18 7.08268Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            <path d="M13.5762 11.4167H13.5836" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            <path d="M13.5762 13.9167H13.5836" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            <path d="M10.498 11.4167H10.5055" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            <path d="M10.498 13.9167H10.5055" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            <path d="M7.41209 11.4167H7.41957" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            <path d="M7.41209 13.9167H7.41957" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                            </svg>
+                                                <button type="button"
+                                                    class="mobile-calender-btn create_event_with_plan">
+                                                    <span class="responsive-text">Calendar</span>
+                                                    <span class="responsive-icon">
+                                                        <svg viewBox="0 0 21 20" fill="none"
+                                                            xmlns="http://www.w3.org/2000/svg">
+                                                            <path d="M7.16406 1.66602V4.16602" stroke="white"
+                                                                stroke-width="1.5" stroke-miterlimit="10"
+                                                                stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path d="M13.8359 1.66602V4.16602" stroke="white"
+                                                                stroke-width="1.5" stroke-miterlimit="10"
+                                                                stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path d="M3.41406 7.57422H17.5807" stroke="white"
+                                                                stroke-width="1.5" stroke-miterlimit="10"
+                                                                stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path
+                                                                d="M18 7.08268V14.166C18 16.666 16.75 18.3327 13.8333 18.3327H7.16667C4.25 18.3327 3 16.666 3 14.166V7.08268C3 4.58268 4.25 2.91602 7.16667 2.91602H13.8333C16.75 2.91602 18 4.58268 18 7.08268Z"
+                                                                stroke="white" stroke-width="1.5" stroke-miterlimit="10"
+                                                                stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path d="M13.5762 11.4167H13.5836" stroke="white"
+                                                                stroke-width="2" stroke-linecap="round"
+                                                                stroke-linejoin="round" />
+                                                            <path d="M13.5762 13.9167H13.5836" stroke="white"
+                                                                stroke-width="2" stroke-linecap="round"
+                                                                stroke-linejoin="round" />
+                                                            <path d="M10.498 11.4167H10.5055" stroke="white"
+                                                                stroke-width="2" stroke-linecap="round"
+                                                                stroke-linejoin="round" />
+                                                            <path d="M10.498 13.9167H10.5055" stroke="white"
+                                                                stroke-width="2" stroke-linecap="round"
+                                                                stroke-linejoin="round" />
+                                                            <path d="M7.41209 11.4167H7.41957" stroke="white"
+                                                                stroke-width="2" stroke-linecap="round"
+                                                                stroke-linejoin="round" />
+                                                            <path d="M7.41209 13.9167H7.41957" stroke="white"
+                                                                stroke-width="2" stroke-linecap="round"
+                                                                stroke-linejoin="round" />
+                                                        </svg>
 
-                                                            {{-- <svg class="d-none" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        {{-- <svg class="d-none" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                             <path d="M3 5.83398H18" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
                                                             <path d="M3 10H18" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
                                                             <path d="M3 14.166H18" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
                                                             </svg> --}}
-                                                        </span>
+                                                    </span>
                                                 </button>
-                                                <a href="{{route('event')}}" class="add_new_event_btn create_event_with_plan" id="add_new_event_respo"
-                                                ><i class="fa-solid fa-plus"></i> New Event</a
-                                            >
+                                                <a href="{{ route('event') }}"
+                                                    class="add_new_event_btn create_event_with_plan"
+                                                    id="add_new_event_respo"><i class="fa-solid fa-plus"></i> New
+                                                    Event</a>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-xl-5">
-                                        <input type="hidden" id="totalmonths" value="{{$numMonths}}"/>
-                                        <input type="hidden" id="startmonths" value="{{$startMonth}}"/>
-                                        <input type="hidden" id="diffmonth" value="{{$diffmonth}}"/>
-                                        <input type="hidden" id="calender_json" value="{{$events_calender_json}}"/>
-                                        <input type="hidden" id="current_page" value="{{ (Request::segment(1)) }}"/>
+                                        <input type="hidden" id="totalmonths" value="{{ $numMonths }}" />
+                                        <input type="hidden" id="startmonths" value="{{ $startMonth }}" />
+                                        <input type="hidden" id="diffmonth" value="{{ $diffmonth }}" />
+                                        <input type="hidden" id="calender_json"
+                                            value="{{ $events_calender_json }}" />
+                                        <input type="hidden" id="current_page"
+                                            value="{{ Request::segment(1) }}" />
 
 
 
@@ -81,51 +118,58 @@ $getSocialLink = getSocialLink();
                                         </div>
 
                                         <div class="responsive-calender-month-wrp">
-                                            <h3 class="responsive-calender-month-text" style="display:none;">{{$startMonthCalender}}</h3>
+                                            <h3 class="responsive-calender-month-text" style="display:none;">
+                                                {{ $startMonthCalender }}</h3>
                                         </div>
                                     </div>
                                 </div>
                                 <div id="responsive-calendar" class="responsive-calendar" style="display:none;">
                                     <h2 class="calendar-heading">All Events</h2>
-                                <div class="weekdays" style="position: sticky;"><div class="day">S</div><div class="day">M</div><div class="day">T</div><div class="day">W</div><div class="day">T</div><div class="day">F</div><div class="day">S</div></div>
-                                <div id="responsive-calender-months" class="responsive-calender-months"></div>
+                                    <div class="weekdays" style="position: sticky;">
+                                        <div class="day">S</div>
+                                        <div class="day">M</div>
+                                        <div class="day">T</div>
+                                        <div class="day">W</div>
+                                        <div class="day">T</div>
+                                        <div class="day">F</div>
+                                        <div class="day">S</div>
+                                    </div>
+                                    <div id="responsive-calender-months" class="responsive-calender-months"></div>
+                                </div>
                             </div>
-                            </div>
-                          </div>
+                        </div>
                     </div>
                 </section>
             </div>
             <div class="tab-pane fade" id="nav-profile-home" role="tabpanel" aria-labelledby="nav-profile-home-tab">
-               @include('front.event.step2')
+                @include('front.event.step2')
             </div>
         </div>
     </div>
 </section>
 
 @push('scripts')
-
-
-<script>
-    var base_url = $("#base_url").val()
-    $('.custome-designcategory').hide();
-    $(document).on("click",".edit_design_tem",function(){
-        var id=$(this).attr('data-id');
-        window.location.href=base_url+"events?design_id="+id;
-    })
-//   $(document).ready(function () {
-//     toastr.options = {
-//                     "closeButton": true,
-//                     "progressBar": true,
-//                     "positionClass": "toast-top-right",
-//                 };
-//             @if (session('msg'))
-//                 toastr.options = {
-//                     "closeButton": true,
-//                     "progressBar": true,
-//                     "positionClass": "toast-top-right",
-//                 };
-//                 toastr.success("{{ session('msg') }}");
-//             @endif
-//         });
-</script>
+    <script>
+        var base_url = $("#base_url").val()
+        $('.custome-designcategory').hide();
+        $(document).on("click", ".edit_design_tem", function() {
+            var id = $(this).attr('data-id');
+            window.location.href = base_url + "events?design_id=" + id;
+        })
+        //   $(document).ready(function () {
+        //     toastr.options = {
+        //                     "closeButton": true,
+        //                     "progressBar": true,
+        //                     "positionClass": "toast-top-right",
+        //                 };
+        //             @if (session('msg'))
+        //                 toastr.options = {
+        //                     "closeButton": true,
+        //                     "progressBar": true,
+        //                     "positionClass": "toast-top-right",
+        //                 };
+        //                 toastr.success("{{ session('msg') }}");
+        //             @endif
+        //         });
+    </script>
 @endpush
