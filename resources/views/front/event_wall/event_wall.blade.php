6152bb4afb10e84935378e5aca40eaece5c0e0aa
Vimal <vimal.cmexpertise@gmail.com>
2025-07-23 18:15:26 +0530

    Update event_wall.blade.php

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -55,13 +55,16 @@
 
                                     <div class="event-center-wall-main">
                                         <!-- ================story================= -->
-                                        <x-event_wall.wall_story :users="$users" :event="$event" :storiesList="$storiesList"
-                                            :wallData="$wallData" />
+                                       @if((isset($display_ad)&& $display_ad==true))
                                             <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7818976609984635"
                                             data-ad-slot="7204833835" data-ad-format="auto" data-full-width-responsive="true"></ins>
                                         <script>
                                             (adsbygoogle = window.adsbygoogle || []).push({});
                                         </script>
+                                       @endif
+                                        <x-event_wall.wall_story :users="$users" :event="$event" :storiesList="$storiesList"
+                                            :wallData="$wallData" />
+                                        
                                         <x-event_wall.wall_crate_poll_photo :users="$users" :event="$event" />
                                         {{-- {{                                    dd($postList)}} --}}
                                         <div class="wall-post-content">
