5b1d579369371be533ce6be6d181169244cdc5cd
Vimal <vimal.cmexpertise@gmail.com>
2025-07-23 12:23:59 +0530

    Update event_potluck.blade.php

--- resources/views/front/event_wall/event_potluck.blade.php
+++ resources/views/front/event_wall/event_potluck.blade.php
@@ -42,6 +42,11 @@
 
                         <!-- ===tab-content-start=== -->
                         <div class="tab-content" id="nav-tabContent">
+                            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7818976609984635"
+                            data-ad-slot="7204833835" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
                             @php
                                 $potluck_active = '';
                                 $potluck_show = '';
