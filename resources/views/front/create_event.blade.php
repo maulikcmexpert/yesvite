a73626954aefd430330845963430184bdffc7f36
Vimal <vimal.cmexpertise@gmail.com>
2025-07-24 12:32:56 +0530

    Update create_event.blade.php

--- resources/views/front/create_event.blade.php
+++ resources/views/front/create_event.blade.php
@@ -581,6 +581,29 @@ $fontColorUser = 'fontcolor' . $firstInitialUser;
                 </ul>
                 @endif
             </div>
+            <div class="mx-auto adsense-center" id="ad-container" style="text-align: center;height:auto;min-height:auto;">  
+
+                <ins id="my-ad" class="adsbygoogle" style="display:block;height:100px"
+                    data-ad-client="ca-pub-7818976609984635" data-ad-slot="7204833835"
+                    data-ad-format="auto" data-full-width-responsive="true"></ins>
+                <script>
+                    const adElement = document.getElementById('my-ad');
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                // if (!adElement.classList.contains('adsbygoogle-noablate')) {
+                //     (adsbygoogle = window.adsbygoogle || []).push({});
+                // }
+                setTimeout(function () {
+        
+                    if(adElement.getAttribute('data-ad-status')=="unfilled"){
+                        document.getElementById('ad-container').style.display = 'none';
+        
+                    }
+                }, 2000);
+        
+                // Hide container if ad is not shown after 2 seconds
+              
+                </script>
+                </div>
             <div class="guest-wrp side-guest-wrp">
                 <div class="guest-pro">
                     <div>
