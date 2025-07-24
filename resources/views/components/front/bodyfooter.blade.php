9d11a5436f318058e42fffc4c411c3a255b27b1d
Vimal <vimal.cmexpertise@gmail.com>
2025-07-24 11:30:46 +0530

    Update bodyfooter.blade.php

--- resources/views/components/front/bodyfooter.blade.php
+++ resources/views/components/front/bodyfooter.blade.php
@@ -68,7 +68,7 @@
                       alt="logo">
               </a>
 
-              <div class="mx-auto adsense-center" id="ad-container" style="text-align: center; height:100px; max-height:100px;">
+              <div class="mx-auto adsense-center small-adsense" id="ad-container">
 
                   <ins id="my-bottomad" class="adsbygoogle" style="display:block;height:100px"
                       data-ad-client="ca-pub-7818976609984635" data-ad-slot="7204833835" data-ad-format="auto"
