24b222f671d71130201be3b1ab307d4df3f3591a
Vimal <vimal.cmexpertise@gmail.com>
2025-07-25 17:34:01 +0530

    Update ApiControllerv1.php

--- app/Http/Controllers/ApiControllerv1.php
+++ app/Http/Controllers/ApiControllerv1.php
@@ -3852,7 +3852,7 @@ class ApiControllerv1 extends Controller
             $eventCreation->static_information = $staticInformation;
             $eventCreation->template_id =(isset($eventData['template_id']) && !empty($eventData['template_id']))? (int)$eventData['template_id']: null;
             // dd($eventData);
-            $eventCreation->event_type =(isset($eventData['event_type']) && !empty($eventData['event_type']))?$eventData['event_type']: '';
+            $eventCreation->event_type =(isset($eventData['event_type']) && !empty($eventData['event_type']))?$eventData['event_type']: $eventData['event_type'];
             $eventCreation->proplan_variant = (isset($eventData['proplan_variant']) && !empty($eventData['proplan_variant'])) ? (int)$eventData['proplan_variant'] : 0;
             $eventCreation->is_template_image = (isset($eventData['is_template_image']) && !empty($eventData['is_template_image'])) ? (int)$eventData['is_template_image'] : 0;
             if (!empty($eventData['invited_user_id'])) {
