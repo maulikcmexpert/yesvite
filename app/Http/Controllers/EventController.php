941dabbb4f88d71f7947e24822646b987d8f78c1
Vimal <vimal.cmexpertise@gmail.com>
2025-07-25 20:36:15 +0530

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -981,7 +981,6 @@ class EventController extends BaseController
 
         $potluck = session('category');https://yesvitedev.cmexpertiseinfotech.in/events#
         // dd($request);
-        // dd($request);
 
         Session::forget('desgin');
         Session::forget('custom_image');
@@ -3134,6 +3133,7 @@ class EventController extends BaseController
             $eventDetail['id'] = (!empty($getEventData->id) && $getEventData->id != NULL) ? $getEventData->id : "";
             // $eventDetail['event_type_id'] = (!empty($getEventData->event_type_id) && $getEventData->event_type_id != NULL) ? $getEventData->event_type_id : "";
             $eventDetail['event_name'] = (!empty($getEventData->event_name) && $getEventData->event_name != NULL) ? $getEventData->event_name : "";
+            $eventDetail['event_type'] = (isset($getEventData->event_type) && $getEventData->event_type != NULL) ? $getEventData->event_type : "";
             $eventDetail['hosted_by'] = (!empty($getEventData->hosted_by) && $getEventData->hosted_by != NULL) ? $getEventData->hosted_by : "";
             $eventDetail['start_date'] = (!empty($getEventData->start_date) && $getEventData->start_date != NULL) ? $getEventData->start_date : "";
             $eventDetail['end_date'] = (!empty($getEventData->end_date) && $getEventData->end_date != NULL) ? $getEventData->end_date : "";
