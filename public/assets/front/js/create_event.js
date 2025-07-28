f2d830c5f2c3c24e86b476b2d0e76b1054340ad2
Vimal <vimal.cmexpertise@gmail.com>
2025-07-28 20:59:07 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -11424,11 +11424,11 @@ $(document).on("click", ".user_choice", function () {
     let buttonText = `Send Invites (0)`;
     if (anyCheckedNotDisabled) {
     // alert(1);
-        // $("a.saveGuestOnly.isdisabled").removeAttr("aria-disabled");
+        $("a.saveGuestOnly.isdisabled").removeAttr("aria-disabled");
         buttonText = ` Send Invites(${checkedCount})`;
     } else {
     // alert(2);
-        // $("a.saveGuestOnly.isdisabled").attr("aria-disabled", "true");
+        $("a.saveGuestOnly.isdisabled").attr("aria-disabled", "true");
     }
 
     $("a.saveGuestOnly.isdisabled").text(buttonText);
