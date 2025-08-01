54753f7e33b5ec66319285a5202e008c1337ad70
Vimal <vimal.cmexpertise@gmail.com>
2025-08-01 17:25:37 +0530

    commit

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -8,7 +8,7 @@
                 <x-event_wall.wall_left_menu :page="$current_page" :eventDetails="$eventDetails" :display_ad="$display_ad" />
                 @if((isset($display_ad)&& $display_ad==true))
                     <div class="mx-auto adsense-center" id="ad-container" style="text-align: center;height:auto;min-height:auto;">  
-                        <img src="{{asset('assets/front/img/demo_ad.jpg')}}" />
+                        {{-- <img src="{{asset('assets/front/img/demo_ad.jpg')}}" /> --}}
                         <ins id="my-adguest" class="adsbygoogle" style="display:block;height:100px"
                             data-ad-client="ca-pub-7818976609984635" data-ad-slot="7204833835"
                             data-ad-format="auto" data-full-width-responsive="true"></ins>
