3461e1fab81318f2720d16ac81fbd4458b621507
Vimal <vimal.cmexpertise@gmail.com>
2025-08-22 11:42:22 +0530

    Update EventWallController.php

--- app/Http/Controllers/EventWallController.php
+++ app/Http/Controllers/EventWallController.php
@@ -1138,7 +1138,7 @@ class EventWallController extends BaseController
             },
             'event_schedule',
             'event_settings' => function ($query) {
-                $query->select('event_id', 'podluck', 'allow_limit', 'adult_only_party', 'event_wall', 'guest_list_visible_to_guests');
+                $query->select('event_id', 'podluck', 'allow_limit', 'adult_only_party', 'event_wall', 'guest_list_visible_to_guests','photos');
             },
             'event_invited_user' => function ($query) {
                 $query->where('is_co_host', '1')->with('user');
