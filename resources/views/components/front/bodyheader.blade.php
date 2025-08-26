4607e99690aaccefbd64f0309a459e924ea92714
Vimal <vimal.cmexpertise@gmail.com>
2025-08-26 09:30:42 +0530

    Update bodyheader.blade.php

--- resources/views/components/front/bodyheader.blade.php
+++ resources/views/components/front/bodyheader.blade.php
@@ -1918,13 +1918,10 @@ $notification_list=getNotificationList();
                               data-ad-client="ca-pub-7818976609984635" data-ad-slot="7204833835"
                               data-full-width-responsive="true"></ins>
                           <script>
-                              (adsbygoogle = window.adsbygoogle || []).push({});
-                          
-                      
+                              (adsbygoogle = window.adsbygoogle || []).push({})
                               const container = document.getElementById("small-adsense-id")
-                                  container.style.height = "100px"
-          
-          
+                              container.style.height = "100px"
+
                           </script>
                           </div>
                         </div>
