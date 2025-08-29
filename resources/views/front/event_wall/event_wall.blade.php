b87187e67dc0cd9244aff2d63ba14f3e1bbc0e5a
Vimal <vimal.cmexpertise@gmail.com>
2025-08-29 12:09:55 +0530

    Update event_wall.blade.php

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -5,7 +5,6 @@
         <div class="row">
             <div class="col-xxl-3 col-xl-3 col-lg-4 event-info-left">
                 <!-- =============mainleft-====================== -->
-                <x-event_wall.wall_left_menu :page="$current_page" :eventDetails="$eventDetails" :display_ad="$display_ad" />
                 @if((isset($display_ad)&& $display_ad==true))
                     <div class="mx-auto adsense-center" id="ad-container" style="text-align: center;height:auto;min-height:auto;">  
                         {{-- <img src="{{asset('assets/front/img/demo_ad.jpg')}}" /> --}}
@@ -17,6 +16,8 @@
                         </script>
                         </div>
                 @endif
+                <x-event_wall.wall_left_menu :page="$current_page" :eventDetails="$eventDetails" :display_ad="$display_ad" />
+                
             </div>
             <div class="col-xxl-6 col-xl-6 col-lg-8 event-info-center">
                 <div class="main-content-center">
@@ -1148,6 +1149,17 @@
                 </div>
             </div>
             <div class="col-xxl-3 col-xl-3 col-lg-0 event-info-right">
+                @if((isset($display_ad)&& $display_ad==true))
+                    <div class="mx-auto adsense-center" id="ad-container" style="text-align: center;height:auto;min-height:auto;">  
+                        {{-- <img src="{{asset('assets/front/img/demo_ad.jpg')}}" /> --}}
+                        <ins id="my-adguest" class="adsbygoogle" style="display:block;height:100px"
+                            data-ad-client="ca-pub-7818976609984635" data-ad-slot="7204833835"
+                            data-ad-format="auto" data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                        </div>
+            @endif
                 <x-event_wall.wall_right_menu :eventInfo="$eventInfo" :event="$event" :login_user_id="$login_user_id"
                     :rsvpSent="$rsvpSent" />
             </div>
