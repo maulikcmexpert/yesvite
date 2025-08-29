b35205e58ebb264fbf865a9e16b0bb5022caf580
Vimal <vimal.cmexpertise@gmail.com>
2025-08-29 15:54:29 +0530

    commit

--- resources/views/front/event_wall/event_wall.blade.php
+++ resources/views/front/event_wall/event_wall.blade.php
@@ -73,7 +73,7 @@
 
                                             @if((isset($display_ad)&& $display_ad==true))
                                                 <div class="mx-auto adsense-center ads-responsive" id="ad-container" style="text-align: center;height:auto;min-height:auto;">  
-                                                    <img src="{{asset('assets/front/img/demo_ad.jpg')}}" />
+                                                    {{-- <img src="{{asset('assets/front/img/demo_ad.jpg')}}" /> --}}
                                                     <ins id="my-adguest" class="adsbygoogle" style="display:block;height:100px"
                                                         data-ad-client="ca-pub-7818976609984635" data-ad-slot="7204833835"
                                                         data-ad-format="auto" data-full-width-responsive="true"></ins>
