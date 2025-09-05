2314ec08ca0937fa3373675c02bca7a0cc742706
Vimal <vimal.cmexpertise@gmail.com>
2025-09-05 16:32:17 +0530

    Update rsvp.blade.php

--- resources/views/front/rsvp.blade.php
+++ resources/views/front/rsvp.blade.php
@@ -97,14 +97,30 @@
                                                @endforeach
                                            @endif
                                        </div>
-    
+                                       @if (isset($display_ad) && $display_ad == true)
+                                       <div class="mx-auto adsense-center" style="text-align: center;">
+                                           <img src="{{ asset('assets/front/img/demo_ad.jpg') }}" />
+
+                                           {{-- <ins class="adsbygoogle"
+                                            id="my-adRsvp"
+                                            style="display:block"
+                                            data-ad-client="ca-pub-7818976609984635"
+                                            data-ad-slot="7204833835"
+                                            data-ad-format="auto"
+                                            data-full-width-responsive="true"></ins>
+                                            <script>
+                        
+                                            (adsbygoogle = window.adsbygoogle || []).push({});
+                                                            </script> --}}
+                                       </div>
+                                   @endif
                                    </div>
 
                                    <div class="col-lg-7">
                                        @if (isset($display_ad) && $display_ad == true)
                                            <div class="mx-auto adsense-center" style="text-align: center;">
                                                <img src="{{ asset('assets/front/img/demo_ad.jpg') }}" />
-aaaa
+
                                                {{-- <ins class="adsbygoogle"
                                                 id="my-adRsvp"
                                                 style="display:block"
