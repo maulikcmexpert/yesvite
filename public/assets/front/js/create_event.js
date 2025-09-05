71a3e3ad4b4517bdb109fd4c58f9cbf053637c87
Vimal <vimal.cmexpertise@gmail.com>
2025-09-05 17:45:02 +0530

    Update create_event.js

--- public/assets/front/js/create_event.js
+++ public/assets/front/js/create_event.js
@@ -11581,6 +11581,8 @@ $(document).on('click', '.select_plan_btn', function () {
         $('.sidebar-adsense').addClass('d-none');
         $('.paid_plan_status').removeClass('d-none');
         $('.select_event_photos').removeClass('disabled_event_photos');
+        $('#eventPhotos').prop('checked',true);
+        $('#request_photo').prop('checked',true);
         if ($("#eventPhotos").is(":checked")) {
             eventData.event_photos = "1";
         }
