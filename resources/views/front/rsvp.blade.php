57e97059d860f16d04dd2dec6499c08b1bc961f7
Vimal <vimal.cmexpertise@gmail.com>
2025-09-10 18:58:28 +0530

    Update rsvp.blade.php

--- resources/views/front/rsvp.blade.php
+++ resources/views/front/rsvp.blade.php
@@ -36,7 +36,7 @@
    <section class="rsvp-wrp new-main-content">
        <!-- ===main-section-start=== -->
        @if (isset($display_ad) && $display_ad == true)
-       <div class="mx-auto adsense-center ads-responsive" style="text-align: center;">
+       <div class="mx-auto adsense-center ads-responsive" style="text-align: center;width:300px;height:90px">
 
         {{-- <img src="{{ asset('assets/front/img/demo_ad.jpg') }}" /> --}}
 
